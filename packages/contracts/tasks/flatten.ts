e673e25b33a98f1751b053dd9902499040147a59
Trajan0x <trajan0x@users.noreply.github.com>
2022-05-24 10:39:37 -0400

    add task

---
+++ packages/contracts/tasks/flatten.ts
@@ -0,0 +1,17 @@
+import { task } from "hardhat/config";
+import * as fsWalk from '@nodelib/fs.walk';
+
+
+task("merge", "Prints the list of accounts", async (_, hre) => {
+  const qualifiedNames = await hre.artifacts.getAllFullyQualifiedNames();
+  console.log(hre.config.paths.sources)
+
+  const entries = fsWalk.walkSync(hre.config.paths.sources, new fsWalk.Settings());
+  for (const entry of entries){
+      let res = await hre.run("flatten", {
+          files: [entry],
+      })
+
+      console.log(res)
+  }
+});
