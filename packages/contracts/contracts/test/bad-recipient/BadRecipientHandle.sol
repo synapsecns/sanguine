0cea2cc2801ae0aa53b16835c93899bdc4b7ba6a
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-05-24 06:21:06 -0400

    rm contracts/tests

--- packages/contracts/contracts/test/bad-recipient/BadRecipientHandle.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-contract BadRecipientHandle {
-    function handle(uint32, bytes32) external pure {} // solhint-disable-line no-empty-blocks
-}
