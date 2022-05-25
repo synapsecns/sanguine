bbae7ac74fa5b1502168ce897a158c7401270716
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-05-24 22:45:09 -0400

    Update contracts (#9)
    * initial commit
    
    * add pebble db
    
    * delete module
    
    * add validator
    
    * validator
    
    * add work
    
    * add working-dir
    
    * golangci
    
    * add args
    
    * add custom args (https://github.com/golangci/golangci-lint/issues/2654)
    
    * downgrade go for (golangci/golangci-lint#2649)
    
    * add go 1.17 to fix ci issues
    
    * add go build
    
    * 1.18 build
    
    * lint
    
    * go build workspaces
    
    * add go version
    
    * add gh auth
    
    * try to add labeler to CI
    
    * use core
    
    * add core
    
    * add golangci-lint
    
    * .golangci/yml
    
    * move golangci
    
    * labeler
    
    * go build
    
    * profile.cov
    
    * repo laytut
    
    * add prs
    
    * leana updateS
    
    * add remappings
    
    * gitignore
    
    * hardhat update
    
    * update contracts
    
    * lint
    
    * add task
    
    * add task
    
    * add db
    
    * finish up leaf storage
    
    * finish up leafdb
    
    * add db
    
    * remove types
    
    * start home.sol tests
    
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>
    Co-authored-by: trajan0x <83933037+trajan0x@users.noreply.github.com>

--- packages/contracts/test/Home.t.sol
+++ packages/contracts/test/Home.t.sol
@@ -1,10 +1,24 @@
 // SPDX-License-Identifier: MIT
 
 import "forge-std/Test.sol";
+import "./utils/Utilities.sol";
+
+import "../contracts/Home.sol";
+
+import "../contracts/UpdaterManager.sol";
 
 pragma solidity 0.8.13;
 
 contract HomeTest is Test {
+    Utilities utils = new Utilities();
+    address[] users = utils.createUsers(10);
+    Home home;
+    address signer;
+    address fakeSigner;
+    address updater;
+    address fakeUpdater;
+    UpdaterManager updaterManager;
+
     function setUp() public {
 
     }
