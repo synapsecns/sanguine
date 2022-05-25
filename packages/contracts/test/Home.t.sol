7540ca2c0c6054e83ba549de34abd17bcd815d9d
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-05-24 22:19:48 -0400

    start home.sol tests

--- packages/contracts/test/Home.t.sol
+++ packages/contracts/test/Home.t.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
 
 import "forge-std/Test.sol";
+import "./utils/Utilities.sol";
+
+import "../contracts/Home.sol";
+
+import "../contracts/UpdaterManager.sol";
 
 pragma solidity 0.8.13;
 
 contract HomeTest is Test {
+    address[] users = createUsers(10);
+    Home home;
+    address signer;
+    address fakeSigner;
+    address updater;
+    address fakeUpdater;
+    UpdaterManager updaterManager;
+
     function setUp() public {
 
     }
