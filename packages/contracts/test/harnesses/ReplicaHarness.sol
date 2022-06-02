183f07997ef2e97148e4d5930203783261498e65
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-06-01 20:54:04 -0400

    rename replica -> replicamanger, replicastorage -> replica

--- packages/contracts/test/harnesses/ReplicaHarness.sol
+++ packages/contracts/test/harnesses/ReplicaHarness.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.13;
 
-import { Replica } from "../../contracts/Replica.sol";
+import { Replica } from "../../contracts/ReplicaManager.sol";
 
 contract ReplicaHarness is Replica {
     constructor(
