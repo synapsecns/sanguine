74e6f615b00e6b4b1281f7d2538b902f3ce7054c
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2022-06-27 17:15:35 +0300

    Use OpenZeppelin Queue implementation (#44)
    * feat: replace Queue with OZ implementation
    
    * fix: queue is stored at (begin; end] instead of [begin; end)
    
    * tests: add gas benchmark for queue operations
    
    * tests: benchmark for queueContains

--- packages/contracts/contracts/libs/Queue.sol
+++ packages/contracts/contracts/libs/Queue.sol
@@ -2,148 +2,166 @@
 pragma solidity 0.8.13;
 
 /**
- * @title QueueLib
- * @author Illusory Systems Inc.
- * @notice Library containing queue struct and operations for queue used by
- * Home and Replica.
- **/
+ * @notice Based on OpenZeppelin's implementation:
+ * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/structs/DoubleEndedQueue.sol
+ */
 library QueueLib {
     /**
-     * @notice Queue struct
-     * @dev Internally keeps track of the `first` and `last` elements through
-     * indices and a mapping of indices to enqueued elements.
-     **/
+     * @dev An operation (e.g. {front}) couldn't be completed due to the queue being empty.
+     */
+    error Empty();
+
+    /**
+     * @dev An operation (e.g. {at}) couldn't be completed due to an index being out of bounds.
+     */
+    error OutOfBounds();
+
+    /**
+     * @dev Indices are unsigned integers because the queue can grow only in one direction. They are 128 bits so begin and end
+     * are packed in a single storage slot for efficient access. Since the items are added one at a time we can safely
+     * assume that these 128-bit indices will not overflow, and use unchecked arithmetic.
+     *
+     * Struct members have an underscore prefix indicating that they are "private" and should not be read or written to
+     * directly. Use the functions provided below instead. Modifying the struct manually may violate assumptions and
+     * lead to unexpected behavior.
+     *
+     * Indices are in the range (begin, end] which means the first item is at data[begin + 1] and the last item is at
+     * data[end].
+     *
+     * This also means that indices are always greater than zero.
+     */
     struct Queue {
-        uint128 first;
-        uint128 last;
-        mapping(uint256 => bytes32) queue;
+        uint128 _begin;
+        uint128 _end;
+        mapping(uint256 => bytes32) _data;
     }
 
     /**
-     * @notice Initializes the queue
-     * @dev Empty state denoted by _q.first > q._last. Queue initialized
-     * with _q.first = 1 and _q.last = 0.
-     **/
-    function initialize(Queue storage _q) internal {
-        if (_q.first == 0) {
-            _q.first = 1;
+     * @dev Inserts an item at the end of the queue.
+     *      OZ analog: pushBack
+     */
+    function enqueue(Queue storage queue, bytes32 value) internal {
+        uint128 backIndex;
+        unchecked {
+            // (begin; end] -> last item is queue._end (insert after that)
+            backIndex = queue._end + 1;
         }
+        queue._data[backIndex] = value;
+        queue._end = backIndex;
     }
 
     /**
-     * @notice Enqueues a single new element
-     * @param _item New element to be enqueued
-     * @return _last Index of newly enqueued element
-     **/
-    function enqueue(Queue storage _q, bytes32 _item) internal returns (uint128 _last) {
-        _last = _q.last + 1;
-        _q.last = _last;
-        if (_item != bytes32(0)) {
-            // saves gas if we're queueing 0
-            _q.queue[_last] = _item;
+     * @dev Removes the item at the beginning of the queue and returns it.
+     *      OZ analog: popFront
+     * Reverts with `Empty` if the queue is empty.
+     */
+    function dequeue(Queue storage queue) internal returns (bytes32 value) {
+        if (isEmpty(queue)) revert Empty();
+        uint128 frontIndex;
+        unchecked {
+            // (begin; end] -> first item is queue._begin + 1
+            frontIndex = queue._begin + 1;
         }
+        value = queue._data[frontIndex];
+        delete queue._data[frontIndex];
+        queue._begin = frontIndex;
     }
 
     /**
-     * @notice Dequeues element at front of queue
-     * @dev Removes dequeued element from storage
-     * @return _item Dequeued element
-     **/
-    function dequeue(Queue storage _q) internal returns (bytes32 _item) {
-        uint128 _last = _q.last;
-        uint128 _first = _q.first;
-        require(_length(_last, _first) != 0, "Empty");
-        _item = _q.queue[_first];
-        if (_item != bytes32(0)) {
-            // saves gas if we're dequeuing 0
-            delete _q.queue[_first];
-        }
-        _q.first = _first + 1;
-    }
-
-    /**
-     * @notice Batch enqueues several elements
-     * @param _items Array of elements to be enqueued
-     * @return _last Index of last enqueued element
-     **/
-    function enqueue(Queue storage _q, bytes32[] memory _items) internal returns (uint128 _last) {
-        _last = _q.last;
-        for (uint256 i = 0; i < _items.length; i += 1) {
-            _last += 1;
-            bytes32 _item = _items[i];
-            if (_item != bytes32(0)) {
-                _q.queue[_last] = _item;
+     * @dev Batch inserts several items at the end of the queue.
+     *      OZ analog: pushBack
+     */
+    function enqueue(Queue storage queue, bytes32[] memory values) internal {
+        uint128 backIndex = queue._end;
+        uint256 len = values.length;
+        for (uint256 i = 0; i < len; ) {
+            // (begin; end] -> last item is backIndex (insert after that)
+            unchecked {
+                ++backIndex;
+            }
+            queue._data[backIndex] = values[i];
+            unchecked {
+                ++i;
             }
         }
-        _q.last = _last;
+        // backIndex is now pointing to the last inserted item
+        queue._end = backIndex;
     }
 
     /**
-     * @notice Batch dequeues `_number` elements
-     * @dev Reverts if `_number` > queue length
-     * @param _number Number of elements to dequeue
-     * @return Array of dequeued elements
-     **/
-    function dequeue(Queue storage _q, uint256 _number) internal returns (bytes32[] memory) {
-        uint128 _last = _q.last;
-        uint128 _first = _q.first;
-        // Cannot underflow unless state is corrupted
-        require(_length(_last, _first) >= _number, "Insufficient");
-
-        bytes32[] memory _items = new bytes32[](_number);
-
-        for (uint256 i = 0; i < _number; i++) {
-            _items[i] = _q.queue[_first];
-            delete _q.queue[_first];
-            _first++;
+     * @dev Batch removes `number` items at the beginning of the queue and returns them.
+     *      OZ analog: popFront
+     * Reverts with `Empty` if `number` > queue length
+     */
+    function dequeue(Queue storage queue, uint256 number)
+        internal
+        returns (bytes32[] memory values)
+    {
+        uint128 frontIndex = queue._begin;
+        unchecked {
+            // This will not underflow assuming all queue operations were done through interface
+            if (number > queue._end - frontIndex) revert Empty();
         }
-        _q.first = _first;
-        return _items;
+        values = new bytes32[](number);
+        for (uint256 i = 0; i < number; ) {
+            // (begin; end] -> first item is frontIndex + 1
+            unchecked {
+                ++frontIndex;
+            }
+            values[i] = queue._data[frontIndex];
+            delete queue._data[frontIndex];
+            unchecked {
+                ++i;
+            }
+        }
+        // firstIndex is now pointing to the last deleted item
+        queue._begin = frontIndex;
     }
 
-    /**
-     * @notice Returns true if `_item` is in the queue and false if otherwise
-     * @dev Linearly scans from _q.first to _q.last looking for `_item`
-     * @param _item Item being searched for in queue
-     * @return True if `_item` currently exists in queue, false if otherwise
-     **/
-    function contains(Queue storage _q, bytes32 _item) internal view returns (bool) {
-        for (uint256 i = _q.last; i >= _q.first; i--) {
-            if (_q.queue[i] == _item) {
-                return true;
+    function contains(Queue storage queue, bytes32 item) internal view returns (bool) {
+        // Most of the time we'll be checking a merkle root that has been recently added,
+        // so checking from back to front is likely to find it faster.
+        uint128 backIndex = queue._end;
+        uint128 frontIndex = queue._begin;
+        // elements are stored at (begin, end] range,
+        // so we need to start from queue._end
+        // and finish at queue._begin + 1
+        for (; backIndex > frontIndex; ) {
+            if (queue._data[backIndex] == item) return true;
+            unchecked {
+                --backIndex;
             }
         }
         return false;
     }
 
     /// @notice Returns last item in queue
-    /// @dev Returns bytes32(0) if queue empty
-    function lastItem(Queue storage _q) internal view returns (bytes32) {
-        return _q.queue[_q.last];
+    /// @dev Returns bytes32(0) if queue is empty
+    function lastItem(Queue storage queue) internal view returns (bytes32 item) {
+        // (begin; end] -> last item is queue._end
+        return queue._data[queue._end];
     }
 
     /// @notice Returns element at front of queue without removing element
     /// @dev Reverts if queue is empty
-    function peek(Queue storage _q) internal view returns (bytes32 _item) {
-        require(!isEmpty(_q), "Empty");
-        _item = _q.queue[_q.first];
+    function peek(Queue storage queue) internal view returns (bytes32 item) {
+        if (isEmpty(queue)) revert Empty();
+        unchecked {
+            // (begin; end] -> first item is queue._begin + 1
+            item = queue._data[queue._begin + 1];
+        }
     }
 
     /// @notice Returns true if queue is empty and false if otherwise
-    function isEmpty(Queue storage _q) internal view returns (bool) {
-        return _q.last < _q.first;
+    function isEmpty(Queue storage queue) internal view returns (bool) {
+        // (begin; end] -> (end <= begin) is empty queue
+        return queue._end <= queue._begin;
     }
 
-    /// @notice Returns number of elements in queue
-    function length(Queue storage _q) internal view returns (uint256) {
-        uint128 _last = _q.last;
-        uint128 _first = _q.first;
-        // Cannot underflow unless state is corrupted
-        return _length(_last, _first);
-    }
-
-    /// @notice Returns number of elements between `_last` and `_first` (used internally)
-    function _length(uint128 _last, uint128 _first) internal pure returns (uint256) {
-        return uint256(_last + 1 - _first);
+    function length(Queue storage queue) internal view returns (uint256) {
+        // The interface preserves the invariant that begin <= end so we assume this will not overflow.
+        unchecked {
+            return queue._end - queue._begin;
+        }
     }
 }
