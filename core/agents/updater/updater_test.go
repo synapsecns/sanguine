be41a3c8125f240a0c832f40c62dbb4fc3fb8457
Trajan0x <trajan0x@users.noreply.github.com>
2022-07-11 20:15:22 +0200

    full updater test

--- core/agents/updater/updater_test.go
+++ core/agents/updater/updater_test.go
@@ -2,6 +2,9 @@ package updater_test
 
 import (
 	"github.com/Flaque/filet"
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	. "github.com/stretchr/testify/assert"
 	"github.com/synapsecns/sanguine/core/agents/updater"
 	"github.com/synapsecns/sanguine/core/config"
@@ -9,8 +12,6 @@ import (
 )
 
 func (u UpdaterSuite) TestUpdaterE2E() {
-	u.T().Skip("could not update")
-
 	testConfig := config.Config{
 		Domains: map[string]config.DomainConfig{
 			"test": u.domainClient.Config(),
@@ -28,8 +29,20 @@ func (u UpdaterSuite) TestUpdaterE2E() {
 	ud, err := updater.NewUpdater(u.GetTestContext(), testConfig)
 	Nil(u.T(), err)
 
+	auth := u.testBackend.GetTxContext(u.GetTestContext(), nil)
+	tx, err := u.homeContract.Dispatch(auth.TransactOpts, gofakeit.Uint32(), [32]byte{}, gofakeit.Uint32(), []byte(gofakeit.Paragraph(3, 2, 1, " ")))
+	Nil(u.T(), err)
+	u.testBackend.WaitForConfirmation(u.GetTestContext(), tx)
+
 	go func() {
 		err = ud.Start(u.GetTestContext())
 		Nil(u.T(), err)
 	}()
+
+	u.Eventually(func() bool {
+		committedRoot, err := u.homeContract.CommittedRoot(&bind.CallOpts{Context: u.GetTestContext()})
+		Nil(u.T(), err)
+
+		return committedRoot != common.Hash{}
+	})
 }
