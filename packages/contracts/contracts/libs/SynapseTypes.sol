fec50fcf53cf1e21614087119f8a70e57c17f6ac
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-07-17 19:50:51 +0300

    feat: strict types

---
+++ packages/contracts/contracts/libs/SynapseTypes.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import { TypedMemView } from "./TypedMemView.sol";
+
+/**
+ * @dev The goal of this library is to assign a type for every bytes29 memory pointer
+ * and enforce strict type checking for every bytes29 operation. This will prevent
+ * a misuse of libraries, i.e. using Attestation functions on a Report pointer.
+ */
+library SynapseTypes {
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                         0X00: SIGNATURE                              ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    // prettier-ignore
+    uint40 internal constant SIGNATURE                  = 0x00_01_00_00_00;
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                         0X01: ATTESTATION                            ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    // prettier-ignore
+    uint40 internal constant ATTESTATION                = 0x01_01_00_00_00;
+    // prettier-ignore
+    uint40 internal constant ATTESTATION_DATA           = 0x01_02_00_00_00;
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                         0X02: REPORT                                 ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    // prettier-ignore
+    uint40 internal constant REPORT                     = 0x02_01_00_00_00;
+    // prettier-ignore
+    uint40 internal constant REPORT_DATA                = 0x02_02_00_00_00;
+}
