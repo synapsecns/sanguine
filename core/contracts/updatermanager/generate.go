7beb5af1f56a9446c704138f7d17e456151adea0
Trajan0x <trajan0x@users.noreply.github.com>
2022-08-16 00:52:19 +0900

    generate fix

--- core/contracts/updatermanager/generate.go
+++ core/contracts/updatermanager/generate.go
@@ -1,3 +1,3 @@
 package updatermanager
 
-//go:generate go run github.com/synapsecns/synapse-node/testutils/codegen/abigen generate --sol ../../../packages/contracts/flattened/UpdaterManager.sol --pkg updatermanager --sol-version 0.8.13 --filename updatemanager
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ../../../packages/contracts/flattened/UpdaterManager.sol --pkg updatermanager --sol-version 0.8.13 --filename updatemanager
