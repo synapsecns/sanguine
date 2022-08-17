74affd7d7cacba611f5151ddd3202115bd9dea22
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-08-17 23:28:17 +0300

    add: verifying Reports in GuardRegistry

--- packages/contracts/contracts/registry/AbstractGuardRegistry.sol
+++ packages/contracts/contracts/registry/AbstractGuardRegistry.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
+import { TypedMemView } from "../libs/TypedMemView.sol";
+import { Report } from "../libs/Report.sol";
+import { Auth } from "../libs/Auth.sol";
+
 abstract contract AbstractGuardRegistry {
+    using Report for bytes;
+    using Report for bytes29;
+    using TypedMemView for bytes29;
+
     function _checkGuardAuth(bytes memory _report)
         internal
         view
-        returns (address _guard, bytes29 _data)
+        returns (address _guard, bytes29 _view)
     {
-        // TODO: check if _report is valid, once guard message standard is finalized
+        _view = _report.castToReport();
+        require(_view.isReport(), "Not a report");
+        _guard = Auth.recoverSigner(_view.reportData(), _view.guardSignature().clone());
+        require(_isGuard(_guard), "Signer is not a guard");
     }
 
     function _isGuard(address _guard) internal view virtual returns (bool);
