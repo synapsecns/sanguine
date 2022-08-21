e889bc08b2d4ec1e6e9eb81b9d0d970863161eca
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-08-21 14:56:10 +0300

    chore: better docs for ReportHub

--- packages/contracts/contracts/hubs/ReportHub.sol
+++ packages/contracts/contracts/hubs/ReportHub.sol
@@ -15,19 +15,23 @@ abstract contract ReportHub is AbstractGuardRegistry, AbstractNotaryRegistry {
     /**
      * @notice Called by the external agent. Submits the signed report for handling.
      * @dev Reverts if either of this is true:
+     *      - Report payload is not properly formatted.
      *      - Report signer is not a Guard.
      *      - Reported notary is not a Notary.
      * @param _report	Payload with Report data and signature
      * @return TRUE if Report was handled correctly.
      */
     function submitReport(bytes memory _report) external returns (bool) {
-        // Check if real Guard & signature
+        // Check if real Guard & signature.
+        // This also checks if Report payload is properly formatted.
         (address _guard, bytes29 _reportView) = _checkGuardAuth(_report);
         bytes29 _attestationView = _reportView.reportedAttestation();
-        // Check if real Notary & signature
+        // Check if real Notary & signature.
+        // This also checks if Attestation payload is properly formatted,
+        // though it's already been checked in _checkGuardAuth(_report) [see Report.sol].
         address _notary = _checkNotaryAuth(_attestationView);
-        // Pass _reportView as the existing bytes29 pointer to report payload
-        // Pass _report to avoid extra memory copy when emitting report payload
+        // Pass _reportView as the existing bytes29 pointer to report payload.
+        // Pass _report to avoid extra memory copy when emitting report payload.
         return _handleReport(_guard, _notary, _attestationView, _reportView, _report);
     }
 
