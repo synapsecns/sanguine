2a8074bfa95a6e5b538677113c88611bda61f144
trajan0x <83933037+trajan0x@users.noreply.github.com>
2022-08-23 10:46:03 -0400

    rename core (#123)
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>

---
+++ agents/testutil/typecast.go
@@ -0,0 +1,132 @@
+package testutil
+
+import (
+	"context"
+	"github.com/synapsecns/sanguine/agents/contracts/origin"
+	"github.com/synapsecns/sanguine/agents/contracts/test/headerharness"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/synapsecns/sanguine/agents/contracts/attestationcollector"
+	"github.com/synapsecns/sanguine/agents/contracts/destination"
+	"github.com/synapsecns/sanguine/agents/contracts/notarymanager"
+	"github.com/synapsecns/sanguine/agents/contracts/test/attestationharness"
+	"github.com/synapsecns/sanguine/agents/contracts/test/destinationharness"
+	"github.com/synapsecns/sanguine/agents/contracts/test/messageharness"
+	"github.com/synapsecns/sanguine/agents/contracts/test/originharness"
+	"github.com/synapsecns/sanguine/agents/contracts/test/tipsharness"
+	"github.com/synapsecns/synapse-node/testutils/backends"
+)
+
+// GetOrigin gets a typecast origin contract.
+func (d *DeployManager) GetOrigin(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *origin.OriginRef) {
+	d.T().Helper()
+
+	originContract := d.GetContractRegistry(backend).Get(ctx, OriginType)
+
+	originHandle, ok := originContract.ContractHandle().(*origin.OriginRef)
+	assert.True(d.T(), ok)
+
+	return originContract, originHandle
+}
+
+// GetMessageHarness gets the message harness.
+func (d *DeployManager) GetMessageHarness(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *messageharness.MessageHarnessRef) {
+	d.T().Helper()
+
+	messageHarnessContract := d.GetContractRegistry(backend).Get(ctx, MessageHarnessType)
+
+	messageHarness, ok := messageHarnessContract.ContractHandle().(*messageharness.MessageHarnessRef)
+	assert.True(d.T(), ok)
+
+	return messageHarnessContract, messageHarness
+}
+
+// GetOriginHarness gets the origin harness.
+func (d *DeployManager) GetOriginHarness(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *originharness.OriginHarnessRef) {
+	d.T().Helper()
+
+	messageHarnessContract := d.GetContractRegistry(backend).Get(ctx, OriginHarnessType)
+
+	messageHarness, ok := messageHarnessContract.ContractHandle().(*originharness.OriginHarnessRef)
+	assert.True(d.T(), ok)
+
+	return messageHarnessContract, messageHarness
+}
+
+// GetAttestationHarness gets the attestation harness.
+func (d *DeployManager) GetAttestationHarness(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *attestationharness.AttestationHarnessRef) {
+	d.T().Helper()
+
+	attestationHarnessContract := d.GetContractRegistry(backend).Get(ctx, AttestationHarnessType)
+
+	attestationHarness, ok := attestationHarnessContract.ContractHandle().(*attestationharness.AttestationHarnessRef)
+	assert.True(d.T(), ok)
+
+	return attestationHarnessContract, attestationHarness
+}
+
+// GetDestinationHarness gets the destination harness.
+func (d *DeployManager) GetDestinationHarness(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *destinationharness.DestinationHarnessRef) {
+	d.T().Helper()
+
+	destinationHarnessContract := d.GetContractRegistry(backend).Get(ctx, DestinationHarnessType)
+	destinationHarness, ok := destinationHarnessContract.ContractHandle().(*destinationharness.DestinationHarnessRef)
+	assert.True(d.T(), ok)
+
+	return destinationHarnessContract, destinationHarness
+}
+
+// GetNotaryManager gets the notary manager.
+func (d *DeployManager) GetNotaryManager(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *notarymanager.NotaryManagerRef) {
+	d.T().Helper()
+
+	notaryManagerContract := d.GetContractRegistry(backend).Get(ctx, NotaryManagerType)
+	notaryManager, ok := notaryManagerContract.ContractHandle().(*notarymanager.NotaryManagerRef)
+	assert.True(d.T(), ok)
+
+	return notaryManagerContract, notaryManager
+}
+
+// GetAttestationCollector gets the attestation collector contract.
+func (d *DeployManager) GetAttestationCollector(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *attestationcollector.AttestationCollectorRef) {
+	d.T().Helper()
+
+	attestationContract := d.GetContractRegistry(backend).Get(ctx, AttestationCollectorType)
+	attestationCollector, ok := attestationContract.ContractHandle().(*attestationcollector.AttestationCollectorRef)
+	assert.True(d.T(), ok)
+
+	return attestationContract, attestationCollector
+}
+
+// GetDestination gets the destination contract.
+func (d *DeployManager) GetDestination(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *destination.DestinationRef) {
+	d.T().Helper()
+
+	destinationContract := d.GetContractRegistry(backend).Get(ctx, DestinationType)
+	destination, ok := destinationContract.ContractHandle().(*destination.DestinationRef)
+	assert.True(d.T(), ok)
+
+	return destinationContract, destination
+}
+
+// GetTipsHarness gets the tips harness for testing.
+func (d *DeployManager) GetTipsHarness(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *tipsharness.TipsHarnessRef) {
+	d.T().Helper()
+
+	tipsContract := d.GetContractRegistry(backend).Get(ctx, TipsHarnessType)
+	tipsHarness, ok := tipsContract.ContractHandle().(*tipsharness.TipsHarnessRef)
+	assert.True(d.T(), ok)
+
+	return tipsContract, tipsHarness
+}
+
+// GetHeaderHarness gets the header harness.
+func (d *DeployManager) GetHeaderHarness(ctx context.Context, backend backends.SimulatedTestBackend) (contract backends.DeployedContract, handle *headerharness.HeaderHarnessRef) {
+	d.T().Helper()
+
+	headerHarnessContract := d.GetContractRegistry(backend).Get(ctx, HeaderHarnessType)
+	headerHarness, ok := headerHarnessContract.ContractHandle().(*headerharness.HeaderHarnessRef)
+	assert.True(d.T(), ok)
+
+	return headerHarnessContract, headerHarness
+}
