ec566e790730b8a048b27422b6324d8fb4daa1dd
Max Planck <99688618+CryptoMaxPlanck@users.noreply.github.com>
2022-08-24 16:28:59 -0400

    add last indexed block for contracts table (#128)

---
+++ scribe/db/datastore/sql/base/lastindexed.go
@@ -0,0 +1,62 @@
+package base
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// StoreLastIndexed stores the last indexed block number for a contract.
+// It updates the value if there is a previous last indexed value, and creates a new
+// entry if there is no previous value.
+func (s Store) StoreLastIndexed(ctx context.Context, contractAddress common.Address, chainID uint32, blockNumber uint64) error {
+	entry := LastIndexedInfo{}
+	dbTx := s.DB().WithContext(ctx).
+		Model(&LastIndexedInfo{}).
+		Where("contract_address = ?", contractAddress.String()).
+		Where("chain_id = ?", chainID).
+		Scan(&entry)
+	if dbTx.Error != nil {
+		return fmt.Errorf("could not retrieve last indexed info: %w", dbTx.Error)
+	}
+	if dbTx.RowsAffected == 0 {
+		dbTx = s.DB().WithContext(ctx).
+			Model(&LastIndexedInfo{}).
+			Create(&LastIndexedInfo{
+				ContractAddress: contractAddress.String(),
+				ChainID:         chainID,
+				LastIndexed:     blockNumber,
+			})
+		if dbTx.Error != nil {
+			return fmt.Errorf("could not store last indexed info: %w", dbTx.Error)
+		}
+		return nil
+	}
+	dbTx = s.DB().WithContext(ctx).
+		Model(&LastIndexedInfo{}).
+		Where("contract_address = ?", contractAddress.String()).
+		Where("chain_id = ?", chainID).
+		Update("last_indexed", blockNumber)
+	if dbTx.Error != nil {
+		return fmt.Errorf("could not update last indexed info: %w", dbTx.Error)
+	}
+	return nil
+}
+
+// RetrieveLastIndexed retrieves the last indexed block number for a contract.
+func (s Store) RetrieveLastIndexed(ctx context.Context, contractAddress common.Address, chainID uint32) (uint64, error) {
+	entry := LastIndexedInfo{}
+	dbTx := s.DB().WithContext(ctx).
+		Model(&LastIndexedInfo{}).
+		Where("contract_address = ?", contractAddress.String()).
+		Where("chain_id = ?", chainID).
+		First(&entry)
+	if dbTx.RowsAffected == 0 {
+		return 0, nil
+	}
+	if dbTx.Error != nil {
+		return 0, fmt.Errorf("could not retrieve last indexed info: %w", dbTx.Error)
+	}
+	return entry.LastIndexed, nil
+}
