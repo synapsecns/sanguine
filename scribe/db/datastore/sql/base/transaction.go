8216f7abd4714c4bc53ad44e9f173fe56f56d42c
Max Planck <maxplanck.crypto@gmail.com>
2022-08-25 19:39:58 -0400

    lint ...

--- scribe/db/datastore/sql/base/transaction.go
+++ scribe/db/datastore/sql/base/transaction.go
@@ -3,7 +3,6 @@ package base
 import (
 	"context"
 	"fmt"
-	"math/big"
 
 	"github.com/ethereum/go-ethereum/core/types"
 )
@@ -27,20 +26,4 @@ func (s Store) StoreEthTx(ctx context.Context, tx *types.Transaction) error {
 	}
 
 	return nil
-}
-
-// getChainID gets the chain id from non-legacy transaction types
-// it is used to check chainids against the chainid passed in the raw id.
-func getChainID(tx *types.Transaction) (hasType bool, chainID *big.Int) {
-	switch tx.Type() {
-	case types.LegacyTxType:
-		return false, nil
-	default:
-		chainID = tx.ChainId()
-		if chainID == nil || chainID.Cmp(big.NewInt(0)) == 0 {
-			return false, nil
-		}
-
-		return true, tx.ChainId()
-	}
 }
