1b4d3f1f88dc54f61ea0fd096537764bb16e6366
Max Planck <maxplanck.crypto@gmail.com>
2022-08-25 18:23:17 -0400

    remove runOnAllDBs

--- scribe/backfill/contract_test.go
+++ scribe/backfill/contract_test.go
@@ -8,48 +8,47 @@ import (
 	. "github.com/stretchr/testify/assert"
 	"github.com/synapsecns/sanguine/ethergo/backends/simulated"
 	"github.com/synapsecns/sanguine/scribe/backfill"
-	"github.com/synapsecns/sanguine/scribe/db"
 )
 
 // TestStartHeightForBackfill ensures the start height for backfill is calculated correctly.
 func (b BackfillSuite) TestStartHeightForBackfill() {
-	b.RunOnAllDBs(func(testDB db.EventDB) {
-		// Get simulated blockchain and deploy the test contract.
-		simulatedChain := simulated.NewSimulatedBackend(b.GetSuiteContext(), b.T())
-		testContract, _ := b.manager.GetTestContract(b.GetTestContext(), simulatedChain)
 
-		// Use the receipt of the contract's deploy tx to get which block it was deployed in.
-		deployedBlockNumber, err := b.getTxBlockNumber(simulatedChain, testContract.DeployTx())
-		Nil(b.T(), err)
+	// Get simulated blockchain and deploy the test contract.
+	simulatedChain := simulated.NewSimulatedBackend(b.GetSuiteContext(), b.T())
+	testContract, _ := b.manager.GetTestContract(b.GetTestContext(), simulatedChain)
 
-		backfiller := backfill.NewContractBackfiller(testDB, testContract, simulatedChain)
+	// Use the receipt of the contract's deploy tx to get which block it was deployed in.
+	deployedBlockNumber, err := b.getTxBlockNumber(simulatedChain, testContract.DeployTx())
+	Nil(b.T(), err)
 
-		// Since useDB is false, this should return the block number of the contract's deploy tx.
-		startHeight, err := backfiller.StartHeightForBackfill(b.GetTestContext(), false)
-		Nil(b.T(), err)
-		Equal(b.T(), deployedBlockNumber, startHeight)
+	backfiller := backfill.NewContractBackfiller(b.testDB, testContract, simulatedChain)
 
-		// Since useDB is true, but we have not filled in when the contract was last indexed,
-		// this should return the block number of the contract's deploy tx.
-		startHeight, err = backfiller.StartHeightForBackfill(b.GetTestContext(), true)
-		Nil(b.T(), err)
-		Equal(b.T(), deployedBlockNumber, startHeight)
+	// Since useDB is false, this should return the block number of the contract's deploy tx.
+	startHeight, err := backfiller.StartHeightForBackfill(b.GetTestContext(), false)
+	Nil(b.T(), err)
+	Equal(b.T(), deployedBlockNumber, startHeight)
 
-		// Now fill in the contract's last indexed block.
-		err = testDB.StoreLastIndexed(b.GetTestContext(), testContract.Address(), uint32(simulatedChain.GetChainID()), 1000)
-		Nil(b.T(), err)
+	// Since useDB is true, but we have not filled in when the contract was last indexed,
+	// this should return the block number of the contract's deploy tx.
+	startHeight, err = backfiller.StartHeightForBackfill(b.GetTestContext(), true)
+	Nil(b.T(), err)
+	Equal(b.T(), deployedBlockNumber, startHeight)
 
-		// Since useDB is false, even though last indexed is filled in,
-		// this should still return the block number of the contract's deploy tx.
-		startHeight, err = backfiller.StartHeightForBackfill(b.GetTestContext(), false)
-		Nil(b.T(), err)
-		Equal(b.T(), deployedBlockNumber, startHeight)
+	// Now fill in the contract's last indexed block.
+	err = b.testDB.StoreLastIndexed(b.GetTestContext(), testContract.Address(), uint32(simulatedChain.GetChainID()), 1000)
+	Nil(b.T(), err)
 
-		// Now that useDB is true and last indexed is filled in, this should return the last indexed block.
-		startHeight, err = backfiller.StartHeightForBackfill(b.GetTestContext(), true)
-		Nil(b.T(), err)
-		Equal(b.T(), uint64(1000), startHeight)
-	})
+	// Since useDB is false, even though last indexed is filled in,
+	// this should still return the block number of the contract's deploy tx.
+	startHeight, err = backfiller.StartHeightForBackfill(b.GetTestContext(), false)
+	Nil(b.T(), err)
+	Equal(b.T(), deployedBlockNumber, startHeight)
+
+	// Now that useDB is true and last indexed is filled in, this should return the last indexed block.
+	startHeight, err = backfiller.StartHeightForBackfill(b.GetTestContext(), true)
+	Nil(b.T(), err)
+	Equal(b.T(), uint64(1000), startHeight)
+
 }
 
 // TestGetLogsSimulated tests the GetLogs function using a simulated blockchain.
@@ -62,72 +61,71 @@ func (b BackfillSuite) TestGetLogsSimulated() {
 	testContract, ref := b.manager.GetTestContract(b.GetTestContext(), simulatedChain)
 	transactOpts := simulatedChain.GetTxContext(b.GetTestContext(), nil)
 
-	b.RunOnAllDBs(func(testDB db.EventDB) {
-		backfiller := backfill.NewContractBackfiller(testDB, testContract, simulatedChain)
+	backfiller := backfill.NewContractBackfiller(b.testDB, testContract, simulatedChain)
 
-		// Emit five events, and then fetch them with GetLogs. The first two will be fetched first,
-		// then the last three after.
-		tx, err := ref.EmitEventA(transactOpts.TransactOpts, big.NewInt(1), big.NewInt(2), big.NewInt(3))
-		Nil(b.T(), err)
-		simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
-		tx, err = ref.EmitEventB(transactOpts.TransactOpts, []byte{4}, big.NewInt(5), big.NewInt(6))
-		Nil(b.T(), err)
-		simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
-		// Get the block that the second transaction was executed in.
-		txBlockNumberA, err := b.getTxBlockNumber(simulatedChain, tx)
-		Nil(b.T(), err)
+	// Emit five events, and then fetch them with GetLogs. The first two will be fetched first,
+	// then the last three after.
+	tx, err := ref.EmitEventA(transactOpts.TransactOpts, big.NewInt(1), big.NewInt(2), big.NewInt(3))
+	Nil(b.T(), err)
+	simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
+	tx, err = ref.EmitEventB(transactOpts.TransactOpts, []byte{4}, big.NewInt(5), big.NewInt(6))
+	Nil(b.T(), err)
+	simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
+	// Get the block that the second transaction was executed in.
+	txBlockNumberA, err := b.getTxBlockNumber(simulatedChain, tx)
+	Nil(b.T(), err)
 
-		tx, err = ref.EmitEventA(transactOpts.TransactOpts, big.NewInt(7), big.NewInt(8), big.NewInt(9))
-		Nil(b.T(), err)
-		simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
-		tx, err = ref.EmitEventB(transactOpts.TransactOpts, []byte{10}, big.NewInt(11), big.NewInt(12))
-		Nil(b.T(), err)
-		simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
-		tx, err = ref.EmitEventA(transactOpts.TransactOpts, big.NewInt(13), big.NewInt(14), big.NewInt(15))
-		Nil(b.T(), err)
-		simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
-		// Get the block that the last transaction was executed in.
-		txBlockNumberB, err := b.getTxBlockNumber(simulatedChain, tx)
-		Nil(b.T(), err)
+	tx, err = ref.EmitEventA(transactOpts.TransactOpts, big.NewInt(7), big.NewInt(8), big.NewInt(9))
+	Nil(b.T(), err)
+	simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
+	tx, err = ref.EmitEventB(transactOpts.TransactOpts, []byte{10}, big.NewInt(11), big.NewInt(12))
+	Nil(b.T(), err)
+	simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
+	tx, err = ref.EmitEventA(transactOpts.TransactOpts, big.NewInt(13), big.NewInt(14), big.NewInt(15))
+	Nil(b.T(), err)
+	simulatedChain.WaitForConfirmation(b.GetTestContext(), tx)
+	// Get the block that the last transaction was executed in.
+	txBlockNumberB, err := b.getTxBlockNumber(simulatedChain, tx)
+	Nil(b.T(), err)
 
-		// Get the logs for the first two events.
-		collectedLogs := []types.Log{}
-		logs, errors, done := backfiller.GetLogs(b.GetTestContext(), 0, txBlockNumberA)
-		for {
-			select {
-			case <-b.GetTestContext().Done():
-				b.T().Error("test timed out")
-			case e := <-errors:
-				b.T().Error(e)
-			case log := <-logs:
-				collectedLogs = append(collectedLogs, log)
-			case <-done:
-				goto Next
-			}
+	// Get the logs for the first two events.
+	collectedLogs := []types.Log{}
+	logs, errors, done := backfiller.GetLogs(b.GetTestContext(), 0, txBlockNumberA)
+	for {
+		select {
+		case <-b.GetTestContext().Done():
+			b.T().Error("test timed out")
+		case e := <-errors:
+			b.T().Error(e)
+		case log := <-logs:
+			collectedLogs = append(collectedLogs, log)
+		case <-done:
+			goto Next
 		}
-	Next:
-		// Check to see if 2 logs were collected.
-		Equal(b.T(), 2, len(collectedLogs))
+	}
+Next:
+	// Check to see if 2 logs were collected.
+	Equal(b.T(), 2, len(collectedLogs))
 
-		// Get the logs for the last three events.
-		collectedLogs = []types.Log{}
-		logs, errors, done = backfiller.GetLogs(b.GetTestContext(), txBlockNumberA+1, txBlockNumberB)
-		for {
-			select {
-			case <-b.GetTestContext().Done():
-				b.T().Error("test timed out")
-			case e := <-errors:
-				b.T().Error(e)
-			case log := <-logs:
-				collectedLogs = append(collectedLogs, log)
-			case <-done:
-				goto Done
-			}
+	// Get the logs for the last three events.
+	collectedLogs = []types.Log{}
+	logs, errors, done = backfiller.GetLogs(b.GetTestContext(), txBlockNumberA+1, txBlockNumberB)
+	for {
+		select {
+		case <-b.GetTestContext().Done():
+			b.T().Error("test timed out")
+		case e := <-errors:
+			b.T().Error(e)
+		case log := <-logs:
+			collectedLogs = append(collectedLogs, log)
+		case <-done:
+			goto Done
 		}
-	Done:
-		// Check to see if 3 logs were collected.
-		Equal(b.T(), 3, len(collectedLogs))
-	})
+	}
+Done:
+	// Check to see if 3 logs were collected.
+	Equal(b.T(), 3, len(collectedLogs))
+
 }
 
 // TestGetLogsMock tests the GetLogs function using a mocked blockchain for errors.
--- scribe/backfill/suite_test.go
+++ scribe/backfill/suite_test.go
@@ -1,12 +1,7 @@
 package backfill_test
 
 import (
-	"database/sql"
-	"fmt"
-	"os"
-	"sync"
 	"testing"
-	"time"
 
 	"github.com/Flaque/filet"
 	. "github.com/stretchr/testify/assert"
@@ -16,15 +11,12 @@ import (
 	"github.com/synapsecns/sanguine/ethergo/signer/signer/localsigner"
 	"github.com/synapsecns/sanguine/ethergo/signer/wallet"
 	"github.com/synapsecns/sanguine/scribe/db"
-	"github.com/synapsecns/sanguine/scribe/db/datastore/sql/mysql"
 	"github.com/synapsecns/sanguine/scribe/db/datastore/sql/sqlite"
-	"github.com/synapsecns/synapse-node/pkg/common"
-	"gorm.io/gorm/schema"
 )
 
 type BackfillSuite struct {
 	*testsuite.TestSuite
-	dbs     []db.EventDB
+	testDB  db.EventDB
 	manager *testutil.DeployManager
 	wallet  wallet.Wallet
 	signer  *localsigner.Signer
@@ -35,7 +27,6 @@ func NewBackfillSuite(tb testing.TB) *BackfillSuite {
 	tb.Helper()
 	return &BackfillSuite{
 		TestSuite: testsuite.NewTestSuite(tb),
-		dbs:       []db.EventDB{},
 	}
 }
 
@@ -45,66 +36,13 @@ func (b *BackfillSuite) SetupTest() {
 	sqliteStore, err := sqlite.NewSqliteStore(b.GetTestContext(), filet.TmpDir(b.T(), ""))
 	Nil(b.T(), err)
 
-	b.dbs = []db.EventDB{sqliteStore}
-	b.setupMysqlDB()
+	b.testDB = sqliteStore
 
 	b.manager = testutil.NewDeployManager(b.T())
 
 	b.wallet, err = wallet.FromRandom()
 	Nil(b.T(), err)
 	b.signer = localsigner.NewSigner(b.wallet.PrivateKey())
-}
-
-// connString gets the connection string.
-func (b *BackfillSuite) connString(dbname string) string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", common.GetEnv("MYSQL_USER", "root"), os.Getenv("MYSQL_PASSWORD"), common.GetEnv("MYSQL_HOST", "127.0.0.1"), common.GetEnvInt("MYSQL_PORT", 3306), dbname)
-}
-
-func (b *BackfillSuite) setupMysqlDB() {
-	// skip if mysql test disabled, this really only needs to be run in ci
-
-	// skip if mysql test disabled
-	if os.Getenv("ENABLE_MYSQL_TEST") == "" {
-		return
-	}
-	// sets up the conn string to the default database
-	connString := b.connString(os.Getenv("MYSQL_DATABASE"))
-	// sets up the myqsl db
-	testDB, err := sql.Open("mysql", connString)
-	Nil(b.T(), err)
-	// close the db once the ocnnection is odne
-	defer func() {
-		Nil(b.T(), testDB.Close())
-	}()
-
-	// override the naming strategy to prevent tests from messing with each other.
-	// todo this should be solved via a proper teardown process or transactions.
-	mysql.NamingStrategy = schema.NamingStrategy{
-		TablePrefix: fmt.Sprintf("test%d_%d_", b.GetTestID(), time.Now().Unix()),
-	}
-
-	mysql.MaxIdleConns = 10
-
-	// create the sql store
-	mysqlStore, err := mysql.NewMysqlStore(b.GetTestContext(), connString)
-	Nil(b.T(), err)
-	// add the db
-	b.dbs = append(b.dbs, mysqlStore)
-}
-
-func (b *BackfillSuite) RunOnAllDBs(testFunc func(testDB db.EventDB)) {
-	b.T().Helper()
-
-	wg := sync.WaitGroup{}
-	for _, testDB := range b.dbs {
-		wg.Add(1)
-		// capture the value
-		go func(testDB db.EventDB) {
-			defer wg.Done()
-			testFunc(testDB)
-		}(testDB)
-	}
-	wg.Wait()
 }
 
 // TestBackfillSuite tests the backfill suite.
