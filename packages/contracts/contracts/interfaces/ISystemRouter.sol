73e42d74b3eafeedced6e4295d1fb104f3f62945
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-09-01 19:35:27 +0300

    fix: update System Router interface

--- packages/contracts/contracts/interfaces/ISystemRouter.sol
+++ packages/contracts/contracts/interfaces/ISystemRouter.sol
@@ -21,4 +21,14 @@ interface ISystemRouter {
         SystemContracts _recipient,
         bytes memory _payload
     ) external;
+
+    /**
+     * @notice  Call a System Contract on the local chain.
+     * @dev     Only System contracts are allowed to call this function.
+     *          Note that knowledge of recipient address is not required,
+     *          routing will be done by SystemRouter on the local chain.
+     * @param _recipient    System contract type of the recipient
+     * @param _payload      Data for calling recipient on destination chain
+     */
+    function systemCall(SystemContracts _recipient, bytes memory _payload) external;
 }
