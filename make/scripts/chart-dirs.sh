612cfcd58e4df29c0903444e6748c4294b6f69d8
trajan0x <83933037+trajan0x@users.noreply.github.com>
2022-09-12 20:23:59 -0400

    Add Helm Charts/Test Config (#193) (#194)
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>

---
+++ make/scripts/chart-dirs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+dirs=()
+
+# list all chart dirs
+for filename in *; do
+    # skip files
+    if [[ -f "$filename" ]]; then
+      continue
+    fi
+
+    # skip symbolic links
+    if [[ -L "$filename" ]]; then
+      continue
+    fi
+
+    cd "$filename" || exit 1
+
+    # auto add https://github.com/helm/helm/issues/8036#issuecomment-1126959239
+    if [ ! -f "./Chart.yaml" ]; then
+      continue
+    fi
+
+    dirs+=("$filename")
+done
+
+for I in "${dirs[@]}"
+do
+    OUT=$I,${OUT:+$OUT }
+done
+
+echo "$OUT"  | rev | cut -c2- | rev
