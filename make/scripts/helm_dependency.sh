c0e9772c52572ce32ee9533af419302db176914f
trajan0x <83933037+trajan0x@users.noreply.github.com>
2022-09-12 20:23:59 -0400

    Add Helm Charts/Test Config (#193) (#194)
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>

---
+++ make/scripts/helm_dependency.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# assumes that we're in the charts dir
+for filename in *; do
+    # skip files
+    if [[ -f "$filename" ]]; then
+      continue
+    fi
+
+    # skip symbolic links
+    if [[ -L "$filename" ]]; then
+      continue
+    fi
+
+    cd "$filename" || exit 1
+
+    # auto add https://github.com/helm/helm/issues/8036#issuecomment-1126959239
+    if [ -f "./Chart.lock" ]; then
+      yq --indent 0 '.dependencies | map(["helm", "repo", "add", .name, .repository] | join(" ")) | .[]' "./Chart.lock"  | sh --;
+    fi
+
+    helm dependency update
+done
