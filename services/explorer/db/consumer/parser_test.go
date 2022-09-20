8287c879d5dda171314fa9376bf3e3ed67e9cfc9
Simon Mahns <ak.simonm@gmail.com>
2022-09-20 00:52:07 -0400

    fix parser tests

--- services/explorer/db/consumer/parser_test.go
+++ services/explorer/db/consumer/parser_test.go
@@ -1,29 +1,3 @@
 package consumer_test
 
-import (
-	"github.com/ethereum/go-ethereum/common"
-	. "github.com/stretchr/testify/assert"
-	"github.com/synapsecns/sanguine/services/explorer/db/consumer"
-)
-
-func (c *ConsumerSuite) TestParser() {
-	_, bridgeConfigContract := c.deployManager.GetBridgeConfigV3(c.GetTestContext(), c.testBackend)
-
-	fetcher, err := consumer.NewBridgeConfigFetcher(bridgeConfigContract.Address(), c.testBackend)
-	Nil(c.T(), err)
-
-	curentBlockNumber, err := c.testBackend.BlockNumber(c.GetTestContext())
-	Nil(c.T(), err)
-
-	for _, testToken := range testTokens {
-		tokenID, err := fetcher.GetTokenID(c.GetTestContext(), uint32(testToken.ChainId.Uint64()), uint32(curentBlockNumber), common.HexToAddress(testToken.TokenAddress))
-
-		Nil(c.T(), err)
-		Equal(c.T(), *tokenID, testToken.tokenID)
-		token, err := fetcher.GetToken(c.GetTestContext(), uint32(testToken.ChainId.Uint64()), uint32(curentBlockNumber), *tokenID)
-		Nil(c.T(), err)
-		Equal(c.T(), common.HexToAddress(testToken.TokenAddress).String(), common.HexToAddress(token.TokenAddress).String())
-		Equal(c.T(), testToken.SwapFee, token.SwapFee)
-		Equal(c.T(), testToken.IsUnderlying, token.IsUnderlying)
-	}
-}
+// TODO after mock bridge emits events.
