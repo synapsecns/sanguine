7b7f11260b7d9e2466c3ba4a295db0e1bd223db6
Max Planck <maxplanck.crypto@gmail.com>
2022-09-22 17:24:15 -0400

    working backfill for swap. need to do bridge events

--- services/explorer/contracts/swap/testswap/contract/TestSwapFlashLoan.sol
+++ services/explorer/contracts/swap/testswap/contract/TestSwapFlashLoan.sol
@@ -4473,16 +4473,18 @@ contract TestSwapFlashLoan is SwapFlashLoan {
         );
     }
     function testAddLiquidity(
-        uint256[] calldata amounts,
-        uint256 minToMint,
-        uint256 deadline
+        address provider,
+        uint256[] memory tokenAmounts,
+        uint256[] memory fees,
+        uint256 invariant,
+        uint256 lpTokenSupply
     ) external {
         emit AddLiquidity(
-            msg.sender,
-            amounts,
-            amounts,
-            minToMint,
-            deadline
+            provider,
+            tokenAmounts,
+            fees,
+            invariant,
+            lpTokenSupply
         );
     }
     function testRemoveLiquidity(
