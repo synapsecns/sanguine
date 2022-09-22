288754e8964128daf9fbae70f6be732faafe8d64
Simon Mahns <ak.simonm@gmail.com>
2022-09-22 13:06:57 -0400

    testing if docker/clickhouse spin up just takes longer on github thus is purging before tests can be run

--- services/explorer/testutil/clickhouse/clickhouse.go
+++ services/explorer/testutil/clickhouse/clickhouse.go
@@ -57,10 +57,10 @@ func NewClickhouseStore(src string) (func(), *int, error) {
 	// Fetch port assigned to container
 	address := fmt.Sprintf("%s:%s", "localhost", resource.GetPort("9000/tcp"))
 
-	// Docker will hard kill the container in 60 seconds (this is a test env)
-	if resource.Expire(60) != nil {
-		return nil, nil, err
-	}
+	//// Docker will hard kill the container in 60 seconds (this is a test env)
+	//if resource.Expire(60) != nil {
+	//	return nil, nil, err
+	//}
 
 	// Teardown function
 	cleanup := func() {
