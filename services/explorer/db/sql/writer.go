3c33d556a016583db3f254db0342e94105150a3e
Max Planck <99688618+CryptoMaxPlanck@users.noreply.github.com>
2022-09-28 17:25:02 -0400

    fix removing some unused explorer things (#227)
    * fix
    
    * uncomment out clickhouse expiry
    
    * linttt
    
    * fix

--- services/explorer/db/sql/writer.go
+++ services/explorer/db/sql/writer.go
@@ -8,6 +8,7 @@ import (
 	"github.com/synapsecns/sanguine/services/explorer/types/bridge"
 	"github.com/synapsecns/sanguine/services/explorer/types/swap"
 	"math/big"
+	"time"
 )
 
 // EventType is an enum for event types.
@@ -133,6 +134,7 @@ func (s *Store) eventToBridgeEvent(event bridge.EventLog, chainID uint32, tokenI
 	}
 
 	return BridgeEvent{
+		InsertTime:         uint64(time.Now().UnixNano()),
 		ContractAddress:    event.GetContractAddress().String(),
 		ChainID:            chainID,
 		EventType:          event.GetEventType().Int(),
@@ -193,6 +195,7 @@ func (s *Store) eventToSwapEvent(event swap.EventLog, chainID uint32, tokenID *s
 	}
 
 	return SwapEvent{
+		InsertTime:      uint64(time.Now().UnixNano()),
 		ContractAddress: event.GetContractAddress().String(),
 		ChainID:         chainID,
 		EventType:       event.GetEventType().Int(),
