f31513bf06d83f3e3e21bb0cbc395d9d43bba523
Trajan0x <trajan0x@users.noreply.github.com>
2022-10-04 13:23:04 -0400

    add set-idle-conns (see: https://github.com/GoogleCloudPlatform/cloud-sql-proxy/issues/1255#issuecomment-1267311588 which is cause dby no connection pooling)

--- agents/db/datastore/sql/mysql/store.go
+++ agents/db/datastore/sql/mysql/store.go
@@ -18,7 +18,7 @@ type Store struct {
 }
 
 // MaxIdleConns is exported here for testing. Tests execute too slowly with a reconnect each time.
-var MaxIdleConns = 0
+var MaxIdleConns = 10
 
 // NamingStrategy is exported here for testing.
 var NamingStrategy = schema.NamingStrategy{}
