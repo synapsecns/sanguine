7d6ed71ecf7a8fd9b19124699510c42615659ffd
Max Planck <maxplanck.crypto@gmail.com>
2022-10-07 17:48:04 -0400

    fix fr

--- services/explorer/consumer/parser.go
+++ services/explorer/consumer/parser.go
@@ -297,20 +297,19 @@ func (p *BridgeParser) ParseAndStore(ctx context.Context, log ethTypes.Log, chai
 	// Get timestamp from consumer
 	timeStamp, err := p.consumerFetcher.FetchClient.GetBlockTime(ctx, int(chainID), int(iFace.GetBlockNumber()))
 	// If we have a timestamp, populate the following attributes of bridgeEvent.
-	if err != nil {
-		return fmt.Errorf("could not fetch timestamp: %w", err)
-	}
-	timeStampBig := uint64(*timeStamp.Response)
-	bridgeEvent.TimeStamp = &timeStampBig
-	// Add the price of the token at the block the event occurred using coin gecko (to bridgeEvent).
-	tokenPrice, symbol := GetTokenMetadataWithTokenID(ctx, *timeStamp.Response, tokenID)
-	if tokenPrice != nil {
-		// Add AmountUSD to bridgeEvent (if price is not nil)
-		bridgeEvent.AmountUSD = GetAmountUSD(iFace.GetAmount(), token.TokenDecimals, tokenPrice)
-		// Add AmountUSD to bridgeEvent (if price is not nil)
-		bridgeEvent.AmountUSD = GetAmountUSD(iFace.GetAmount(), token.TokenDecimals, tokenPrice)
-		// Add TokenSymbol to bridgeEvent
-		bridgeEvent.TokenSymbol = ToNullString(symbol)
+	if err == nil {
+		timeStampBig := uint64(*timeStamp.Response)
+		bridgeEvent.TimeStamp = &timeStampBig
+		// Add the price of the token at the block the event occurred using coin gecko (to bridgeEvent).
+		tokenPrice, symbol := GetTokenMetadataWithTokenID(ctx, *timeStamp.Response, tokenID)
+		if tokenPrice != nil {
+			// Add AmountUSD to bridgeEvent (if price is not nil)
+			bridgeEvent.AmountUSD = GetAmountUSD(iFace.GetAmount(), token.TokenDecimals, tokenPrice)
+			// Add AmountUSD to bridgeEvent (if price is not nil)
+			bridgeEvent.AmountUSD = GetAmountUSD(iFace.GetAmount(), token.TokenDecimals, tokenPrice)
+			// Add TokenSymbol to bridgeEvent
+			bridgeEvent.TokenSymbol = ToNullString(symbol)
+		}
 	}
 
 	// TODO fix GetTxSender, make this actually successfully get a sender value
