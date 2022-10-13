40da5a89bc42a0ecbb465e57aa8c550057b29ea9
Simon <ak.simonm@gmail.com>
2022-10-13 13:13:41 -0400

    Scribe SQL indexes (#251)

--- services/scribe/db/datastore/sql/base/log.go
+++ services/scribe/db/datastore/sql/base/log.go
@@ -85,7 +85,7 @@ func (s Store) ConfirmLogsInRange(ctx context.Context, startBlock, endBlock uint
 	rangeQuery := fmt.Sprintf("%s BETWEEN ? AND ?", BlockNumberFieldName)
 	dbTx := s.DB().WithContext(ctx).
 		Model(&Log{}).
-		Order(BlockNumberFieldName).
+		Order(BlockNumberFieldName+" desc").
 		Where(rangeQuery, startBlock, endBlock).
 		Where(&Log{ChainID: chainID}).
 		Update(ConfirmedFieldName, true)
@@ -135,7 +135,7 @@ func (s Store) RetrieveLogsWithFilter(ctx context.Context, logFilter db.LogFilte
 	dbTx := s.DB().WithContext(ctx).
 		Model(&Log{}).
 		Where(&query).
-		Order(fmt.Sprintf("%s, %s", BlockNumberFieldName, BlockIndexFieldName)).
+		Order(fmt.Sprintf("%s desc, %s desc", BlockNumberFieldName, BlockIndexFieldName)).
 		Offset((page - 1) * PageSize).
 		Limit(PageSize).
 		Find(&dbLogs)
@@ -162,7 +162,7 @@ func (s Store) RetrieveLogsInRange(ctx context.Context, logFilter db.LogFilter, 
 		Model(&Log{}).
 		Where(&queryFilter).
 		Where(rangeQuery, startBlock, endBlock).
-		Order(fmt.Sprintf("%s, %s", BlockNumberFieldName, BlockIndexFieldName)).
+		Order(fmt.Sprintf("%s desc, %s desc", BlockNumberFieldName, BlockIndexFieldName)).
 		Offset((page - 1) * PageSize).
 		Limit(PageSize).
 		Find(&dbLogs)
