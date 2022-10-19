fa7995b2fb279527c529c5bd3ac6e24a6539db97
Simon <ak.simonm@gmail.com>
2022-10-19 09:16:40 -0400

    lint

--- services/explorer/api/server.go
+++ services/explorer/api/server.go
@@ -85,7 +85,6 @@ func Start(ctx context.Context, cfg Config) error {
 
 // InitDB initializes a database given a database type and path.
 func InitDB(ctx context.Context, address string) (db.ConsumerDB, error) {
-
 	if address == "default" {
 		cleanup, port, err := clickhouse.NewClickhouseStore("explorer")
 		if cleanup == nil {
