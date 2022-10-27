55a31e88218d5c5f2d68964a677b0f154451feca
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-10-27 18:49:20 +0100

    SystemRouter simplification + docs

--- packages/contracts-core/contracts/system/SystemRouter.sol
+++ packages/contracts-core/contracts/system/SystemRouter.sol
@@ -4,32 +4,61 @@ pragma solidity 0.8.17;
 import { BasicClient } from "../client/BasicClient.sol";
 import { LocalDomainContext } from "../context/LocalDomainContext.sol";
 import { TypedMemView } from "../libs/TypedMemView.sol";
-import { SystemMessage } from "../libs/SystemMessage.sol";
+import { SystemCall } from "../libs/SystemCall.sol";
 import { ISystemRouter } from "../interfaces/ISystemRouter.sol";
 import { Tips } from "../libs/Tips.sol";
 
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
 /**
- * @notice Sends and receives system messages:
- * an internal messaging channel between system contracts.
- * This makes it possible to send message to any system contract (e.g. Origin) on another chain
- * without knowing its address in advance, making easy cross-chain setups possible.
- * What is more, knowing System Router address on destination chain is also
- * not required. Instead, a special SYSTEM_ROUTER value is used as sender/recipient
- * for the system messages (see SystemMessage.sol for more details).
+ * @notice Router for calls between system contracts (aka "System Calls").
+ * SystemRouter makes it possible to perform a call from one system contract to another
+ * without knowing the recipient address. This works for both on-chain calls, when caller and
+ * recipient are deployed on the same chain, and for cross-chain calls, when caller and
+ * recipient are deployed on different chains.
  *
- * SystemRouter keeps track of all system contracts deployed on current chain,
- * and routes messages to/from them. System contracts are supposed to have
- * external methods, guarded by onlySystemRouter modifier. These methods could
- * be called cross-chain from any of the system contracts.
+ * SystemRouter allows both calls and "multi calls". Multicall performs a series of calls,
+ * calling requested recipients one by one, supplying the requested payloads. The whole multicall
+ * will fail, if any of the calls reverts.
+ *
+ * SystemRouter keeps track of all system contracts deployed on current chain. This enables sending
+ * calls "to Origin", "to Destination" without actually knowing the Origin/Destination address.
+ *
+ * For the on-chain calls, SystemRouter is simply forwarding the call to the requested recipient(s).
+ *
+ * For the cross-chain calls, SystemRouter sends a message to SystemRouter on destination chain,
+ * in order for it to forward the call to the request recipient(s). SystemRouter doesn't need to be
+ * aware of System Router address on destination chain. It uses a special value SYSTEM_ROUTER for
+ * the recipient instead. Origin contract on origin chain enforces the invariant that only
+ * the system router could specify such a recipient. For these messages Origin uses SYSTEM_ROUTER
+ * for "sender" field, instead of actual System Router address (as it does for usual messages).
+ * Destination contract routes messages, where SYSTEM_ROUTER is specified as recipient, to a local
+ * System Router. System Router only accepts incoming messages with "sender = SYSTEM_ROUTER",
+ * enforcing the invariant that only origin's System Router is able to send cross-chain messages to
+ * destination's System Router, assuming no optimistic verification fraud (more on this below).
+ *
+ * @dev Security considerations
+ * System Router only accepts calls from the system contracts, so it's not possible for the attacker
+ * to initiate a system call through the system router. However, every system contract that wants
+ * to expose one of its external functions for the system calls, should do the following:
+ * 1. Such functions should have the same last three arguments:
+ * - someFunction(<...>, uint32 origin, ISystemRouter.SystemEntity caller, uint256 rootSubmittedAt)
+ * These arguments are filled by System Routers on origin and destination chain. This allows
+ * the recipient to set the restrictions for receiving the call in a very granular way.
+ * To perform a call, use payload = abi.encodeWithSelector(someFunction.selector, <...>);
+ * 2. Guard such function with `onlySystemRouter` modifier to prevent unauthorized direct calls.
+ * Guard function with additional modifiers based on `origin`, `caller`, `rootSubmittedAt`.
+ * `rootSubmittedAt` based modifier is a must for receiving cross-chain system calls.  Any Notary
+ * can potentially commit fraud, and try to execute an arbitrary message, including
+ * a "message to System Router". By enforcing a minimum optimistic latency for the recipient this
+ * attack can be militated, assuming there is at least one honest Guard willing to report the fraud.
  */
 contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
     using Address for address;
     using TypedMemView for bytes;
     using TypedMemView for bytes29;
-    using SystemMessage for bytes;
-    using SystemMessage for bytes29;
+    using SystemCall for bytes;
+    using SystemCall for bytes29;
 
     /*╔══════════════════════════════════════════════════════════════════════╗*\
     ▏*║                             CONSTRUCTOR                              ║*▕
@@ -74,6 +103,7 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
     ) external {
         /// @dev This will revert if msg.sender is not a system contract
         SystemEntity caller = _getSystemEntity(msg.sender);
+        // Append calldata with (origin, caller) to form the payload
         bytes memory payload = _formatCalldata(caller, _data);
         if (_destination == _localDomain()) {
             /// @dev Passing current timestamp for consistency
@@ -83,7 +113,9 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
             _localSystemCall(uint8(_recipient), payload, block.timestamp);
         } else {
             bytes[] memory systemCalls = new bytes[](1);
-            systemCalls[0] = SystemMessage.formatSystemCall(uint8(_recipient), payload);
+            systemCalls[0] = SystemCall.formatSystemCall(uint8(_recipient), payload);
+            // To generalize things, a remote system call is always a multicall.
+            // In case of a "usual call", this is a multicall with exactly one call inside.
             _remoteSystemCall(_destination, _optimisticSeconds, systemCalls);
         }
     }
@@ -105,6 +137,7 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
         uint256 amount = _recipients.length;
         bytes[] memory payloads = new bytes[](amount);
         for (uint256 i = 0; i < amount; ++i) {
+            // Append calldata with (origin, caller) to form the payload
             payloads[i] = _formatCalldata(caller, _dataArray[i]);
         }
         _multiCall(_destination, _optimisticSeconds, _recipients, payloads);
@@ -128,7 +161,7 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
          * Destination is supposed to reject messages
          * from unknown chains, so we can skip origin check here.
          */
-        return SystemMessage.SYSTEM_ROUTER;
+        return SystemCall.SYSTEM_ROUTER;
     }
 
     /*╔══════════════════════════════════════════════════════════════════════╗*\
@@ -145,24 +178,15 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
         uint256 _rootSubmittedAt,
         bytes memory _message
     ) internal override {
+        // Deserialize the message into a series of system calls to perform
         bytes[] memory systemMessages = abi.decode(_message, (bytes[]));
         uint256 amount = systemMessages.length;
         for (uint256 i = 0; i < amount; ++i) {
-            bytes29 message = systemMessages[i].castToSystemMessage();
-            require(message.isSystemMessage(), "Not a system message");
-            (SystemMessage.MessageFlag messageType, bytes29 body) = message.unpackMessage();
-            if (messageType == SystemMessage.MessageFlag.Call) {
-                /// @dev System Contract has to check root submission time
-                /// to confirm that optimistic period for given function has passed.
-                _localSystemCall(
-                    body.callRecipient(),
-                    body.callPayload().clone(),
-                    _rootSubmittedAt
-                );
-            } else {
-                // Sanity check: no other MessageFlag values exist
-                assert(false);
-            }
+            bytes29 _view = systemMessages[i].castToSystemCall();
+            // Check that payload in a properly formatted system call
+            require(_view.isSystemCall(), "Not a system call");
+            // Route the system call to specified recipient
+            _localSystemCall(_view.callRecipient(), _view.callPayload().clone(), _rootSubmittedAt);
         }
     }
 
@@ -173,10 +197,12 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
     ) internal {
         address recipient = _getSystemAddress(_recipient);
         require(recipient != address(0), "System Contract not set");
-        // this will call recipient and bubble the revert from the external call
+        // recipient.functionCall() calls recipient and bubbles the revert from the external call
+        // We add `rootSubmittedAt` as the last argument.
+        // (origin, caller) were added by the System Router on origin chain, see _formatCalldata()
+        // So the last arguments for the call are: (origin, caller, rootSubmittedAt)
         recipient.functionCall(abi.encodePacked(_payload, _rootSubmittedAt));
-        /// @dev add root timestamp as the last argument
-        /// uint256 type allows us to use encodePacked w/o casting
+        // uint256 takes the full word of storage, so we can use encodePacked here w/o casting
     }
 
     function _remoteSystemCall(
@@ -184,11 +210,11 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
         uint32 _optimisticSeconds,
         bytes[] memory _systemCalls
     ) internal {
+        // Serialize the series of system calls into a byte string
         bytes memory message = abi.encode(_systemCalls);
         /**
-         * @dev Origin should recognize SystemRouter as the "true sender"
-         *      and use SYSTEM_ROUTER address as "sender" instead. This enables not
-         *      knowing SystemRouter address on remote chain in advance.
+         * @dev Origin will use SYSTEM_ROUTER as "sender" field for messages
+         * sent by System Router.
          */
         _send(_destination, _optimisticSeconds, Tips.emptyTips(), message);
     }
@@ -201,17 +227,16 @@ contract SystemRouter is LocalDomainContext, BasicClient, ISystemRouter {
     ) internal {
         uint256 amount = _recipients.length;
         if (_destination == _localDomain()) {
+            // Perform an on-chain multicall
             for (uint256 i = 0; i < amount; ++i) {
                 /// @dev Passing current timestamp for consistency, see systemCall() for details
                 _localSystemCall(uint8(_recipients[i]), _payloads[i], block.timestamp);
             }
         } else {
+            // Perform a cross-chain multicall
             bytes[] memory systemCalls = new bytes[](amount);
             for (uint256 i = 0; i < amount; ++i) {
-                systemCalls[i] = SystemMessage.formatSystemCall(
-                    uint8(_recipients[i]),
-                    _payloads[i]
-                );
+                systemCalls[i] = SystemCall.formatSystemCall(uint8(_recipients[i]), _payloads[i]);
             }
             _remoteSystemCall(_destination, _optimisticSeconds, systemCalls);
         }
