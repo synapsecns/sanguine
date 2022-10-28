446ca581c5214ee1158fee3675261e8d682b9660
Simon <ak.simonm@gmail.com>
2022-10-28 19:22:38 -0400

    Update fetcher_test.go

--- services/explorer/consumer/fetcher_test.go
+++ services/explorer/consumer/fetcher_test.go
@@ -91,7 +91,6 @@ func (c *ConsumerSuite) TestTimeToBlockNumber() {
 		if closestBlockTime > timeDiff {
 			closestBlockTime = timeDiff
 			closestBlockNumber = blockNumber
-
 		}
 	}
 	Equal(c.T(), closestBlockNumber, blockNumberFetched)
