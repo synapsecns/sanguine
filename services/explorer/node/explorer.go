8b9e97ce6ffc92f46658fbed2d3b10e026da1f33
Max Planck <maxplanck.crypto@gmail.com>
2022-11-10 17:04:50 -0500

    explorer.go

--- services/explorer/node/explorer.go
+++ services/explorer/node/explorer.go
@@ -17,13 +17,13 @@ import (
 
 // ExplorerBackfiller is a backfiller that aggregates all backfilling from ChainBackfillers.
 type ExplorerBackfiller struct {
-	// consumerDB is the database to store consumer data in
+	// consumerDB is the database to store consumer data in.
 	consumerDB db.ConsumerDB
-	// clients is a mapping of chain IDs -> clients
+	// clients is a mapping of chain IDs -> clients.
 	clients map[uint32]bind.ContractBackend
-	// ChainBackfillers is a mapping of chain IDs -> chain backfillers
+	// ChainBackfillers is a mapping of chain IDs -> chain backfillers.
 	ChainBackfillers map[uint32]*backfill.ChainBackfiller
-	// config is the config for the backfiller
+	// config is the config for the backfiller.
 	config config.Config
 }
 
@@ -31,27 +31,23 @@ type ExplorerBackfiller struct {
 //
 // nolint:gocognit
 func NewExplorerBackfiller(consumerDB db.ConsumerDB, config config.Config, clients map[uint32]bind.ContractBackend) (*ExplorerBackfiller, error) {
-	// initialize the list of chain backfillers
 	chainBackfillers := make(map[uint32]*backfill.ChainBackfiller)
-
-	// create the consumer Fetcher
 	fetcher := consumer.NewFetcher(gqlClient.NewClient(http.DefaultClient, config.ScribeURL))
-
-	// create the bridge config Fetcher
 	bridgeConfigRef, err := bridgeconfig.NewBridgeConfigRef(common.HexToAddress(config.BridgeConfigAddress), clients[config.BridgeConfigChainID])
-
 	if err != nil || bridgeConfigRef == nil {
 		return nil, fmt.Errorf("could not create bridge config Fetcher: %w", err)
 	}
-	// initialize each chain backfiller
+
+	// Initialize each chain backfiller.
 	for _, chainConfig := range config.Chains {
-		// create the chain backfiller
 		chainBackfiller, err := getChainBackfiller(consumerDB, chainConfig, fetcher, clients[chainConfig.ChainID], common.HexToAddress(config.BridgeConfigAddress), bridgeConfigRef)
 		if err != nil {
 			return nil, fmt.Errorf("could not get chain backfiller: %w", err)
 		}
+
 		chainBackfillers[chainConfig.ChainID] = chainBackfiller
 	}
+
 	return &ExplorerBackfiller{
 		consumerDB:       consumerDB,
 		clients:          clients,
@@ -62,24 +58,22 @@ func NewExplorerBackfiller(consumerDB db.ConsumerDB, config config.Config, clien
 
 // Backfill iterates over each chain backfiller and calls Backfill concurrently on each one.
 func (e ExplorerBackfiller) Backfill(ctx context.Context) error {
-	// initialize the errgroup
 	g, groupCtx := errgroup.WithContext(ctx)
 
-	// iterate over each chain backfiller
 	for i := range e.config.Chains {
-		// capture func literal
 		chainConfig := e.config.Chains[i]
 		chainBackfiller := e.ChainBackfillers[chainConfig.ChainID]
-		// call Backfill concurrently
+
 		g.Go(func() error {
 			err := chainBackfiller.Backfill(groupCtx)
 			if err != nil {
 				return fmt.Errorf("could not backfill chain %d: %w", chainConfig.ChainID, err)
 			}
+
 			return nil
 		})
 	}
-	// wait for all backfills to finish
+
 	if err := g.Wait(); err != nil {
 		return fmt.Errorf("could not backfill explorer: %w", err)
 	}
@@ -93,8 +87,11 @@ func getChainBackfiller(consumerDB db.ConsumerDB, chainConfig config.ChainConfig
 	if err != nil || newConfigFetcher == nil {
 		return nil, fmt.Errorf("could not get bridge abi: %w", err)
 	}
+
 	swapParsers := make(map[common.Address]*consumer.SwapParser)
+
 	var bridgeParser *consumer.BridgeParser
+
 	for i := range chainConfig.Contracts {
 		switch chainConfig.Contracts[i].ContractType {
 		case "bridge":
@@ -107,13 +104,17 @@ func getChainBackfiller(consumerDB db.ConsumerDB, chainConfig config.ChainConfig
 			if err != nil || swapFetcher == nil {
 				return nil, fmt.Errorf("could not create swap Fetcher: %w", err)
 			}
+
 			swapParser, err := consumer.NewSwapParser(consumerDB, common.HexToAddress(chainConfig.Contracts[i].Address), *swapFetcher, fetcher)
 			if err != nil || swapParser == nil {
 				return nil, fmt.Errorf("could not create swap parser: %w", err)
 			}
+
 			swapParsers[common.HexToAddress(chainConfig.Contracts[i].Address)] = swapParser
 		}
 	}
+
 	chainBackfiller := backfill.NewChainBackfiller(consumerDB, bridgeParser, swapParsers, *fetcher, chainConfig)
+	
 	return chainBackfiller, nil
 }
