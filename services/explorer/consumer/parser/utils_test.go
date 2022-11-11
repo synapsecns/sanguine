db97555362f6410c4c4e85ac1c4a1648a472c421
Simon <ak.simonm@gmail.com>
2022-11-11 16:00:44 -0500

    test2

--- services/explorer/consumer/parser/utils_test.go
+++ services/explorer/consumer/parser/utils_test.go
@@ -3,14 +3,12 @@ package parser_test
 import (
 	. "github.com/stretchr/testify/assert"
 	"github.com/synapsecns/sanguine/services/explorer/consumer/parser"
-	"os"
 	"path/filepath"
 	"testing"
 )
 
 func TestOpenYaml(t *testing.T) {
-	pwd, _ := os.Getwd()
-	path := pwd + filepath.Clean("/static/tokenIDToCoinGeckoID.yaml")
+	path, _ := filepath.Abs("../../static/tokenIDToCoinGeckoID.yaml")
 	parsedYaml, err := parser.OpenYaml(path)
 	Nil(t, err)
 	NotNil(t, parsedYaml)
