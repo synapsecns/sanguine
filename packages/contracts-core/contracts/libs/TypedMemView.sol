6d50948ae521a8f4ad227b233b5fb7481ef9a367
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-11-13 16:43:01 +0000

    simplify nibbleHex()

--- packages/contracts-core/contracts/libs/TypedMemView.sol
+++ packages/contracts-core/contracts/libs/TypedMemView.sol
@@ -64,65 +64,17 @@ library TypedMemView {
     uint256 public constant LOW_12_MASK = 0xffffffffffffffffffffffff;
     uint8 public constant TWELVE_BYTES = 96;
 
+    // For nibble encoding
+    bytes private constant NIBBLE_LOOKUP = "0123456789abcdef";
+
     /**
-     * @notice      Returns the encoded hex character that represents
-     *              the lower 4 bits of the argument.
-     * @param _b    The byte
-     * @return      char - The encoded hex character
+     * @notice Returns the encoded hex character that represents the lower 4 bits of the argument.
+     * @param _byte     The byte
+     * @return _char    The encoded hex character
      */
-    // solhint-disable-next-line code-complexity
-    function nibbleHex(uint8 _b) internal pure returns (uint8 char) {
-        // This can probably be done more efficiently, but it's only in error
-        // paths, so we don't really care :)
-        uint8 _nibble = _b | 0xf0; // set top 4, keep bottom 4
-        if (_nibble == 0xf0) {
-            return 0x30;
-        } // 0
-        if (_nibble == 0xf1) {
-            return 0x31;
-        } // 1
-        if (_nibble == 0xf2) {
-            return 0x32;
-        } // 2
-        if (_nibble == 0xf3) {
-            return 0x33;
-        } // 3
-        if (_nibble == 0xf4) {
-            return 0x34;
-        } // 4
-        if (_nibble == 0xf5) {
-            return 0x35;
-        } // 5
-        if (_nibble == 0xf6) {
-            return 0x36;
-        } // 6
-        if (_nibble == 0xf7) {
-            return 0x37;
-        } // 7
-        if (_nibble == 0xf8) {
-            return 0x38;
-        } // 8
-        if (_nibble == 0xf9) {
-            return 0x39;
-        } // 9
-        if (_nibble == 0xfa) {
-            return 0x61;
-        } // a
-        if (_nibble == 0xfb) {
-            return 0x62;
-        } // b
-        if (_nibble == 0xfc) {
-            return 0x63;
-        } // c
-        if (_nibble == 0xfd) {
-            return 0x64;
-        } // d
-        if (_nibble == 0xfe) {
-            return 0x65;
-        } // e
-        if (_nibble == 0xff) {
-            return 0x66;
-        } // f
+    function nibbleHex(uint8 _byte) internal pure returns (uint8 _char) {
+        uint8 _nibble = _byte & 0x0f; // keep bottom 4, 0 top 4
+        _char = uint8(NIBBLE_LOOKUP[_nibble]);
     }
 
     /**
