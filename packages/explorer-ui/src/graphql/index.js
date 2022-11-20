4b163efaf821bc57b75b815c267e0873e1055125
Trajan0x <trajan0x@users.noreply.github.com>
2022-11-20 17:26:41 -0500

    fix switch case

--- packages/explorer-ui/src/graphql/index.js
+++ packages/explorer-ui/src/graphql/index.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL ? process.env.API_URL : 'http://localhost:4000/graphql'
+export const API_URL = process.env.REACT_API_URL ? process.env.REACT_API_URL : 'http://localhost:4000/graphql'
