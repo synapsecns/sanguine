b112c645984d9bbec788ab5b2f194b6f9801b8e9
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-12-07 14:14:17 +0000

    update Hubs

--- packages/contracts-core/contracts/events/AttestationHubEvents.sol
+++ packages/contracts-core/contracts/events/AttestationHubEvents.sol
@@ -4,8 +4,9 @@ pragma solidity 0.8.17;
 abstract contract AttestationHubEvents {
     /**
      * @notice Emitted when an attestation is submitted to AttestationHub.
-     * @param notary        Notary who signed the attestation
+     * @param guards        Guards who signed the attestation
+     * @param notaries      Notaries who signed the attestation
      * @param attestation   Raw payload with attestation data and notary signature
      */
-    event AttestationAccepted(address indexed notary, bytes attestation);
+    event AttestationAccepted(address[] guards, address[] notaries, bytes attestation);
 }
