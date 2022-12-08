aa5dc59c876f0e5e5e443f80d5293888d92c1cb9
Simon <ak.simonm@gmail.com>
2022-12-08 15:11:21 -0500

    parser

--- services/explorer/consumer/parser/bridgeparser.go
+++ services/explorer/consumer/parser/bridgeparser.go
@@ -48,7 +48,7 @@ func NewBridgeParser(consumerDB db.ConsumerDB, bridgeAddress common.Address, bri
 	if err != nil {
 		return nil, fmt.Errorf("could not create %T: %w", bridgev1.SynapseBridgeFilterer{}, err)
 	}
-	idPath := filepath.Clean("./static/tokenIDToCoinGeckoID.yaml")
+	idPath := filepath.Clean("../static/tokenIDToCoinGeckoID.yaml")
 	if err != nil {
 		return nil, fmt.Errorf("could find path to yaml file: %w", err)
 	}
