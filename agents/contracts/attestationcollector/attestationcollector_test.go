021b926524be2d59937b94b94a93763a5be8076d
protojoe <486323+joecroninallen@users.noreply.github.com>
2022-12-14 09:02:33 -0800

    Making helper function launchTest in attestationcollector_test.go

--- agents/contracts/attestationcollector/attestationcollector_test.go
+++ agents/contracts/attestationcollector/attestationcollector_test.go
@@ -17,8 +17,11 @@ import (
 	"github.com/synapsecns/sanguine/agents/types"
 )
 
-// TestAttestationCollectorSuite tests submitting an attesation with one guard and one notary.
-func (a AttestationCollectorSuite) TestSubmitAttestationOneGuardOneNotary() {
+func (a AttestationCollectorSuite) launchTest(amountGuards, amountNotaries int) {
+	GreaterOrEqual(a.T(), amountGuards+amountNotaries, 1)
+	LessOrEqual(a.T(), amountGuards, 1)
+	LessOrEqual(a.T(), amountNotaries, 1)
+
 	txContextAttestationCollector := a.testBackendAttestation.GetTxContext(a.GetTestContext(), a.attestationContractMetadata.OwnerPtr())
 
 	// Create a channel and subscription to receive AttestationAccepted events as they are emitted.
@@ -44,16 +47,22 @@ func (a AttestationCollectorSuite) TestSubmitAttestationOneGuardOneNotary() {
 	encodedAttestation, err := types.EncodeAttestation(unsignedAttestation)
 	Nil(a.T(), err)
 
-	notarySignature, err := a.notarySigner.SignMessage(a.GetTestContext(), core.BytesToSlice(hashedAttestation), false)
-	Nil(a.T(), err)
-
-	guardSignature, err := a.guardSigner.SignMessage(a.GetTestContext(), core.BytesToSlice(hashedAttestation), false)
-	Nil(a.T(), err)
-
+	notarySignatures := []types.Signature{}
+	if amountNotaries == 1 {
+		notarySignature, err := a.notarySigner.SignMessage(a.GetTestContext(), core.BytesToSlice(hashedAttestation), false)
+		Nil(a.T(), err)
+		notarySignatures = append(notarySignatures, notarySignature)
+	}
+	guardSignatures := []types.Signature{}
+	if amountGuards == 1 {
+		guardSignature, err := a.guardSigner.SignMessage(a.GetTestContext(), core.BytesToSlice(hashedAttestation), false)
+		Nil(a.T(), err)
+		guardSignatures = append(guardSignatures, guardSignature)
+	}
 	signedAttestation := types.NewSignedAttestation(
 		unsignedAttestation,
-		[]types.Signature{guardSignature},
-		[]types.Signature{notarySignature})
+		guardSignatures,
+		notarySignatures)
 	encodedGuardSignatures, err := types.EncodeSignatures(signedAttestation.GuardSignatures())
 	Nil(a.T(), err)
 	encodedNotarySignatures, err := types.EncodeSignatures(signedAttestation.NotarySignatures())
@@ -92,152 +101,19 @@ func (a AttestationCollectorSuite) TestSubmitAttestationOneGuardOneNotary() {
 
 		break
 	}
+}
+
+// TestAttestationCollectorSuite tests submitting an attesation with one guard and one notary.
+func (a AttestationCollectorSuite) TestSubmitAttestationOneGuardOneNotary() {
+	a.launchTest(1, 1)
 }
 
 // TestSubmitAttestationOnlyOneNotary tests submitting an attesation with only one notary.
 func (a AttestationCollectorSuite) TestSubmitAttestationOnlyOneNotary() {
-	txContextAttestationCollector := a.testBackendAttestation.GetTxContext(a.GetTestContext(), a.attestationContractMetadata.OwnerPtr())
-
-	// Create a channel and subscription to receive AttestationAccepted events as they are emitted.
-	attestationSink := make(chan *attestationcollector.AttestationCollectorAttestationAccepted)
-	subAttestation, err := a.attestationContract.WatchAttestationAccepted(&bind.WatchOpts{Context: a.GetTestContext()}, attestationSink)
-	Nil(a.T(), err)
-
-	// Create an attestation
-	origin := uint32(a.testBackendOrigin.GetBigChainID().Uint64())
-	destination := uint32(a.testBackendDestination.GetChainID())
-	// destination := origin + 1
-	nonce := gofakeit.Uint32()
-	root := common.BigToHash(new(big.Int).SetUint64(gofakeit.Uint64()))
-	attestKey := types.AttestationKey{
-		Origin:      origin,
-		Destination: destination,
-		Nonce:       nonce,
-	}
-	unsignedAttestation := types.NewAttestation(attestKey.GetRawKey(), root)
-	hashedAttestation, err := notary.HashAttestation(unsignedAttestation)
-	Nil(a.T(), err)
-
-	encodedAttestation, err := types.EncodeAttestation(unsignedAttestation)
-	Nil(a.T(), err)
-
-	notarySignature, err := a.notarySigner.SignMessage(a.GetTestContext(), core.BytesToSlice(hashedAttestation), false)
-	Nil(a.T(), err)
-
-	signedAttestation := types.NewSignedAttestation(
-		unsignedAttestation,
-		[]types.Signature{},
-		[]types.Signature{notarySignature})
-	encodedGuardSignatures, err := types.EncodeSignatures(signedAttestation.GuardSignatures())
-	Nil(a.T(), err)
-	encodedNotarySignatures, err := types.EncodeSignatures(signedAttestation.NotarySignatures())
-	Nil(a.T(), err)
-
-	attestation, err := a.attestationHarness.FormatAttestation(
-		&bind.CallOpts{Context: a.GetTestContext()},
-		encodedAttestation,
-		encodedGuardSignatures,
-		encodedNotarySignatures,
-	)
-	Nil(a.T(), err)
-
-	// Submit the attestation to get an AttestationSubmitted event.
-	txSubmitAttestation, err := a.attestationContract.SubmitAttestation(txContextAttestationCollector.TransactOpts, attestation)
-	Nil(a.T(), err)
-	a.testBackendAttestation.WaitForConfirmation(a.GetTestContext(), txSubmitAttestation)
-
-	watchCtx, cancel := context.WithTimeout(a.GetTestContext(), time.Second*10)
-	defer cancel()
-
-	select {
-	// check for errors and fail
-	case <-watchCtx.Done():
-		a.T().Error(a.T(), fmt.Errorf("test context completed %w", a.GetTestContext().Err()))
-	case <-subAttestation.Err():
-		a.T().Error(a.T(), subAttestation.Err())
-	// get AttestationSubmitted event
-	case item := <-attestationSink:
-		parser, err := attestationcollector.NewParser(a.attestationContract.Address())
-		Nil(a.T(), err)
-		// Check to see if the event was an AttestationSubmitted event.
-		eventType, ok := parser.EventType(item.Raw)
-		True(a.T(), ok)
-		Equal(a.T(), eventType, attestationcollector.AttestationAcceptedEvent)
-
-		break
-	}
+	a.launchTest(0, 1)
 }
 
 // TestSubmitAttestationOnlyOneGuard tests submitting an attesation with only one guard.
 func (a AttestationCollectorSuite) TestSubmitAttestationOnlyOneGuard() {
-	txContextAttestationCollector := a.testBackendAttestation.GetTxContext(a.GetTestContext(), a.attestationContractMetadata.OwnerPtr())
-
-	// Create a channel and subscription to receive AttestationAccepted events as they are emitted.
-	attestationSink := make(chan *attestationcollector.AttestationCollectorAttestationAccepted)
-	subAttestation, err := a.attestationContract.WatchAttestationAccepted(&bind.WatchOpts{Context: a.GetTestContext()}, attestationSink)
-	Nil(a.T(), err)
-
-	// Create an attestation
-	origin := uint32(a.testBackendOrigin.GetBigChainID().Uint64())
-	destination := uint32(a.testBackendDestination.GetChainID())
-	// destination := origin + 1
-	nonce := gofakeit.Uint32()
-	root := common.BigToHash(new(big.Int).SetUint64(gofakeit.Uint64()))
-	attestKey := types.AttestationKey{
-		Origin:      origin,
-		Destination: destination,
-		Nonce:       nonce,
-	}
-	unsignedAttestation := types.NewAttestation(attestKey.GetRawKey(), root)
-	hashedAttestation, err := notary.HashAttestation(unsignedAttestation)
-	Nil(a.T(), err)
-
-	encodedAttestation, err := types.EncodeAttestation(unsignedAttestation)
-	Nil(a.T(), err)
-
-	guardSignature, err := a.guardSigner.SignMessage(a.GetTestContext(), core.BytesToSlice(hashedAttestation), false)
-	Nil(a.T(), err)
-
-	signedAttestation := types.NewSignedAttestation(
-		unsignedAttestation,
-		[]types.Signature{guardSignature},
-		[]types.Signature{})
-	encodedGuardSignatures, err := types.EncodeSignatures(signedAttestation.GuardSignatures())
-	Nil(a.T(), err)
-	encodedNotarySignatures, err := types.EncodeSignatures(signedAttestation.NotarySignatures())
-	Nil(a.T(), err)
-
-	attestation, err := a.attestationHarness.FormatAttestation(
-		&bind.CallOpts{Context: a.GetTestContext()},
-		encodedAttestation,
-		encodedGuardSignatures,
-		encodedNotarySignatures,
-	)
-	Nil(a.T(), err)
-
-	// Submit the attestation to get an AttestationSubmitted event.
-	txSubmitAttestation, err := a.attestationContract.SubmitAttestation(txContextAttestationCollector.TransactOpts, attestation)
-	Nil(a.T(), err)
-	a.testBackendAttestation.WaitForConfirmation(a.GetTestContext(), txSubmitAttestation)
-
-	watchCtx, cancel := context.WithTimeout(a.GetTestContext(), time.Second*10)
-	defer cancel()
-
-	select {
-	// check for errors and fail
-	case <-watchCtx.Done():
-		a.T().Error(a.T(), fmt.Errorf("test context completed %w", a.GetTestContext().Err()))
-	case <-subAttestation.Err():
-		a.T().Error(a.T(), subAttestation.Err())
-	// get AttestationSubmitted event
-	case item := <-attestationSink:
-		parser, err := attestationcollector.NewParser(a.attestationContract.Address())
-		Nil(a.T(), err)
-		// Check to see if the event was an AttestationSubmitted event.
-		eventType, ok := parser.EventType(item.Raw)
-		True(a.T(), ok)
-		Equal(a.T(), eventType, attestationcollector.AttestationAcceptedEvent)
-
-		break
-	}
+	a.launchTest(1, 0)
 }
