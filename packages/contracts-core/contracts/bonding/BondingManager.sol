de63d1fdaebb50f2056e09461462a514dd729057
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2022-12-16 11:28:52 +0000

    Line ordering to get around "unreachable code"

--- packages/contracts-core/contracts/bonding/BondingManager.sol
+++ packages/contracts-core/contracts/bonding/BondingManager.sol
@@ -32,16 +32,16 @@ abstract contract BondingManager is SystemContract {
     ) external override onlySystemRouter {
         bool forwardUpdate;
         if (_callOrigin == _localDomain()) {
-            // Only Origin can slash agents on local domain
-            _assertEntityAllowed(ORIGIN, _caller);
             // Forward information about slashed agent to remote chains
             forwardUpdate = true;
+            // Only Origin can slash agents on local domain
+            _assertEntityAllowed(ORIGIN, _caller);
         } else {
-            // Validate security params for cross-chain slashing
-            _assertCrossChainSlashing(_rootSubmittedAt, _callOrigin, _caller);
             // Forward information about slashed agent to remote chains
             // only if BondingManager is deployed on Synapse Chain
             forwardUpdate = _onSynapseChain();
+            // Validate security params for cross-chain slashing
+            _assertCrossChainSlashing(_rootSubmittedAt, _callOrigin, _caller);
         }
         // Forward information about the slashed agent to local Registries
         // Forward information about slashed agent to remote chains if needed
