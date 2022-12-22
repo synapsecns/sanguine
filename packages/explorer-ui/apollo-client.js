0f43d02b54e3a7001a8580d7b7d202579ad0aece
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-12-21 20:04:26 -0500

    fix apollo GET settings

--- packages/explorer-ui/apollo-client.js
+++ packages/explorer-ui/apollo-client.js
@@ -2,12 +2,12 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { API_URL } from '@graphql'
 
 const link = new HttpLink({
-  uri: 'http://24.199.82.219:80/graphql',
+  uri: API_URL,
   useGETForQueries: true,
 })
 
 const client = new ApolloClient({
-  uri: link,
+  link: link,
   cache: new InMemoryCache(),
   fetchPolicy: 'network-only',
   fetchOptions: {
