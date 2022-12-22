dd75e394162022128de3c70d7bf14c6374c070a1
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-12-21 20:45:20 -0500

    add /address, /chain, /token

---
+++ packages/explorer-ui/pages/address/[address].js
@@ -0,0 +1,5 @@
+import { Address } from '@components/pages/Address'
+
+export default function AddressRoute() {
+  return <Address />
+}
---
+++ packages/explorer-ui/pages/chain/[chainId].js
@@ -0,0 +1,5 @@
+import { Chain } from '@components/pages/Chain'
+
+export default function chainId() {
+  return <Chain />
+}
