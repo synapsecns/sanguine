4c0cbc19ab40f6ed94a2bac319e2e34a7aa0e2f6
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2022-12-28 01:45:34 -0500

    Explorer UI polishes (#456)
    * * Remove LATEST_BRIDGE_TRANSACTIONS query
    * Add all asset images that were missing
    * Pagination component to track URL query
    
    * change graphql query comment to reflect new behavior
    
    * rebuild new vercel project

--- packages/explorer-ui/pages/txs/index.js
+++ packages/explorer-ui/pages/txs/index.js
@@ -3,10 +3,6 @@ import {UniversalSearch} from "@components/pages/Home/UniversalSearch";
 import {BridgeTransactionTable} from "@components/BridgeTransaction/BridgeTransactionTable";
 import { StandardPageContainer } from '@components/layouts/StandardPageContainer'
 import {
-  BRIDGE_AMOUNT_STATISTIC, COUNT_BY_CHAIN_ID,
-  COUNT_BY_TOKEN_ADDRESS,
-  GET_HISTORICAL_STATS,
-  GET_LATEST_BRIDGE_TRANSACTIONS_QUERY,
   GET_BRIDGE_TRANSACTIONS_QUERY
 } from "@graphql/queries";
 import {API_URL} from '@graphql'
@@ -31,24 +27,13 @@ const client = new ApolloClient({
 
 
 export default function Txs({queryResult}) {
-  let bridgeTransactionsTable
-  if ('latestBridgeTransactions' in queryResult) {
-    let { latestBridgeTransactions } = queryResult
-    bridgeTransactionsTable = latestBridgeTransactions
-  } else {
-    let { bridgeTransactions } = queryResult
-    bridgeTransactionsTable = bridgeTransactions
-  }
-
-
+  let { bridgeTransactions: bridgeTransactionsTable } = queryResult
 
   bridgeTransactionsTable = _.orderBy(
     bridgeTransactionsTable,
     'fromInfo.time',
     ['desc']
   ).slice(0, 25)
-
-  console.log(queryResult)
 
   return (
     <>
@@ -93,7 +78,7 @@ export async function getServerSideProps(context) {
     result = data
   } else {
     let { data } = await client.query({
-      query: GET_LATEST_BRIDGE_TRANSACTIONS_QUERY,
+      query: GET_BRIDGE_TRANSACTIONS_QUERY,
       variables: {
         includePending: false,
         page: context.query.p ?? 1,
