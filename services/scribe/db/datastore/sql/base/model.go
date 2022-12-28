871b04449448d6b7f9adbf0e96a90f9c88cde31b
Trajan0x <trajan0x@users.noreply.github.com>
2022-12-27 19:52:55 -0500

    add primary key for txhash

--- services/scribe/db/datastore/sql/base/model.go
+++ services/scribe/db/datastore/sql/base/model.go
@@ -63,7 +63,7 @@ type Log struct {
 	// BlockNumber is the block in which the transaction was included
 	BlockNumber uint64 `gorm:"column:block_number;index:idx_block_number,priority:1,sort:desc"`
 	// TxHash is the hash of the transaction
-	TxHash string `gorm:"column:tx_hash;primaryKey"`
+	TxHash string `gorm:"column:tx_hash;primaryKey;index:idx_tx_hash,priority:1,sort:desc"`
 	// TxIndex is the index of the transaction in the block
 	TxIndex uint64 `gorm:"tx_index"`
 	// BlockHash is the hash of the block in which the transaction was included
