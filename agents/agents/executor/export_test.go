1465312f5fbfaad67429f90a6803f546f2f978e5
Max Planck <99688618+CryptoMaxPlanck@users.noreply.github.com>
2023-01-04 13:49:30 -0500

    General executor improvements (#468)
    * start
    
    * privatize
    
    * privatize
    
    * lint
    
    * try to build tree from db in
    
    * add newTreeFromDB to

--- agents/agents/executor/export_test.go
+++ agents/agents/executor/export_test.go
@@ -11,10 +11,12 @@ import (
 	"github.com/synapsecns/sanguine/agents/contracts/destination"
 	"github.com/synapsecns/sanguine/agents/contracts/origin"
 	"github.com/synapsecns/sanguine/agents/domains/evm"
+	"github.com/synapsecns/sanguine/agents/types"
 	"github.com/synapsecns/sanguine/core/merkle"
 	ethergoChain "github.com/synapsecns/sanguine/ethergo/chain"
 	"github.com/synapsecns/sanguine/services/scribe/client"
 	pbscribe "github.com/synapsecns/sanguine/services/scribe/grpc/types/types/v1"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -29,11 +31,67 @@ func (e Executor) GetMerkleTree(chainID uint32, domain uint32) *merkle.Historica
 	return e.chainExecutors[chainID].merkleTrees[domain]
 }
 
+func (e Executor) VerifyMessageMerkleProof(message types.Message) (bool, error) {
+	return e.verifyMessageMerkleProof(message)
+}
+
+func (e Executor) VerifyMessageOptimisticPeriod(ctx context.Context, message types.Message) (*uint32, error) {
+	return e.verifyMessageOptimisticPeriod(ctx, message)
+}
+
+func NewTreeFromDB(ctx context.Context, chainID uint32, domain uint32, executorDB db.ExecutorDB) (*merkle.HistoricalTree, error) {
+	return newTreeFromDB(ctx, chainID, domain, executorDB)
+}
+
+func (e Executor) OverrideMerkleTree(chainID uint32, domain uint32, tree *merkle.HistoricalTree) {
+	e.chainExecutors[chainID].merkleTrees[domain] = tree
+}
+
+func (e Executor) Start(ctx context.Context) error {
+	g, _ := errgroup.WithContext(ctx)
+
+	for _, chain := range e.config.Chains {
+		chain := chain
+
+		g.Go(func() error {
+			return e.streamLogs(ctx, e.grpcClient, e.grpcConn, chain, originContract)
+		})
+
+		g.Go(func() error {
+			return e.streamLogs(ctx, e.grpcClient, e.grpcConn, chain, destinationContract)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return fmt.Errorf("error when streaming logs: %w", err)
+	}
+
+	return nil
+}
+
+func (e Executor) Listen(ctx context.Context) error {
+	g, _ := errgroup.WithContext(ctx)
+
+	for _, chain := range e.config.Chains {
+		chain := chain
+
+		g.Go(func() error {
+			return e.receiveLogs(ctx, chain.ChainID)
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return fmt.Errorf("error when receiving logs: %w", err)
+	}
+
+	return nil
+}
+
 // NewExecutorInjectedBackend creates a new Executor suitable for testing since it does not need a valid omnirpcURL.
 //
 //nolint:cyclop
 func NewExecutorInjectedBackend(ctx context.Context, config config.Config, executorDB db.ExecutorDB, scribeClient client.ScribeClient, clients map[uint32]Backend, urls map[uint32]string) (*Executor, error) {
-	chainExecutors := make(map[uint32]*ChainExecutor)
+	chainExecutors := make(map[uint32]*chainExecutor)
 	conn, err := grpc.DialContext(ctx, fmt.Sprintf("%s:%d", scribeClient.URL, scribeClient.GRPCPort), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, fmt.Errorf("could not dial grpc: %w", err)
@@ -76,7 +134,7 @@ func NewExecutorInjectedBackend(ctx context.Context, config config.Config, execu
 			return nil, fmt.Errorf("could not bind destination contract: %w", err)
 		}
 
-		chainExecutors[chain.ChainID] = &ChainExecutor{
+		chainExecutors[chain.ChainID] = &chainExecutor{
 			chainID: chain.ChainID,
 			lastLog: &logOrderInfo{
 				blockNumber: 0,
@@ -97,7 +155,10 @@ func NewExecutorInjectedBackend(ctx context.Context, config config.Config, execu
 				continue
 			}
 
-			tree := merkle.NewTree()
+			tree, err := newTreeFromDB(ctx, chain.ChainID, destinationChain.ChainID, executorDB)
+			if err != nil {
+				return nil, fmt.Errorf("could not get tree from db: %w", err)
+			}
 
 			chainExecutors[chain.ChainID].merkleTrees[destinationChain.ChainID] = tree
 		}
