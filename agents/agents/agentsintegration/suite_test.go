c77106d45c9f4a1eb2c6e714b6a70ea9317ea7e2
joecroninallen <486323+joecroninallen@users.noreply.github.com>
2023-01-04 12:15:22 -0500

    This is a basic integration test for guard and notary working together. (#473)
    * Adding GuardMVP worker that verifies the submitted attestation on the Destination
    
    * Fixing lint error
    
    * Fixing another lint issue
    
    * This adds an integration test for the guard and notary handling just one message

---
+++ agents/agents/agentsintegration/suite_test.go
@@ -0,0 +1,34 @@
+package agentsintegration_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/synapsecns/sanguine/agents/testutil"
+	"github.com/synapsecns/sanguine/ethergo/chain/chainwatcher"
+)
+
+// AgentsIntegrationSuite tests all the agents working together.
+type AgentsIntegrationSuite struct {
+	*testutil.SimulatedBackendsTestSuite
+}
+
+// NewAgentsIntegrationSuite creates a new agents integration suite.
+func NewAgentsIntegrationSuite(tb testing.TB) *AgentsIntegrationSuite {
+	tb.Helper()
+
+	return &AgentsIntegrationSuite{
+		SimulatedBackendsTestSuite: testutil.NewSimulatedBackendsTestSuite(tb),
+	}
+}
+
+func (u *AgentsIntegrationSuite) SetupTest() {
+	chainwatcher.PollInterval = time.Second
+
+	u.SimulatedBackendsTestSuite.SetupTest()
+}
+
+func TestAgentsIntegrationSuite(t *testing.T) {
+	suite.Run(t, NewAgentsIntegrationSuite(t))
+}
