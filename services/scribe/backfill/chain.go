c8accfc99b0d45d1973bc116833a9c4b12f18f99
Simon <ak.simonm@gmail.com>
2023-01-04 19:24:43 -0600

    up

--- services/scribe/backfill/chain.go
+++ services/scribe/backfill/chain.go
@@ -29,6 +29,8 @@ type ChainBackfiller struct {
 	minBlockHeight uint64
 	// chainConfig is the config for the backfiller.
 	chainConfig config.ChainConfig
+	// refreshRate is the rate at which the backfiller will refresh when livefilling.
+	refreshRate int
 }
 
 // Used for handling logging of various context types.
@@ -41,18 +43,10 @@ const (
 // NewChainBackfiller creates a new backfiller for a chain. This is done by passing through all the function parameters
 // into the ChainBackfiller struct, as well as iterating through all the contracts in the chain config and creating
 // ContractBackfillers for each contract.
-func NewChainBackfiller(chainID uint32, eventDB db.EventDB, client []ScribeBackend, chainConfig config.ChainConfig) (*ChainBackfiller, error) {
+func NewChainBackfiller(eventDB db.EventDB, client []ScribeBackend, chainConfig config.ChainConfig, refreshRate int) (*ChainBackfiller, error) {
 	var contractBackfillers []*ContractBackfiller
 
 	startHeights := make(map[string]uint64)
-
-	if chainConfig.BlockTimeChunkCount == 0 {
-		chainConfig.BlockTimeChunkCount = 40
-	}
-
-	if chainConfig.BlockTimeChunkSize == 0 {
-		chainConfig.BlockTimeChunkSize = 50
-	}
 
 	if chainConfig.ContractSubChunkSize == 0 {
 		chainConfig.ContractSubChunkSize = 600
@@ -64,6 +58,10 @@ func NewChainBackfiller(chainID uint32, eventDB db.EventDB, client []ScribeBacke
 
 	if chainConfig.StoreConcurrency == 0 {
 		chainConfig.StoreConcurrency = 20
+	}
+
+	if refreshRate == 0 {
+		refreshRate = 1
 	}
 
 	if chainConfig.StoreConcurrencyThreshold == 0 {
@@ -86,13 +84,14 @@ func NewChainBackfiller(chainID uint32, eventDB db.EventDB, client []ScribeBacke
 	}
 
 	return &ChainBackfiller{
-		chainID:             chainID,
+		chainID:             chainConfig.ChainID,
 		eventDB:             eventDB,
 		client:              client,
 		contractBackfillers: contractBackfillers,
 		startHeights:        startHeights,
 		minBlockHeight:      minBlockHeight,
 		chainConfig:         chainConfig,
+		refreshRate:         refreshRate,
 	}, nil
 }
 
@@ -100,10 +99,7 @@ func NewChainBackfiller(chainID uint32, eventDB db.EventDB, client []ScribeBacke
 // If `onlyOneBlock` is true, the backfiller will only backfill the block at `currentBlock`.
 //
 //nolint:gocognit,cyclop
-func (c ChainBackfiller) Backfill(ctx context.Context, onlyOneBlock *uint64) error {
-	var currentBlock uint64
-	var err error
-
+func (c ChainBackfiller) Backfill(ctx context.Context, onlyOneBlock *uint64, livefill bool) error {
 	// Create a new context for the chain so all chains don't halt when backfilling is completed.
 	chainCtx := context.WithValue(ctx, chainContextKey, fmt.Sprintf("%d-%d", c.chainID, c.minBlockHeight))
 	backfillGroup, backfillCtx := errgroup.WithContext(chainCtx)
@@ -112,78 +108,95 @@ func (c ChainBackfiller) Backfill(ctx context.Context, onlyOneBlock *uint64) err
 		Factor: 2,
 		Jitter: true,
 		Min:    1 * time.Second,
-		Max:    10 * time.Second,
+		Max:    3 * time.Second,
 	}
 
 	timeout := time.Duration(0)
 	startTime := time.Now()
 
-	if onlyOneBlock == nil {
-		// Retry until block height for the current chain is retrieved.
-		for {
-			select {
-			case <-backfillCtx.Done():
-				LogEvent(ErrorLevel, "Context canceled", LogData{"cid": c.chainID, "bn": currentBlock, "bd": b.Duration(), "a": b.Attempt(), "e": backfillCtx.Err(), "bt": true})
-
-				return fmt.Errorf("%s context canceled: %w", backfillCtx.Value(chainContextKey), backfillCtx.Err())
-			case <-time.After(timeout):
-				currentBlock, err = c.client[0].BlockNumber(backfillCtx)
-
-				if err != nil {
-					timeout = b.Duration()
-					LogEvent(InfoLevel, "Could not get block number, bad connection to rpc likely", LogData{"cid": c.chainID, "e": err.Error()})
-					continue
-				}
-			}
-			b.Reset()
-
-			break
-		}
-	} else {
-		c.minBlockHeight = *onlyOneBlock
-		currentBlock = *onlyOneBlock + 1
-	}
-
 	for i := range c.contractBackfillers {
 		contractBackfiller := c.contractBackfillers[i]
 		startHeight := c.startHeights[contractBackfiller.address]
 
-		if onlyOneBlock != nil {
-			startHeight = *onlyOneBlock
-			currentBlock = *onlyOneBlock
-		}
-
-		LogEvent(InfoLevel, "Starting backfilling contracts", LogData{"cid": c.chainID, "bn": currentBlock})
+		LogEvent(InfoLevel, "Starting livefilling contracts", LogData{"cid": c.chainID})
 		backfillGroup.Go(func() error {
 			timeout = time.Duration(0)
+
 			for {
 				select {
 				case <-backfillCtx.Done():
-					LogEvent(ErrorLevel, "Could not backfill data, context canceled", LogData{"cid": c.chainID, "ca": contractBackfiller.address, "bn": currentBlock, "sh": startHeight, "bd": b.Duration(), "a": b.Attempt(), "e": backfillCtx.Err()})
+					LogEvent(ErrorLevel, "Could not livefill contract, context canceled", LogData{"cid": c.chainID, "ca": contractBackfiller.address, "sh": startHeight, "bd": b.Duration(), "a": b.Attempt(), "e": backfillCtx.Err()})
 
 					return fmt.Errorf("%s chain context canceled: %w", backfillCtx.Value(chainContextKey), backfillCtx.Err())
 				case <-time.After(timeout):
-					err = contractBackfiller.Backfill(backfillCtx, startHeight, currentBlock)
+					var latestBlock *uint64
+					var err error
 
+					// onlyOneBlock is used for amending single blocks with a blockhash discrepancies or for testing.
+					if onlyOneBlock != nil {
+						startHeight = *onlyOneBlock
+						latestBlock = onlyOneBlock
+					} else {
+						latestBlock, err = c.getLatestBlock(backfillCtx)
+						if err != nil {
+							return fmt.Errorf("could not get current block number while backfilling: %w", err)
+						}
+					}
+
+					err = contractBackfiller.Backfill(backfillCtx, startHeight, *latestBlock)
 					if err != nil {
 						timeout = b.Duration()
-						LogEvent(WarnLevel, "Could not backfill contract, retrying", LogData{"cid": c.chainID, "ca": contractBackfiller.address, "bn": currentBlock, "sh": startHeight, "bd": b.Duration(), "a": b.Attempt(), "e": err.Error()})
+						LogEvent(WarnLevel, "Could not backfill contract, retrying", LogData{"cid": c.chainID, "ca": contractBackfiller.address, "sh": startHeight, "bd": b.Duration(), "a": b.Attempt(), "e": err.Error()})
 
 						continue
 					}
 
-					return nil
+					if !livefill {
+						return nil
+					}
+					timeout = time.Duration(c.refreshRate) * time.Second
+					LogEvent(InfoLevel, "Continuing to livefill contract", LogData{"cid": c.chainID, "ca": contractBackfiller.address, "sh": startHeight, "bd": b.Duration(), "a": b.Attempt()})
 				}
 			}
 		})
 	}
 
 	if err := backfillGroup.Wait(); err != nil {
-		LogEvent(ErrorLevel, "Could not backfill with error group", LogData{"cid": c.chainID, "bn": currentBlock, "bd": b.Duration(), "a": b.Attempt(), "e": err.Error(), "bt": true})
+		LogEvent(ErrorLevel, "Could not backfill with error group", LogData{"cid": c.chainID, "bd": b.Duration(), "a": b.Attempt(), "e": err.Error(), "bt": true})
 
 		return fmt.Errorf("could not backfill: %w", err)
 	}
-	LogEvent(WarnLevel, "Finished backfilling blocktimes and contracts", LogData{"cid": c.chainID, "eh": currentBlock, "t": time.Since(startTime).Hours()})
+	LogEvent(WarnLevel, "Finished backfilling blocktimes and contracts", LogData{"cid": c.chainID, "t": time.Since(startTime).Hours()})
 
 	return nil
+}
+
+func (c *ChainBackfiller) getLatestBlock(ctx context.Context) (*uint64, error) {
+	var currentBlock uint64
+	var err error
+	b := &backoff.Backoff{
+		Factor: 2,
+		Jitter: true,
+		Min:    1 * time.Second,
+		Max:    10 * time.Second,
+	}
+
+	timeout := time.Duration(0)
+	for {
+		select {
+		case <-ctx.Done():
+
+			return nil, fmt.Errorf("%s context canceled: %w", ctx.Value(chainContextKey), ctx.Err())
+		case <-time.After(timeout):
+			currentBlock, err = c.client[0].BlockNumber(ctx)
+
+			if err != nil {
+				timeout = b.Duration()
+				LogEvent(InfoLevel, "Could not get block number, bad connection to rpc likely", LogData{"cid": c.chainID, "e": err.Error()})
+				continue
+			}
+		}
+
+		return &currentBlock, nil
+	}
 }
