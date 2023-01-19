494fb5c15aea811177514145595fd2b3af1270d6
Trajan0x <trajan0x@users.noreply.github.com>
2023-01-19 17:45:12 -0500

    add publish token

--- contrib/terraform-provider-iap/provider/iap-tunnel-ssh.go
+++ contrib/terraform-provider-iap/provider/iap-tunnel-ssh.go
@@ -97,6 +97,7 @@ func dataSourceProxy(d *schema.ResourceData, meta interface{}) error {
 
 	errChan := make(chan error)
 
+	log.Printf("[INFO] creating tunnel")
 	go func() {
 		err := tm.StartProxy(context.Background())
 		if err != nil {
@@ -111,9 +112,11 @@ func dataSourceProxy(d *schema.ResourceData, meta interface{}) error {
 	case <-time.NewTimer(time.Second * 5).C:
 		break
 	case err := <-errChan:
+		log.Printf("[ERROR] Received error while booting provider: %v", err)
 		return err
 	}
 
+	log.Printf("[DEBUG] Finished creating proxy on port %d", localPort)
 	err = d.Set("proxy_url", fmt.Sprintf("http://localhost:%d", localPort))
 	if err != nil {
 		return fmt.Errorf("could not set proxy_url: %w", err)
