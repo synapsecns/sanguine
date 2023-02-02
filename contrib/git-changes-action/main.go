9fd829eefd3da9666ecc7b48eed9d5ef919de9d2
Trajan0x <trajan0x@users.noreply.github.com>
2023-02-02 04:52:39 -0500

    add more refs

--- contrib/git-changes-action/main.go
+++ contrib/git-changes-action/main.go
@@ -6,11 +6,14 @@ import (
 	"fmt"
 	"github.com/sethvargo/go-githubactions"
 	"github.com/synapsecns/sanguine/contrib/git-changes-action/detector"
+	"os"
 )
 
 func main() {
-	workingDirectory := githubactions.GetInput("working_directory")
-	ref := githubactions.GetInput("ref")
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
 	rawIncludeDeps := githubactions.GetInput("include_deps")
 	// TODO: we might need to use a pr as a base
 	includeDeps := false
@@ -18,7 +21,7 @@ func main() {
 		includeDeps = true
 	}
 
-	modules, err := detector.DetectChangedModules(workingDirectory, ref, includeDeps)
+	modules, err := detector.DetectChangedModules(workingDirectory, os.Getenv("GITHUB_REF"), includeDeps)
 	if err != nil {
 		panic(err)
 	}
