5faf26660549de99cc8ea8b40ee2af820d2de6df
Trajan0x <trajan0x@users.noreply.github.com>
2023-02-02 22:15:20 -0500

    update export test

--- contrib/terraform-provider-kubeproxy/provider/export_test.go
+++ contrib/terraform-provider-kubeproxy/provider/export_test.go
@@ -7,14 +7,17 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+// ValidateConfigNulls is a wrapper around validateConfigNulls for testing.
 func ValidateConfigNulls(ctx context.Context, v cty.Value, path cty.Path) []*tfprotov5.Diagnostic {
 	return validateConfigNulls(ctx, v, path)
 }
 
+// CombinedSchema is a wrapper around combinedSchema for testing.
 func (r *RawProviderServer) CombinedSchema() *tfprotov5.Schema {
 	return r.combinedSchema
 }
 
+// GoogleProvider is a wrapper around googleProvider for testing.
 func (r *RawProviderServer) GoogleProvider() *schema.Provider {
 	return r.googleProvider
 }
