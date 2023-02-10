3fcb8bfad34edd3f5c76744620d8287f34b3b7f8
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-02-10 14:54:44 +0000

    Structure for Snapshot

--- packages/contracts-core/contracts/libs/Structures.sol
+++ packages/contracts-core/contracts/libs/Structures.sol
@@ -6,3 +6,6 @@ pragma solidity 0.8.17;
 
 /// @dev See State.sol: (bytes32,uint32,uint32,uint40,uint40): 32+4+4+5+5
 uint256 constant STATE_LENGTH = 50;
+
+/// @dev Maximum amount of states in a single snapshot
+uint256 constant SNAPSHOT_MAX_STATES = 32;
