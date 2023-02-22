40094de848f2d0c68a762efb4aa1d3b6d63ee77c
Max Planck <maxplanck.crypto@gmail.com>
2023-02-22 18:12:26 -0500

    rpc thing for scribe init to 1 and log in start [goreleaser]

--- services/scribe/node/scribe.go
+++ services/scribe/node/scribe.go
@@ -46,6 +46,7 @@ func NewScribe(eventDB db.EventDB, clients map[uint32][]backfill.ScribeBackend, 
 //
 //nolint:cyclop
 func (s Scribe) Start(ctx context.Context) error {
+	logger.Errorf("Starting scribe")
 	confirmationRefreshRate := s.config.ConfirmationRefreshRate
 
 	if confirmationRefreshRate == 0 {
