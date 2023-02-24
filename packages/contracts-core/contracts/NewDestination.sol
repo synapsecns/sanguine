e3cb3a8cec6e193b8ecdff676c5731da595b57ae
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-02-24 23:10:13 +0000

    New Destination: attestations, prove messages

---
+++ packages/contracts-core/contracts/NewDestination.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+// ═════════════════════════════ INTERNAL IMPORTS ══════════════════════════════
+import { DomainContext } from "./context/DomainContext.sol";
+import { DestinationAttestation, SnapAttestationHub } from "./hubs/SnapAttestationHub.sol";
+import { SnapAttestation, StatementHub } from "./hubs/StatementHub.sol";
+import { Header, HeaderLib } from "./libs/Header.sol";
+import { MerkleLib } from "./libs/Merkle.sol";
+import { Message, MessageLib } from "./libs/Message.sol";
+import { StateLib } from "./libs/State.sol";
+import { ORIGIN_TREE_DEPTH } from "./libs/Structures.sol";
+import { SystemRegistry } from "./system/SystemRegistry.sol";
+
+contract DestinationNew is StatementHub, SnapAttestationHub, SystemRegistry {
+    // TODO: Attach library functions to custom types globally
+    using HeaderLib for Header;
+    using MessageLib for Message;
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                      CONSTRUCTOR & INITIALIZER                       ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    constructor(uint32 _domain) DomainContext(_domain) {}
+
+    /// @notice Initializes Origin contract:
+    /// - msg.sender is set as contract owner
+    function initialize() external initializer {
+        // Initialize SystemContract: msg.sender is set as "owner"
+        __SystemContract_initialize();
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                          ACCEPT STATEMENTS                           ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    /**
+     * @notice Submit a SnapAttestation signed by a Notary.
+     * @dev Will revert if either of these is true:
+     *  - Attestation payload is not properly formatted.
+     *  - Attestation signer is not an active Notary for local domain.
+     *  - Attestation's snapshot root has been previously submitted.
+     */
+    function submitAttestation(bytes memory _attPayload, bytes memory _attSignature)
+        external
+        returns (bool wasAccepted)
+    {
+        // This will revert if payload is not an attestation, or signer is not an active Notary
+        (SnapAttestation snapAtt, uint32 domain, address notary) = _verifyAttestation(
+            _attPayload,
+            _attSignature
+        );
+        // Check that Notary is active on local domain
+        require(domain == localDomain, "Wrong Notary domain");
+        // This will revert if snapshot root has been previously submitted
+        _acceptAttestation(snapAtt, notary);
+        return true;
+    }
+
+    /*╔══════════════════════════════════════════════════════════════════════╗*\
+    ▏*║                            INTERNAL LOGIC                            ║*▕
+    \*╚══════════════════════════════════════════════════════════════════════╝*/
+
+    function _isIgnoredAgent(uint32 _domain, address)
+        internal
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        // Destination only keeps track of local Notaries and Guards
+        return _domain != localDomain && _domain != 0;
+    }
+
+    /**
+     * @notice Attempts to prove the validity of the cross-chain message.
+     * First, the origin Merkle Root is reconstructed using the origin proof.
+     * Then the origin state's "left leaf" is reconstructed using the origin domain.
+     * After that the snapshot Merkle Root is reconstructed using the snapshot proof.
+     * Finally, the optimistic period is checked for the derived snapshot root.
+     * @dev Reverts if any of the checks fail.
+     * @param _msg          Typed memory view over message payload
+     * @param _originProof  Proof of inclusion of message in the Origin Merkle Tree
+     * @param _snapProof    Proof of inclusion of Origin State Left Leaf into Snapshot Merkle Tree
+     * @param _snapIndex    Index of Origin State Left Leaf in the Snapshot Merkle Tree
+     * @return snapshotRoot Derived merkle root of the Snapshot Merkle Tree
+     * @return destAtt      Rest of attestation data that Destination keeps track of
+     */
+    function _prove(
+        Message _msg,
+        bytes32[ORIGIN_TREE_DEPTH] calldata _originProof,
+        bytes32[] calldata _snapProof,
+        uint256 _snapIndex
+    ) internal view returns (bytes32 snapshotRoot, DestinationAttestation memory destAtt) {
+        Header header = _msg.header();
+        // Reconstruct Origin Merkle Root using the origin proof
+        // Message index in the tree is (nonce - 1), as nonce starts from 1
+        bytes32 originRoot = MerkleLib.branchRoot(_msg.leaf(), _originProof, header.nonce() - 1);
+        // Reconstruct left sub-leaf of the Origin State: (merkleRoot, originDomain)
+        bytes32 leftLeaf = StateLib.leftLeaf(originRoot, header.origin());
+
+        // TODO: implement branchRoot function that takes dynamic sized proof as variable
+        // snapshotRoot = MerkleLib.branchRoot(leftLeaf, _snapProof, _snapIndex);
+
+        // Fetch the attestation data for the snapshot root
+        destAtt = _rootAttestation(snapshotRoot);
+        // Check if snapshot root has been submitted
+        require(!destAtt.isEmpty(), "Invalid snapshot root");
+        // Check that snapshot proof length matches the height of Snapshot Merkle Tree
+        require(_snapProof.length == destAtt.height, "Invalid proof length");
+        // Check if Notary who submitted the snapshot is still active
+        require(_isActiveAgent(localDomain, destAtt.notary), "Inactive notary");
+        // Check if optimistic period has passed
+        require(
+            block.timestamp >= header.optimisticSeconds() + destAtt.destTimestamp,
+            "!optimisticSeconds"
+        );
+    }
+}
