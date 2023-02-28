8e96caea83ad6a85033e13d610c1b3e2b0ff0cb5
Trajan0x <trajan0x@users.noreply.github.com>
2023-02-28 15:19:23 -0500

    fix tidy script

--- make/scripts/tidy.sh
+++ make/scripts/tidy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-for f in $(find . -name go.mod)
-  do (cd $(dirname $f); go mod tidy)
+find . -name go.mod -print0 | while IFS= read -r -d '' f; do
+  (cd "$(dirname "$f")" || exit; go mod tidy)
 done
+
+
+
