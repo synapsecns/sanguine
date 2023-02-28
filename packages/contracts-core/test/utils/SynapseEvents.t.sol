dd9e84de5a40e28c8888c74f92164f00783aeec2
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-02-28 17:31:06 +0000

    Remove older deprecated Events

--- packages/contracts-core/test/utils/SynapseEvents.t.sol
+++ packages/contracts-core/test/utils/SynapseEvents.t.sol
@@ -3,13 +3,10 @@ pragma solidity 0.8.17;
 
 // ============ Production events ============
 import "../../contracts/events/AgentRegistryEvents.sol";
-import "../../contracts/events/GuardRegistryEvents.sol";
-import "../../contracts/events/NotaryRegistryEvents.sol";
 // ============ Harness events ============
 import "../harnesses/events/AgentRegistryHarnessEvents.sol";
 import "../harnesses/events/BasicClientHarnessEvents.sol";
 import "../harnesses/events/ClientHarnessEvents.sol";
-import "../harnesses/events/NotaryRegistryHarnessEvents.sol";
 import "../harnesses/events/SystemContractHarnessEvents.sol";
 // ============ Mocks events ============
 import "../mocks/events/SystemContractMockEvents.sol";
@@ -17,13 +14,10 @@ import "../mocks/events/SystemContractMockEvents.sol";
 // solhint-disable-next-line no-empty-blocks
 abstract contract SynapseEvents is
     AgentRegistryEvents,
-    GuardRegistryEvents,
-    NotaryRegistryEvents,
     // Harnesses events
     AgentRegistryHarnessEvents,
     BasicClientHarnessEvents,
     ClientHarnessEvents,
-    NotaryRegistryHarnessEvents,
     SystemContractHarnessEvents,
     // Mocks events
     SystemContractMockEvents
