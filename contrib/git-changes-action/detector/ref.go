2d822fb7c78dd3748c4922cf7626d10e27f37b1f
Trajan0x <trajan0x@users.noreply.github.com>
2023-03-01 19:05:37 -0500

    reduce limit

--- contrib/git-changes-action/detector/ref.go
+++ contrib/git-changes-action/detector/ref.go
@@ -75,7 +75,7 @@ func getChangedFilesFromAPI(ctx context.Context, ghContext *actionscore.Context,
 
 			files, res, err = client.PullRequests.ListFiles(reqCtx, repoOwner, repoName, prNumber, &github.ListOptions{
 				Page:    page,
-				PerPage: 30,
+				PerPage: 10,
 			})
 			if err != nil {
 				return fmt.Errorf("could not get files: %w", err)
