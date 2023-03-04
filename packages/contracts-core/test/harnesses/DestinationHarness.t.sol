332f087ae25f485ccaff873a0c0dc3bbd375f311
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-03-04 13:02:35 +0000

    Add/remove agents in harnesses for go tests

--- packages/contracts-core/test/harnesses/DestinationHarness.t.sol
+++ packages/contracts-core/test/harnesses/DestinationHarness.t.sol
@@ -6,4 +6,14 @@ import { Destination } from "../../contracts/Destination.sol";
 // TODO: remove/adapt when "go generate" is updated
 contract DestinationHarness is Destination {
     constructor(uint32 _domain) Destination(_domain) {}
+
+    /// @notice Adding agents in Go tests
+    function addAgent(uint32 _domain, address _account) external onlyOwner returns (bool) {
+        return _addAgent(_domain, _account);
+    }
+
+    /// @notice Removing agents in Go tests
+    function removeAgent(uint32 _domain, address _account) external onlyOwner returns (bool) {
+        return _removeAgent(_domain, _account);
+    }
 }
