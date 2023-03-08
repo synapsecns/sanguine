413c19ee4962a3618167e1b3489dcbb509b6fe2b
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-03-08 20:53:02 +0000

    Update Summit deploy in Go

--- agents/testutil/deployers.go
+++ agents/testutil/deployers.go
@@ -70,19 +70,19 @@ func NewSummitDeployer(registry deployer.GetOnlyContractRegistry, backend backen
 //nolint:dupword
 func (a SummitDeployer) Deploy(ctx context.Context) (contracts.DeployedContract, error) {
 	return a.DeploySimpleContract(ctx, func(transactOps *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, interface{}, error) {
-		summitAddress, summitTx, collector, err := summit.DeploySummit(transactOps, backend)
+		summitAddress, summitTx, summit, err := summit.DeploySummit(transactOps, backend, uint32(a.Backend().GetChainID()))
 		if err != nil {
 			return common.Address{}, nil, nil, fmt.Errorf("could not deploy summit: %w", err)
 		}
 
 		auth := a.Backend().GetTxContext(ctx, &transactOps.From)
-		initTx, err := collector.Initialize(auth.TransactOpts)
+		initTx, err := summit.Initialize(auth.TransactOpts)
 		if err != nil {
 			return common.Address{}, nil, nil, fmt.Errorf("could not initialize attestation collector: %w", err)
 		}
 		a.Backend().WaitForConfirmation(ctx, initTx)
 
-		return summitAddress, summitTx, collector, nil
+		return summitAddress, summitTx, summit, nil
 	}, func(address common.Address, backend bind.ContractBackend) (interface{}, error) {
 		return summit.NewSummitRef(address, backend)
 	})
