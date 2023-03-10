58c89b5fc538b53af111e0e2d61934a370795582
protojoe <486323+joecroninallen@users.noreply.github.com>
2023-03-10 05:12:53 -0500

    Trying to deploy summit harness

---
+++ agents/contracts/test/summitharness/generate.go
@@ -0,0 +1,5 @@
+// Package summitharness generates abi data for contract SummitHarness.t.sol
+package summitharness
+
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol  ../../../../packages/contracts-core/flattened/SummitHarness.t.sol --pkg summitharness --sol-version 0.8.17 --filename summitharness
+// line after go:generate cannot be left blank
