b1277c291cf8f5b46cf40303332de4346816fb57
trajan0x <83933037+trajan0x@users.noreply.github.com>
2023-03-10 04:19:51 -0500

    add omnirpc testhelper (#644)
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>
    
     - Adds a test helper to omnirpc to speed up the use of multiple backends
     - add a mocktester for mocking out test failure 
     - seperate out some core/testsuite functions into utils that can be used outside of the core
     - refactor scribe to use omnirpc test helper

---
+++ services/omnirpc/testhelper/doc.go
@@ -0,0 +1,3 @@
+// Package testhelper is used for setting up ominrpc instances for any number of backends.
+// this is purely a helper package and should be used solely for testing.
+package testhelper
