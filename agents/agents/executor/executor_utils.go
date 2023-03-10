acab60b0d03b7172924e3a1283d3d818fbcd3fc8
Max Planck <maxplanck.crypto@gmail.com>
2023-03-10 04:46:01 -0500

    scribe not working but it will try to execute successfully

--- agents/agents/executor/executor_utils.go
+++ agents/agents/executor/executor_utils.go
@@ -38,8 +38,8 @@ func (e Executor) logToAttestation(log ethTypes.Log, chainID uint32) (*types.Att
 }
 
 // logToSnapshot converts the log to a snapshot.
-func (e Executor) logToSnapshot(log ethTypes.Log) (*types.Snapshot, error) {
-	snapshot, domain, ok := e.summitParser.ParseSnapshotAccepted(log)
+func (e Executor) logToSnapshot(log ethTypes.Log, chainID uint32) (*types.Snapshot, error) {
+	snapshot, domain, ok := (*e.chainExecutors[chainID].summitParser).ParseSnapshotAccepted(log)
 	if !ok {
 		return nil, fmt.Errorf("could not parse snapshot")
 	}
@@ -60,7 +60,14 @@ func (e Executor) logType(log ethTypes.Log, chainID uint32) contractEventType {
 	}
 
 	//nolint:nestif
-	if eventType, ok := e.chainExecutors[chainID].originParser.EventType(log); ok && eventType == origin.DispatchEvent {
+	if e.chainExecutors[chainID].summitParser != nil {
+		if eventType, ok := (*e.chainExecutors[chainID].summitParser).EventType(log); ok {
+			contractEvent.contractType = summitContract
+			if eventType == summit.SnapshotAcceptedEvent {
+				contractEvent.eventType = snapshotAcceptedEvent
+			}
+		}
+	} else if eventType, ok := e.chainExecutors[chainID].originParser.EventType(log); ok && eventType == origin.DispatchEvent {
 		contractEvent.contractType = originContract
 		contractEvent.eventType = dispatchEvent
 	} else if eventType, ok := e.chainExecutors[chainID].destinationParser.EventType(log); ok {
@@ -69,11 +76,6 @@ func (e Executor) logType(log ethTypes.Log, chainID uint32) contractEventType {
 			contractEvent.eventType = attestationAcceptedEvent
 		} else if eventType == destination.ExecutedEvent {
 			contractEvent.eventType = executedEvent
-		}
-	} else if eventType, ok := e.summitParser.EventType(log); ok {
-		contractEvent.contractType = summitContract
-		if eventType == summit.SnapshotAcceptedEvent {
-			contractEvent.eventType = snapshotAcceptedEvent
 		}
 	}
 
@@ -122,37 +124,6 @@ func (e Executor) getEarliestStateInRange(ctx context.Context, origin, destinati
 
 	return stateWithEarliestAttestation, nil
 }
-
-//// setMinimumTimes goes through a list of messages and sets the minimum time for each message
-//// that has an associated attestation.
-//// The messages need to be sorted by nonce, and the attestations by their destination submission time (which can be via block number or block time).
-// func (e Executor) setMinimumTimes(ctx context.Context, messages []types.Message, attestations []execTypes.DBAttestation) error {
-//	messageIndex := 0
-//	attestationIndex := 0
-//	for messageIndex < len(messages) && attestationIndex < len(attestations) {
-//		if messages[messageIndex].Nonce() <= *attestations[attestationIndex].Nonce {
-//			minimumTime := *attestations[attestationIndex].DestinationTimestamp + uint64(messages[messageIndex].OptimisticSeconds())
-//			originDomain := messages[messageIndex].OriginDomain()
-//			destinationDomain := messages[messageIndex].DestinationDomain()
-//			nonce := messages[messageIndex].Nonce()
-//			messageMask := execTypes.DBMessage{
-//				ChainID:     &originDomain,
-//				Destination: &destinationDomain,
-//				Nonce:       &nonce,
-//			}
-//			err := e.executorDB.SetMinimumTime(ctx, messageMask, minimumTime)
-//			if err != nil {
-//				return fmt.Errorf("could not set minimum time: %w", err)
-//			}
-//
-//			messageIndex++
-//		} else {
-//			attestationIndex++
-//		}
-//	}
-//
-//	return nil
-//}
 
 // verifyAfter guarantees the chronological ordering of logs.
 func (l logOrderInfo) verifyAfter(log ethTypes.Log) bool {
