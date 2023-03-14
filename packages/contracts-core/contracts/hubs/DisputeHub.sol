d109e0b62797d27900fbbb224bf149f208349041
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-03-14 20:02:46 +0000

    DisputeHub: handling State reports

--- packages/contracts-core/contracts/hubs/DisputeHub.sol
+++ packages/contracts-core/contracts/hubs/DisputeHub.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 // ═════════════════════════════ INTERNAL IMPORTS ══════════════════════════════
-import { Snapshot, StatementHub, StateReport } from "./StatementHub.sol";
+import { Attestation, Snapshot, StatementHub, StateReport } from "./StatementHub.sol";
 import { DisputeHubEvents } from "../events/DisputeHubEvents.sol";
 import { IDisputeHub } from "../interfaces/IDisputeHub.sol";
 
@@ -14,7 +14,22 @@ abstract contract DisputeHub is StatementHub, DisputeHubEvents, IDisputeHub {
         bytes memory _snapPayload,
         bytes memory _snapSignature
     ) external returns (bool wasAccepted) {
-        // TODO: implement
+        // This will revert if payload is not a state report
+        StateReport report = _wrapStateReport(_srPayload);
+        // This will revert if the report signer is not an active Guard
+        address guard = _verifyStateReport(report, _srSignature);
+        // This will revert if payload is not a snapshot
+        Snapshot snapshot = _wrapSnapshot(_snapPayload);
+        // This will revert if the snapshot signer is not an active Agent
+        (uint32 domain, address notary) = _verifySnapshot(snapshot, _snapSignature);
+        // Snapshot signer needs to be a Notary, not a Guard
+        require(domain != 0, "Snapshot signer is not a Notary");
+        // Snapshot state and reported state need to be the same
+        // This will revert if state index is out of range
+        require(snapshot.state(_stateIndex).equals(report.state()), "States don't match");
+        // Reported State was used by the Notary for their signed snapshot => open dispute
+        _openDispute(guard, domain, notary);
+        return true;
     }
 
     /// @inheritdoc IDisputeHub
@@ -26,7 +41,23 @@ abstract contract DisputeHub is StatementHub, DisputeHubEvents, IDisputeHub {
         bytes memory _attPayload,
         bytes memory _attSignature
     ) external returns (bool wasAccepted) {
-        // TODO: implement
+        // This will revert if payload is not a state report
+        StateReport report = _wrapStateReport(_srPayload);
+        // This will revert if the report signer is not an active Guard
+        address guard = _verifyStateReport(report, _srSignature);
+        // This will revert if payload is not an attestation
+        Attestation att = _wrapAttestation(_attPayload);
+        // This will revert if signer is not an active Notary
+        (uint32 domain, address notary) = _verifyAttestation(att, _attSignature);
+        // This will revert if any of these is true:
+        //  - Attestation root is not equal to Merkle Root derived from State and Snapshot Proof.
+        //  - Snapshot Proof has length different to Attestation height.
+        //  - Snapshot Proof's first element does not match the State metadata.
+        //  - State index is out of range.
+        _verifySnapshotMerkle(att, _stateIndex, report.state(), _snapProof);
+        // Reported State was used by the Notary for their signed attestation => open dispute
+        _openDispute(guard, domain, notary);
+        return true;
     }
 
     /// @dev Opens a Dispute between a Guard and a Notary.
