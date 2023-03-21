17f6aef48ad85ae3f7cc2ef4c5b3e080c59c5360
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-03-21 18:23:10 +0000

    Adjust harnesses (to be completed later)

--- packages/contracts-core/test/harnesses/DestinationHarness.t.sol
+++ packages/contracts-core/test/harnesses/DestinationHarness.t.sol
@@ -2,24 +2,19 @@
 pragma solidity 0.8.17;
 
 import { Destination } from "../../contracts/Destination.sol";
+import { IAgentManager } from "../../contracts/interfaces/IAgentManager.sol";
 
 import { SystemRouterMock } from "../mocks/system/SystemRouterMock.t.sol";
 
 /// @notice Harness for standalone Go tests.
 /// Do not use for tests requiring interactions between messaging contracts.
 contract DestinationHarness is Destination {
-    constructor(uint32 _domain) Destination(_domain) {
+    constructor(uint32 _domain, address _agentManager)
+        Destination(_domain, IAgentManager(_agentManager))
+    {
         // Add Mock for SystemRouter for standalone tests
         systemRouter = new SystemRouterMock();
     }
 
-    /// @notice Adding agents in Go tests
-    function addAgent(uint32 _domain, address _account) external onlyOwner returns (bool) {
-        return _addAgent(_domain, _account);
-    }
-
-    /// @notice Removing agents in Go tests
-    function removeAgent(uint32 _domain, address _account) external onlyOwner returns (bool) {
-        return _removeAgent(_domain, _account);
-    }
+    // TODO: add / remove Agents in standalone Go tests
 }
