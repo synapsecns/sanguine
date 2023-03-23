307b67e7b6d6865b523748e2a9e1018c28039201
Trajan0x <trajan0x@users.noreply.github.com>
2023-03-23 01:48:01 -0400

    cleanup sigs

--- ethergo/parser/abi/internal/generate.go
+++ ethergo/parser/abi/internal/generate.go
@@ -1,4 +1,4 @@
 package internal
 
-//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ./signature.sol --pkg internal --sol-version 0.8.4 --filename signature
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ./testsignature.sol --pkg internal --sol-version 0.8.4 --filename testsignature
 // there needs to be a trailing line after go:generate or generation will fail
