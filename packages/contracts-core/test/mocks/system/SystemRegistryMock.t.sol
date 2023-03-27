49397386e41ee8dcb6ae0d09d4a785db6365ef4c
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-03-27 18:07:16 +0100

    Update mock

--- packages/contracts-core/test/mocks/system/SystemRegistryMock.t.sol
+++ packages/contracts-core/test/mocks/system/SystemRegistryMock.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import { ISystemRegistry } from "../../../contracts/interfaces/ISystemRegistry.sol";
+import { AgentStatus, ISystemRegistry } from "../../../contracts/interfaces/ISystemRegistry.sol";
 
 contract SystemRegistryMock is ISystemRegistry {
     /// @notice Prevents this contract from being included in the coverage report
@@ -9,7 +9,5 @@ contract SystemRegistryMock is ISystemRegistry {
 
     function managerSlash(uint32 _domain, address _agent) external {}
 
-    function isActiveAgent(address _account) external view returns (bool isActive, uint32 domain) {}
-
-    function isActiveAgent(uint32 _domain, address _account) external view returns (bool) {}
+    function agentStatus(address _agent) external view returns (AgentStatus memory) {}
 }
