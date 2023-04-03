5f8e85d6c969341cd1dfe87b58d772d5fb1d550d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-04-03 19:37:21 +0100

    BaseClient: unit tests for reverts

--- packages/contracts-core/test/suite/client/BaseClient.t.sol
+++ packages/contracts-core/test/suite/client/BaseClient.t.sol
@@ -39,6 +39,20 @@ contract BaseClientTest is SynapseTest {
         client.sendBaseMessage{value: totalTips}(destination_, tipsPayload, content);
     }
 
+    function test_sendBaseMessage_revert_recipientNotSet(address user, RawTips memory rt) public {
+        // There is no trustedSender for this domain => will revert in BaseClient
+        uint32 destination_ = 0;
+        vm.label(user, "User");
+        // Set some sensible limit for fuzzed tips values
+        rt.boundTips(1e20);
+        uint256 totalTips = rt.castToTips().totalTips();
+        bytes memory tipsPayload = rt.formatTips();
+        vm.deal(user, totalTips);
+        vm.expectRevert("BaseClient: !recipient");
+        vm.prank(user);
+        client.sendBaseMessage{value: totalTips}(destination_, tipsPayload, "");
+    }
+
     function test_receiveBaseMessage(
         RawHeader memory rh,
         uint256 rootSubmittedAt,
@@ -58,5 +72,56 @@ contract BaseClientTest is SynapseTest {
         emit BaseMessageReceived(0, rh.origin, rh.nonce, content);
         vm.prank(destination);
         client.receiveBaseMessage(rh.origin, rh.nonce, sender, rootSubmittedAt, content);
+    }
+
+    function test_receiveBaseMessage_revert_notDestination(RawHeader memory rh, uint256 rootSubmittedAt, address caller)
+        public
+    {
+        vm.assume(rh.origin != 0 && rh.origin != DOMAIN_LOCAL);
+        vm.assume(caller != destination);
+        // Get expected values for receiving a message
+        uint32 optimisticPeriod = client.optimisticPeriod();
+        bytes32 sender = client.trustedSender(rh.origin);
+        // Set some sensible restrictions for timestamps
+        rootSubmittedAt = bound(rootSubmittedAt, 1, 1e10);
+        vm.warp(rootSubmittedAt + optimisticPeriod);
+        vm.expectRevert("BaseClient: !destination");
+        vm.prank(caller);
+        client.receiveBaseMessage(rh.origin, rh.nonce, sender, rootSubmittedAt, "");
+    }
+
+    function test_receiveBaseMessage_revert_notTrustedSender(
+        RawHeader memory rh,
+        uint256 rootSubmittedAt,
+        bytes32 sender
+    ) public {
+        vm.assume(rh.origin != 0 && rh.origin != DOMAIN_LOCAL);
+        vm.assume(sender != client.trustedSender(rh.origin));
+        // Get expected values for receiving a message
+        uint32 optimisticPeriod = client.optimisticPeriod();
+        // Set some sensible restrictions for timestamps
+        rootSubmittedAt = bound(rootSubmittedAt, 1, 1e10);
+        vm.warp(rootSubmittedAt + optimisticPeriod);
+        vm.expectRevert("BaseClient: !trustedSender");
+        vm.prank(destination);
+        client.receiveBaseMessage(rh.origin, rh.nonce, sender, rootSubmittedAt, "");
+    }
+
+    function test_receiveBaseMessage_revert_optimisticPeriodNotOver(
+        RawHeader memory rh,
+        uint256 rootSubmittedAt,
+        uint256 secondsPassed
+    ) public {
+        vm.assume(rh.origin != 0 && rh.origin != DOMAIN_LOCAL);
+        // Get expected values for receiving a message
+        uint32 optimisticPeriod = client.optimisticPeriod();
+        bytes32 sender = client.trustedSender(rh.origin);
+        // Set some sensible restrictions for timestamps
+        rootSubmittedAt = bound(rootSubmittedAt, 1, 1e10);
+        secondsPassed = bound(secondsPassed, 0, optimisticPeriod - 1);
+        vm.warp(rootSubmittedAt + secondsPassed);
+        vm.expectRevert("BaseClient: !optimisticPeriod");
+        vm.prank(destination);
+        client.receiveBaseMessage(rh.origin, rh.nonce, sender, rootSubmittedAt, "");
     }
 }
