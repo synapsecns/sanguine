15d9ee9ca8156f9fbf60dcc9064bcf7bf6d51cbe
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-04-03 16:15:50 +0100

    Chore: BasicClient -> BaseClient

--- packages/contracts-core/contracts/client/BaseClient.sol
+++ packages/contracts-core/contracts/client/BaseClient.sol
@@ -6,17 +6,17 @@ import {IMessageRecipient} from "../interfaces/IMessageRecipient.sol";
 import {InterfaceOrigin} from "../interfaces/InterfaceOrigin.sol";
 
 /**
- * @dev Basic implementation of IMessageRecipient interface, to be used as recipient of
- * messages passed by Destination contract.
- * BasicClient could be used as a backbone for cross-chain apps, assuming:
+ * @dev Implementation of IMessageRecipient interface, to be used as the recipient of
+ * base messages passed by the Destination contract.
+ * BaseClient could be used as a backbone for cross-chain apps, assuming:
  * - A single app contract per chain (aka trusted sender)
- * - Only app contracts from other chains are able to send messages to app (enforced in BasicClient)
- * - App is responsible for enforcing optimistic period (not enforced in BasicClient)
+ * - Only app contracts from other chains are able to send messages to app
+ * - App is responsible for enforcing optimistic period
  *
- * Note: BasicClient is forever stateless, meaning it can be potentially used as a parent
+ * Note: BaseClient is forever stateless, meaning it can be potentially used as a parent
  * for the upgradeable contract without worrying about storage collision.
  */
-abstract contract BasicClient is IMessageRecipient {
+abstract contract BaseClient is IMessageRecipient {
     /*╔══════════════════════════════════════════════════════════════════════╗*\
     ▏*║                              IMMUTABLES                              ║*▕
     \*╚══════════════════════════════════════════════════════════════════════╝*/
@@ -53,8 +53,8 @@ abstract contract BasicClient is IMessageRecipient {
     function handle(uint32 origin_, uint32 nonce, bytes32 sender, uint256 rootSubmittedAt, bytes memory content)
         external
     {
-        require(msg.sender == destination, "BasicClient: !destination");
-        require(sender == trustedSender(origin_) && sender != bytes32(0), "BasicClient: !trustedSender");
+        require(msg.sender == destination, "BaseClient: !destination");
+        require(sender == trustedSender(origin_) && sender != bytes32(0), "BaseClient: !trustedSender");
         /// @dev root timestamp wasn't checked => potentially unsafe
         /// No need to pass both origin and sender: sender == trustedSender(origin)
         _handleUnsafe(origin_, nonce, rootSubmittedAt, content);
@@ -99,7 +99,7 @@ abstract contract BasicClient is IMessageRecipient {
         internal
     {
         bytes32 recipient = trustedSender(destination_);
-        require(recipient != bytes32(0), "BasicClient: !recipient");
+        require(recipient != bytes32(0), "BaseClient: !recipient");
         InterfaceOrigin(origin).sendBaseMessage{value: msg.value}(
             destination_, recipient, optimisticSeconds, tipsPayload, content
         );
