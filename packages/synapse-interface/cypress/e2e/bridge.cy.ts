11939a5c9c2cccce4cdb169b977b3cab24ca3fea
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-12 18:23:58 -0400

    Init bridge tests by description

--- packages/synapse-interface/cypress/e2e/bridge.cy.ts
+++ packages/synapse-interface/cypress/e2e/bridge.cy.ts
@@ -1,5 +1,9 @@
 describe('Bridge', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  beforeEach(() => cy.visit('/'))
+
+  it('should initially load all available origin networks', () => {})
+
+  it('should load possible origin tokens, given a specific chainId', () => {})
+
+  it('should load possible destination tokens, given a specific chainId and origin token', () => {})
 })
