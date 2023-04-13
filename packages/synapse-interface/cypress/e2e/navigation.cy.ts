75b27f538c4b61f7c1576cfc75a6a0407332798c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-13 15:00:50 -0400

    Navbar desktop hidden when screen is less than 1024px

--- packages/synapse-interface/cypress/e2e/navigation.cy.ts
+++ packages/synapse-interface/cypress/e2e/navigation.cy.ts
@@ -3,12 +3,22 @@ const WINDOW_CONSTANTS = 'window.json'
 describe('Navbar ', () => {
   beforeEach(() => cy.visit('/'))
 
-  it('will be visible when screen width is =<1024px', () => {
+  it('will be visible when screen width is >=1024px', () => {
     cy.fixture(WINDOW_CONSTANTS).then((fixture) => {
       const largeBreakpoint = fixture.breakpoints.large
       cy.viewport(largeBreakpoint.width, largeBreakpoint.height)
 
       cy.get('nav[data-test-id="desktop-nav"]').should('be.visible')
+    })
+  })
+
+  it('will be hidden when screen with is <1024px', () => {
+    cy.fixture(WINDOW_CONSTANTS).then((fixture) => {
+      const lessThanLargeBreakpoint = fixture.breakpoints.lessThanLarge
+      const { width, height } = lessThanLargeBreakpoint
+      cy.viewport(width, height)
+
+      cy.get('nav[data-test-id="desktop-nav"]').should('be.hidden')
     })
   })
 })
