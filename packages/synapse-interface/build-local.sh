f615108739428c9e29339c4c4e25d1eaa878f815
Simon <ak.simonm@gmail.com>
2023-04-17 17:00:32 -0400

    ?

--- packages/synapse-interface/build-local.sh
+++ packages/synapse-interface/build-local.sh
@@ -3,8 +3,7 @@ echo "Building synapse-interface with local sdk-router"
 cd ../sdk-router || exit
 pwd
 yarn install
-# cd ../synapse-interface || exit
-# pwd
-# yarn add ./packages/sdk-router
+cd ../synapse-interface || exit
+pwd
+yarn add ./packages/sdk-router
 echo "Added sdk-router, building site..."
-
