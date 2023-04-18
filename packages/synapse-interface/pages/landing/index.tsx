bee5e11b0d4db00b145864bcab810aa8a46ee1f3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-18 11:10:02 -0700

    Landing Page container

--- packages/synapse-interface/pages/landing/index.tsx
+++ packages/synapse-interface/pages/landing/index.tsx
@@ -1,5 +1,26 @@
+import Grid from '@/components/ui/tailwind/Grid'
+import Card from '@/components/ui/tailwind/Card'
+import Button from '@/components/ui/tailwind/Button'
+
+import { LandingPageWrapper } from '@/components/layouts/LandingPageWrapper'
+
+function LandingPageContainer({ children }) {
+  return (
+    <div
+      data-test-id="landing-page-container"
+      className="relative px-4 md:px-24"
+    >
+      {children}
+    </div>
+  )
+}
+
 const LandingPage = () => {
-  return <>lorem ipsum</>
+  return (
+    <LandingPageWrapper>
+      <LandingPageContainer>hello</LandingPageContainer>
+    </LandingPageWrapper>
+  )
 }
 
 export default LandingPage
