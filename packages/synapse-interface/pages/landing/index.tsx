bda79ea8938466ea1170af7b8bf0c28633ba0079
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-18 11:16:42 -0700

    Add SectionContainer for all landing page sections, create HeroSection component

--- packages/synapse-interface/pages/landing/index.tsx
+++ packages/synapse-interface/pages/landing/index.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/ui/tailwind/Button'
 
 import { LandingPageWrapper } from '@/components/layouts/LandingPageWrapper'
 
-function LandingPageContainer({ children }) {
+function LandingPageContainer({ children }: { children: React.ReactNode }) {
   return (
     <div
       data-test-id="landing-page-container"
@@ -15,10 +15,43 @@ function LandingPageContainer({ children }) {
   )
 }
 
+function SectionContainer({ children }: { children: React.ReactNode }) {
+  return <div className="py-6 md:py-12 space-y-[1rem]">{children}</div>
+}
+
+function HeroSection() {
+  return (
+    <SectionContainer>
+      <div
+        className={`
+          text-left text-4xl font-medium text-white
+          md:text-[46px] md:text-center pb-4
+        `}
+      >
+        <p>Secure cross-chain</p>
+        <p>communication</p>
+      </div>
+      <div className="hidden text-left text-secondaryTextColor md:text-center lg:text-center md:block lg:block">
+        <p>Synapse is the most widely used, extensible, secure cross-</p>
+        <p>chain communications network. Build truly cross-chain</p>
+        <p>applications using the Synapse Protocol.</p>
+      </div>
+      <div className="text-left text-secondaryTextColor md:text-center lg:text-center xs:block sm:block md:hidden lg:hidden">
+        <p>
+          Synapse is the most widely used, extensible, secure cross-chain
+          communications network. Build truly cross-chain applications using the
+          Synapse Protocol.
+        </p>
+      </div>
+    </SectionContainer>
+  )
+}
 const LandingPage = () => {
   return (
     <LandingPageWrapper>
-      <LandingPageContainer>hello</LandingPageContainer>
+      <LandingPageContainer>
+        <HeroSection />
+      </LandingPageContainer>
     </LandingPageWrapper>
   )
 }
