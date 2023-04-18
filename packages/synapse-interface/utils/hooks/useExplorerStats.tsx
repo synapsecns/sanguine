c13cfba452fa3b2ba4a72dc2a32ae4b45a60fece
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-18 13:29:26 -0700

    useExplorerStats utility hooks file to hold landing page stats functions

---
+++ packages/synapse-interface/utils/hooks/useExplorerStats.tsx
@@ -0,0 +1,41 @@
+import useSWR from 'swr'
+import numeral from 'numeral'
+import _ from 'lodash'
+
+import { useSwr } from '@hooks/useSwr'
+import { BRIDGESYN_ANALYTICS_API, LLAMA_API_URL } from '@/constants/urls'
+
+export function getTotalBridgeVolume() {
+  const { data } = useSwr(`${BRIDGESYN_ANALYTICS_API}/volume/total/in`)
+
+  let totalAmt
+  if (data?.totals) {
+    totalAmt = _.sum(_.values(data?.totals))
+    return numeral(totalAmt).format(`$0,0`)
+  } else {
+    return undefined
+  }
+}
+
+export function getTotalPoolVolume() {
+  const { data } = useSwr(`${BRIDGESYN_ANALYTICS_API}/pools/volume/total`)
+
+  let totalAmt
+  if (data?.totals) {
+    totalAmt = _.sum(_.values(data?.totals))
+    return numeral(totalAmt).format(`$0,0`)
+  } else {
+    return undefined
+  }
+}
+
+export function getTotalValueLocked() {
+  const { data: tvlData } = useSwr(LLAMA_API_URL)
+
+  if (tvlData?.currentChainTvls) {
+    const currentTvlAmt = _.sum(_.values(tvlData?.currentChainTvls))
+    return numeral(currentTvlAmt).format(`$0,0`)
+  } else {
+    return undefined
+  }
+}
