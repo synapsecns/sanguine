0b8b5cee443e176a39f128a10e05089706ea827e
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-19 10:29:52 -0700

    Refactor: HowItWorksSection

--- packages/synapse-interface/pages/landing/index.tsx
+++ packages/synapse-interface/pages/landing/index.tsx
@@ -40,29 +40,7 @@ import SecuritySection from './sections/SecuritySection'
 import BridgeSection from './sections/BridgeSection'
 import ExplorerSection from './sections/ExplorerSection'
 import IntegrationSection from './sections/IntegrationSection'
-
-function HowItWorksSection() {
-  return (
-    <SectionContainer>
-      <div className="flex justify-center">
-        <div className="w-2/3">
-          <div className="mt-12 mb-3 text-4xl text-center text-white">
-            How it all works
-          </div>
-          <div className="mt-6 -mb-12 text-center text-secondaryTextColor">
-            Smart contracts from one chain use the Synapse Messaging Router to
-            send the message to the destination chain, where a corresponding
-            Messaging Router sends it to the destination contract. Messages are
-            optimistically verified to ensure security and trustlessness.
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-center mx-50">
-        <HowSynapseWorks />
-      </div>
-    </SectionContainer>
-  )
-}
+import HowItWorksSection from './sections/HowItWorksSection'
 
 interface useCaseProp {
   title: string
---
+++ packages/synapse-interface/pages/landing/sections/HowItWorksSection.tsx
@@ -0,0 +1,25 @@
+import { SectionContainer } from '../shared'
+import { HowSynapseWorks } from '@/components/icons/LandingIcons/HowSynapseWorks'
+
+export default function HowItWorksSection() {
+  return (
+    <SectionContainer>
+      <div className="flex justify-center">
+        <div className="w-2/3">
+          <div className="mt-12 mb-3 text-4xl text-center text-white">
+            How it all works
+          </div>
+          <div className="mt-6 -mb-12 text-center text-secondaryTextColor">
+            Smart contracts from one chain use the Synapse Messaging Router to
+            send the message to the destination chain, where a corresponding
+            Messaging Router sends it to the destination contract. Messages are
+            optimistically verified to ensure security and trustlessness.
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center mx-50">
+        <HowSynapseWorks />
+      </div>
+    </SectionContainer>
+  )
+}
