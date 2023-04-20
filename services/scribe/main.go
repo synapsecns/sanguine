47bd84aa199e63891cd905400cb8b895261d43b5
Trajan0x <trajan0x@users.noreply.github.com>
2023-04-21 00:08:13 +0100

    add

--- services/scribe/main.go
+++ services/scribe/main.go
@@ -2,15 +2,11 @@
 package main
 
 import (
+	"github.com/synapsecns/sanguine/services/scribe/cmd"
 	"github.com/synapsecns/sanguine/services/scribe/metadata"
 	"os"
-	"time"
-
-	"github.com/synapsecns/sanguine/services/scribe/cmd"
 )
 
 func main() {
-	// used for a debug container only, do not merge this
-	time.Sleep(time.Hour)
 	cmd.Start(os.Args, metadata.BuildInfo())
 }
