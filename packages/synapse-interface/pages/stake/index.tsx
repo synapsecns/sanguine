f2e4af00980db9fbb1ac8a9988bfacfb40246161
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-21 21:55:09 -0700

    Wrap Stake page with LandingPageWrapper

--- packages/synapse-interface/pages/stake/index.tsx
+++ packages/synapse-interface/pages/stake/index.tsx
@@ -1,3 +1,7 @@
-const StakePage = () => {}
+import { LandingPageWrapper } from '@/components/layouts/LandingPageWrapper'
+
+const StakePage = () => {
+  return <LandingPageWrapper>lorem ipsum</LandingPageWrapper>
+}
 
 export default StakePage
