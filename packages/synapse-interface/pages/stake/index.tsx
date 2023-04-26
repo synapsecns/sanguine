3fd1a1db332eb7898c95d901f13109c2498198c3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-26 16:44:41 -0700

    Add main and page header elements

--- packages/synapse-interface/pages/stake/index.tsx
+++ packages/synapse-interface/pages/stake/index.tsx
@@ -5,14 +5,25 @@ import {
   STAKING_MAP_TOKENS,
   POOLS_BY_CHAIN,
 } from '@/constants/tokens'
+import { PageHeader } from '@/components/PageHeader'
 
 const StakePage = () => {
   const { chain: connectedChain } = useNetwork()
   const connectedChainId = connectedChain ? connectedChain.id : undefined
 
   const availableStakingTokens = POOLS_BY_CHAIN[connectedChainId]
+  console.log('availableStakingTokens: ', availableStakingTokens)
 
-  return <LandingPageWrapper>lorem ipsum</LandingPageWrapper>
+  return (
+    <LandingPageWrapper>
+      <main
+        data-test-id="stake-page"
+        className="flex items-center justify-between px-20 mx-10 space-x-2"
+      >
+        <PageHeader title="Stake" subtitle="Stake your LP Tokens." />
+      </main>
+    </LandingPageWrapper>
+  )
 }
 
 export default StakePage
