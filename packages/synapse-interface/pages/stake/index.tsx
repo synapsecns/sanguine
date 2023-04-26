4f6baccce5b883d429f279eea056218f6b1a6136
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-26 15:24:02 -0700

    Store current connected chain id to filter available stakes

--- packages/synapse-interface/pages/stake/index.tsx
+++ packages/synapse-interface/pages/stake/index.tsx
@@ -1,6 +1,10 @@
 import { LandingPageWrapper } from '@/components/layouts/LandingPageWrapper'
+import { useNetwork } from 'wagmi'
 
 const StakePage = () => {
+  const { chain: connectedChain } = useNetwork()
+  const connectedChainId = connectedChain ? connectedChain.id : undefined
+
   return <LandingPageWrapper>lorem ipsum</LandingPageWrapper>
 }
 
