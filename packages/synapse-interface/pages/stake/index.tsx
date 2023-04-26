1a6409c5006a1af346638170b752197c5542050b
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-26 15:53:39 -0700

    Filter for available stakes given current connected chainId

--- packages/synapse-interface/pages/stake/index.tsx
+++ packages/synapse-interface/pages/stake/index.tsx
@@ -1,9 +1,16 @@
 import { LandingPageWrapper } from '@/components/layouts/LandingPageWrapper'
 import { useNetwork } from 'wagmi'
+import {
+  STAKABLE_TOKENS,
+  STAKING_MAP_TOKENS,
+  POOLS_BY_CHAIN,
+} from '@/constants/tokens'
 
 const StakePage = () => {
   const { chain: connectedChain } = useNetwork()
   const connectedChainId = connectedChain ? connectedChain.id : undefined
+
+  const availableStakingTokens = POOLS_BY_CHAIN[connectedChainId]
 
   return <LandingPageWrapper>lorem ipsum</LandingPageWrapper>
 }
