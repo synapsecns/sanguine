9b12dc4a242742e7dd361dc0d61164c87d32f161
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-27 10:42:08 -0700

    Refactor: Move NoStakeCard into own file

--- packages/synapse-interface/pages/stake/index.tsx
+++ packages/synapse-interface/pages/stake/index.tsx
@@ -11,29 +11,9 @@ import {
 } from '@/constants/tokens'
 import { CHAINS_BY_ID, ChainsByChainID } from '@/constants/chains'
 import { PageHeader } from '@/components/PageHeader'
+import NoStakeCard from './NoStakeCard'
 import Card from '@/components/ui/tailwind/Card'
 import Grid from '@/components/ui/tailwind/Grid'
-
-function NoStakeCard({ chain }: { chain?: Chain }) {
-  const chainName = chain?.chainName ?? 'current network'
-  const networkColor = chain?.color
-  return (
-    <Card
-      divider={false}
-      className={`
-        transform transition-all duration-100
-        rounded-xl max-w-[420px]
-      `}
-    >
-      <div className="w-full pt-4 text-center text-gray-400">
-        No stakes available on{' '}
-        <span className={`${getNetworkTextColor(networkColor)} font-medium`}>
-          {chainName}
-        </span>
-      </div>
-    </Card>
-  )
-}
 
 const StakePage = () => {
   const { chain: connectedChain } = useNetwork()
