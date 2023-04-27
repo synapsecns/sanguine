2a3962b00d5d99c480fab3642b783e07989b6ffd
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-27 12:08:58 -0700

    SwapCardProps

--- packages/synapse-interface/pages/stake/StakeCard.tsx
+++ packages/synapse-interface/pages/stake/StakeCard.tsx
@@ -1,3 +1,11 @@
-const SwapCard = () => {}
+import { getTokenOnChain } from '@/utils/hooks/useTokenInfo'
+import { Token } from '@/utils/types'
+
+interface SwapCardProps {
+  chainId: number
+  token: Token
+}
+
+const SwapCard = ({ chainId, token }: SwapCardProps) => {}
 
 export default SwapCard
