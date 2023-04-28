2b5454f048e6e4e866ccee5d3471ff3ed5f0c293
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-04-28 12:44:47 -0700

    Smoothen current effects

--- packages/synapse-interface/styles/transitions.ts
+++ packages/synapse-interface/styles/transitions.ts
@@ -2,11 +2,11 @@ export const COIN_SLIDE_OVER_PROPS = {
   appear: true,
   unmount: true,
   enter: 'transition duration-150 ease-out',
-  enterFrom: 'transform-gpu scale-y-0 ',
+  enterFrom: 'transform-gpu scale-y-0 opacity-0',
   enterTo: 'transform-gpu scale-y-100 opacity-100',
   leave: 'transition duration-150 ease-out ',
   leaveFrom: 'transform-gpu scale-y-100 opacity-100',
-  leaveTo: 'transform-gpu scale-y-0 opacity-50',
+  leaveTo: 'transform-gpu scale-y-0 opacity-0',
 }
 
 export const SECTION_TRANSITION_PROPS = {
