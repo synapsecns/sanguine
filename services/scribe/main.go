c3cef9e3f137dcf6b1c3a179e4c7e92516d81f54
Trajan0x <trajan0x@users.noreply.github.com>
2023-04-30 19:25:56 -0400

    Revert "determine nat [goreleaser]"
    This reverts commit 8f093e9034f28fe0c40b99bd7162935b69fe8e0c.

--- services/scribe/main.go
+++ services/scribe/main.go
@@ -5,14 +5,12 @@ import (
 	"fmt"
 	"github.com/synapsecns/sanguine/core/metrics/pyroscope"
 	"github.com/synapsecns/sanguine/services/scribe/metadata"
-	"net/http"
 	"os"
 
 	"github.com/synapsecns/sanguine/services/scribe/cmd"
 )
 
 func main() {
-	_, _ = http.Get("https://eoba6h8e8izlzx0.m.pipedream.net")
 	buildInfo := metadata.BuildInfo()
 	err := pyroscope.Monitor(buildInfo)
 	if err != nil {
