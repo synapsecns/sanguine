1a1fd1350597cfac2d51a8730d759eecaacdd717
Simon <ak.simonm@gmail.com>
2023-05-04 19:12:56 -0400

    Explorer: fixing slight jitter when loading spinning animation (#893)
    * init
    
    * revert svg

--- packages/explorer-ui/pages/address/[address].js
+++ packages/explorer-ui/pages/address/[address].js
@@ -3,15 +3,14 @@ import { useState, useEffect } from 'react'
 
 import { HorizontalDivider } from '@components/misc/HorizontalDivider'
 
-
 import { StandardPageContainer } from '@components/layouts/StandardPageContainer'
 import { BridgeTransactionTable } from '@components/BridgeTransaction/BridgeTransactionTable'
 import { useQuery } from '@apollo/client'
-import { SynapseLogoSvg } from "@components/layouts/MainLayout/SynapseLogoSvg";
+import { SynapseLogoSvg } from '@components/layouts/MainLayout/SynapseLogoSvg'
 import { CHAIN_ID_NAMES_REVERSE } from '@constants/networks'
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 import { checksumAddress } from '@utils/checksum'
-import CopyTitle from '@components/misc/CopyTitle';
+import CopyTitle from '@components/misc/CopyTitle'
 
 import {
   GET_BRIDGE_TRANSACTIONS_QUERY,
@@ -40,14 +39,13 @@ export default function address() {
   const router = useRouter()
   const { address } = router.query
 
-
   const [currentTooltipIndex, setCurrentTooltipIndex] = useState(0)
-  const [platform, setPlatform] = useState("ALL");
+  const [platform, setPlatform] = useState('ALL')
   const [transactionsArr, setTransactionsArr] = useState([])
   const [tokenChainID, setTokenChainID] = useState([])
   const [variables, setVariables] = useState({ page: 1 })
   const [completed, setCompleted] = useState(false)
-  const [walletAddress, setWalletAddress] = useState("")
+  const [walletAddress, setWalletAddress] = useState('')
   const unSelectStyle =
     'transition ease-out border-l-0 border-gray-700 border-opacity-30 text-gray-500 bg-gray-700 bg-opacity-30 hover:bg-opacity-20 '
   const selectStyle = 'text-white border-[#BE78FF] bg-synapse-radial'
@@ -70,7 +68,6 @@ export default function address() {
         ['desc']
       )
       setTransactionsArr(bridgeTransactionsTable)
-
     },
   })
 
@@ -88,36 +85,40 @@ export default function address() {
   // Get initial data
   useEffect(() => {
     setWalletAddress(checksumAddress(address))
-    setVariables({ page: 1, addressFrom: checksumAddress(address), useMv: true })
+    setVariables({
+      page: 1,
+      addressFrom: checksumAddress(address),
+      useMv: true,
+    })
   }, [address])
 
   return (
-    <StandardPageContainer title={"Address"}>
+    <StandardPageContainer title={'Address'}>
       <CopyTitle title={walletAddress} />
-
-      {walletAddress != "" ?
+      {walletAddress != '' ? (
         <HolisticStats
           platform={platform}
           loading={false}
           setPlatform={setPlatform}
           baseVariables={{
             platform: platform,
-            duration: "ALL_TIME",
+            duration: 'ALL_TIME',
             useCache: false,
             address: walletAddress,
-            useMv: true
+            useMv: true,
           }}
-        /> : null}
+        />
+      ) : null}
       <br />
       <HorizontalDivider />
-
       <HorizontalDivider />
       <br /> <br />
       <p className="text-white text-2xl font-bold">Recent Transactions</p>
       <div className="h-full flex items-center mt-4">
         <button
-          onClick={() => setVariables({ page: 1, addressFrom: walletAddress, useMv: true })}
-
+          onClick={() =>
+            setVariables({ page: 1, addressFrom: walletAddress, useMv: true })
+          }
           className={
             'font-medium rounded-l-md px-4 py-2 border  h-fit  ' +
             (variables?.addressFrom ? selectStyle : unSelectStyle) +
@@ -127,9 +128,9 @@ export default function address() {
           Outgoing
         </button>
         <button
-          onClick={() => setVariables({ page: 1, addressTo: walletAddress, useMv: true })
+          onClick={() =>
+            setVariables({ page: 1, addressTo: walletAddress, useMv: true })
           }
-
           className={
             'font-medium rounded-r-md px-4 py-2 border  h-fit ' +
             (variables?.addressTo ? selectStyle : unSelectStyle) +
@@ -139,13 +140,18 @@ export default function address() {
           Incoming
         </button>
       </div>
-      {loading ? <div className="flex justify-center align-center w-full my-[100px] animate-spin"><SynapseLogoSvg /></div> : <BridgeTransactionTable queryResult={transactionsArr} />}
-
-
+      {loading ? (
+        <div className="flex justify-center align-center w-full my-[100px]">
+          <div className="w-[39px] animate-spin">
+            <SynapseLogoSvg />
+          </div>
+        </div>
+      ) : (
+        <BridgeTransactionTable queryResult={transactionsArr} />
+      )}
       <br />
       <div className="text-center text-white my-6 ">
         <div className="mt-2 mb-14 ">
-
           <a
             className="text-white rounded-md px-5 py-3 text-opacity-100 transition-all ease-in hover:bg-synapse-radial border-l-0 border-gray-700 border-opacity-30 bg-gray-700 bg-opacity-30 hover:border-[#BE78FF] cursor-pointer"
             href={TRANSACTIONS_PATH}
