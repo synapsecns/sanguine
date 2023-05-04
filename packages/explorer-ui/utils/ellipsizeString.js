596f68b5b4c03ea3ff8b89b071f83a6b4986c5d8
Simon <ak.simonm@gmail.com>
2023-05-04 17:23:06 -0400

    Adding links to destination and origin bridge contract on tx view (#888)

--- packages/explorer-ui/utils/ellipsizeString.js
+++ packages/explorer-ui/utils/ellipsizeString.js
@@ -2,19 +2,20 @@ export function ellipsizeString({ string, limiter = 4, isZeroX = false }) {
   if (limiter === 0) {
     return string
   } else {
-    if (isZeroX){
+    if (isZeroX) {
       return (
         string &&
-        `${string.toLowerCase().slice(0, limiter+ 2)}...${string
+        `${string.toLowerCase().slice(0, limiter + 2)}...${string
           .toLowerCase()
-          .slice(-limiter , string.length)}`
+          .slice(-limiter, string.length)}`
       )
-    }else {
-    return (
-      string &&
-      `${string.toLowerCase().slice(0, limiter)}...${string
-        .toLowerCase()
-        .slice(-limiter , string.length)}`
-    )
-  }}
+    } else {
+      return (
+        string &&
+        `${string.toLowerCase().slice(0, limiter)}...${string
+          .toLowerCase()
+          .slice(-limiter, string.length)}`
+      )
+    }
+  }
 }
--- packages/explorer-ui/utils/styles/muiStyles.js
+++ packages/explorer-ui/utils/styles/muiStyles.js
@@ -1,151 +1,138 @@
 export const inputStyle = {
-  width: "100%",
-  backgroundColor: "rgba(255, 255, 255, 0.03)",
+  width: '100%',
+  backgroundColor: 'rgba(255, 255, 255, 0.03)',
   input: {
     color: 'rgba(255, 255, 255, 1)',
-    my: "1px",
+    my: '1px',
 
-    "&::placeholder": {
-      color: 'rgba(255, 255, 255, 0.6)'
+    '&::placeholder': {
+      color: 'rgba(255, 255, 255, 0.6)',
     },
-
-
   },
   borderRadius: 2,
-  "& label.MuiInputLabel-root": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.MuiInputLabel-root': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& label.Mui-focused": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.Mui-focused': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "rgba(255, 255, 255, 0.2)",
-
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)',
   },
 
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
-      borderRadius: "7px",
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
+      borderRadius: '7px',
     },
-    "&:hover fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+    '&:hover fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&.Mui-focused fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.6)",
+    '&.Mui-focused fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.6)',
     },
-
-  }
+  },
 }
 
 export const dateInputStyle = {
-  width: "100%",
+  width: '100%',
   borderRadius: 2,
-  backgroundColor: "rgba(255, 255, 255, 0.03)",
-  svg: { color: "rgba(255, 255, 255, 0.6) " },
+  backgroundColor: 'rgba(255, 255, 255, 0.03)',
+  svg: { color: 'rgba(255, 255, 255, 0.6) ' },
   input: {
-    my: "1px",
-    "&::placeholder": {
-      color: 'rgba(255, 255, 255, 0.6)'
+    my: '1px',
+    '&::placeholder': {
+      color: 'rgba(255, 255, 255, 0.6)',
     },
     color: 'rgba(255, 255, 255, 1)',
   },
 
-  "& label": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& label.Mui-focused": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.Mui-focused': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "rgba(255, 255, 255, 0.2)",
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)',
   },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&:hover fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+    '&:hover fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&.Mui-focused fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.6)",
+    '&.Mui-focused fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.6)',
     },
-
-
   },
 }
 
 export const comboSelectStyle = {
-  width: "40%",
+  width: '40%',
   borderRadius: 2,
-  backgroundColor: "rgba(255, 255, 255, 0.03)",
-  svg: { color: "rgba(255, 255, 255, 0.6) " },
+  backgroundColor: 'rgba(255, 255, 255, 0.03)',
+  svg: { color: 'rgba(255, 255, 255, 0.6) ' },
 
-  "& label": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& label.Mui-focused": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.Mui-focused': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "rgba(255, 255, 255, 0.2)",
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)',
   },
-  "& div .MuiSelect-select": {
-    my: "1px",
-    color: "#ffffff"
+  '& div .MuiSelect-select': {
+    my: '1px',
+    color: '#ffffff',
   },
-  "& .MuiOutlinedInput-root": {
-
-    "& fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&:hover fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+    '&:hover fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&.Mui-focused fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.6)",
+    '&.Mui-focused fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.6)',
     },
-
-
   },
 }
 export const comboSelectStyleSmall = {
-  width: "100%",
-  backgroundColor: "rgba(255, 255, 255, 0.03)",
-  svg: { color: "rgba(255, 255, 255, 0.6) " },
-  "& label": {
-    color: "rgba(255, 255, 255, 0.6)",
+  width: '100%',
+  backgroundColor: 'rgba(255, 255, 255, 0.03)',
+  svg: { color: 'rgba(255, 255, 255, 0.6) ' },
+  '& label': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& label.Mui-focused": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.Mui-focused': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "rgba(255, 255, 255, 0.2)",
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)',
   },
-  "& div .MuiSelect-select": {
-    my: "1px",
-    color: "#ffffff",
+  '& div .MuiSelect-select': {
+    my: '1px',
+    color: '#ffffff',
   },
-  "& .MuiOutlinedInput-root": {
-
-    "& fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
-      borderRadius: " 0 7px 7px 0",
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
+      borderRadius: ' 0 7px 7px 0',
     },
-    "&:hover fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+    '&:hover fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&.Mui-focused fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.6)",
+    '&.Mui-focused fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.6)',
     },
-
-
   },
 }
 
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
 export const MenuProps = {
   PaperProps: {
     style: {
@@ -153,46 +140,40 @@ export const MenuProps = {
       width: 250,
     },
   },
-};
-
+}
 
 export const inputStyleRounded = {
-  width: "100%",
-  backgroundColor: "rgba(255, 255, 255, 0.03)",
+  width: '100%',
+  backgroundColor: 'rgba(255, 255, 255, 0.03)',
   input: {
     color: 'rgba(255, 255, 255, 1)',
-    my: "1px",
+    my: '1px',
 
-    "&::placeholder": {
-      color: 'rgba(255, 255, 255, 0.6)'
+    '&::placeholder': {
+      color: 'rgba(255, 255, 255, 0.6)',
     },
-
-
   },
 
-  "& label.MuiInputLabel-root": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.MuiInputLabel-root': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& label.Mui-focused": {
-    color: "rgba(255, 255, 255, 0.6)",
+  '& label.Mui-focused': {
+    color: 'rgba(255, 255, 255, 0.6)',
   },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "rgba(255, 255, 255, 0.2)",
-
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'rgba(255, 255, 255, 0.2)',
   },
 
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
-      borderRadius: "7px 0 0 7px",
-
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
+      borderRadius: '7px 0 0 7px',
     },
-    "&:hover fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.2)",
+    '&:hover fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.2)',
     },
-    "&.Mui-focused fieldset": {
-      borderColor: "rgba(255, 255, 255, 0.6)",
+    '&.Mui-focused fieldset': {
+      borderColor: 'rgba(255, 255, 255, 0.6)',
     },
-
-  }
+  },
 }
