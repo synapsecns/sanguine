bd0b57d763ac95c1496551f49230383258dbb272
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-05-05 13:42:22 +0100

    Tests for updating remote gas data

--- packages/contracts-core/test/suite/GasOracle.t.sol
+++ packages/contracts-core/test/suite/GasOracle.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {GasData, InterfaceDestination} from "../../contracts/interfaces/InterfaceDestination.sol";
 import {Random, MessagingBaseTest} from "./base/MessagingBase.t.sol";
 import {GasOracle, SynapseTest} from "../utils/SynapseTest.t.sol";
 
@@ -37,20 +38,89 @@ contract GasOracleTest is MessagingBaseTest {
     }
 
     function test_getGasData(RawGasData256 memory rgd256) public {
-        GasOracle(gasOracle).setGasData({
-            domain: localDomain(),
-            gasPrice: rgd256.gasPrice,
-            dataPrice: rgd256.dataPrice,
-            execBuffer: rgd256.execBuffer,
-            amortAttCost: rgd256.amortAttCost,
-            etherPrice: rgd256.etherPrice,
-            markup: rgd256.markup
-        });
+        setGasData(localDomain(), rgd256);
         RawGasData memory rgd = rgd256.compress();
         assertEq(GasOracle(gasOracle).getGasData(), rgd.encodeGasData());
     }
 
     function test_getDecodedData(uint32 domain, RawGasData256 memory rgd256) public {
+        setGasData(domain, rgd256);
+        RawGasData256 memory expected = rgd256.compress().decompress();
+        checkGasData(domain, expected);
+    }
+
+    function test_updateGasData_beforeIncreaseOptimisticPeriod(Random memory random, uint256 timePassed) public {
+        uint32 domain = random.nextUint32();
+        vm.assume(domain != 0 && domain != localDomain());
+        timePassed = timePassed % GasOracle(gasOracle).GAS_DATA_INCREASED_OPTIMISTIC_PERIOD();
+        RawGasData memory current = random.nextGasData();
+        setGasData(domain, current.decompress());
+        RawGasData memory updated = random.nextGasData();
+        // Force getGasData(domain) to return (updated, timePassed)
+        vm.mockCall(
+            destination,
+            abi.encodeWithSelector(InterfaceDestination.getGasData.selector, domain),
+            abi.encode(updated.castToGasData(), timePassed)
+        );
+        address caller = random.nextAddress();
+        vm.prank(caller);
+        GasOracle(gasOracle).updateGasData(domain);
+        checkGasData(domain, current.decompress());
+    }
+
+    function test_updateGasData_parametersOnlyIncreased(Random memory random, uint256 timePassed) public {
+        uint32 domain = random.nextUint32();
+        vm.assume(domain != 0 && domain != localDomain());
+        timePassed = bound(
+            timePassed,
+            GasOracle(gasOracle).GAS_DATA_INCREASED_OPTIMISTIC_PERIOD(),
+            GasOracle(gasOracle).GAS_DATA_DECREASED_OPTIMISTIC_PERIOD() - 1
+        );
+        RawGasData memory current = random.nextGasData();
+        setGasData(domain, current.decompress());
+        RawGasData memory updated = random.nextGasData();
+        // Force getGasData(domain) to return (updated, timePassed)
+        vm.mockCall(
+            destination,
+            abi.encodeWithSelector(InterfaceDestination.getGasData.selector, domain),
+            abi.encode(updated.castToGasData(), timePassed)
+        );
+        address caller = random.nextAddress();
+        vm.prank(caller);
+        GasOracle(gasOracle).updateGasData(domain);
+        RawGasData256 memory cur256 = current.decompress();
+        RawGasData256 memory upd256 = updated.decompress();
+        RawGasData256 memory expected = RawGasData256({
+            gasPrice: max(cur256.gasPrice, upd256.gasPrice),
+            dataPrice: max(cur256.dataPrice, upd256.dataPrice),
+            execBuffer: max(cur256.execBuffer, upd256.execBuffer),
+            amortAttCost: max(cur256.amortAttCost, upd256.amortAttCost),
+            etherPrice: max(cur256.etherPrice, upd256.etherPrice),
+            markup: max(cur256.markup, upd256.markup)
+        });
+        checkGasData(domain, expected);
+    }
+
+    function test_updateGasData_fullyUpdated(Random memory random, uint256 timePassed) public {
+        uint32 domain = random.nextUint32();
+        vm.assume(domain != 0 && domain != localDomain());
+        timePassed = bound(timePassed, GasOracle(gasOracle).GAS_DATA_DECREASED_OPTIMISTIC_PERIOD(), 30 days);
+        RawGasData memory current = random.nextGasData();
+        setGasData(domain, current.decompress());
+        RawGasData memory updated = random.nextGasData();
+        // Force getGasData(domain) to return (updated, timePassed)
+        vm.mockCall(
+            destination,
+            abi.encodeWithSelector(InterfaceDestination.getGasData.selector, domain),
+            abi.encode(updated.castToGasData(), timePassed)
+        );
+        address caller = random.nextAddress();
+        vm.prank(caller);
+        GasOracle(gasOracle).updateGasData(domain);
+        checkGasData(domain, updated.decompress());
+    }
+
+    function setGasData(uint32 domain, RawGasData256 memory rgd256) public {
         GasOracle(gasOracle).setGasData({
             domain: domain,
             gasPrice: rgd256.gasPrice,
@@ -60,7 +130,9 @@ contract GasOracleTest is MessagingBaseTest {
             etherPrice: rgd256.etherPrice,
             markup: rgd256.markup
         });
-        RawGasData256 memory expected = rgd256.compress().decompress();
+    }
+
+    function checkGasData(uint32 domain, RawGasData256 memory expected) public {
         (
             uint256 gasPrice,
             uint256 dataPrice,
@@ -78,6 +150,10 @@ contract GasOracleTest is MessagingBaseTest {
     }
 
     // ══════════════════════════════════════════════════ HELPERS ══════════════════════════════════════════════════════
+
+    function max(uint256 a, uint256 b) public pure returns (uint256) {
+        return a > b ? a : b;
+    }
 
     /// @notice Returns local domain for the tested contract
     function localDomain() public pure override returns (uint32) {
