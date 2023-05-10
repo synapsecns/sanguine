7fcb5fe941150fc585b2887ef2a203abc670fa53
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-05-10 16:58:48 +0100

    Use MessageRecipient as base for Test Client

--- packages/contracts-core/contracts/client/TestClient.sol
+++ packages/contracts-core/contracts/client/TestClient.sol
@@ -2,43 +2,19 @@
 pragma solidity 0.8.17;
 
 // ══════════════════════════════ LIBRARY IMPORTS ══════════════════════════════
-import {CallerNotDestination} from "../libs/Errors.sol";
-import {Request, RequestLib} from "../libs/Request.sol";
 import {TypeCasts} from "../libs/TypeCasts.sol";
 // ═════════════════════════════ INTERNAL IMPORTS ══════════════════════════════
-import {IMessageRecipient} from "../interfaces/IMessageRecipient.sol";
-import {InterfaceOrigin} from "../interfaces/InterfaceOrigin.sol";
+import {MessageRecipient} from "./MessageRecipient.sol";
 
-contract TestClient is IMessageRecipient {
-    /// @notice Local chain Origin: used for sending messages
-    address public immutable origin;
-
-    /// @notice Local chain Destination: used for receiving messages
-    address public immutable destination;
-
+contract TestClient is MessageRecipient {
     event MessageReceived(
         uint32 origin, uint32 nonce, bytes32 sender, uint256 proofMaturity, uint32 version, bytes content
     );
 
     event MessageSent(uint32 destination, uint32 nonce, bytes32 sender, bytes32 recipient, bytes content);
 
-    constructor(address origin_, address destination_) {
-        origin = origin_;
-        destination = destination_;
-    }
-
-    /// @inheritdoc IMessageRecipient
-    function receiveBaseMessage(
-        uint32 origin_,
-        uint32 nonce,
-        bytes32 sender,
-        uint256 proofMaturity,
-        uint32 version,
-        bytes memory content
-    ) external payable {
-        if (msg.sender != destination) revert CallerNotDestination();
-        emit MessageReceived(origin_, nonce, sender, proofMaturity, version, content);
-    }
+    // solhint-disable-next-line no-empty-blocks
+    constructor(address origin_, address destination_) MessageRecipient(origin_, destination_) {}
 
     function sendMessage(
         uint32 destination_,
@@ -47,13 +23,22 @@ contract TestClient is IMessageRecipient {
         uint64 gasLimit,
         uint32 version,
         bytes memory content
-    ) external {
+    ) external payable {
         bytes32 recipient = TypeCasts.addressToBytes32(recipientAddress);
-        // TODO: figure out the logic for a message test
-        Request request = RequestLib.encodeRequest(0, gasLimit, version);
-        (uint32 nonce,) = InterfaceOrigin(origin).sendBaseMessage(
-            destination_, recipient, optimisticSeconds, Request.unwrap(request), content
-        );
+        MessageRequest memory request = MessageRequest({gasDrop: 0, gasLimit: gasLimit, version: version});
+        (uint32 nonce,) = _sendBaseMessage(destination_, recipient, optimisticSeconds, request, content);
         emit MessageSent(destination_, nonce, TypeCasts.addressToBytes32(address(this)), recipient, content);
+    }
+
+    /// @inheritdoc MessageRecipient
+    function _receiveBaseMessageUnsafe(
+        uint32 origin_,
+        uint32 nonce,
+        bytes32 sender,
+        uint256 proofMaturity,
+        uint32 version,
+        bytes memory content
+    ) internal override {
+        emit MessageReceived(origin_, nonce, sender, proofMaturity, version, content);
     }
 }
