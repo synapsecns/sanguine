6a516151ccb314be7fac5ebb9bc8bcaadd230eeb
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-05-11 16:17:19 -0700

    Create useBridgeQuoteProps interface to describe shape of params

--- packages/synapse-interface/utils/hooks/useBridgeQuote.tsx
+++ packages/synapse-interface/utils/hooks/useBridgeQuote.tsx
@@ -1,4 +1,14 @@
+import { Address } from 'wagmi'
 import { useSynapseContext } from '../providers/SynapseProvider'
+import { BigNumberish } from 'ethers'
+
+interface useBridgeQuoteProps {
+  fromChainId: number
+  toChainId: number
+  fromTokenAddress: string | Address
+  toTokenAddress: string | Address
+  inputValue: BigNumberish
+}
 
 /*
   useBridgeQuote hook
@@ -6,7 +16,13 @@ import { useSynapseContext } from '../providers/SynapseProvider'
   - Calculates slippage by subtracting fee from input amount (checks to ensure proper num of decimals are in use - ask someone about stable swaps if you want to learn more)
   - Wraps SynapseSDK calls in Promise wrapper to be cancellable when new request is made
   */
-export const useBridgeQuote = () => {
+export const useBridgeQuote = ({
+  fromChainId,
+  toChainId,
+  fromTokenAddress,
+  toTokenAddress,
+  inputValue,
+}: useBridgeQuoteProps) => {
   const SynapseSDK = useSynapseContext()
 
   return <></>
