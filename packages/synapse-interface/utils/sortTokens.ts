e8444f9487bdb3685a85d9f29c02cc041da09110
Simon <ak.simonm@gmail.com>
2023-05-13 13:56:56 -0500

    rm comments

--- packages/synapse-interface/utils/sortTokens.ts
+++ packages/synapse-interface/utils/sortTokens.ts
@@ -15,63 +15,6 @@ export const sortByVisibilityRank = (tokens: Token[]) => {
     (a, b) => b.visibilityRank - a.visibilityRank
   )
 }
-// move to utils
-// export const sortByTokenBalance = async (
-//   tokens: Token[],
-//   chainId: number,
-//   address: any
-// ) => {
-//   let i = 0
-//   const tokensWithBalances: any[] = []
-//   const zeroTokensWithBalances: any[] = []
-//   // go through all tokens and retrieve token balances
-//   while (i < tokens.length) {
-//     if (chainId === undefined || address === undefined) {
-//       tokensWithBalances.push({
-//         token: tokens[i],
-//         balance: Zero,
-//       })
-//       i++
-//       continue
-//     }
-//     const tokenAddr = tokens[i].addresses[chainId as keyof Token['addresses']]
-
-//     let rawTokenBalance: any
-//     // Check for native token
-//     if (tokenAddr === '' || tokenAddr === AddressZero) {
-//       const data = await fetchBalance({
-//         address,
-//         chainId,
-//       })
-//       rawTokenBalance = data
-//     } else if (tokenAddr?.length > 0) {
-//       const data = await fetchBalance({
-//         address,
-//         token: `0x${tokenAddr.slice(2)}`,
-//         chainId,
-//       })
-//       rawTokenBalance = data
-//     }
-
-//     // manages two the array of tokens with zero balances and non-zero balances
-//     if (rawTokenBalance) {
-//       if (rawTokenBalance?.value._hex !== '0x00') {
-//         zeroTokensWithBalances.push({
-//           token: tokens[i],
-//           balance: rawTokenBalance.value,
-//         })
-//       } else {
-//         tokensWithBalances.push({
-//           token: tokens[i],
-//           balance: rawTokenBalance.value,
-//         })
-//       }
-//     }
-//     i++
-//   }
-
-//   return zeroTokensWithBalances.concat(tokensWithBalances)
-// }
 
 const sortArrayByBalance = (array) => {
   return array.sort((a, b) => {
