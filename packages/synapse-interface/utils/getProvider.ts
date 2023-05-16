41fd849d932f2db7a01934fcb89130b48695184c
Simon <ak.simonm@gmail.com>
2023-05-16 17:22:20 -0400

    lint

--- packages/synapse-interface/utils/getProvider.ts
+++ packages/synapse-interface/utils/getProvider.ts
@@ -1,5 +1,7 @@
-import { CHAINS_BY_ID } from '@/constants/chains'
 import { JsonRpcProvider } from '@ethersproject/providers'
+
+import { CHAINS_BY_ID } from '@/constants/chains'
+
 export const getProvider = (chainId: number): JsonRpcProvider => {
   const chain = CHAINS_BY_ID[chainId]
   return new JsonRpcProvider(chain.rpc)
