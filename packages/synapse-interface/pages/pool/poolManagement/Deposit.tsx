f7df2bdfb01ee540deb0120630992f6acc0f6337
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-05-17 12:42:14 -0700

    Deposit in Pool to show loading inputs

--- packages/synapse-interface/pages/pool/poolManagement/Deposit.tsx
+++ packages/synapse-interface/pages/pool/poolManagement/Deposit.tsx
@@ -31,12 +31,6 @@ const Deposit = ({
   poolData: PoolData
   poolUserData: PoolUserData
 }) => {
-  console.log('pool:', pool)
-  console.log('chainId:', chainId)
-  console.log('address:', address)
-  console.log('poolData:', poolData)
-  console.log('poolUserData:', poolUserData)
-
   // todo store sum in here?
   const [inputValue, setInputValue] = useState<{
     bn: Record<string, BigNumber>
@@ -208,7 +202,7 @@ const Deposit = ({
   return (
     <div className="flex-col">
       <div className="px-2 pt-1 pb-4 bg-bgLight rounded-xl">
-        {/* {pool && poolUserData && poolData ? (
+        {pool && poolUserData && poolData ? (
           poolUserData.tokens.map((tokenObj, i) => {
             const balanceToken = correctToken(tokenObj.token)
             return (
@@ -223,13 +217,12 @@ const Deposit = ({
               />
             )
           })
-        ) : ( */}
-        <>
-          <LoadingTokenInput />
-          <LoadingTokenInput />
-          <LoadingTokenInput />
-        </>
-        {/* )} */}
+        ) : (
+          <>
+            <LoadingTokenInput />
+            <LoadingTokenInput />
+          </>
+        )}
       </div>
       {actionBtn}
       {depositQuote.priceImpact && depositQuote.priceImpact?.gt(Zero) && (
