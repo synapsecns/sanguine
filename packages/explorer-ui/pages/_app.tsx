5fb1eab90f99c5e5c92a927c599ef804565c0c5c
Trajan0x <trajan0x@users.noreply.github.com>
2023-05-18 18:40:29 +0100

    finish ga

--- packages/explorer-ui/pages/_app.tsx
+++ packages/explorer-ui/pages/_app.tsx
@@ -1,17 +1,21 @@
 import { ApolloProvider } from '@apollo/client'
 import { PageWrapper } from '@components/layouts//MainLayout'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import client from '../apollo-client'
 import '../styles/global.css'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
-    <ApolloProvider client={client}>
-      <PageWrapper>
-        <Component {...pageProps} />
-      </PageWrapper>
-    </ApolloProvider>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <ApolloProvider client={client}>
+        <PageWrapper>
+          <Component {...pageProps} />
+        </PageWrapper>
+      </ApolloProvider>
+    </>
   )
 }
 
-export default MyApp
+export default App
