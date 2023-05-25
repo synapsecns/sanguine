68eb14abc31df2b7e05ddc693fbb26402774765b
Simon <ak.simonm@gmail.com>
2023-05-25 09:43:18 -0400

    add tests

--- packages/rest-api/app.test.ts
+++ packages/rest-api/app.test.ts
@@ -9,13 +9,17 @@ describe('Server Endpoints', () => {
       expect(res.statusCode).toEqual(200)
       expect(res.text).toContain('<h1>Invalid Params</h1>')
     })
-
-    // Add more tests to check valid swap
-    // it('should respond with swap quote for valid request', async () => {
-    //   const res = await request(app).get('/swap?...');
-    //   expect(res.statusCode).toEqual(200);
-    //   // expect...
-    // });
+  })
+  describe('/swap passing params', () => {
+    it('should respond with swap quote for valid request', async () => {
+      const res = await request(server).get(
+        '/swap?chain=1&fromToken=USDC&toToken=DAI&amount=100'
+      )
+      expect(res.statusCode).toEqual(200)
+      expect(res.maxAmountOutStr.length).toBeGreaterThan(0)
+      expect(res.routerAddress.length).toBeGreaterThan(0)
+      expect(res.query.length).toBeGreaterThan(0)
+    })
   })
 
   describe('/bridge endpoint', () => {
@@ -24,13 +28,19 @@ describe('Server Endpoints', () => {
       expect(res.statusCode).toEqual(200)
       expect(res.text).toContain('<h1>Invalid Request</h1>')
     })
+  })
 
-    // Add more tests to check valid bridge
-    // it('should respond with bridge quote for valid request', async () => {
-    //   const res = await request(app).get('/bridge?...');
-    //   expect(res.statusCode).toEqual(200);
-    //   // expect...
-    // });
+  describe('/bridge passing params', () => {
+    it('should respond with bridge quote for valid request', async () => {
+      const res = await request(server).get(
+        '/bridge?fromChain=1&toChain=42161&fromToken=USDC&toToken=USDC&amount=1000000'
+      )
+      expect(res.statusCode).toEqual(200)
+      expect(res.maxAmountOutStr.length).toBeGreaterThan(0)
+      expect(res.routerAddress.length).toBeGreaterThan(0)
+      expect(res.originQuery.length).toBeGreaterThan(0)
+      expect(res.destQuery.length).toBeGreaterThan(0)
+    })
   })
   afterAll((done) => {
     server.close(done)
