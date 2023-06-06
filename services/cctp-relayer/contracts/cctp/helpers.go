d6a2454d810a45f6f17c74f5ad5f88ef37d77a1e
Daniel Wasserman <wassermandaniel8@gmail.com>
2023-06-06 13:25:06 -0500

    Feat: add helpers.go

---
+++ services/cctp-relayer/contracts/cctp/helpers.go
@@ -0,0 +1,39 @@
+package cctp
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+type ContractRef interface {
+	Address() common.Address
+}
+
+// SynapseCCTPRef is a bound cctp contract that conforms to vm.ContractRef.
+//
+//nolint:golint
+type SynapseCCTPRef struct {
+	*SynapseCCTP
+	address common.Address
+}
+
+// Address is the contract address.
+func (s SynapseCCTPRef) Address() common.Address {
+	return s.address
+}
+
+// NewBondingManagerRef creates a new SynapseCCTPRef contract with a contract ref.
+func NewSynapseCCTPRef(address common.Address, backend bind.ContractBackend) (*BondingManagerRef, error) {
+	cctpContract, err := NewSynapseCCTP(address, backend)
+	if err != nil {
+		return nil, err
+	}
+
+	return &BondingManagerRef{
+		SynapseCCTP: cctpContract,
+		address:     address,
+	}, nil
+}
+
+var _ vm.ContractRef = SynapseCCTPRef{}
