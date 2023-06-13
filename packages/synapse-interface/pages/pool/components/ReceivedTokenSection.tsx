e804c6270fc58f5a350c4d333cd2ccd1e7d78376
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-06-13 17:28:37 +0100

    bug: fix one token withdrawal, adjust SDK to not rely on poolTokens call (#1011)

--- packages/synapse-interface/pages/pool/components/ReceivedTokenSection.tsx
+++ packages/synapse-interface/pages/pool/components/ReceivedTokenSection.tsx
@@ -11,17 +11,25 @@ const ReceivedTokenSection = ({
   poolTokens: Token[]
   chainId: number
 }) => {
+  // Cannot handle nativeTokens currently without state reworking to make accessible the pools object
+  // const tokensArray = poolTokens ? poolTokens : pool.nativeTokens;  // Using the appropriate tokens array
+  const tokensArray = poolTokens
+  // If the "ALL" key exists, use its array. Otherwise, convert the outputs object into an array
+  const outputsArray = withdrawQuote.outputs.ALL ? withdrawQuote.outputs.ALL : Object.values(withdrawQuote.outputs);
+
   return (
     <div className="text-center sm:text-left">
       <p className="text-sm font-medium bg-opacity-70 pb-0.5 text-secondaryTextColor text-opacity-50 mb-2">
         You will receive
       </p>
-      {poolTokens &&
-        poolTokens
-          .filter((token) => withdrawQuote.outputs[token.addresses[chainId]])
-          .map((token) => {
+      {tokensArray &&
+        outputsArray
+          .filter((output) => tokensArray[output.index])  // Using the output's index to filter the tokens
+          .map((output) => {
+            const token = tokensArray[output.index];  // Using the output's index to fetch the corresponding token
             return (
-              <div className="flex items-center">
+              // Added a key for mapped elements
+              <div className="flex items-center" key={token.addresses[chainId]}>
                 <img
                   alt=""
                   className="inline-block -mt-0.5 w-4 mr-1"
@@ -29,7 +37,7 @@ const ReceivedTokenSection = ({
                 ></img>
                 <span className="text-sm text-white">
                   {formatBNToString(
-                    withdrawQuote.outputs[token.addresses[chainId]].value,
+                    output.value,  // Adjusted to use output.value directly
                     token.decimals[chainId],
                     6
                   )}
