6ca92a336c924a7b0e7a613bfcae37abba365013
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-06-14 18:25:03 +0100

    Add verification scripts

---
+++ packages/contracts-core/script/verify-syn-chain.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This script verifies Messaging Contracts on a given chain
+# Usage: ./script/sh/verify-contract.sh <chainName>
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+CHAIN_NAME=$1
+
+if [ -z "$CHAIN_NAME" ]; then
+  echo -e "${RED}Error: Please provide a chain name as the first argument.${NC}"
+  exit 1
+fi
+
+./script/sh/verify-contract.sh $CHAIN_NAME "BondingManager"
+./script/sh/verify-contract.sh $CHAIN_NAME "Destination"
+./script/sh/verify-contract.sh $CHAIN_NAME "GasOracle"
+./script/sh/verify-contract.sh $CHAIN_NAME "Inbox"
+./script/sh/verify-contract.sh $CHAIN_NAME "Origin"
+./script/sh/verify-contract.sh $CHAIN_NAME "Summit"
