0b17d0e00943b77dea94ea187d64d31bb4d97932
joecroninallen <486323+joecroninallen@users.noreply.github.com>
2023-06-20 12:29:51 -0400

    Making notary add itself to its remote chain (#1052)

--- agents/domains/evm/lightmanager.go
+++ agents/domains/evm/lightmanager.go
@@ -44,6 +44,23 @@ type lightManagerContract struct {
 }
 
 //nolint:dupl
+func (a lightManagerContract) transactOptsSetup(ctx context.Context, signer signer.Signer) (*bind.TransactOpts, error) {
+	transactor, err := signer.GetTransactor(ctx, a.client.GetBigChainID())
+	if err != nil {
+		return nil, fmt.Errorf("could not sign tx: %w", err)
+	}
+
+	transactOpts, err := a.nonceManager.NewKeyedTransactor(transactor)
+	if err != nil {
+		return nil, fmt.Errorf("could not create tx: %w", err)
+	}
+
+	transactOpts.Context = ctx
+
+	return transactOpts, nil
+}
+
+//nolint:dupl
 func (a lightManagerContract) GetAgentStatus(ctx context.Context, bondedAgentSigner signer.Signer) (types.AgentStatus, error) {
 	rawStatus, err := a.contract.AgentStatus(&bind.CallOpts{Context: ctx}, bondedAgentSigner.Address())
 	if err != nil {
@@ -53,4 +70,38 @@ func (a lightManagerContract) GetAgentStatus(ctx context.Context, bondedAgentSig
 	agentStatus := types.NewAgentStatus(rawStatus.Flag, rawStatus.Domain, rawStatus.Index)
 
 	return agentStatus, nil
+}
+
+//nolint:dupl
+func (a lightManagerContract) GetAgentRoot(ctx context.Context) ([32]byte, error) {
+	agentRoot, err := a.contract.AgentRoot(&bind.CallOpts{Context: ctx})
+	if err != nil {
+		return [32]byte{}, fmt.Errorf("could not retrieve agent root: %w", err)
+	}
+
+	return agentRoot, nil
+}
+
+func (a lightManagerContract) UpdateAgentStatus(
+	ctx context.Context,
+	unbondedSigner signer.Signer,
+	bondedSigner signer.Signer,
+	agentStatus types.AgentStatus,
+	agentProof [][32]byte) error {
+	transactOpts, err := a.transactOptsSetup(ctx, unbondedSigner)
+	if err != nil {
+		return fmt.Errorf("could not setup transact opts: %w", err)
+	}
+
+	lightManagerAgentStatus := lightmanager.AgentStatus{
+		Flag:   agentStatus.Flag(),
+		Domain: agentStatus.Domain(),
+		Index:  agentStatus.Index(),
+	}
+	_, err = a.contract.UpdateAgentStatus(transactOpts, bondedSigner.Address(), lightManagerAgentStatus, agentProof)
+	if err != nil {
+		return fmt.Errorf("could not submit attestation: %w", err)
+	}
+
+	return nil
 }
