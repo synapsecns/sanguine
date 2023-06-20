9acde79ab9bc31ba6e1e113a05b172035119cc38
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-06-19 17:00:16 -0700

    Init useTokenBalances and useTokenApprovals hooks to use within portfolio hook

--- packages/synapse-interface/utils/hooks/usePortfolioBalances.tsx
+++ packages/synapse-interface/utils/hooks/usePortfolioBalances.tsx
@@ -1,3 +1,12 @@
-export const usePortfolioBalances = () => {
-  return
-}
+import { useAccount } from 'wagmi'
+import { Address, multicall } from '@wagmi/core'
+import { BRIDGABLE_TOKENS } from '@/constants/tokens'
+
+//move to constants file later
+const MULTICALL3_ADDRESS: Address = '0xcA11bde05977b3631167028862bE2a173976CA11'
+
+export const usePortfolioBalances = () => {}
+
+const useTokenBalances = () => {}
+
+const useTokenApprovals = () => {}
