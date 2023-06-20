41e5a5d5b8db91037a7eb99d293b605eb74e44c8
protojoe <486323+joecroninallen@users.noreply.github.com>
2023-06-20 01:19:29 -0400

    fixing lint errors, still have error in notary

--- agents/types/agent_flag_type.go
+++ agents/types/agent_flag_type.go
@@ -6,10 +6,16 @@ package types
 type AgentFlagType uint8
 
 const (
+	// AgentFlagUnknown means agent is not part of agent set.
 	AgentFlagUnknown AgentFlagType = iota
+	// AgentFlagActive means agent is active.
 	AgentFlagActive
+	// AgentFlagUnstaking means agent is unstaking.
 	AgentFlagUnstaking
+	// AgentFlagResting means agent has staked but not currently doing any work.
 	AgentFlagResting
+	// AgentFlagFraudulent means agent has been accused of fraud and is awaiting fraud resolution.
 	AgentFlagFraudulent
+	// AgentFlagSlashed means that the agent has been slashed and will not be a part of agent set in the next epoch.
 	AgentFlagSlashed
 )
