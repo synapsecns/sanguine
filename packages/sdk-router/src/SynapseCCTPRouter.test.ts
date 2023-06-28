a43f23b8ae2d832317f64e9ffa618f6847640cd0
Max Planck <maxplanck.crypto@gmail.com>
2023-06-28 04:58:03 +0100

    more fixes

--- packages/sdk-router/src/SynapseCCTPRouter.test.ts
+++ packages/sdk-router/src/SynapseCCTPRouter.test.ts
@@ -31,7 +31,9 @@ describe('SynapseCCTPRouter', () => {
           new SynapseCCTPRouter(Number(chainId), provider).routerContract
             .address
         ).toEqual(
-          CCTP_ROUTER_ADDRESS[parseInt(chainId) as keyof typeof CCTP_ROUTER_ADDRESS]
+          CCTP_ROUTER_ADDRESS[
+            parseInt(chainId) as keyof typeof CCTP_ROUTER_ADDRESS
+          ]
         )
       }
     })
