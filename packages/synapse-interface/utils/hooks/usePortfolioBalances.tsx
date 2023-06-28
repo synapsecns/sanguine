e523819b60a98871d340aad470a0a3b81621b453
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-06-28 11:36:18 -0700

    test

--- packages/synapse-interface/utils/hooks/usePortfolioBalances.tsx
+++ packages/synapse-interface/utils/hooks/usePortfolioBalances.tsx
@@ -6,6 +6,7 @@ import { Token } from '../types'
 import { AddressZero } from '@ethersproject/constants'
 import multicallABI from '@/constants/abis/multicall.json'
 import { getSortedBridgableTokens } from '../actions/getSortedBridgableTokens'
+import { ChainId } from '@/constants/chains'
 
 //move to constants file later
 const MULTICALL3_ADDRESS: Address = '0xcA11bde05977b3631167028862bE2a173976CA11'
@@ -13,11 +14,13 @@ const MULTICALL3_ADDRESS: Address = '0xcA11bde05977b3631167028862bE2a173976CA11'
 export const usePortfolioBalances = () => {
   const availableChains = Object.keys(BRIDGABLE_TOKENS)
 
-  const foo = getSortedBridgableTokens(1)
-  // const bar = getSortedBridgableTokens(42161)
+  const test1 = getSortedBridgableTokens(ChainId.ARBITRUM)
+  const test2 = getSortedBridgableTokens(ChainId.ETH)
+  const test3 = getSortedBridgableTokens(ChainId.AVALANCHE)
 
-  console.log('foo: ', foo)
-  // console.log('bar: ', bar)
+  console.log('test1: ', test1)
+  console.log('test2: ', test2)
+  console.log('test3: ', test3)
 }
 
 const useTokenApprovals = () => {}
