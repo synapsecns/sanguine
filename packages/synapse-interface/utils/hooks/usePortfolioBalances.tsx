5c461a77483d6424d8f3a70feb5d21a81966d7dd
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-06-28 14:35:31 -0700

    Add static typing

--- packages/synapse-interface/utils/hooks/usePortfolioBalances.tsx
+++ packages/synapse-interface/utils/hooks/usePortfolioBalances.tsx
@@ -3,22 +3,14 @@ import { useAccount } from 'wagmi'
 import { Address, multicall, erc20ABI, getAccount } from '@wagmi/core'
 import { BRIDGABLE_TOKENS } from '@/constants/tokens'
 import { Token } from '../types'
-import { AddressZero } from '@ethersproject/constants'
-import multicallABI from '@/constants/abis/multicall.json'
-import { getSortedBridgableTokens } from '../actions/getSortedBridgableTokens'
-import { ChainId } from '@/constants/chains'
 import { sortByTokenBalance } from '../sortTokens'
-import { fetchBalance } from '@wagmi/core'
 import { BigNumber } from 'ethers'
 
-//move to constants file later
-const MULTICALL3_ADDRESS: Address = '0xcA11bde05977b3631167028862bE2a173976CA11'
-
-export const getTokensByChainId = async (chainId: number) => {
+export const getTokensByChainId = async (
+  chainId: number
+): Promise<TokenBalance[]> => {
   const { address } = getAccount()
-
   const tokens = BRIDGABLE_TOKENS[chainId]
-
   return await sortByTokenBalance(tokens, chainId, address)
 }
 
