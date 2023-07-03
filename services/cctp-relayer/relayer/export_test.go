d35f926b347398e2eb9d1c24e9d22ef4967e88d8
dwasse <wassermandaniel8@gmail.com>
2023-07-03 05:58:54 -0400

    CCTP relayer listens for `CircleRequestFulfilled` events (#1091)
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>

--- services/cctp-relayer/relayer/export_test.go
+++ services/cctp-relayer/relayer/export_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/synapsecns/sanguine/services/cctp-relayer/contracts/cctp"
 	relayTypes "github.com/synapsecns/sanguine/services/cctp-relayer/types"
 	omniClient "github.com/synapsecns/sanguine/services/omnirpc/client"
 )
@@ -17,6 +18,11 @@ func (c *CCTPRelayer) HandleLog(parentCtx context.Context, log *types.Log, origi
 // HandleCircleRequestSent wraps fetchAndStoreCircleRequestSent for testing.
 func (c *CCTPRelayer) HandleCircleRequestSent(parentCtx context.Context, txhash common.Hash, originChain uint32) (msg *relayTypes.Message, err error) {
 	return c.fetchAndStoreCircleRequestSent(parentCtx, txhash, originChain)
+}
+
+// StoreCircleRequestFulfilled wraps storeCircleRequestFulfilled for testing.
+func (c *CCTPRelayer) StoreCircleRequestFulfilled(parentCtx context.Context, log *types.Log, event *cctp.SynapseCCTPEventsCircleRequestFulfilled, chainID uint32) (err error) {
+	return c.storeCircleRequestFulfilled(parentCtx, log, event, chainID)
 }
 
 // FetchAttestation wraps fetchAttestation for testing.
