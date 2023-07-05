37aa26d63e3fb5ee87461727630a6688a5cb81fb
Daniel Wasserman <wassermandaniel8@gmail.com>
2023-07-05 13:31:14 -0500

    Feat: add request.go with cctp EventLog impl

--- services/explorer/types/cctp/event.go
+++ services/explorer/types/cctp/event.go
@@ -21,27 +21,27 @@ type EventLog interface {
 	// GetDestinationChainID returns the chain id of the CCTP transfer.
 	GetDestinationChainID() *big.Int
 	// GetSender returns the sender of the CCTP transfer.
-	GetSender() common.Address
+	GetSender() *common.Address
 	// GetNonce returns the nonce of the CCTP transfer.
-	GetNonce() uint64
+	GetNonce() *uint64
 	// GetBurnToken returns the burn token of the CCTP transfer.
-	GetBurnToken() common.Address
+	GetBurnToken() *common.Address
 	// GetMintToken returns the mint token of the CCTP transfer.
-	GetMintToken() common.Address
+	GetMintToken() *common.Address
 	// GetSentAmount returns the sent amount of the CCTP transfer.
 	GetSentAmount() *big.Int
 	// GetReceivedAmount returns the received amount of the CCTP transfer.
 	GetReceivedAmount() *big.Int
 	// GetRequestVersion returns the request version of the CCTP transfer.
-	GetRequestVersion() uint32
+	GetRequestVersion() *uint32
 	// GetFormattedRequest returns the formatted request of the CCTP transfer.
 	GetFormattedRequest() []byte
 	// GetRequestID returns the request id of the CCTP transfer.
 	GetRequestID() [32]byte
 	// GetRecipient returns the receipient of the CCTP transfer.
-	GetRecipient() common.Address
+	GetRecipient() *common.Address
 	// GetFee returns the fee of the CCTP transfer.
 	GetFee() *big.Int
 	// GetToken returns the address of the received token.
-	GetToken() common.Address
+	GetToken() *common.Address
 }
