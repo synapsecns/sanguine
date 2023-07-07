106e84e515b95f093e50c41c78bb298414888d79
Trajan0x <trajan0x@users.noreply.github.com>
2023-07-07 17:55:05 +0100

    backup

--- ethergo/sdks/arbitrum/sdk.go
+++ ethergo/sdks/arbitrum/sdk.go
@@ -1,15 +1,19 @@
 package arbitrum
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/synapsecns/sanguine/core"
 	"github.com/synapsecns/sanguine/ethergo/sdks/arbitrum/contracts/arbgasinfo"
 	"github.com/synapsecns/sanguine/ethergo/sdks/arbitrum/contracts/nodeinterface"
 )
 
 // SDK is an interface for interacting with the Arbitrum SDK.
 type SDK interface {
-	// TODO:
+	EstimateGas(ctx context.Context, call ethereum.CallMsg) (gas uint64, err error)
 }
 
 type arbitrumSDKImpl struct {
@@ -40,4 +44,29 @@ func NewArbitrumSDK(client bind.ContractBackend, options ...Option) (SDK, error)
 		nodeInterface: nodeInterface,
 		gasInfo:       gasInfo,
 	}, nil
+}
+
+func (a *arbitrumSDKImpl) EstimateGas(ctx context.Context, call ethereum.CallMsg) (uint64, error) {
+	if call.To == nil {
+		return 0, errors.New("call.To cannot be nil")
+	}
+	// TODO: maybe need to copy the logic that sets the gasprice if it's empty?
+
+	a.nodeInterface.GetGasEstimateComponents(&bind.TransactOpts{
+		From: call.From,
+		// note: this is ignored
+		GasLimit: call.Gas,
+		GasPrice: core.CopyBigInt(call.GasPrice),
+		GasFeeCap: core.CopyBigInt(call.GasFeeCap),
+		GasTipCap: core.CopyBigInt(call.GasTipCap),
+		Value: co
+	}, *call.To, false, call.Data)
+}
+
+// this is a type assertion used to make sure the arbitrum sdk matches the standard contracttransactor interface
+// methods for gas without doing the whole things
+// will fail to compile if methods are different
+type unfiormMethodAssertion interface {
+	bind.ContractTransactor
+	SDK
 }
