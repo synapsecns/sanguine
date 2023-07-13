f82f3af3cc359aa69eb5ed71210d1b91658c4b58
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-13 15:38:51 -0700

    WarningMessage component

--- packages/synapse-interface/components/Warning.tsx
+++ packages/synapse-interface/components/Warning.tsx
@@ -1,4 +1,5 @@
-import { Token } from '@/utils/types'
+import { Chain, Token } from '@/utils/types'
+import { CHAINS_BY_ID } from '@/constants/chains'
 
 interface WarningProps {
   originChainId: number
@@ -13,5 +14,35 @@ export const Warning = ({
   originToken,
   destinationToken,
 }: WarningProps) => {
-  return <></>
+  const originChain: Chain = CHAINS_BY_ID[originChainId]
+  const destinationChain: Chain = CHAINS_BY_ID[destinationChainId]
+
+  const { name: originChainName } = originChain
+  const { name: destinationChainName } = destinationChain
+
+  if (originChainName === 'Ethereum' && destinationChainName === 'Avalanche') {
+    return (
+      <WarningMessage
+        header="USD Coin from Ethereum to Avalanche is not yet available."
+        message="Synapse is currently opening liquidity routes for the new USDC to USDC cross-chain transfer protocol.
+        Follow Twitter or Discord for updates when this route becomes available."
+      />
+    )
+  }
+  return <>{originChainName}</>
+}
+
+const WarningMessage = ({
+  header,
+  message,
+}: {
+  header: string
+  message: string
+}) => {
+  return (
+    <div className="bg-[#353038] text-white text-m p-3">
+      <p>{header}</p>
+      <p>{message}</p>
+    </div>
+  )
 }
