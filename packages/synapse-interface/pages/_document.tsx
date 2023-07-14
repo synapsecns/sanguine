a0241f7c717eb3ed3e39fc7741d2901e886a56e1
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-14 09:31:26 -0700

    Title

--- packages/synapse-interface/pages/_document.tsx
+++ packages/synapse-interface/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="en">
-      <Head title="Synapse Protocol" />
+      <Head>
+        <title>Synapse Protocol</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
