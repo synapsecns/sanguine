b54cd3fac345720747826ff05b6b2fe927d085d7
Simon <ak.simonm@gmail.com>
2023-07-18 16:14:14 -0400

    export

---
+++ services/scribe/scribe/export_test.go
@@ -0,0 +1,10 @@
+package scribe
+
+import (
+	"github.com/synapsecns/sanguine/services/scribe/config"
+)
+
+// GetLivefillContracts returns the array of livefill contracts for testing.
+func (c *ChainIndexer) GetLivefillContracts() []config.ContractConfig {
+	return c.livefillContracts
+}
