b37a726ec3c86fde43ced26d1c844d8e995b52f3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-18 19:41:24 -0700

    Add initial state statically typed

--- packages/synapse-interface/slices/portfolio/reducer.ts
+++ packages/synapse-interface/slices/portfolio/reducer.ts
@@ -13,29 +13,39 @@ export const fetchAndStorePortfolioBalances = createAsyncThunk(
   }
 )
 
+export enum FetchState {
+  IDLE = 'idle',
+  LOADING = 'loading',
+  VALID = 'valid',
+  INVALID = 'invalid',
+}
 export interface PortfolioState {
   balancesAndAllowances: NetworkTokenBalancesAndAllowances
+  status: FetchState
+  error?: string
+}
+
+const initialState: PortfolioState = {
+  balancesAndAllowances: {},
+  status: FetchState.IDLE,
+  error: null,
 }
 
 export const portfolioSlice = createSlice({
   name: 'portfolio',
-  initialState: {
-    balancesAndAllowances: {},
-    status: 'idle',
-    error: null,
-  },
+  initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchAndStorePortfolioBalances.pending, (state) => {
-        state.status = 'loading'
+        state.status = FetchState.LOADING
       })
       .addCase(fetchAndStorePortfolioBalances.fulfilled, (state, action) => {
-        state.status = 'succeeded'
+        state.status = FetchState.VALID
         state.balancesAndAllowances = action.payload.balancesAndAllowances
       })
       .addCase(fetchAndStorePortfolioBalances.rejected, (state, action) => {
-        state.status = 'failed'
+        state.status = FetchState.INVALID
         state.error = action.error.message
       })
   },
