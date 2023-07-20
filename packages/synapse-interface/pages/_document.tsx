0a0db915ea6afde2fc7f953c58df057e539b061f
jonah <57741810+bigboydiamonds@users.noreply.github.com>
2023-07-20 11:52:32 -0700

    Update to follow rules (#1154)
    Co-authored-by: Jonah Lin <57741810+linjonah@users.noreply.github.com>

--- packages/synapse-interface/pages/_document.tsx
+++ packages/synapse-interface/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="en">
-      <Head>
-        <title>Synapse Protocol</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
