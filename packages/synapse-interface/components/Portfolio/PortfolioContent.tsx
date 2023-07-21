78634d8940537e8e10c03fab1b562c5096bdb321
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-20 17:54:26 -0700

    ...

--- packages/synapse-interface/components/Portfolio/PortfolioContent.tsx
+++ packages/synapse-interface/components/Portfolio/PortfolioContent.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Address } from 'wagmi'
-import { NetworkTokenBalancesAndAllowances } from '@/utils/hooks/fetchPortfolioBalances'
+import { NetworkTokenBalancesAndAllowances } from '@/utils/actions/fetchPortfolioBalances'
 import {
   SingleNetworkPortfolio,
   PortfolioHeader,
