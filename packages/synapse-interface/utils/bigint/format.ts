78b2802cf47ea845de8afbb818986bc7633466eb
abtestingalpha <abtestingalpha@gmail.com>
2023-07-22 08:59:07 -0400

    Adds try catch on bigint formatters, fixes decimal issue on apy tooltip

--- packages/synapse-interface/utils/bigint/format.ts
+++ packages/synapse-interface/utils/bigint/format.ts
@@ -1,45 +1,45 @@
+import { txErrorHandler } from '../txErrorHandler'
+
 export const formatBigIntToString = (
   bi: bigint,
   nativePrecision: number,
   decimalPlaces?: number
 ) => {
-  // Check if input is zero
-  if (bi === 0n) {
-    return '0.0'
+  try {
+    // Check if input is zero
+    if (bi === 0n) {
+      return '0.0'
+    }
+
+    // Check if the input is negative
+    const isNegative = bi < 0n
+    if (isNegative) {
+      // Convert to positive for the calculation
+      bi = -bi
+    }
+    // Convert to string and add padding zeros if necessary
+    let str = bi.toString().padStart(nativePrecision, '0')
+
+    // Insert decimal point
+    const idx = str.length - nativePrecision
+    str = `${str.slice(0, idx)}.${str.slice(idx)}`
+
+    // Trim to desired number of decimal places
+    if (decimalPlaces !== undefined) {
+      const decimalIdx = str.indexOf('.')
+      str = str.slice(0, decimalIdx + decimalPlaces + 1)
+    }
+
+    // Add the negative sign back if necessary
+    if (isNegative) {
+      str = '-' + str
+    }
+
+    return str
+  } catch (error) {
+    console.log(`error`, error)
+    txErrorHandler(error)
   }
-
-  // Check if the input is negative
-  const isNegative = bi < 0n
-  if (isNegative) {
-    // Convert to positive for the calculation
-    bi = -bi
-  }
-  // Convert to string and add padding zeros if necessary
-  let str = bi.toString().padStart(nativePrecision, '0')
-
-  // Insert decimal point
-  const idx = str.length - nativePrecision
-  str = `${str.slice(0, idx)}.${str.slice(idx)}`
-
-  // Trim to desired number of decimal places
-  if (decimalPlaces !== undefined) {
-    const decimalIdx = str.indexOf('.')
-    str = str.slice(0, decimalIdx + decimalPlaces + 1)
-  }
-
-  // Add the negative sign back if necessary
-  if (isNegative) {
-    str = '-' + str
-  }
-
-  return str
-}
-
-export const bigIntToFixed = (bigIntValue, numDecimals = 2) => {
-  const asString = bigIntValue.toString()
-  const truncatedString = asString.slice(0, numDecimals)
-
-  return `${truncatedString}.${asString.slice(numDecimals)}`
 }
 
 export const powBigInt = (base, exponent) => {
@@ -55,21 +55,22 @@ export const formatBigIntToPercentString = (
   nativePrecison: number,
   decimalPlaces = 2
 ) => {
-  // Calculate the conversion factor based on the native precision and required decimal places
-  const conversionFactor = powBigInt(
-    10n,
-    BigInt(nativePrecison - 2 + decimalPlaces)
-  )
+  try {
+    // Calculate the conversion factor based on the native precision and required decimal places
+    const conversionFactor = powBigInt(
+      10n,
+      BigInt(nativePrecison - 2 + decimalPlaces)
+    )
 
-  // Convert the bigint to a floating-point number, preserving the requested number of decimal places
-  const num = Number(bn) / Number(conversionFactor)
+    // Convert the bigint to a floating-point number, preserving the requested number of decimal places
+    const num = Number(bn) / Number(conversionFactor)
 
-  // Format the number as a percentage string
-  return `${num.toFixed(decimalPlaces)}%`
-}
-
-export const fixNumberToPercentageString = (num, numDecimals = 2) => {
-  return `${num?.toFixed(numDecimals)}%`
+    // Format the number as a percentage string
+    return `${num.toFixed(decimalPlaces)}%`
+  } catch (error) {
+    console.log(`error`, error)
+    txErrorHandler(error)
+  }
 }
 
 // Some environments have issues with RegEx that contain back-tracking, so we cannot
@@ -140,27 +141,32 @@ export const commifyBigIntWithDefault = (big: bigint, decimals: number) => {
 }
 
 export const stringToBigInt = (rawVal: string, rawDecimals: number) => {
-  let value = rawVal.replace(/[$,]/g, '')
-  if (['.', '0.', '', '.0'].includes(value)) {
-    return 0n
+  try {
+    let value = rawVal.replace(/[$,]/g, '')
+    if (['.', '0.', '', '.0'].includes(value)) {
+      return 0n
+    }
+    if (value.startsWith('.')) {
+      value = `0${value}`
+    }
+
+    // Scale the decimal number up by the appropriate number of decimals.
+    const scaleFactor = BigInt(10 ** rawDecimals)
+
+    // Split the input into whole and fractional parts.
+    const [wholePart, fractionalPart = '0'] = value.split('.')
+
+    // Convert the whole part directly to a BigInt.
+    const wholeBigInt = BigInt(wholePart) * scaleFactor
+
+    // For the fractional part, first scale it up to the right size, then trim any excess decimal places.
+    const fractionalBigInt = BigInt(
+      fractionalPart.padEnd(rawDecimals, '0').slice(0, rawDecimals)
+    )
+
+    return wholeBigInt + fractionalBigInt
+  } catch (error) {
+    console.log(error)
+    txErrorHandler(error)
   }
-  if (value.startsWith('.')) {
-    value = `0${value}`
-  }
-
-  // Scale the decimal number up by the appropriate number of decimals.
-  const scaleFactor = BigInt(10 ** rawDecimals)
-
-  // Split the input into whole and fractional parts.
-  const [wholePart, fractionalPart = '0'] = value.split('.')
-
-  // Convert the whole part directly to a BigInt.
-  const wholeBigInt = BigInt(wholePart) * scaleFactor
-
-  // For the fractional part, first scale it up to the right size, then trim any excess decimal places.
-  const fractionalBigInt = BigInt(
-    fractionalPart.padEnd(rawDecimals, '0').slice(0, rawDecimals)
-  )
-
-  return wholeBigInt + fractionalBigInt
 }
