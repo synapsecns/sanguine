aeaa448a994526f7324e253750fad0fb85d5b174
Simon <ak.simonm@gmail.com>
2023-07-25 03:44:17 -0400

    lint

--- core/metrics/noopexporter_test.go
+++ core/metrics/noopexporter_test.go
@@ -1,21 +1,24 @@
 package metrics_test
 
 import (
+	"context"
 	. "github.com/stretchr/testify/assert"
 	"github.com/synapsecns/sanguine/core/metrics"
 	"testing"
 )
 
 func TestNoopExporter(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	exporter := metrics.NewNoOpExporter()
 	temporality := exporter.Temporality(1)
 	NotNil(t, temporality)
 	aggregation := exporter.Aggregation(1)
 	NotNil(t, aggregation)
-	export := exporter.Export(nil, nil)
+	export := exporter.Export(ctx, nil)
 	Nil(t, export)
-	flush := exporter.ForceFlush(nil)
+	flush := exporter.ForceFlush(ctx)
 	Nil(t, flush)
-	shutdown := exporter.Shutdown(nil)
+	shutdown := exporter.Shutdown(ctx)
 	Nil(t, shutdown)
 }
