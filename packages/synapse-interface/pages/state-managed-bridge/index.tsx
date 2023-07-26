2580b98fd13bd28696d19a80270075bd316395e9
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-26 11:18:44 -0700

    ...

--- packages/synapse-interface/pages/state-managed-bridge/index.tsx
+++ packages/synapse-interface/pages/state-managed-bridge/index.tsx
@@ -100,10 +100,8 @@ import {
   usePortfolioBalances,
   useFetchPortfolioBalances,
 } from '@/slices/portfolio/hooks'
-import {
-  updateSingleTokenAllowance,
-  FetchState,
-} from '@/slices/portfolio/actions'
+import { FetchState } from '@/slices/portfolio/actions'
+import { updateSingleTokenAllowance } from '@/slices/portfolio/actions'
 
 // NOTE: These are idle utility functions that will be re-written to
 // support sorting by desired mechanism
@@ -170,6 +168,7 @@ const StateManagedBridge = () => {
 
   const dispatch = useAppDispatch()
 
+  const fromTokens = BRIDGABLE_TOKENS[fromChainId]
   const fromChainIds = Object.keys(CHAINS_BY_ID).map((id) => Number(id))
   const toChainIds = Object.keys(CHAINS_BY_ID).map((id) => Number(id))
 
@@ -599,9 +598,11 @@ const StateManagedBridge = () => {
                 <TokenSlideOver
                   key="fromBlock"
                   isOrigin={true}
-                  tokens={separateAndSortTokensWithBalances(
-                    supportedFromTokens
-                  )}
+                  tokens={
+                    portfolioStatus === FetchState.VALID
+                      ? separateAndSortTokensWithBalances(supportedFromTokens)
+                      : sortByVisibilityRank(fromTokens)
+                  }
                   chainId={fromChainId}
                   selectedToken={fromToken}
                 />{' '}
