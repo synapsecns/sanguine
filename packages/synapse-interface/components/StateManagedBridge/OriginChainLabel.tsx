09714e4da5b510aa4351f7a6fabbfac215db9a3c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-27 12:25:48 -0700

    style

--- packages/synapse-interface/components/StateManagedBridge/OriginChainLabel.tsx
+++ packages/synapse-interface/components/StateManagedBridge/OriginChainLabel.tsx
@@ -54,7 +54,7 @@ export const OriginChainLabel = ({
           }}
           tabIndex={0}
           data-test-id="bridge-origin-chain-list-button"
-          className="w-8 h-8 px-1.5 py-1.5 bg-[#C4C4C4] bg-opacity-10 rounded-full hover:cursor-pointer group"
+          className="w-8 h-8 px-1.5 py-1.5 ml-3 bg-[#C4C4C4] bg-opacity-10 rounded-full hover:cursor-pointer group"
         >
           <ChevronDownIcon className="text-gray-300 transition transform-gpu group-hover:opacity-50 group-active:rotate-180" />
         </button>
