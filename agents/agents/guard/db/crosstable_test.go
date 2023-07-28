7efee302f7abe832e5844f88e9810a54d1c604fc
Max Planck <maxplanck.crypto@gmail.com>
2023-07-28 16:00:24 -0400

    wip: crosstable

--- agents/agents/guard/db/crosstable_test.go
+++ agents/agents/guard/db/crosstable_test.go
@@ -7,25 +7,71 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	. "github.com/stretchr/testify/assert"
 	"github.com/synapsecns/sanguine/agents/agents/guard/db"
+	"github.com/synapsecns/sanguine/agents/types"
 )
 
 func (t *DBSuite) TestGetUpdateAgentStatusParameters() {
 	t.RunOnAllDBs(func(testDB db.GuardDB) {
+		guardAddress := common.BigToAddress(big.NewInt(gofakeit.Int64()))
+
 		addressA := common.BigToAddress(big.NewInt(gofakeit.Int64()))
 		addressB := common.BigToAddress(big.NewInt(gofakeit.Int64()))
 		addressC := common.BigToAddress(big.NewInt(gofakeit.Int64()))
 
 		agentRootA := common.BigToHash(big.NewInt(gofakeit.Int64()))
 		agentRootB := common.BigToHash(big.NewInt(gofakeit.Int64()))
+		agentRootC := common.BigToHash(big.NewInt(gofakeit.Int64()))
 
-		// Insert two rows into the `AgentTree` table.
+		// Insert three rows into the `AgentTree` table.
 		err := testDB.StoreAgentTree(
 			t.GetTestContext(),
 			agentRootA,
 			addressA,
 			gofakeit.Uint64(),
-			[][32]byte{[32]byte{gofakeit.Uint8()}},
+			[][32]byte{{gofakeit.Uint8()}},
 		)
-		Nil()
+		Nil(t.T(), err)
+		err = testDB.StoreAgentTree(
+			t.GetTestContext(),
+			agentRootB,
+			addressB,
+			gofakeit.Uint64(),
+			[][32]byte{{gofakeit.Uint8()}},
+		)
+		Nil(t.T(), err)
+		err = testDB.StoreAgentTree(
+			t.GetTestContext(),
+			agentRootC,
+			addressC,
+			gofakeit.Uint64(),
+			[][32]byte{{gofakeit.Uint8()}},
+		)
+		Nil(t.T(), err)
+
+		// Insert two rows into `Dispute`, one will have matching agent address to a `AgentTree` row and with status `Resolved`.
+		err = testDB.StoreDispute(
+			t.GetTestContext(),
+			big.NewInt(gofakeit.Int64()),
+			types.Resolved,
+			guardAddress,
+			gofakeit.Uint32(),
+			addressA,
+		)
+		Nil(t.T(), err)
+		err = testDB.StoreDispute(
+			t.GetTestContext(),
+			big.NewInt(gofakeit.Int64()),
+			types.Opened,
+			guardAddress,
+			gofakeit.Uint32(),
+			addressB,
+		)
+		Nil(t.T(), err)
+
+		// Get the matching agent tree from the database.
+		agentTrees, err := testDB.GetUpdateAgentStatusParameters(t.GetTestContext())
+		Nil(t.T(), err)
+
+		Equal(t.T(), 1, len(agentTrees))
 	})
 }
