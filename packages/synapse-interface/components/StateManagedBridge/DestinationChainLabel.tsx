8858417f13232f845834597756cc9f233c191381
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-07-28 11:59:54 -0700

    Update Destination side

--- packages/synapse-interface/components/StateManagedBridge/DestinationChainLabel.tsx
+++ packages/synapse-interface/components/StateManagedBridge/DestinationChainLabel.tsx
@@ -1,4 +1,4 @@
-import { createRef } from 'react'
+import { createRef, useRef } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/outline'
 import { CHAINS_BY_ID } from '@constants/chains'
 import { getNetworkButtonBorder } from '@/styles/chains'
@@ -11,6 +11,7 @@ import { setShowToChainSlideOver } from '@/slices/bridgeDisplaySlice'
 import { getOrderedChains } from '@/utils/getOrderedChains'
 import { RootState } from '@/store/store'
 import { segmentAnalyticsEvent } from '@/contexts/SegmentAnalyticsProvider'
+import useWindowSize from '@/utils/hooks/useWindowSize'
 
 const scrollableRef = createRef<HTMLDivElement>()
 
@@ -23,7 +24,12 @@ export const DestinationChainLabel = ({
   chainId: number
   connectedChainId: number
 }) => {
+  const leftRef = useRef<HTMLDivElement>(null)
+  const rightRef = useRef<HTMLDivElement>(null)
   const [orderedChains, setOrderedChains] = useState<number[]>([])
+  const [chainListWidth, setChainListWidth] = useState<number>(0)
+  const [width, height] = useWindowSize()
+
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -36,18 +42,35 @@ export const DestinationChainLabel = ({
     }
   }
 
+  useEffect(() => {
+    if (width && leftRef.current && rightRef.current) {
+      const distance = getDistanceBetweenElements(
+        leftRef.current,
+        rightRef.current
+      )
+      setChainListWidth(distance)
+    }
+  }, [width])
+
   return (
     <div
       data-test-id="destination-chain-label"
       className="flex items-center justify-between"
     >
-      <div className={`text-gray-400 block text-sm mr-2`}>Dest.</div>
+      <div ref={leftRef} className={`text-gray-400 block text-sm mr-2`}>
+        Dest.
+      </div>
       <div className="relative flex ml-auto">
-        <div className="flex items-center relative overflow-x-auto overflow-y-hidden w-[230px] min-[475px]:w-full scrollbar-hide">
+        <div
+          className="relative flex items-center overflow-x-auto overflow-y-hidden scrollbar-hide"
+          style={{
+            maxWidth: chainListWidth ? `${chainListWidth - 30}px` : 'full',
+          }}
+        >
           <div className="hidden sticky min-w-[15px] h-full left-[-3px] bg-gradient-to-l from-transparent to-bgLight max-[475px]:block">
             &nbsp;
           </div>
-          <div className="flex items-center last:ml-0">
+          <div className="flex items-center [&>*:last-child]:mr-0">
             {orderedChains.map((id: number, key: number) => {
               return Number(id) === chainId ? (
                 <SelectedChain chainId={id} key={id} />
@@ -65,7 +88,7 @@ export const DestinationChainLabel = ({
           </div>
         </div>
 
-        <div className="max-[475px]:pl-1">
+        <div ref={rightRef} className="max-[475px]:pl-1">
           <button
             onClick={() => {
               dispatch(setShowToChainSlideOver(true))
@@ -168,4 +191,16 @@ const chainOrderBySwapSide = (
     chainId,
     chains.map((id) => `${id}`)
   )
+}
+
+export function getDistanceBetweenElements(
+  element1: HTMLElement,
+  element2: HTMLElement
+): number {
+  const rect1 = element1.getBoundingClientRect()
+  const rect2 = element2.getBoundingClientRect()
+
+  const distance = Math.abs(rect2.left - rect1.right)
+
+  return distance
 }
