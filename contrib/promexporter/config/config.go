6dc953417d7f9563a7d1590110f636a9cd19cb41
Trajan0x <trajan0x@users.noreply.github.com>
2023-07-28 22:44:12 +0100

    [goreleaser]

--- contrib/promexporter/config/config.go
+++ contrib/promexporter/config/config.go
@@ -18,14 +18,26 @@ type Config struct {
 	DFKUrl string `yaml:"dfk_url" default:"https://defi-kingdoms-community-api-gateway-co06z8vi.uc.gateway.dev/graphql"`
 	// DFKPending is the list of pending heroes
 	DFKPending []DFKPending `yaml:"dfk_pending"`
+	// GasChecks is the list of gas checks
+	GasChecks []GasChecks `yaml:"gas_checks"`
 }
 
-// DFkPending contains the config for the DFK pending metric
+// DFKPending contains the config for the DFK pending metric
 type DFKPending struct {
 	// Owner is the owner of the pending heroes
 	Owner string `yaml:"owner"`
 	// ChainName is the name of the chain
 	ChainName string `yaml:"chain_name"`
+}
+
+// GasChecks contains the config for the gas checks
+type GasChecks struct {
+	// ChainID is the chain id
+	ChainIDs []int `yaml:"chain_ids"`
+	// Address is the address of the contract
+	Address string `yaml:"address"`
+	// Name of the address entity
+	Name string `yaml:"name"`
 }
 
 func DecodeConfig(filePath string) (_ Config, err error) {
@@ -49,6 +61,18 @@ func DecodeConfig(filePath string) (_ Config, err error) {
 		{
 			Owner:     "0xEE258eF5F4338B37E9BA9dE6a56382AdB32056E2",
 			ChainName: "klatyn",
+		},
+	}
+
+	// note: when you want to add bridges, you can use the router to look up bridge addresses
+	// and get the gas limit from there
+	cfg.GasChecks = []GasChecks{
+		{
+			Address: "0x230a1ac45690b9ae1176389434610b9526d2f21b",
+			ChainIDs: []int{
+				1, 10, 25, 56, 137, 250, 288, 1088, 1284, 1285, 2000, 7700, 8217, 42161, 43115, 53935, 1313161554, 1666600000,
+			},
+			Name: "validators",
 		},
 	}
 
