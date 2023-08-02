3d7fd06b273c958845d5a44168052e48b601626d
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-08-02 12:49:31 +0100

    Bump SDK version, remove custom base router address logic, add block count for base

--- packages/synapse-interface/actions/swapPoolCalculateAddLiquidity.ts
+++ packages/synapse-interface/actions/swapPoolCalculateAddLiquidity.ts
@@ -4,7 +4,6 @@ import { Token } from '@/utils/types'
 import { SYNAPSE_ROUTER_ABI } from '@/constants/abis/synapseRouter'
 
 const ROUTER_ADDRESS = '0x7E7A0e201FD38d3ADAA9523Da6C109a07118C96a'
-const BASE_ROUTER_ADDRESS = '0x6C8c6E68604e78B549C96907bfe9EBdaaC04e3B3'
 
 export const swapPoolCalculateAddLiquidity = async ({
   chainId,
@@ -15,16 +14,6 @@ export const swapPoolCalculateAddLiquidity = async ({
   pool: Token
   inputs: bigint[]
 }) => {
-  if (chainId === 8453) {
-    const amount = await readContract({
-      chainId,
-      address: BASE_ROUTER_ADDRESS,
-      abi: SYNAPSE_ROUTER_ABI,
-      functionName: 'calculateAddLiquidity',
-      args: [pool.swapAddresses[chainId], inputs],
-    })
-    return amount
-  } else {
     const amount = await readContract({
       chainId,
       address: ROUTER_ADDRESS,
@@ -33,5 +22,4 @@ export const swapPoolCalculateAddLiquidity = async ({
       args: [pool.swapAddresses[chainId], inputs],
     })
     return amount
-  }
 }
