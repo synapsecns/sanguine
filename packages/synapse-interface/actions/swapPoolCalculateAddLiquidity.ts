0f7b2cd2883a1483516c388d776b4d091054ec60
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-08-02 01:49:10 +0100

    adjusts router address (temporarily) on base in sdk) and adds if statement to handle diff base address to frontend add liquidity calculation

--- packages/synapse-interface/actions/swapPoolCalculateAddLiquidity.ts
+++ packages/synapse-interface/actions/swapPoolCalculateAddLiquidity.ts
@@ -4,6 +4,7 @@ import { Token } from '@/utils/types'
 import { SYNAPSE_ROUTER_ABI } from '@/constants/abis/synapseRouter'
 
 const ROUTER_ADDRESS = '0x7E7A0e201FD38d3ADAA9523Da6C109a07118C96a'
+const BASE_ROUTER_ADDRESS = '0x6C8c6E68604e78B549C96907bfe9EBdaaC04e3B3'
 
 export const swapPoolCalculateAddLiquidity = async ({
   chainId,
@@ -14,13 +15,24 @@ export const swapPoolCalculateAddLiquidity = async ({
   pool: Token
   inputs: bigint[]
 }) => {
-  const amount = await readContract({
-    chainId,
-    address: ROUTER_ADDRESS,
-    abi: SYNAPSE_ROUTER_ABI,
-    functionName: 'calculateAddLiquidity',
-    args: [pool.swapAddresses[chainId], inputs],
-  })
+  if (chainId === 8453) {
+    const amount = await readContract({
+      chainId,
+      address: BASE_ROUTER_ADDRESS,
+      abi: SYNAPSE_ROUTER_ABI,
+      functionName: 'calculateAddLiquidity',
+      args: [pool.swapAddresses[chainId], inputs],
+    })
+    return amount
+  } else {
+    const amount = await readContract({
+      chainId,
+      address: ROUTER_ADDRESS,
+      abi: SYNAPSE_ROUTER_ABI,
+      functionName: 'calculateAddLiquidity',
+      args: [pool.swapAddresses[chainId], inputs],
+    })
+    return amount
+  }
 
-  return amount
 }
