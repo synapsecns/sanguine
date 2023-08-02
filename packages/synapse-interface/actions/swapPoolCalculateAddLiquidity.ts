a45ce3a9d68f148f7ef33579577c1cf7b8d88ded
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-08-02 13:25:21 +0100

    Adjust base router and add bridgewatcher base block confs (#1222)
    * Bump SDK version, remove custom base router address logic, add block count for base
    
    * lint
    
    * add klaytn est blocks

--- packages/synapse-interface/actions/swapPoolCalculateAddLiquidity.ts
+++ packages/synapse-interface/actions/swapPoolCalculateAddLiquidity.ts
@@ -4,7 +4,6 @@ import { Token } from '@/utils/types'
 import { SYNAPSE_ROUTER_ABI } from '@/constants/abis/synapseRouter'
 
 const ROUTER_ADDRESS = '0x7E7A0e201FD38d3ADAA9523Da6C109a07118C96a'
-const BASE_ROUTER_ADDRESS = '0x6C8c6E68604e78B549C96907bfe9EBdaaC04e3B3'
 
 export const swapPoolCalculateAddLiquidity = async ({
   chainId,
@@ -15,23 +14,12 @@ export const swapPoolCalculateAddLiquidity = async ({
   pool: Token
   inputs: bigint[]
 }) => {
-  if (chainId === 8453) {
-    const amount = await readContract({
-      chainId,
-      address: BASE_ROUTER_ADDRESS,
-      abi: SYNAPSE_ROUTER_ABI,
-      functionName: 'calculateAddLiquidity',
-      args: [pool.swapAddresses[chainId], inputs],
-    })
-    return amount
-  } else {
-    const amount = await readContract({
-      chainId,
-      address: ROUTER_ADDRESS,
-      abi: SYNAPSE_ROUTER_ABI,
-      functionName: 'calculateAddLiquidity',
-      args: [pool.swapAddresses[chainId], inputs],
-    })
-    return amount
-  }
+  const amount = await readContract({
+    chainId,
+    address: ROUTER_ADDRESS,
+    abi: SYNAPSE_ROUTER_ABI,
+    functionName: 'calculateAddLiquidity',
+    args: [pool.swapAddresses[chainId], inputs],
+  })
+  return amount
 }
--- packages/synapse-interface/constants/bridge.ts
+++ packages/synapse-interface/constants/bridge.ts
@@ -56,6 +56,8 @@ export const BRIDGE_REQUIRED_CONFIRMATIONS = {
   [CHAINS.METIS.id]: 33,
   [CHAINS.DOGE.id]: 33,
   [CHAINS.CANTO.id]: 20,
+  [CHAINS.BASE.id]: 750,
+  [CHAINS.KLAYTN.id]: 20,
 }
 
 export const DEFAULT_FROM_TOKEN_SYMBOL = 'USDC'
