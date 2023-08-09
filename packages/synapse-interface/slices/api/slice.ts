463ed113e60436625c8c05a84f1df4c622ec6363
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-08-08 19:07:45 -0700

    Add gql ts helper pkgs, add userHistoricalActivityQuery

--- packages/synapse-interface/slices/api/slice.ts
+++ packages/synapse-interface/slices/api/slice.ts
@@ -1,9 +1,57 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { gql } from 'graphql-request'
 
 const SCRIBE_URL: string = 'https://explorer.omnirpc.io/graphiql'
 
 export const api = createApi({
   reducerPath: 'dataApi',
   baseQuery: fetchBaseQuery({ baseUrl: SCRIBE_URL }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    userHistoricalActivityQuery: builder.query({
+      query: ({ address, startTime }) => gql`
+        query GetUserHistoricalActivity($address: String!, $startTime: Int!) {
+          bridgeTransactions(
+            pending: false
+            addressFrom: $address
+            startTime: $startTime
+            page: 1
+          ) {
+            fromInfo {
+              chainID
+              destinationChainID
+              address
+              txnHash
+              value
+              formattedValue
+              USDValue
+              tokenAddress
+              tokenSymbol
+              blockNumber
+              time
+              formattedTime
+              formattedEventType
+              eventType
+            }
+            toInfo {
+              chainID
+              destinationChainID
+              address
+              txnHash
+              value
+              formattedValue
+              USDValue
+              tokenAddress
+              tokenSymbol
+              blockNumber
+              time
+              formattedTime
+              formattedEventType
+              eventType
+            }
+            kappa
+          }
+        }
+      `,
+    }),
+  }),
 })
