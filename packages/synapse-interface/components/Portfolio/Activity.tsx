1ea248916324e4c8df2c2de8beef28fc253d0e8a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-08-09 12:14:42 -0700

    Only setting query params on mount works

--- packages/synapse-interface/components/Portfolio/Activity.tsx
+++ packages/synapse-interface/components/Portfolio/Activity.tsx
@@ -1,29 +1,37 @@
-import { useAccount } from 'wagmi'
+import { useMemo } from 'react'
+import { useAccount, Address } from 'wagmi'
 import { useGetUserHistoricalActivityQuery } from '@/slices/api/generated'
-import { getTimeMinutesFromNow } from '@/utils/time'
+import { getTimeMinutesBeforeNow } from '@/utils/time'
 
 export const Activity = () => {
   const { address } = useAccount()
-  const currentTime: number = getTimeMinutesFromNow(0)
-  const shouldSkip: boolean = !address || !currentTime
-  const {
-    data: history,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = useGetUserHistoricalActivityQuery(
-    {
-      address: address,
-      startTime: currentTime,
-    },
-    {
-      pollingInterval: 5000,
-      skip: shouldSkip,
-    }
-  )
+  const oneMonthInMinutes: number = 43200
+  const queryTime: number = getTimeMinutesBeforeNow(oneMonthInMinutes)
 
-  console.log('history:', history)
+  const currentAddress: Address = useMemo(() => address, [address])
+  const shouldSkip: boolean = !address || !queryTime
+
+  console.log('currentAddress: ', currentAddress)
+  console.log('queryTime: ', queryTime)
+  console.log('shouldSkip: ', shouldSkip)
+
+  const { data, isLoading, isSuccess, isError, error } =
+    useGetUserHistoricalActivityQuery(
+      {
+        address: currentAddress ?? '',
+        startTime: queryTime,
+        // address: '0xF080B794AbF6BB905F2330d25DF545914e6027F8',
+        // startTime: 1689015547,
+      },
+      {
+        skip: shouldSkip,
+      }
+    )
+
+  const historicalActivityData = useMemo(() => {
+    console.log('data: ', data)
+    console.log('isSuccess: ', isSuccess)
+  }, [data, shouldSkip])
 
   return <>Activity</>
 }
