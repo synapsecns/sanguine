401736d3f83e907a200b43c8f3601b437bb63e9e
abtestingalpha <abtestingalpha@gmail.com>
2023-08-14 16:24:18 -0400

    Case insensitive sorting on symbol

--- packages/synapse-interface/components/StateManagedBridge/helpers/sortByPriorityRank.ts
+++ packages/synapse-interface/components/StateManagedBridge/helpers/sortByPriorityRank.ts
@@ -3,5 +3,9 @@ import _ from 'lodash'
 import { Token } from '@/utils/types'
 
 export const sortByPriorityRank = (tokens: Token[]) => {
-  return _.orderBy(tokens, ['priorityRank', 'symbol'], ['asc', 'asc'])
+  return _.orderBy(
+    tokens,
+    [(token) => token.priorityRank, (token) => token.symbol.toLowerCase()],
+    ['asc', 'asc']
+  )
 }
