bda9e94c7eb0d6879b1b761d6b221c7b1de32ef3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-08-16 14:23:46 -0700

    Explorer link to link to wallet activity

--- packages/synapse-interface/components/Portfolio/Activity.tsx
+++ packages/synapse-interface/components/Portfolio/Activity.tsx
@@ -83,7 +83,7 @@ export const Activity = () => {
                     />
                   )
                 )}
-              <ExplorerLink />
+              <ExplorerLink connectedAddress={address} />
             </ActivitySection>
           )}
         </>
