06399230ab2022e2c7dedb888f254a13af6c18d8
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-08-22 15:40:48 -0700

    Update balances on destination chain when new txn in userHistoricalTransactions

--- packages/synapse-interface/slices/portfolio/updater.tsx
+++ packages/synapse-interface/slices/portfolio/updater.tsx
@@ -3,15 +3,22 @@ import { Address } from '@wagmi/core'
 import { watchPendingTransactions, waitForTransaction } from '@wagmi/core'
 import { useAppDispatch } from '@/store/hooks'
 import { useBridgeState } from '../bridge/hooks'
+import { useTransactionsState } from '../transactions/hooks'
+import { TransactionsState } from '../transactions/reducer'
 import { BridgeState } from '../bridge/reducer'
 import { fetchAndStoreSingleNetworkPortfolioBalances } from './hooks'
 import { useAccount } from 'wagmi'
 import { RecentBridgeTransaction } from '../bridge/actions'
+import { BridgeTransaction } from '../api/generated'
 
 export default function Updater(): null {
   const dispatch = useAppDispatch()
   const { address } = useAccount()
   const { recentBridgeTransactions }: BridgeState = useBridgeState()
+  const {
+    userHistoricalTransactions,
+    isUserHistoricalTransactionsLoading,
+  }: TransactionsState = useTransactionsState()
 
   // Update balances when transaction resolves
   useEffect(() => {
@@ -42,6 +49,23 @@ export default function Updater(): null {
       )
     })()
   }, [recentBridgeTransactions, address, dispatch])
+
+  useEffect(() => {
+    if (!address || isUserHistoricalTransactionsLoading) return
+    if (userHistoricalTransactions && userHistoricalTransactions.length === 0) {
+      const updateDestinationBalancesForLastTransaction = async () => {
+        const lastTransaction: BridgeTransaction = userHistoricalTransactions[0]
+        const destinationChainId: number = lastTransaction.toInfo?.chainID
+        dispatch(
+          fetchAndStoreSingleNetworkPortfolioBalances({
+            address: address as Address,
+            chainId: destinationChainId,
+          })
+        )
+      }
+      updateDestinationBalancesForLastTransaction()
+    }
+  }, [userHistoricalTransactions, address])
 
   return null
 }
