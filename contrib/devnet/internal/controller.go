ee81b8cdc7cc02f97e4a9a691f6761c7137960ae
Trajan0x <trajan0x@users.noreply.github.com>
2023-08-24 14:40:47 +0100

    more fixes

--- contrib/devnet/internal/controller.go
+++ contrib/devnet/internal/controller.go
@@ -8,14 +8,14 @@ import (
 	"github.com/synapsecns/sanguine/core/metrics"
 	"github.com/synapsecns/sanguine/core/retry"
 	omnirpcClient "github.com/synapsecns/sanguine/services/omnirpc/client"
+	"golang.org/x/sync/errgroup"
 	"os"
 	"os/exec"
 	"path"
 	"time"
 )
 
-// list of all chainIDs in the docker-compose.
-var chainIDs = []int{42, 43, 44}
+const build = false
 
 // Up brings the devnet up.
 func Up(ctx context.Context, metricHandler metrics.Handler) error {
@@ -29,9 +29,14 @@ func Up(ctx context.Context, metricHandler metrics.Handler) error {
 
 	log.Info("Running devnet.")
 
-	cmd := exec.Command("docker", "compose", "up", "-d")
+	args := []string{"docker", "compose", "up", "-d"}
+	if build {
+		args = append(args, "--build")
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Dir = devnetPath
-	cmd.Env = os.Environ()
+	cmd.Env = append(os.Environ(), fmt.Sprintf("PWD=%s", devnetPath))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -39,10 +44,57 @@ func Up(ctx context.Context, metricHandler metrics.Handler) error {
 		return err
 	}
 
-	retry.WithBackoff(ctx, func(ctx context.Context) error {
-		rpcClient, err := omnirpcClient.NewOmnirpcClient("http://localhost:9001", metricHandler)
-
-	}, retry.WithMaxTotalTime(20*time.Second))
+	err = checkLiveness(ctx, metricHandler)
+	if err != nil {
+		return fmt.Errorf("failed to check liveness: %w", err)
+	}
 
 	return nil
+}
+
+func checkLiveness(ctx context.Context, metricHandler metrics.Handler) error {
+	log.Info("Checking liveness.")
+
+	err := retry.WithBackoff(ctx, func(ctx context.Context) error {
+		rpcClient := omnirpcClient.NewOmnirpcClient("http://localhost:9001", metricHandler)
+
+		chainIDs, err := rpcClient.GetChainIDs(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to get chain IDs: %w", err)
+		}
+
+		g, gctx := errgroup.WithContext(ctx)
+
+		for i := range chainIDs {
+			chainID := chainIDs[i] // capture func literal
+
+			g.Go(func() error {
+				chainClient, err := rpcClient.GetChainClient(gctx, chainID)
+				if err != nil {
+					return fmt.Errorf("failed to get chain client: %w", err)
+				}
+
+				_, err = chainClient.ChainID(gctx)
+				if err != nil {
+					return fmt.Errorf("failed to connect to chain %d: %w", chainID, err)
+				}
+
+				return nil
+			})
+		}
+
+		err = g.Wait()
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, retry.WithMaxTotalTime(20*time.Second))
+
+	if err != nil {
+		return fmt.Errorf("failed to connect to chains: %w", err)
+	}
+
+	return nil
+
 }
--- contrib/devnet/internal/controller_test.go
+++ contrib/devnet/internal/controller_test.go
@@ -3,6 +3,7 @@ package internal_test
 import (
 	"context"
 	"github.com/synapsecns/sanguine/contrib/devnet/internal"
+	"github.com/synapsecns/sanguine/core/metrics"
 	"testing"
 	"time"
 )
@@ -11,7 +12,7 @@ func TestInternal(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*10)
 	defer cancel()
 
-	err := internal.Up(ctx)
+	err := internal.Up(ctx, metrics.NewNullHandler())
 	if err != nil {
 		t.Fatal(err)
 	}
