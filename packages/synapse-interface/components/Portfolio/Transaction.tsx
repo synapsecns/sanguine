833a43fb6ac1ddc01f8198d8423f51b1e7246036
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-08-23 18:15:18 -0700

    Create enum and types for Transaction component

--- packages/synapse-interface/components/Portfolio/Transaction.tsx
+++ packages/synapse-interface/components/Portfolio/Transaction.tsx
@@ -1,7 +1,41 @@
+import { Chain, Token } from '@/utils/types'
+
 export enum TransactionType {
   PENDING,
-  RECENT,
-  STUCK,
+  HISTORICAL,
 }
 
-export const Transaction = () => {}
+export enum TransactionStatus {
+  PENDING_WALLET_ACTION,
+  INITIALIZING,
+  PENDING,
+  COMPLETED,
+}
+
+export interface TransactionProps {
+  originChain: Chain
+  originToken: Token
+  originValue: number
+  destinationChain: Chain
+  destinationToken: Token
+  destinationValue?: number
+  startedTimestamp: number
+  completedTimestamp?: number
+  estimatedDuration: number
+  transactionStatus: TransactionStatus
+  transactionType: TransactionType
+}
+
+export const Transaction = ({
+  originChain,
+  originToken,
+  originValue,
+  destinationChain,
+  destinationToken,
+  destinationValue,
+  startedTimestamp,
+  completedTimestamp,
+  estimatedDuration,
+  transactionStatus,
+  transactionType,
+}: TransactionProps) => {}
