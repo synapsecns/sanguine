24503715ada2fd79475b1380a0c457a68865b715
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-09-07 18:03:06 -0700

    EstimatedDuration

--- packages/synapse-interface/components/Portfolio/Activity.tsx
+++ packages/synapse-interface/components/Portfolio/Activity.tsx
@@ -274,37 +274,6 @@ export const getTransactionExplorerLink = ({
   }
 }
 
-export const EstimatedDuration = ({
-  timeRemaining,
-  transactionStatus,
-}: {
-  timeRemaining: number
-  transactionStatus: TransactionStatus
-}) => {
-  return (
-    <div
-      data-test-id="estimated-duration"
-      className="text-[#C2C2D6] text-sm flex flex-col"
-    >
-      {timeRemaining >= 0 ? (
-        <React.Fragment>
-          <div>
-            {timeRemaining} - {timeRemaining + 1} min
-          </div>
-          {transactionStatus !== TransactionStatus.PENDING_WALLET_ACTION && (
-            <ProcessingIcon className="fill-[#343036] mt-0.5" />
-          )}
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <div>Waiting... </div>
-          <ProcessingIcon className="fill-[#343036] mt-0.5" />
-        </React.Fragment>
-      )}
-    </div>
-  )
-}
-
 export const Completed = ({
   transactionCompletedTime,
   connectedAddress,
