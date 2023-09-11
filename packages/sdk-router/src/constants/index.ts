2a837b3db9c3421800edcc4b9a6c3393dd34cf7c
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-09-11 11:14:50 +0100

    Adjust synapseRouter test

--- packages/sdk-router/src/constants/index.ts
+++ packages/sdk-router/src/constants/index.ts
@@ -23,6 +23,29 @@ export enum SupportedChainId {
   HARMONY = 1666600000,
 }
 
+export const PUBLIC_PROVIDER_URLS: { [chainId: number]: string } = {
+  [SupportedChainId.ETH]: 'https://eth.llamarpc.com',
+  [SupportedChainId.OPTIMISM]: 'https://mainnet.optimism.io',
+  [SupportedChainId.CRONOS]: 'https://evm.cronos.org',
+  [SupportedChainId.BSC]: 'https://bsc-dataseed1.ninicoin.io/',
+  [SupportedChainId.POLYGON]: 'https://polygon.llamarpc.com',
+  [SupportedChainId.FANTOM]: 'https://rpc3.fantom.network',
+  [SupportedChainId.BOBA]: 'https://boba-ethereum.gateway.tenderly.co',
+  [SupportedChainId.METIS]: 'https://andromeda.metis.io/?owner=1088',
+  [SupportedChainId.MOONBEAM]: 'https://moonbeam.public.blastapi.io',
+  [SupportedChainId.MOONRIVER]: 'https://moonriver.public.blastapi.io',
+  [SupportedChainId.DOGECHAIN]: 'https://rpc.ankr.com/dogechain',
+  [SupportedChainId.CANTO]: 'https://mainnode.plexnode.org:8545',
+  [SupportedChainId.KLAYTN]: 'https://klaytn.api.onfinality.io/public',
+  [SupportedChainId.BASE]: 'https://developer-access-mainnet.base.org',
+  [SupportedChainId.ARBITRUM]: 'https://arb1.arbitrum.io/rpc',
+  [SupportedChainId.AVALANCHE]: 'https://api.avax.network/ext/bc/C/rpc',
+  [SupportedChainId.DFK]:
+    'https://subnets.avax.network/defi-kingdoms/dfk-chain/rpc',
+  [SupportedChainId.AURORA]: 'https://mainnet.aurora.dev',
+  [SupportedChainId.HARMONY]: 'https://api.s0.t.hmny.io',
+}
+
 /**
  * List of supported chain ids, where SynapseBridge is deployed.
  */
---
+++ packages/sdk-router/src/router/synapseRouter.test.ts
@@ -0,0 +1,83 @@
+import { Provider } from '@ethersproject/abstract-provider'
+import { BigNumber, providers } from 'ethers'
+
+import {
+  PUBLIC_PROVIDER_URLS,
+  ROUTER_ADDRESS_MAP,
+  SupportedChainId,
+} from '../constants'
+import { SynapseRouter } from './synapseRouter'
+import { RouterQuery } from './query'
+
+describe('SynapseRouter', () => {
+  const ethAddress = ROUTER_ADDRESS_MAP[SupportedChainId.ETH]
+  const ethProvider: Provider = new providers.JsonRpcProvider(
+    PUBLIC_PROVIDER_URLS[SupportedChainId.ETH]
+  )
+
+  const recipient = '0x0000000000000000000000000000000000001337'
+  const ethNusd = '0x1B84765dE8B7566e4cEAF4D0fD3c5aF52D3DdE4F'
+  const emptyQuery: RouterQuery = {
+    swapAdapter: '0x0000000000000000000000000000000000000000',
+    tokenOut: '0x0000000000000000000000000000000000000000',
+    minAmountOut: BigNumber.from(0),
+    deadline: BigNumber.from(0),
+    rawParams: '0x',
+  }
+
+  describe('#constructor', () => {
+    it('fails with undefined chain id', () => {
+      expect(
+        () => new SynapseRouter(undefined as any, ethProvider, ethAddress)
+      ).toThrow('CHAIN_ID_UNDEFINED')
+    })
+
+    it('fails with zero chain id', () => {
+      expect(() => new SynapseRouter(0, ethProvider, ethAddress)).toThrow(
+        'CHAIN_ID_UNDEFINED'
+      )
+    })
+
+    it('fails with undefined provider', () => {
+      expect(
+        () =>
+          new SynapseRouter(SupportedChainId.ETH, undefined as any, ethAddress)
+      ).toThrow('PROVIDER_UNDEFINED')
+    })
+
+    it('fails with undefined address', () => {
+      expect(
+        () =>
+          new SynapseRouter(SupportedChainId.ETH, ethProvider, undefined as any)
+      ).toThrow('ADDRESS_UNDEFINED')
+    })
+
+    it('fails with empty address', () => {
+      expect(
+        () => new SynapseRouter(SupportedChainId.ETH, ethProvider, '')
+      ).toThrow('ADDRESS_UNDEFINED')
+    })
+  })
+
+  describe('Bridge', () => {
+    const synapseRouter = new SynapseRouter(
+      SupportedChainId.ETH,
+      ethProvider,
+      ethAddress
+    )
+
+    it('Bridge with nUSD', async () => {
+      const { data } = await synapseRouter.bridge(
+        recipient,
+        SupportedChainId.ARBITRUM,
+        ethNusd,
+        BigNumber.from(10),
+        emptyQuery,
+        emptyQuery
+      )
+      expect(data?.length).toBeGreaterThan(0)
+    })
+  })
+
+  // TODO (Chi): figure out mocking, add more tests
+})
