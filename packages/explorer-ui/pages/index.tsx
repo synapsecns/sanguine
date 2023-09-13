19380ae3904a4962977a62a9f7228247fce59c29
Trajan0x <trajan0x@users.noreply.github.com>
2023-09-12 20:26:01 -0400

    hotfix pt 2

--- packages/explorer-ui/pages/index.tsx
+++ packages/explorer-ui/pages/index.tsx
@@ -6,6 +6,13 @@ import {
   RANKED_CHAINIDS_BY_VOLUME,
 } from '@graphql/queries'
 import { API_URL } from '@graphql'
+import ReactGA from 'react-ga'
+
+// TODO: someone should add this to the .env, disable if blank, etc.
+// this is being added as a hotfix to assess user load on the synapse explorer api
+// I'd recommend moving this to a sushi-style analytics provider wrapper.
+const TRACKING_ID = "G-BBC13LQXBD"
+ReactGA.initialize(TRACKING_ID)
 
 const link = new HttpLink({
   uri: API_URL,
