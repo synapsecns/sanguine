cf5483d80d1b05a8b0cdccb74332c3dc495edba6
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-09-13 20:22:54 -0700

    Connect onSearchInput to SearchBar to display searchInput value updating based on user input

--- packages/synapse-interface/components/Portfolio/SearchBar.tsx
+++ packages/synapse-interface/components/Portfolio/SearchBar.tsx
@@ -1,15 +1,24 @@
 import Fuse from 'fuse.js'
 import SlideSearchBox from '@/pages/bridge/SlideSearchBox'
-import { usePortfolioBalances } from '@/slices/portfolio/hooks'
 import { NetworkTokenBalancesAndAllowances } from '@/utils/actions/fetchPortfolioBalances'
+import {
+  usePortfolioActionHandlers,
+  usePortfolioState,
+} from '@/slices/portfolio/hooks'
+import { PortfolioState } from '@/slices/portfolio/reducer'
 
 export const SearchBar = () => {
-  const userTokenBalances: NetworkTokenBalancesAndAllowances =
-    usePortfolioBalances()
+  const { onSearchInput } = usePortfolioActionHandlers()
+
+  const { searchInput }: PortfolioState = usePortfolioState()
 
   return (
     <div data-test-id="portfolio-search-bar" className="ml-auto">
-      <SlideSearchBox placeholder="Filter" searchStr="" onSearch={() => null} />
+      <SlideSearchBox
+        placeholder="Filter"
+        searchStr={searchInput}
+        onSearch={onSearchInput}
+      />
     </div>
   )
 }
