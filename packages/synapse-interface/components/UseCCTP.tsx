fc08745719c03d47a7a7bbce5adfaf7c8ec8ec07
abtestingalpha <abtestingalpha@gmail.com>
2023-09-21 13:05:48 -0400

    Side by side selection

--- packages/synapse-interface/components/UseCCTP.tsx
+++ packages/synapse-interface/components/UseCCTP.tsx
@@ -18,34 +18,47 @@ export const UseCCTP = () => {
   }
 
   return canBeCCTP ? (
-    <div className="flex flex-col bg-[#353038] text-white text-sm p-3 rounded-md mt-4 space-y-2">
-      <div className="mb-2 font-bold">Transaction routing type</div>
-      <div className="flex space-x-2">
-        <div>
-          <input
-            type="radio"
-            id="synapse-router-radio"
-            name="cctp"
-            value="true"
-            checked={excludeCCTP}
-            onChange={handleRadioChange}
-          />
-        </div>
-        <div className="flex justify-between w-full">
-          <div>Synapse Router</div>
+    <div className="flex justify-between mt-4 text-sm text-white border border-white rounded-md bg-bgLight border-opacity-20">
+      <div
+        className={`flex justify-center p-2 w-1/2 ${
+          excludeCCTP ? 'bg-bgBase rounded-l-md' : ''
+        }`}
+      >
+        <div className="flex flex-col">
+          <div className="flex items-center space-x-2">
+            <input
+              className="cursor-pointer"
+              type="radio"
+              id="synapse-router-radio"
+              name="cctp"
+              value="true"
+              checked={excludeCCTP}
+              onChange={handleRadioChange}
+            />
+            <div>5-7 min</div>
+          </div>
+          <div className="text-sm text-secondaryTextColor">~0.02%</div>
         </div>
       </div>
-      <div className="flex space-x-2">
-        <input
-          type="radio"
-          id="cctp-radio"
-          name="cctp"
-          value="false"
-          checked={!excludeCCTP}
-          onChange={handleRadioChange}
-        />
-        <div className="flex justify-between w-full">
-          <div>Circle cross-chain transfer protocol (CCTP)</div>
+      <div
+        className={`flex justify-center p-2 w-1/2 ${
+          !excludeCCTP ? 'bg-bgBase rounded-r-md' : ''
+        }`}
+      >
+        <div className="flex flex-col">
+          <div className="flex items-center space-x-2">
+            <input
+              className="cursor-pointer"
+              type="radio"
+              id="cctp-radio"
+              name="cctp"
+              value="false"
+              checked={!excludeCCTP}
+              onChange={handleRadioChange}
+            />
+            <div>15-25 min</div>
+          </div>
+          <div className="text-secondaryTextColor">Zero slippage</div>
         </div>
       </div>
     </div>
