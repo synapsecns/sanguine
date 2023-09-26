bba33b2fdcb01a10ccd4ed1336030ab80b8a52b3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-09-26 09:47:58 -0700

    retrigger gh actions

--- packages/synapse-interface/components/Portfolio/SearchBar.tsx
+++ packages/synapse-interface/components/Portfolio/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { Address } from 'viem'
 import { XIcon } from '@heroicons/react/outline'
 
