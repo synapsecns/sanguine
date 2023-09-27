dcc693b284e6e23e58af2763020f288a0437c951
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-09-27 16:39:16 -0700

    Bridge Updater component can access bridge quotes based on current toTokens in store

--- packages/synapse-interface/slices/bridge/updater.tsx
+++ packages/synapse-interface/slices/bridge/updater.tsx
@@ -1,9 +1,49 @@
+import { useEffect, useMemo } from 'react'
+import { Address } from 'viem'
 import { useBridgeState } from '@/slices/bridge/hooks'
 import { BridgeState } from './reducer'
+import {
+  BridgeQuoteRequest,
+  fetchBridgeQuotes,
+} from '@/utils/actions/fetchBridgeQuotes'
+import { BridgeQuote, Token } from '@/utils/types'
+import { stringToBigInt } from '@/utils/bigint/format'
+import { useSynapseContext } from '@/utils/providers/SynapseProvider'
 
 export default function Updater(): null {
-  const { fromChainId, toChainId, fromToken, toTokens }: BridgeState =
-    useBridgeState()
+  const { synapseSDK } = useSynapseContext()
+  const {
+    fromChainId,
+    toChainId,
+    fromToken,
+    toTokens,
+    fromValue,
+  }: BridgeState = useBridgeState()
+
+  useEffect(() => {
+    if (fromChainId && toChainId && fromToken && fromValue) {
+      const bridgeQuoteRequests: BridgeQuoteRequest[] = toTokens.map(
+        (token: Token) => {
+          return {
+            originChainId: fromChainId,
+            destinationChainId: toChainId,
+            originTokenAddress: fromToken?.addresses[fromChainId] as Address,
+            destinationTokenAddress: token?.addresses[toChainId] as Address,
+            amount: stringToBigInt(fromValue, fromToken.decimals[fromChainId]),
+          }
+        }
+      )
+
+      ;(async () => {
+        const bridgeQuotes = await fetchBridgeQuotes(
+          bridgeQuoteRequests,
+          synapseSDK
+        )
+
+        console.log('bridgeQuotes:', bridgeQuotes)
+      })()
+    }
+  }, [fromChainId, toChainId, fromToken, fromValue, toTokens, synapseSDK])
 
   return null
 }
