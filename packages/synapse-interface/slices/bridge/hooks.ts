bd502211afb3d7c2b1712983b507370e14baad7f
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-09-28 12:45:26 -0700

    init fechAndStoreBridgeQuotes async thunk

--- packages/synapse-interface/slices/bridge/hooks.ts
+++ packages/synapse-interface/slices/bridge/hooks.ts
@@ -1,6 +1,17 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+
 import { RootState } from '@/store/store'
 import { useAppSelector } from '@/store/hooks'
+import {
+  fetchBridgeQuote,
+  BridgeQuoteRequest,
+} from '@/utils/actions/fetchBridgeQuotes'
 
 export const useBridgeState = (): RootState['bridge'] => {
   return useAppSelector((state) => state.bridge)
 }
+
+export const fetchAndStoreBridgeQuotes = createAsyncThunk(
+  'bridge/fetchAndStoreBridgeQuotes',
+  async () => {}
+)
