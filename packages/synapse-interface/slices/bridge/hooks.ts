1adaacd2b8288176395fedaa2bf02a0d422f1561
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-09-28 12:48:52 -0700

    add fetchAndStoreBridgeQuote to use for current bridge selections

--- packages/synapse-interface/slices/bridge/hooks.ts
+++ packages/synapse-interface/slices/bridge/hooks.ts
@@ -12,6 +12,14 @@ export const useBridgeState = (): RootState['bridge'] => {
   return useAppSelector((state) => state.bridge)
 }
 
+export const fetchAndStoreBridgeQuote = createAsyncThunk(
+  'bridge/fetchAndStoreBridgeQuote',
+  async (request: BridgeQuoteRequest, synapseSDK: any) => {
+    const bridgeQuote = await fetchBridgeQuote(request, synapseSDK)
+    return bridgeQuote
+  }
+)
+
 export const fetchAndStoreBridgeQuotes = createAsyncThunk(
   'bridge/fetchAndStoreBridgeQuotes',
   async (requests: BridgeQuoteRequest[], synapseSDK: any) => {
