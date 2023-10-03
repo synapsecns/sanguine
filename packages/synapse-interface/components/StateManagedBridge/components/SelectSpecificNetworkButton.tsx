b0b992175eaa727b77d420c16fbfb0cc5f13c4ff
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-10-03 16:00:34 -0700

    Update TokenVisualizer in FromChainList

--- packages/synapse-interface/components/StateManagedBridge/components/SelectSpecificNetworkButton.tsx
+++ packages/synapse-interface/components/StateManagedBridge/components/SelectSpecificNetworkButton.tsx
@@ -15,6 +15,7 @@ import {
   TokenWithBalanceAndAllowances,
   sortTokensByBalanceDescending,
 } from '@/utils/actions/fetchPortfolioBalances'
+import { PortfolioTokenVisualizer } from '@/components/Portfolio/PortfolioContent/SingleNetworkPortfolio'
 
 export const SelectSpecificNetworkButton = ({
   itemChainId,
@@ -120,60 +121,93 @@ const ChainTokens = ({
 }: {
   balanceTokens: TokenWithBalanceAndAllowances[]
 }) => {
-  const [isHovered, setIsHovered] = useState(false)
-  const hasOneToken = useMemo(
-    () => balanceTokens && balanceTokens.length > 0,
-    [balanceTokens]
-  )
-  const hasTwoTokens = useMemo(
-    () => balanceTokens && balanceTokens.length > 1,
-    [balanceTokens]
-  )
-  const numOverTwoTokens = useMemo(
-    () =>
-      balanceTokens && balanceTokens.length - 2 > 0
-        ? balanceTokens.length - 2
-        : 0,
-    [balanceTokens]
-  )
+  const [isT1Hovered, setIsT1Hovered] = useState<boolean>(false)
+  const [isT2Hovered, setIsT2Hovered] = useState<boolean>(false)
+  const [isT3Hovered, setIsT3Hovered] = useState<boolean>(false)
 
+  const hasNoTokens: boolean =
+    !balanceTokens || (balanceTokens && balanceTokens.length === 0)
+  const hasOneToken: boolean = balanceTokens && balanceTokens.length > 0
+  const hasTwoTokens: boolean = balanceTokens && balanceTokens.length > 1
+  const numOverTwoTokens: number =
+    balanceTokens && balanceTokens.length - 2 > 0 ? balanceTokens.length - 2 : 0
+  const hasOnlyOneToken: boolean = balanceTokens && balanceTokens.length === 1
+  const hasOnlyTwoTokens: boolean = balanceTokens && balanceTokens.length === 2
+
+  if (hasNoTokens) {
+    return (
+      <div
+        data-test-id="portfolio-token-visualizer"
+        className="flex flex-row items-center mr-4 cursor-pointer hover-trigger text-secondary"
+      >
+        -
+      </div>
+    )
+  }
   return (
     <div
-      data-test-id="chain-tokens"
-      className="flex flex-row items-center cursor-pointer hover-trigger"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      data-test-id="portfolio-token-visualizer"
+      className="flex flex-row items-center space-x-2 cursor-pointer hover-trigger"
     >
       {hasOneToken && (
-        <Image
-          loading="lazy"
-          className="w-5 h-5 rounded-md"
-          alt={`${balanceTokens[0].token.symbol} img`}
-          src={balanceTokens[0].token.icon}
-        />
+        <div>
+          <Image
+            loading="lazy"
+            className="w-6 h-6 rounded-md"
+            alt={`${balanceTokens[0].token.symbol} img`}
+            src={balanceTokens[0].token.icon}
+            onMouseEnter={() => setIsT1Hovered(true)}
+            onMouseLeave={() => setIsT1Hovered(false)}
+          />
+          <div className="relative">
+            <HoverContent isHovered={isT1Hovered}>
+              <div className="whitespace-nowrap">
+                {balanceTokens[0]?.parsedBalance}{' '}
+                {balanceTokens[0]?.token.symbol}
+              </div>
+            </HoverContent>
+          </div>
+        </div>
+      )}
+      {hasOnlyOneToken && (
+        <div className="text-white whitespace-nowrap">
+          {balanceTokens[0].parsedBalance} {balanceTokens[0].token.symbol}
+        </div>
       )}
       {hasTwoTokens && (
-        <Image
-          loading="lazy"
-          className="w-5 h-5 ml-1.5 rounded-md"
-          alt={`${balanceTokens[1].token.symbol} img`}
-          src={balanceTokens[1].token.icon}
-        />
+        <div>
+          <Image
+            loading="lazy"
+            className="w-6 h-6 rounded-md"
+            alt={`${balanceTokens[1].token.symbol} img`}
+            src={balanceTokens[1].token.icon}
+            onMouseEnter={() => setIsT2Hovered(true)}
+            onMouseLeave={() => setIsT2Hovered(false)}
+          />
+          <div className="relative">
+            <HoverContent isHovered={isT2Hovered}>
+              <div className="whitespace-nowrap">
+                {balanceTokens[1]?.parsedBalance}{' '}
+                {balanceTokens[1]?.token.symbol}
+              </div>
+            </HoverContent>
+          </div>
+        </div>
       )}
       {numOverTwoTokens > 0 && (
-        <div className="ml-1.5 text-white">+ {numOverTwoTokens}</div>
+        <div
+          className="text-white"
+          onMouseEnter={() => setIsT3Hovered(true)}
+          onMouseLeave={() => setIsT3Hovered(false)}
+        >
+          + {numOverTwoTokens}
+        </div>
       )}
       <div className="relative inline-block">
-        {isHovered && (
-          <div
-            className={`
-              absolute -ml-28 z-50 hover-content p-2 text-white
-              border border-solid border-[#252537]
-              bg-[#101018] rounded-md
-            `}
-          >
-            {balanceTokens.map(
-              (token: TokenWithBalanceAndAllowances, key: number) => {
+        <HoverContent isHovered={isT3Hovered}>
+          {balanceTokens?.map(
+            (token: TokenWithBalanceAndAllowances, key: number) => {
+              if (key > 1) {
                 const tokenSymbol = token.token.symbol
                 const balance = token.parsedBalance
                 return (
@@ -182,10 +216,32 @@ const ChainTokens = ({
                   </div>
                 )
               }
-            )}
-          </div>
-        )}
+            }
+          )}
+        </HoverContent>
       </div>
     </div>
   )
+}
+
+export const HoverContent = ({
+  isHovered,
+  children,
+}: {
+  isHovered: boolean
+  children: React.ReactNode
+}) => {
+  if (isHovered) {
+    return (
+      <div
+        className={`
+          absolute -ml-28 z-50 hover-content p-2 text-white
+          border border-solid border-[#252537]
+          bg-[#101018] rounded-md text-left
+        `}
+      >
+        {children}
+      </div>
+    )
+  }
 }
