01dc66bd96022633eb057965fe3ef2ee725e2a7f
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-10-16 10:15:49 -0700

    Refactor isValidAddress to use viem

--- packages/synapse-interface/utils/isValidAddress.tsx
+++ packages/synapse-interface/utils/isValidAddress.tsx
@@ -1,20 +1,22 @@
-import { getAddress } from '@ethersproject/address'
-import { getAddress as getAddressViem, Address } from 'viem'
+import { getAddress, Address, InvalidAddressError } from 'viem'
 
 export const isValidAddress = (address: string): boolean => {
   try {
     const validatedAddress: string = getAddress(address)
     return true
-  } catch (e: any) {
+  } catch (e: InvalidAddressError | any) {
+    console.error('isValidAddress error: ', e)
     return false
   }
 }
 
-export const getValidAddress = (address: string): Address | any => {
+export const getValidAddress = (
+  address: string
+): Address | InvalidAddressError => {
   try {
-    const validatedAddress: Address = getAddressViem(address)
+    const validatedAddress: Address = getAddress(address)
     return validatedAddress
-  } catch (e: any) {
+  } catch (e: InvalidAddressError | any) {
     console.error('getValidAddress error: ', e)
     return null
   }
