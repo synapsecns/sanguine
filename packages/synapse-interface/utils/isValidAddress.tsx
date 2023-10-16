4416f0a463353717da467814638b1fe567ec502d
jonah <57741810+bigboydiamonds@users.noreply.github.com>
2023-10-16 16:17:18 -0700

    Fix/masquerade checksum (#1455)
    * refactor get address functionality to use viem and return checksummed address
    
    * auto checksum address when querying from search input
    
    * rm previous check var
    
    * Refactor isValidAddress to use viem
    
    * ...
    
    * Update var naming more semantically
    
    * clean
    
    ---------
    
    Co-authored-by: Jonah Lin <57741810+linjonah@users.noreply.github.com>

--- packages/synapse-interface/utils/isValidAddress.tsx
+++ packages/synapse-interface/utils/isValidAddress.tsx
@@ -1,10 +1,21 @@
-import { getAddress } from '@ethersproject/address'
+import { getAddress, Address, InvalidAddressError } from 'viem'
 
 export const isValidAddress = (address: string): boolean => {
   try {
     const validatedAddress: string = getAddress(address)
     return true
-  } catch (e: any) {
+  } catch (e: InvalidAddressError | any) {
+    console.error('isValidAddress error: ', e)
     return false
+  }
+}
+
+export const getValidAddress = (address: string): Address | any => {
+  try {
+    const validatedAddress: Address = getAddress(address)
+    return validatedAddress
+  } catch (e: InvalidAddressError | any) {
+    console.error('getValidAddress error: ', e)
+    return null
   }
 }
