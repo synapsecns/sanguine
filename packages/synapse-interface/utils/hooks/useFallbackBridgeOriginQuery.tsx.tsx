034b9292be1f2d2e46fd5ee80328f0cf6d9c7606
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-10-17 16:27:22 -0700

    Construct validQueryParams and return null in guardrail statements

--- packages/synapse-interface/utils/hooks/useFallbackBridgeOriginQuery.tsx.tsx
+++ packages/synapse-interface/utils/hooks/useFallbackBridgeOriginQuery.tsx.tsx
@@ -1,14 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import {
   useLazyGetOriginBridgeTxFallbackQuery,
   BridgeType,
 } from '@/slices/api/generated'
+import { useTransactionsState } from '@/slices/transactions/hooks'
+import { TransactionsState } from '@/slices/transactions/reducer'
 
-interface useFallbackBridgeOriginQueryProps {
-  useFallback: boolean
+interface fallbackBridgeOriginQueryProps {
   chainId?: number
   txnHash?: string
   bridgeType?: BridgeType
+}
+interface useFallbackBridgeOriginQueryProps
+  extends fallbackBridgeOriginQueryProps {
+  useFallback: boolean
 }
 
 export const useFallbackBridgeOriginQuery = ({
@@ -17,8 +22,21 @@ export const useFallbackBridgeOriginQuery = ({
   txnHash,
   bridgeType,
 }: useFallbackBridgeOriginQueryProps) => {
+  const { fallbackQueryTransactions }: TransactionsState =
+    useTransactionsState()
+
   const [fetchFallbackBridgeOriginQuery, fetchedFallbackQueries] =
     useLazyGetOriginBridgeTxFallbackQuery({ pollingInterval: 30000 })
 
+  const validQueryParams: fallbackBridgeOriginQueryProps | null =
+    useMemo(() => {
+      if (typeof chainId !== 'number') return null
+      if (!txnHash) return null
+      if (!bridgeType) return null
+
+      return { chainId, txnHash, bridgeType }
+    }, [chainId, txnHash, bridgeType])
+
+  useEffect(() => {}, [useFallback, chainId, txnHash, bridgeType])
   return null
 }
