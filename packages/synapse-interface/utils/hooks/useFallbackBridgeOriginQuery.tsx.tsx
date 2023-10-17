2b3b1a17ae948fbc5beeecb9dec9215bd390e71a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-10-17 16:14:55 -0700

    Define props

--- packages/synapse-interface/utils/hooks/useFallbackBridgeOriginQuery.tsx.tsx
+++ packages/synapse-interface/utils/hooks/useFallbackBridgeOriginQuery.tsx.tsx
@@ -1,6 +1,22 @@
-import { useLazyGetOriginBridgeTxFallbackQuery } from '@/slices/api/generated'
+import { useState, useEffect } from 'react'
+import {
+  useLazyGetOriginBridgeTxFallbackQuery,
+  BridgeType,
+} from '@/slices/api/generated'
 
-export const useFallbackBridgeOriginQuery = () => {
+interface useFallbackBridgeOriginQueryProps {
+  useFallback: boolean
+  chainId?: number
+  txnHash?: string
+  bridgeType?: BridgeType
+}
+
+export const useFallbackBridgeOriginQuery = ({
+  useFallback,
+  chainId,
+  txnHash,
+  bridgeType,
+}: useFallbackBridgeOriginQueryProps) => {
   const [fetchFallbackBridgeOriginQuery, fetchedFallbackQueries] =
     useLazyGetOriginBridgeTxFallbackQuery({ pollingInterval: 30000 })
 
