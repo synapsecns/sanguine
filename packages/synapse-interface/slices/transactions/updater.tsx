e249223cbfa6f2ad182be3989c30a9c5212001ac
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-10-19 16:14:42 -0700

    Tested fallback functionality works as intended, now need to combine what data to show

--- packages/synapse-interface/slices/transactions/updater.tsx
+++ packages/synapse-interface/slices/transactions/updater.tsx
@@ -293,11 +293,11 @@ export default function Updater(): null {
       pendingAwaitingCompletionTransactions.forEach(
         (pendingTransaction: BridgeTransaction) => {
           const isCompleted: boolean =
-            // userHistoricalTransactions.some(
-            //   (historicalTransaction: BridgeTransaction) => {
-            //     return historicalTransaction.kappa === pendingTransaction.kappa
-            //   }
-            // ) ||
+            userHistoricalTransactions.some(
+              (historicalTransaction: BridgeTransaction) => {
+                return historicalTransaction.kappa === pendingTransaction.kappa
+              }
+            ) ||
             fallbackQueryHistoricalTransactions.some(
               (historicalTransaction: BridgeTransaction) => {
                 return historicalTransaction.kappa === pendingTransaction.kappa
@@ -378,31 +378,31 @@ export default function Updater(): null {
    * Handle removing fallback historical transaction from state
    * when identical transaction gets picked up by Explorer
    */
-  // useEffect(() => {
-  //   const hasUserHistoricalTransactions: boolean =
-  //     Array.isArray(userHistoricalTransactions) &&
-  //     !isUserHistoricalTransactionsLoading
+  useEffect(() => {
+    const hasUserHistoricalTransactions: boolean =
+      Array.isArray(userHistoricalTransactions) &&
+      !isUserHistoricalTransactionsLoading
 
-  //   if (
-  //     hasUserHistoricalTransactions &&
-  //     checkTransactionsExist(fallbackQueryHistoricalTransactions)
-  //   ) {
-  //     fallbackQueryHistoricalTransactions.forEach(
-  //       (fallbackTransaction: BridgeTransaction) => {
-  //         if (
-  //           userHistoricalTransactions.some(
-  //             (historicalTransaction: BridgeTransaction) =>
-  //               historicalTransaction.kappa === fallbackTransaction.kappa
-  //           )
-  //         ) {
-  //           dispatch(
-  //             removeFallbackQueryHistoricalTransaction(fallbackTransaction)
-  //           )
-  //         }
-  //       }
-  //     )
-  //   }
-  // }, [fallbackQueryHistoricalTransactions])
+    if (
+      hasUserHistoricalTransactions &&
+      checkTransactionsExist(fallbackQueryHistoricalTransactions)
+    ) {
+      fallbackQueryHistoricalTransactions.forEach(
+        (fallbackTransaction: BridgeTransaction) => {
+          if (
+            userHistoricalTransactions.some(
+              (historicalTransaction: BridgeTransaction) =>
+                historicalTransaction.kappa === fallbackTransaction.kappa
+            )
+          ) {
+            dispatch(
+              removeFallbackQueryHistoricalTransaction(fallbackTransaction)
+            )
+          }
+        }
+      )
+    }
+  }, [fallbackQueryHistoricalTransactions])
 
   return null
 }
