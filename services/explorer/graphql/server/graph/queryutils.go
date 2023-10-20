d8467eedf273448a2221befb59f48995379c30ff
Simon <ak.simonm@gmail.com>
2023-10-20 17:25:13 -0500

    add test

--- services/explorer/graphql/server/graph/queryutils.go
+++ services/explorer/graphql/server/graph/queryutils.go
@@ -1837,6 +1837,9 @@ func (r *queryResolver) checkIfChainIDExists(chainIDNeeded uint32, bridgeType mo
 }
 
 func (r *queryResolver) getContractAddressFromType(chainID uint32, contractType model.ContractType) (string, error) {
+	if _, ok := r.Config.Chains[chainID]; !ok {
+		return "", fmt.Errorf("chain ID not found")
+	}
 	switch contractType {
 	case model.ContractTypeBridge:
 		return r.Config.Chains[chainID].Contracts.Bridge, nil
