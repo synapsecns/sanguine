b7593f314bf9b11b69777c49b6a706b98742b0d3
Trajan0x <trajan0x@users.noreply.github.com>
2023-10-23 16:24:35 +0100

    middleware based sanitizer [goreleaser]

---
+++ core/dbcommon/escape_test.go
@@ -0,0 +1,36 @@
+package dbcommon
+
+import (
+	"testing"
+)
+
+func TestMysqlRealEscapeString(t *testing.T) {
+	// ' to \\'
+	// " to \\"
+	// \r to \\r
+	// \n to \\n
+	// \\ to \\\\
+	// \x00 (null byte) to \\x00
+	// \x1a (substitute character) to \\Z
+	tests := []struct {
+		input  string
+		expect string
+	}{
+		{"hello", "hello"},
+		{"he'llo", "he\\'llo"},
+		{"he\"llo", "he\\\"llo"},
+		{"he\nllo", "he\\\nllo"},
+		{"he\rllo", "he\\\rllo"},
+		{"hello\\world", "hello\\\\world"},
+		{"he\x00llo", "he\\\x00llo"},
+		{"he\x1allo", "he\\Zllo"},
+	}
+
+	for i, tt := range tests {
+		_ = i
+		result := MysqlRealEscapeString(tt.input)
+		if result != tt.expect {
+			t.Errorf("For input '%s', expected '%s' but got '%s'", tt.input, tt.expect, result)
+		}
+	}
+}
