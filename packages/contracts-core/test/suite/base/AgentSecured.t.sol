87c00735fa572a2d18506363ee1fb2679b7d33e6
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-10-23 18:14:28 +0100

    First draft for dispute tests in `AgentSecured`

--- packages/contracts-core/test/suite/base/AgentSecured.t.sol
+++ packages/contracts-core/test/suite/base/AgentSecured.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {CallerNotAgentManager} from "../../../contracts/libs/Errors.sol";
+import {IAgentSecured} from "../../../contracts/interfaces/IAgentSecured.sol";
 import {Origin} from "../../utils/SynapseTest.t.sol";
 import {MessagingBaseTest} from "./MessagingBase.t.sol";
 
@@ -13,5 +15,47 @@ abstract contract AgentSecuredTest is MessagingBaseTest {
         Origin impl = new Origin(domain, agentManager, inbox_, gasOracle_);
         // Etch the implementation code to effectively update the values of immutables
         vm.etch(localOrigin(), address(impl).code);
+    }
+
+    // ════════════════════════════════════════════ TESTS: OPEN DISPUTE ════════════════════════════════════════════════
+
+    function openTestDispute() public {
+        vm.prank(localAgentManager());
+        localAgentSecured().openDispute(1, 2);
+    }
+
+    function test_openDispute() public {
+        // TODO: add expectations
+        openTestDispute();
+    }
+
+    function test_openDispute_revert_onlyAgentManager(address caller) public {
+        vm.assume(caller != localAgentManager());
+        vm.expectRevert(CallerNotAgentManager.selector);
+        vm.prank(caller);
+        localAgentSecured().openDispute(1, 2);
+    }
+
+    // ══════════════════════════════════════════ TESTS: RESOLVE DISPUTE ═══════════════════════════════════════════════
+
+    function test_resolveDispute() public {
+        openTestDispute();
+        // TODO: add expectations
+        vm.prank(localAgentManager());
+        localAgentSecured().resolveDispute(1, 2);
+    }
+
+    function test_resolveDispute_revert_onlyAgentManager(address caller) public {
+        vm.assume(caller != localAgentManager());
+        vm.expectRevert(CallerNotAgentManager.selector);
+        vm.prank(caller);
+        localAgentSecured().resolveDispute(1, 2);
+    }
+
+    // ══════════════════════════════════════════════════ HELPERS ══════════════════════════════════════════════════════
+
+    /// @dev Returns tested contract as `IAgentSecured`
+    function localAgentSecured() internal view returns (IAgentSecured) {
+        return IAgentSecured(localContract());
     }
 }
