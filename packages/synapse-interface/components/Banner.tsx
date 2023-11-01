0edda14d60eb880f2af654deb52a8e9142801233
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-10-31 18:23:08 -0700

    Duplicate existing banner to create ServiceInterruptedBanner

--- packages/synapse-interface/components/Banner.tsx
+++ packages/synapse-interface/components/Banner.tsx
@@ -91,3 +91,97 @@ export const Banner = () => {
     </div>
   )
 }
+
+export const InterruptedServiceBanner = () => {
+  const [hasMounted, setHasMounted] = useState(false)
+  const [showBanner, setShowBanner] = useState(false)
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  useEffect(() => {
+    if (hasMounted) {
+      const storedBannerVersion = localStorage.getItem('bannerVersion')
+      const storedShowBanner = localStorage.getItem('showBanner')
+
+      setShowBanner(
+        storedBannerVersion !== BANNER_VERSION ||
+          storedShowBanner === null ||
+          storedShowBanner === 'true'
+      )
+    }
+  }, [hasMounted])
+
+  useEffect(() => {
+    if (hasMounted) {
+      localStorage.setItem('showBanner', showBanner.toString())
+      localStorage.setItem('bannerVersion', BANNER_VERSION)
+    }
+  }, [showBanner, hasMounted])
+
+  if (!showBanner || !hasMounted) return null
+
+  return (
+    <div
+      className="flex items-center justify-center mx-auto lg:flex-row"
+      style={{
+        background:
+          'linear-gradient(310.65deg, rgba(172, 143, 255, 0.2) -17.9%, rgba(255, 0, 255, 0.2) 86.48%)',
+      }}
+    >
+      <div
+        id="banner-default"
+        className="flex items-center pt-1 pb-1 pl-3 pr-3 rounded-md text-primaryTextColor"
+        role="alert"
+      >
+        <div className="m-1 font-thin">
+          <div className="container mx-auto">
+            <p className="text-md">
+              {/* TODO: replace w/ tweetlink directly */}
+              <a
+                href={
+                  'https://twitter.com/SynapseProtocol/status/1701315115692466220'
+                }
+                target={'_blank'}
+              >
+                Synapse Explorer and the transaction watcher may not appear
+                during planned maintenance from 0500–1900 UTC. Transactions will
+                still go through as expected. Please confirm transactions using
+                the native explorer for your destination chain during this time.
+              </a>
+            </p>
+          </div>
+        </div>
+        <button
+          type="button"
+          className={`
+            inline-flex items-center justify-center
+            h-7 w-7
+            ml-auto -mx-1.5 -my-1.5 p-1.5
+            text-primaryTextColor
+          `}
+          data-dismiss-target="#banner-default"
+          aria-label="Close"
+          onClick={() => setShowBanner(false)}
+        >
+          <svg
+            className="w-[9px] h-[9px]"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
--- packages/synapse-interface/pages/index.tsx
+++ packages/synapse-interface/pages/index.tsx
@@ -7,8 +7,8 @@ import ReactGA from 'react-ga'
 // TODO: someone should add this to the .env, disable if blank, etc.
 // this is being added as a hotfix to assess user load on the synapse explorer api
 // I'd recommend moving this to a sushi-style analytics provider wrapper.
-const TRACKING_ID = "G-BBC13LQXBD"
-ReactGA.initialize(TRACKING_ID);
+const TRACKING_ID = 'G-BBC13LQXBD'
+ReactGA.initialize(TRACKING_ID)
 
 const Home = () => {
   return (
@@ -17,7 +17,7 @@ const Home = () => {
         data-test-id="bridge-page"
         className="relative z-0 flex-1 h-full overflow-y-auto focus:outline-none"
       >
-        <Banner />
+        {/* <Banner /> */}
         <div className="flex flex-col-reverse justify-center gap-16 px-4 py-20 mx-auto lg:flex-row 2xl:w-3/4 sm:mt-6 sm:px-8 md:px-12">
           <Portfolio />
           <StateManagedBridge />
