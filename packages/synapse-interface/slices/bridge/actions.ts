37e347105d9fec3e66fb7089d9b7e0c84b2308c0
jonah <57741810+bigboydiamonds@users.noreply.github.com>
2023-11-08 11:02:05 -0800

    Fe/move pending bridge transactions (#1546)
    * Move `pendingBridgeTransactions` actions to `transactions`
    
    * Move `pendingBridgeTransactions` reducers to `transactions`
    
    * Update `resetTransactionsState` reducer to reset `pendingBridgeTransactions` and `fallbackQueryHistoricalTransactions`
    
    * Replace all instances of `pendingBridgeTransactions` to updated
    
    * Update version tracking to reset cache
    
    * Remove bridge from `persistedReducers`
    
    * Update `pendingBridgeTransactions` instance
    
    * Clean imports
    
    * ...
    
    * Update actions naming
    
    * cf
    
    ---------
    
    Co-authored-by: Jonah Lin <57741810+linjonah@users.noreply.github.com>

--- packages/synapse-interface/slices/bridge/actions.ts
+++ packages/synapse-interface/slices/bridge/actions.ts
@@ -1,42 +1,12 @@
 import { createAction } from '@reduxjs/toolkit'
 
-import { Chain, Token } from '@/utils/types'
-
-export interface PendingBridgeTransaction {
-  id: number
-  originChain: Chain
-  originToken: Token
-  originValue: string
-  destinationChain: Chain
-  destinationToken: Token
-  transactionHash?: string
-  timestamp: number
-  isSubmitted: boolean
-  estimatedTime: number
-  bridgeModuleName: string
-}
-
-export const addPendingBridgeTransaction =
-  createAction<PendingBridgeTransaction>('bridge/addPendingBridgeTransaction')
-export const updatePendingBridgeTransaction = createAction<{
-  id: number
-  timestamp: number
-  transactionHash: string
-  isSubmitted: boolean
-}>('bridge/updatePendingBridgeTransaction')
-export const removePendingBridgeTransaction = createAction<number>(
-  'bridge/removePendingBridgeTransaction'
-)
-export const updatePendingBridgeTransactions = createAction<
-  PendingBridgeTransaction[]
->('bridge/updatePendingBridgeTransactions')
-export const resetBridgeInputs = createAction<void>('bridge/resetBridgeInputs')
-export const resetFetchedBridgeQuotes = createAction<void>(
-  'bridge/resetFetchedBridgeQuotes'
-)
 export const updateDebouncedFromValue = createAction<string>(
   'bridge/updateDebouncedFromValue'
 )
 export const updateDebouncedToTokensFromValue = createAction<string>(
   'bridge/updateDebouncedToTokensFromValue'
+)
+export const resetBridgeInputs = createAction<void>('bridge/resetBridgeInputs')
+export const resetFetchedBridgeQuotes = createAction<void>(
+  'bridge/resetFetchedBridgeQuotes'
 )
