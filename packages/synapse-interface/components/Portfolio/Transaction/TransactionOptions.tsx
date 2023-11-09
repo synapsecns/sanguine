dfdd563afdf51e82d0dafbdb9f17db845a95c3a6
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-11-09 14:52:46 -0800

    Update Options dropdown styling

--- packages/synapse-interface/components/Portfolio/Transaction/TransactionOptions.tsx
+++ packages/synapse-interface/components/Portfolio/Transaction/TransactionOptions.tsx
@@ -157,7 +157,7 @@ export function TransactionPopoverContainer({
       <Popover.Panel
         style={{ boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)' }}
         className={`
-          absolute z-10 top-[30px] left-[40px] transform-gpu
+          absolute z-10 top-[30px] left-[75px] transform-gpu
           -translate-x-full border border-separator bg-surface
           w-screen max-w-xs rounded-md overflow-hidden shadow-md
         `}
