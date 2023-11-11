31299851e02e70be618e01bbe2dd16734ca7799f
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-11-11 20:50:16 +0000

    Increase hook timeout

--- packages/sdk-router/vitest.config.js
+++ packages/sdk-router/vitest.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
       reporter: ['text', 'html'],
       exclude: ['node_modules/'],
     },
+    // We are using larger timeout values than usual because our tests are currently
+    // being run against forked blockchains.
+    hookTimeout: 30000,
     testTimeout: 30000,
   },
 })
