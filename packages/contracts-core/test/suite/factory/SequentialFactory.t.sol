7fb70222476395845348ffbba737342be8531bdd
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-11-18 20:28:27 +0000

    Refactor factory deployment tests

--- packages/contracts-core/test/suite/factory/SequentialFactory.t.sol
+++ packages/contracts-core/test/suite/factory/SequentialFactory.t.sol
@@ -53,47 +53,47 @@ contract SequentialFactoryTest is Test {
 
     // ═══════════════════════════════════════ HAPPY PATH: FIRST DEPLOYMENT ════════════════════════════════════════════
 
-    function test_deploy_contractA_nonceOne_correctContract() public {
+    function test_deploy_nonce01_contractA_correctContract() public {
         address deployedAt = deploy(type(A).creationCode);
         A a = A(deployedAt);
         assertEq(a.value(), 1337);
     }
 
-    function test_deploy_contractA_nonceOne_incrementsNonce() public {
+    function test_deploy_nonce01_contractA_incrementsNonce() public {
         deploy(type(A).creationCode);
         assertEq(factory.getNonce(), 2);
     }
 
-    function test_deploy_contractA_nonceOne_predictDeployment() public {
+    function test_deploy_nonce01_contractA_predictDeployment() public {
         address predicted = factory.predictDeployment(1);
         address deployedAt = deploy(type(A).creationCode);
         assertEq(predicted, deployedAt);
     }
 
-    function test_deploy_contractA_nonceOne_predictNextDeployment() public {
+    function test_deploy_nonce01_contractA_predictNextDeployment() public {
         address predicted = factory.predictNextDeployment();
         address deployedAt = deploy(type(A).creationCode);
         assertEq(predicted, deployedAt);
     }
 
-    function test_deploy_contractB_nonceOne_correctContract() public {
+    function test_deploy_nonce01_contractB_correctContract() public {
         address deployedAt = deploy(getOneArgCode(42));
         B b = B(deployedAt);
         assertEq(b.value(), 42);
     }
 
-    function test_deploy_contractB_nonceOne_incrementsNonce() public {
+    function test_deploy_nonce01_contractB_incrementsNonce() public {
         deploy(getOneArgCode(42));
         assertEq(factory.getNonce(), 2);
     }
 
-    function test_deploy_contractB_nonceOne_predictDeployment() public {
+    function test_deploy_nonce01_contractB_predictDeployment() public {
         address predicted = factory.predictDeployment(1);
         address deployedAt = deploy(getOneArgCode(42));
         assertEq(predicted, deployedAt);
     }
 
-    function test_deploy_contractB_nonceOne_predictNextDeployment() public {
+    function test_deploy_nonce01_contractB_predictNextDeployment() public {
         address predicted = factory.predictNextDeployment();
         address deployedAt = deploy(getOneArgCode(42));
         assertEq(predicted, deployedAt);
@@ -101,54 +101,54 @@ contract SequentialFactoryTest is Test {
 
     // ═════════════════════════════════════ HAPPY PATH: NON-FIRST DEPLOYMENT ══════════════════════════════════════════
 
-    function test_deploy_contractA_nonceEleven_correctContract() public {
+    function test_deploy_nonce11_contractA_correctContract() public {
         deployTenContracts();
         address deployedAt = deploy(type(A).creationCode);
         A a = A(deployedAt);
         assertEq(a.value(), 1337);
     }
 
-    function test_deploy_contractA_nonceEleven_incrementsNonce() public {
+    function test_deploy_nonce11_contractA_incrementsNonce() public {
         deployTenContracts();
         deploy(type(A).creationCode);
         assertEq(factory.getNonce(), 12);
     }
 
-    function test_deploy_contractA_nonceEleven_predictDeployment() public {
+    function test_deploy_nonce11_contractA_predictDeployment() public {
         deployTenContracts();
         address predicted = factory.predictDeployment(11);
         address deployedAt = deploy(type(A).creationCode);
         assertEq(predicted, deployedAt);
     }
 
-    function test_deploy_contractA_nonceEleven_predictNextDeployment() public {
+    function test_deploy_nonce11_contractA_predictNextDeployment() public {
         deployTenContracts();
         address predicted = factory.predictNextDeployment();
         address deployedAt = deploy(type(A).creationCode);
         assertEq(predicted, deployedAt);
     }
 
-    function test_deploy_contractB_nonceEleven_correctContract() public {
+    function test_deploy_nonce11_contractB_correctContract() public {
         deployTenContracts();
         address deployedAt = deploy(getOneArgCode(42));
         B b = B(deployedAt);
         assertEq(b.value(), 42);
     }
 
-    function test_deploy_contractB_nonceEleven_incrementsNonce() public {
+    function test_deploy_nonce11_contractB_incrementsNonce() public {
         deployTenContracts();
         deploy(getOneArgCode(42));
         assertEq(factory.getNonce(), 12);
     }
 
-    function test_deploy_contractB_nonceEleven_predictDeployment() public {
+    function test_deploy_nonce11_contractB_predictDeployment() public {
         deployTenContracts();
         address predicted = factory.predictDeployment(11);
         address deployedAt = deploy(getOneArgCode(42));
         assertEq(predicted, deployedAt);
     }
 
-    function test_deploy_contractB_nonceEleven_predictNextDeployment() public {
+    function test_deploy_nonce11_contractB_predictNextDeployment() public {
         deployTenContracts();
         address predicted = factory.predictNextDeployment();
         address deployedAt = deploy(getOneArgCode(42));
@@ -175,52 +175,52 @@ contract SequentialFactoryTest is Test {
 
     // ══════════════════════════════════════════════════ REVERTS ══════════════════════════════════════════════════════
 
-    function test_deploy_nonceOne_reverts_whenCallerNotOwner() public {
+    function test_deploy_nonce01_reverts_whenCallerNotOwner() public {
         vm.expectRevert("Ownable: caller is not the owner");
         vm.prank(attacker);
         factory.deploy(1, type(A).creationCode);
     }
 
-    function test_deploy_nonceEleven_reverts_whenCallerNotOwner() public {
+    function test_deploy_nonce11_reverts_whenCallerNotOwner() public {
         deployTenContracts();
         vm.expectRevert("Ownable: caller is not the owner");
         vm.prank(attacker);
         factory.deploy(11, type(A).creationCode);
     }
 
-    function test_deploy_nonceOne_reverts_whenNonceIsLower() public {
+    function test_deploy_nonce01_reverts_whenNonceIsLower() public {
         vm.expectRevert(abi.encodeWithSelector(SequentialFactory.SequentialFactory__InvalidNonce.selector, 0, 1));
         vm.prank(owner);
         factory.deploy(0, type(A).creationCode);
     }
 
-    function test_deploy_nonceOne_reverts_whenNonceIsHigher() public {
+    function test_deploy_nonce01_reverts_whenNonceIsHigher() public {
         vm.expectRevert(abi.encodeWithSelector(SequentialFactory.SequentialFactory__InvalidNonce.selector, 2, 1));
         vm.prank(owner);
         factory.deploy(2, type(A).creationCode);
     }
 
-    function test_deploy_nonceEleven_reverts_whenNonceIsLower() public {
+    function test_deploy_nonce11_reverts_whenNonceIsLower() public {
         deployTenContracts();
         vm.expectRevert(abi.encodeWithSelector(SequentialFactory.SequentialFactory__InvalidNonce.selector, 10, 11));
         vm.prank(owner);
         factory.deploy(10, type(A).creationCode);
     }
 
-    function test_deploy_nonceEleven_reverts_whenNonceIsHigher() public {
+    function test_deploy_nonce11_reverts_whenNonceIsHigher() public {
         deployTenContracts();
         vm.expectRevert(abi.encodeWithSelector(SequentialFactory.SequentialFactory__InvalidNonce.selector, 12, 11));
         vm.prank(owner);
         factory.deploy(12, type(A).creationCode);
     }
 
-    function test_deploy_nonceOne_revert_whenDeploymentFails() public {
+    function test_deploy_nonce01_reverts_whenDeploymentFails() public {
         vm.expectRevert(abi.encodeWithSelector(SequentialFactory.SequentialFactory__DeploymentFailed.selector, 1));
         vm.prank(owner);
         factory.deploy(1, type(UnDeployable).creationCode);
     }
 
-    function test_deploy_nonceEleven_revert_whenDeploymentFails() public {
+    function test_deploy_nonce11_reverts_whenDeploymentFails() public {
         deployTenContracts();
         vm.expectRevert(abi.encodeWithSelector(SequentialFactory.SequentialFactory__DeploymentFailed.selector, 11));
         vm.prank(owner);
