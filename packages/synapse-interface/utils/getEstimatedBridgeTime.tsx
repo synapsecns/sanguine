1693d85977c05ecf69269b650fbffde1f3ab771c
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-11-28 11:27:21 -0800

    Add return cases for `getEstimatedBridgeTime`

--- packages/synapse-interface/utils/getEstimatedBridgeTime.tsx
+++ packages/synapse-interface/utils/getEstimatedBridgeTime.tsx
@@ -1,5 +1,6 @@
-import { Chain } from 'viem'
 import { useSynapseContext } from './providers/SynapseProvider'
+import { BRIDGE_REQUIRED_CONFIRMATIONS } from '@/constants/bridge'
+import { Chain } from './types'
 
 export const getEstimatedBridgeTime = ({
   bridgeOriginChain,
@@ -11,4 +12,27 @@ export const getEstimatedBridgeTime = ({
   formattedEventType: string
 }) => {
   const { synapseSDK } = useSynapseContext()
+
+  if (!bridgeOriginChain) return null
+
+  if (bridgeModuleName) {
+    return synapseSDK.getEstimatedTime(bridgeOriginChain.id, bridgeModuleName)
+  }
+
+  if (formattedEventType) {
+    const fetchedBridgeModuleName: string =
+      synapseSDK.getBridgeModuleName(formattedEventType)
+
+    return synapseSDK.getEstimatedTime(
+      bridgeOriginChain?.id,
+      fetchedBridgeModuleName
+    )
+  }
+
+  // Fallback estimated time when inputs invalid
+  return (
+    (BRIDGE_REQUIRED_CONFIRMATIONS[bridgeOriginChain.id] *
+      bridgeOriginChain.blockTime) /
+    1000
+  )
 }
