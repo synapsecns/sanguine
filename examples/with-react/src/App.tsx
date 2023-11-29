250f02bc17f384f32e455d968d2878cdfdc4958a
abtestingalpha <abtestingalpha@gmail.com>
2023-11-29 12:38:07 -0500

    Adds examples React app

---
+++ examples/with-react/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Bridge } from 'synapse-widget'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Bridge />
+      </header>
+    </div>
+  )
+}
+
+export default App
