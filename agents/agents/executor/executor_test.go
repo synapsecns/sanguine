52476629c3745441ceca9b667ea90b9778ca6ed2
Daniel Wasserman <wassermandaniel8@gmail.com>
2023-11-29 18:43:27 -0600

    Fix: remove unnecessary test condition

--- agents/agents/executor/executor_test.go
+++ agents/agents/executor/executor_test.go
@@ -618,14 +618,6 @@ func (e *ExecutorSuite) TestExecutor() {
 	err = e.ExecutorTestDB.StoreAttestation(e.GetTestContext(), destinationAttestation, destination, 1, 1)
 	e.Nil(err)
 
-	mask := execTypes.DBMessage{
-		ChainID:     &chainID,
-		Destination: &destination,
-	}
-	executableMessages, err := e.ExecutorTestDB.GetExecutableMessages(e.GetTestContext(), mask, uint64(time.Now().Unix()), 1)
-	e.Nil(err)
-	e.Len(executableMessages, 0)
-
 	// Make sure there is one executable message in the database.
 	e.Eventually(func() bool {
 		mask := execTypes.DBMessage{
