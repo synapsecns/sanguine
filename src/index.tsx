26ceba47aa18142abe980c9963e5d3c1c17f4bb7
abtestingalpha <abtestingalpha@gmail.com>
2023-11-29 23:41:51 -0500

    Adds receipt

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { SynapseSDK } from '@synapsecns/sdk-router'
 import { fetchBridgeQuote } from '@/utils/fetchBridgeQuote'
 import { formatBigIntToString } from '@/utils/formatBigIntToString'
 import { stringToBigInt } from '@/utils/stringToBigInt'
+import { cleanNumberInput } from '@/utils/cleanNumberInput'
 
 const originChainId = 1
 const originTokenAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
@@ -14,7 +15,7 @@ export const Bridge = ({ chainIds, providers }) => {
   const synapseSDK = new SynapseSDK(chainIds, providers)
   const [quote, setQuote] = useState<any>()
   const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [inputAmount, setInputAmount] = useState<number>(0)
+  const [inputAmount, setInputAmount] = useState<string>('')
 
   const handleFetchQuote = async () => {
     setIsLoading(true)
@@ -26,7 +27,7 @@ export const Bridge = ({ chainIds, providers }) => {
           originTokenAddress,
           destinationChainId,
           destinationTokenAddress,
-          amount: stringToBigInt(inputAmount.toString(), 6),
+          amount: stringToBigInt(inputAmount, 6),
         },
         synapseSDK
       )
@@ -38,6 +39,13 @@ export const Bridge = ({ chainIds, providers }) => {
     } finally {
       setIsLoading(false)
     }
+  }
+
+  const handleInputAmountChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const cleanedInput: string = cleanNumberInput(event.target.value)
+    setInputAmount(cleanedInput)
   }
 
   const maxAmountOut = useMemo(() => {
@@ -62,10 +70,10 @@ export const Bridge = ({ chainIds, providers }) => {
         </div>
         <div className="flex items-center justify-between p-2">
           <input
-            placeholder="Amount"
+            placeholder=""
             className="text-xl"
             value={inputAmount}
-            onChange={(e) => setInputAmount(Number(e.target.value))}
+            onChange={handleInputAmountChange}
           />
           <div className="flex items-center space-x-1 rounded-lg bg-[#F5F5F5] pb-1 pl-2 pr-2 pt-1">
             <div>USDC</div>
@@ -73,7 +81,7 @@ export const Bridge = ({ chainIds, providers }) => {
           </div>
         </div>
       </div>
-      <div className="mb-2 bg-white border border-[#DCDCDC] rounded-md">
+      <div className="bg-white border border-[#DCDCDC] rounded-md mb-2">
         <div className="flex items-center justify-between p-2">
           <div className="flex items-center space-x-1 rounded-lg bg-[#F5F5F5] pb-1 pl-2 pr-2 pt-1">
             <div>Arbitrum</div>
@@ -93,12 +101,72 @@ export const Bridge = ({ chainIds, providers }) => {
           </div>
         </div>
       </div>
+      {quote ? <Receipt quote={quote} /> : null}
       <button
-        className="h-[43px] rounded-md w-full bg-white border border-[#DCDCDC]"
+        className="h-[43px] rounded-md w-full bg-white border border-[#DCDCDC] mt-2"
         onClick={handleFetchQuote}
       >
         {isLoading ? 'Fetching' : 'Fetch Bridge Quote'}
       </button>
+    </div>
+  )
+}
+
+const Receipt = ({ quote }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false)
+  const estTime = useMemo(() => {
+    return quote.estimatedTime / 60
+  }, [quote])
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded)
+  }
+  return (
+    <div>
+      <div className="flex items-center justify-end">
+        <div className="text-sm">
+          {estTime} min via <span className="text-[#0000FF]">Synapse</span>
+        </div>
+        <div onClick={handleToggle}>
+          {isExpanded ? <DoubleArrowUp /> : <DoubleArrowDown />}
+        </div>
+      </div>
+      {isExpanded && (
+        <div className="border border-[#DCDCDC] p-2 text-sm mt-2">
+          <div className="flex items-center justify-between">
+            <div>Router</div>
+            <div className="text-[#0000FF]">{quote.bridgeModuleName}</div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Origin</div>
+            <div>Ethereum</div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Destination</div>
+            <div>Arbitrum</div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Send</div>
+            <div>
+              {formatBigIntToString(
+                quote.originQuery.minAmountOut.toString(),
+                6,
+                4
+              )}
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <div>Receive</div>
+            <div>
+              {formatBigIntToString(
+                quote.destQuery.minAmountOut.toString(),
+                6,
+                4
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
@@ -115,6 +183,60 @@ const ArrowSvg = () => {
       <path
         d="M0.497159 0.727273H9.58807L5.04261 8.45455L0.497159 0.727273Z"
         fill="#696969"
+      />
+    </svg>
+  )
+}
+
+const DoubleArrowDown = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 5.5L10 9.5L14 5.5"
+        stroke="#A9A9A9"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6 11.5L10 15.5L14 11.5"
+        stroke="#A9A9A9"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+
+const DoubleArrowUp = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6 15.5L10 11.5L14 15.5"
+        stroke="#A9A9A9"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6 9.5L10 5.5L14 9.5"
+        stroke="#A9A9A9"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
   )
