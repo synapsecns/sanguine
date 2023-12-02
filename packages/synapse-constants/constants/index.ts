10ec5f344502073fd5483f0518845af035565d28
defi-moses <jakedinero@protonmail.com>
2023-12-02 18:45:15 +0000

    1.03

--- packages/synapse-constants/constants/index.ts
+++ packages/synapse-constants/constants/index.ts
@@ -1,9 +1,3 @@
-import * as TOKENS from './tokens'
-import * as CHAINS from './chains'
-import { Token, Chain } from './types'
-
-// Customizable lists
-export const CUSTOM_BRIDGEABLE_TOKENS = [TOKENS.ETH, TOKENS.USDC, TOKENS.USDCe]
-export const CUSTOM_CHAINS = [CHAINS.ETH, CHAINS.ARBITRUM, CHAINS.OPTIMISM]
-
-export { Token, Chain, TOKENS, CHAINS }
+export * as TOKENS from './tokens'
+export * as CHAINS from './chains'
+export * from './types'
