1bdfc4d402cd3a0d6cbf43f581d7680bd0d82440
abtestingalpha <abtestingalpha@gmail.com>
2023-12-04 14:48:34 -0500

    Removes unused type

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,6 @@
-export interface CustomTheme {
-  primary?: string
-  secondary?: string
-  accent?: string
-  small?: string
-  separator?: string
-  background?: string
-  surface?: string
-}
-
 export interface CustomThemeVariables {
-  '--h'?: string,
-  '--s'?: string,
+  '--h'?: string
+  '--s'?: string
   '--primary'?: string
   '--secondary'?: string
   '--accent'?: string
