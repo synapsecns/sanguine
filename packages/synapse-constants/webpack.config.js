76c4be236e6de0a9afb8755f5e63b69085969ee6
defi-moses <jakedinero@protonmail.com>
2023-12-05 09:41:02 +0000

    properly configured webpack with the necessary builds

--- packages/synapse-constants/webpack.config.js
+++ packages/synapse-constants/webpack.config.js
@@ -1,4 +1,10 @@
+const path = require('path')
 module.exports = {
+  entry: './dist/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
