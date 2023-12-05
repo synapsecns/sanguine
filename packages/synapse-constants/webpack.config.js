0b033786f0df26c8b555bcf91578c1d74388f3c2
defi-moses <jakedinero@protonmail.com>
2023-12-05 10:44:21 +0000

    adding a js loader

--- packages/synapse-constants/webpack.config.js
+++ packages/synapse-constants/webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
         test: /\.svg$/,
         use: [
           {
