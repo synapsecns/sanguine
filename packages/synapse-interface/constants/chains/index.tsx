b10495535a25158f277ecdaefe3c62c79797b70c
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-12-07 04:58:47 +0000

    pause bsc (#1603)
    * pause bsc
    
    * fix bsc pause thru network selector

--- packages/synapse-interface/constants/chains/index.tsx
+++ packages/synapse-interface/constants/chains/index.tsx
@@ -39,8 +39,8 @@ export const CHAIN_IDS = getids() // used to be ids
 export const CHAINS_BY_ID = getChainsByID()
 export const ORDERED_CHAINS_BY_ID = CHAINS_ARR.map((chain) => String(chain.id))
 
-export const PAUSED_FROM_CHAIN_IDS = []
-export const PAUSED_TO_CHAIN_IDS = [all.DOGE.id]
+export const PAUSED_FROM_CHAIN_IDS = [all.BNB.id]
+export const PAUSED_TO_CHAIN_IDS = [all.DOGE.id, all.BNB.id]
 
 export const ChainId = {
   ETH: 1,
