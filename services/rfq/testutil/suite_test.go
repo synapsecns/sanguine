e9375763f4ae5156e9ed90232fee7ab464216b85
Trajan0x <trajan0x@users.noreply.github.com>
2023-12-13 18:56:25 +0000

    allow custom evm version targeting

--- services/rfq/testutil/suite_test.go
+++ services/rfq/testutil/suite_test.go
@@ -1,8 +1,10 @@
-package testutil
+package testutil_test
 
 import (
 	"github.com/stretchr/testify/suite"
 	"github.com/synapsecns/sanguine/core/testsuite"
+	"github.com/synapsecns/sanguine/ethergo/manager"
+	"github.com/synapsecns/sanguine/services/rfq/testutil"
 	"testing"
 )
 
@@ -20,5 +22,11 @@ func NewTestUtilSuite(tb testing.TB) *TestUtilSuite {
 }
 
 func TestTestUtilSuite(t *testing.T) {
-	suite.Run(t)
+	suite.Run(t, NewTestUtilSuite(t))
+}
+
+func (s *TestUtilSuite) TestDependencies() {
+	manager.AssertDependenciesCorrect(s.GetTestContext(), s.T(), func() manager.IDeployManager {
+		return testutil.NewDeployManager(s.T())
+	})
 }
