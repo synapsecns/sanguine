912485a89c6288988cb8b4e0a66cebe76e8778e5
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-12-13 21:54:34 +0000

    set up rest server integration suite test

--- services/rfq/api/rest/server.go
+++ services/rfq/api/rest/server.go
@@ -27,6 +27,28 @@ func NewAPI(
 	store db.ApiDB,
 ) (*APIServer, error) {
 	engine := SetupRouter()
+	fmt.Println("Context:", ctx)
+	fmt.Println("Config:", cfg)
+	fmt.Println("Handler:", handler)
+	fmt.Println("OmniRPCClient:", omniRPCClient)
+	fmt.Println("Store:", store)
+	fmt.Println("engine:", engine)
+
+	if ctx == nil {
+		return nil, fmt.Errorf("context is nil")
+	}
+	if handler == nil {
+		return nil, fmt.Errorf("handler is nil")
+	}
+	if omniRPCClient == nil {
+		return nil, fmt.Errorf("omniRPCClient is nil")
+	}
+	if store == nil {
+		return nil, fmt.Errorf("store is nil")
+	}
+	if engine == nil {
+		return nil, fmt.Errorf("engine is nil")
+	}
 	return &APIServer{
 		cfg:           cfg,
 		db:            store,
@@ -37,10 +59,12 @@ func NewAPI(
 }
 
 func (r *APIServer) Run(ctx context.Context) error {
+	fmt.Println(r.engine)
 	err := r.engine.Run()
 	if err != nil {
 		return fmt.Errorf("could not run rest api server: %w", err)
 	}
+	fmt.Println("RFQ API Server is running")
 	return nil
 }
 
