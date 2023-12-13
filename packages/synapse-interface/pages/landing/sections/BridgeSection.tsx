2bddfcb5545ff4d5fa7571ee3e1cd36c33c7f831
Lawson Kight <lawsonkight@users.noreply.github.com>
2023-12-12 17:32:50 -0800

    responsive improvements

--- packages/synapse-interface/pages/landing/sections/BridgeSection.tsx
+++ packages/synapse-interface/pages/landing/sections/BridgeSection.tsx
@@ -13,43 +13,40 @@ import { DeveloperIcon } from '@/components/icons/LandingIcons/DeveloperIcon'
 
 export default function BridgeSection() {
   return (
-    <SectionContainer dataTestId="landing-bridge-section">
-      <Grid cols={{ sm: 1, md: 2 }} gap={10} className="py-6 mx-auto 2xl:w-3/4">
-        <div
-          className={`
-            absolute hidden w-screen -mt-12
-            bg-black lg:block bg-opacity-20
-            -left-12 -z-10
-          `}
-          style={{ height: '50rem' }}
-        />
-        <div className="flex items-center max-w-md">
+    <SectionContainer
+      styles="flex flex-wrap gap-8 justify-center max-w-4xl m-auto"
+      dataTestId="landing-bridge-section"
+    >
+      <div className="flex flex-col lg:flex-row gap-8 w-full items-center">
+        <div className="flex items-center w-full max-w-lg">
           <div>
-            <div className="mb-4 text-4xl font-medium text-left text-white ">
+            <h2 className="mb-4 text-4xl font-medium text-left text-white">
               Powering the most popular bridge
-            </div>
-            <div className="font-normal leading-7 text-left text-secondaryTextColor">
-              <span className="font-medium text-white">Synapse Bridge</span> is
+            </h2>
+            <p className="font-normal leading-7 text-left text-secondaryTextColor">
+              <strong className="font-medium text-white">Synapse Bridge</strong> is
               built on top of the cross-chain infrastructure enabling users to
               seamlessly transfer assets across all blockchains. The Bridge has
               become the most widely-used method to move assets cross-chain,
               offering low cost, fast, and secure bridging.
-            </div>
+            </p>
           </div>
         </div>
-        <div className="justify-center hidden md:block">
+        <div className="hidden xs:block">
           <BridgeImage />
         </div>
-      </Grid>
-
-      <div className="flex justify-center md:hidden">
-        <BridgeImageSmall />
+        <div className="block xs:hidden">
+          <BridgeImageSmall />
+        </div>
       </div>
+
+      {/* <div className="flex justify-center md:hidden">
+        <BridgeImageSmall />
+      </div> */}
 
       <Grid
         cols={{ sm: 1, md: 3 }}
         gap={6}
-        className="mx-auto mb-4 sm:py-6 md:pt-12 md:pb-0 2xl:w-3/4"
       >
         <SupportCard header="Deep Liquidity" image={<DeepLiquidityIcon />}>
           Swap native assets using our cross-chain AMM liquidity pools
