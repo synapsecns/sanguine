3c90bc9f9234080fd143e6653f52e75df10d7263
Trajan0x <trajan0x@users.noreply.github.com>
2023-12-14 20:57:07 +0000

    m

--- services/rfq/relayer/inventory/manager_test.go
+++ services/rfq/relayer/inventory/manager_test.go
@@ -8,18 +8,18 @@ import (
 )
 
 func (i *InventoryTestSuite) TestInventoryBoot() {
-	i.NotPanics(func() {
-		cfg := relconfig.Config{
-			Tokens: map[int][]string{},
-		}
+	cfg := relconfig.Config{
+		Tokens: map[int][]string{},
+	}
 
-		for _, backend := range i.backends {
-			handle, _ := i.manager.GetMockERC20(i.GetTestContext(), backend)
-			cfg.Tokens[int(backend.GetChainID())] = []string{handle.Address().String()}
-		}
+	for _, backend := range i.backends {
+		handle, _ := i.manager.GetMockERC20(i.GetTestContext(), backend)
+		cfg.Tokens[int(backend.GetChainID())] = []string{handle.Address().String()}
+	}
 
-		_, err := inventory.NewInventoryManager(i.GetTestContext(), omnirpcClient.NewOmnirpcClient(i.omnirpcURL, metrics.Get()), metrics.Get(), cfg, i.relayer.Address(), i.db)
-		i.Require().NoError(err)
+	im, err := inventory.NewInventoryManager(i.GetTestContext(), omnirpcClient.NewOmnirpcClient(i.omnirpcURL, metrics.Get()), metrics.Get(), cfg, i.relayer.Address(), i.db)
+	i.Require().NoError(err)
 
-	})
+	_ = im
+
 }
