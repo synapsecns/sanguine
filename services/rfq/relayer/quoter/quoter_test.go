f0b6622e1f034c7f1686ee5e7a3ba1bb75638358
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-12-14 22:07:29 +0000

    functionalize quoter, WIP asap to re-structue token matching

---
+++ services/rfq/relayer/quoter/quoter_test.go
@@ -0,0 +1,13 @@
+package quoter_test
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func (s *QuoterSuite) TestGenerateQuotes() {
+	quotesReturned, err := s.manager.GenerateQuotes(1, common.HexToAddress("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"), big.NewInt(100))
+	s.Require().NoError(err)
+	s.Require().Len(quotesReturned, 1)
+}
---
+++ services/rfq/relayer/quoter/suite_test.go
@@ -0,0 +1,47 @@
+package quoter_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/synapsecns/sanguine/core/testsuite"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/quoter"
+)
+
+// Server suite is the main API server test suite.
+type QuoterSuite struct {
+	*testsuite.TestSuite
+	manager quoter.QuoterManager
+}
+
+// NewServerSuite creates a end-to-end test suite.
+func NewQuoterSuite(tb testing.TB) *QuoterSuite {
+	tb.Helper()
+	return &QuoterSuite{
+		TestSuite: testsuite.NewTestSuite(tb),
+	}
+}
+
+func (c *QuoterSuite) SetupTest() {
+	c.TestSuite.SetupTest()
+	// Setup
+	manager := quoter.QuoterManager{}
+	c.manager = manager
+	c.manager.SetQuotableTokens(map[string][]string{
+		"42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831": {"1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "10-0x0b2c639c533813f4aa9d7837caf62653d097ff85"},
+		"1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48":     {"42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831", "10-0x0b2c639c533813f4aa9d7837caf62653d097ff85"},
+		"10-0x0b2c639c533813f4aa9d7837caf62653d097ff85":    {"1-0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "42161-0xaf88d065e77c8cc2239327c5edb3a432268e5831"},
+		// Add more mock quotableTokens if needed
+	})
+
+}
+
+func (c *QuoterSuite) SetupSuite() {
+	c.TestSuite.SetupSuite()
+
+}
+
+// TestConfigSuite runs the integration test suite.
+func TestQuoterSuite(t *testing.T) {
+	suite.Run(t, NewQuoterSuite(t))
+}
