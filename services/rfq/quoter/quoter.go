59fc094ffa64f6b878a154e6eaa2da1c84d463d9
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2023-12-14 09:13:39 +0000

    begin quoter calling non-existant WIP inventory functions

--- services/rfq/quoter/quoter.go
+++
@@ -1,8 +0,0 @@
-// Quoter submits quotes to the RFQ API for which assets the relayer is willing to relay.
-package quoter
-
-type Quoter interface {
-	// SubmitQuote submits a quote to the RFQ API.
-	SubmitQuote() error
-	// GetQuote gets a quote from the RFQ API.
-}
