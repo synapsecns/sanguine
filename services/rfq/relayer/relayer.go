0b8a8f802789cb368824b5bb4a3469979e9e013e
Trajan0x <trajan0x@users.noreply.github.com>
2023-12-14 01:41:35 +0000

    backup

---
+++ services/rfq/relayer/relayer.go
@@ -0,0 +1,15 @@
+package relayer
+
+import (
+	"context"
+)
+
+// Relayer is the core of the relayer application
+type Relayer struct {
+	cfg config.Config
+}
+
+// NewRelayer creates a new relayer.
+func NewRelayer(ctx context.Context, cfg config.Config) {
+
+}
