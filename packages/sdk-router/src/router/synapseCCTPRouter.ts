2e9e93fa1ba1b7df80f7b84c201697d798c38b4f
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-18 21:52:45 +0300

    Sdk: expose bridge tx status (#1658)
    * Add placeholder logic for new functions
    
    * Chore: fix spelling
    
    * Add tests with expected behavior
    
    * Generalize bridgeModule -> RouterSet retrieval
    
    * Add skeleton for the end implementation
    
    * Implement ID / status for SynapseBridge
    
    * CCTP status check
    
    * Define behavior for destination CCTP tx
    
    * SynapseCCTP: getBridgeID
    
    * Add test for incorrect tx hash
    
    * Check that transaction receipt is not null
    
    * Tests for generalized "incorrect origin tx" behavior
    
    * Generalized tool for log extraction
    
    * Adapt synapseCCTP, add check to synapseBridge
    
    * Chore: bridgeId -> synapseTxId
    
    * Extra coverage to keep Code Rabbi happy

--- packages/sdk-router/src/router/synapseCCTPRouter.ts
+++ packages/sdk-router/src/router/synapseCCTPRouter.ts
@@ -8,6 +8,8 @@ import cctpRouterAbi from '../abi/SynapseCCTPRouter.json'
 import { SynapseCCTPRouter as SynapseCCTPRouterContract } from '../typechain/SynapseCCTPRouter'
 import { Router } from './router'
 import { Query, narrowToCCTPRouterQuery, reduceToQuery } from './query'
+import cctpAbi from '../abi/SynapseCCTP.json'
+import { getMatchingTxLog } from '../utils/logs'
 import { BigintIsh } from '../constants'
 import {
   BridgeToken,
@@ -27,6 +29,10 @@ export class SynapseCCTPRouter extends Router {
   public readonly address: string
 
   private readonly routerContract: SynapseCCTPRouterContract
+  private cctpContractCache: Contract | undefined
+
+  // All possible events emitted by the SynapseCCTP contract in the origin transaction
+  private readonly originEvents = ['CircleRequestSent']
 
   constructor(chainId: number, provider: Provider, address: string) {
     // Parent constructor throws if chainId or provider are undefined
@@ -113,5 +119,43 @@ export class SynapseCCTPRouter extends Router {
       narrowToCCTPRouterQuery(originQuery),
       narrowToCCTPRouterQuery(destQuery)
     )
+  }
+
+  /**
+   * @inheritdoc Router.getSynapseTxId
+   */
+  public async getSynapseTxId(txHash: string): Promise<string> {
+    const cctpContract = await this.getCctpContract()
+    const cctpLog = await getMatchingTxLog(
+      this.provider,
+      txHash,
+      cctpContract,
+      this.originEvents
+    )
+    // RequestID always exists in the log as we are using the correct ABI
+    const parsedLog = cctpContract.interface.parseLog(cctpLog)
+    return parsedLog.args.requestID
+  }
+
+  /**
+   * @inheritdoc Router.getBridgeTxStatus
+   */
+  public async getBridgeTxStatus(synapseTxId: string): Promise<boolean> {
+    const cctpContract = await this.getCctpContract()
+    return cctpContract.isRequestFulfilled(synapseTxId)
+  }
+
+  private async getCctpContract(): Promise<Contract> {
+    // Populate the cache if necessary
+    if (!this.cctpContractCache) {
+      const cctpAddress = await this.routerContract.synapseCCTP()
+      this.cctpContractCache = new Contract(
+        cctpAddress,
+        new Interface(cctpAbi),
+        this.provider
+      )
+    }
+    // Return the cached contract
+    return this.cctpContractCache
   }
 }
