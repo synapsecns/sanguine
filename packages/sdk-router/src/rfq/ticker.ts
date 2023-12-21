29f40e5900313f4cafe5bbbcf8369ba4d103e49b
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-21 15:04:55 +0000

    Chore: docs

--- packages/sdk-router/src/rfq/ticker.ts
+++ packages/sdk-router/src/rfq/ticker.ts
@@ -10,12 +10,24 @@ export type Ticker = {
   destToken: ChainToken
 }
 
+/**
+ * Marshalls a ChainToken object into a string. Follows the format of "chainId:token".
+ *
+ * @param chainToken - The ChainToken object to marshall.
+ * @returns The marshalled string.
+ */
 export const marshallChainToken = (chainToken: ChainToken): string => {
   return `${chainToken.chainId}:${chainToken.token}`
 }
 
+/**
+ * Unmarshalls a string into a ChainToken object. Follows the format of "chainId:token".
+ *
+ * @param chainTokenStr - The string to unmarshall.
+ * @returns The unmarshalled ChainToken object.
+ * @throws Will throw an error if the string is not in the correct format.
+ */
 export const unmarshallChainToken = (chainTokenStr: string): ChainToken => {
-  // The unmashalled string should be in the format of "chainId:token"
   const items = chainTokenStr.split(':')
   if (items.length !== 2) {
     throw new Error(`Can not unmarshall "${chainTokenStr}": invalid format`)
@@ -34,14 +46,26 @@ export const unmarshallChainToken = (chainTokenStr: string): ChainToken => {
   }
 }
 
+/**
+ * Marshalls a Ticker object into a string. Follows the format of "originChainId:originToken-destChainId:destToken".
+ *
+ * @param ticker - The Ticker object to marshall.
+ * @returns The marshalled string.
+ */
 export const marshallTicker = (ticker: Ticker): string => {
   return `${marshallChainToken(ticker.originToken)}-${marshallChainToken(
     ticker.destToken
   )}`
 }
 
+/**
+ * Unmarshalls a string into a Ticker object. Follows the format of "originChainId:originToken-destChainId:destToken".
+ *
+ * @param tickerStr - The string to unmarshall.
+ * @returns The unmarshalled Ticker object.
+ * @throws Will throw an error if the string is not in the correct format.
+ */
 export const unmarshallTicker = (tickerStr: string): Ticker => {
-  // The unmashalled string should be in the format of "originChainId:originToken-destChainId:destToken"
   const items = tickerStr.split('-')
   if (items.length !== 2) {
     throw new Error(`Can not unmarshall "${tickerStr}": invalid format`)
