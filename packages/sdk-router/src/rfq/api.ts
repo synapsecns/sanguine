a5cd3ad4d76058f8a08261899b2f38ace68e74a7
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-23 12:37:20 +0000

    Adjust getBridgeRoutes to current API

---
+++ packages/sdk-router/src/rfq/api.ts
@@ -0,0 +1,11 @@
+import { FastBridgeQuote } from './quote'
+
+/**
+ * Hits Quoter API /quotes endpoint to get all quotes.
+ *
+ * @returns A promise that resolves to the list of quotes.
+ */
+export const getAllQuotes = async (): Promise<FastBridgeQuote[]> => {
+  // TODO: Implement
+  return []
+}
--- packages/sdk-router/src/rfq/fastBridgeSet.ts
+++ packages/sdk-router/src/rfq/fastBridgeSet.ts
@@ -9,7 +9,6 @@ import {
 } from '../constants'
 import {
   BridgeRoute,
-  BridgeToken,
   FeeConfig,
   SynapseModule,
   SynapseModuleSet,
@@ -18,7 +17,9 @@ import {
 } from '../module'
 import { ChainProvider } from '../router'
 import { FastBridge } from './fastBridge'
-import { Ticker, marshallTicker } from './ticker'
+import { marshallTicker } from './ticker'
+import { FastBridgeQuote, applyQuote } from './quote'
+import { getAllQuotes } from './api'
 
 export class FastBridgeSet extends SynapseModuleSet {
   public readonly bridgeModuleName = 'SynapseRFQ'
@@ -71,53 +72,39 @@ export class FastBridgeSet extends SynapseModuleSet {
     tokenOut: string,
     amountIn: BigintIsh
   ): Promise<BridgeRoute[]> {
-    const allTickers: Ticker[] = await this.getSupportedTickers(
+    // Get all quotes that result in the final token
+    const allQuotes: FastBridgeQuote[] = await this.getQuotes(
       originChainId,
-      destChainId
+      destChainId,
+      tokenOut
     )
-    // Get all tickets that could fulfill the tokenIn -> tokenOut cross-chain swap
-    const originRoutes = allTickers
-      .filter(
-        (ticker) =>
-          // Filter tickers that have destination token matching tokenOut
-          ticker.destToken.token.toLowerCase() === tokenOut.toLowerCase() &&
-          // Check if the origin token matches tokenIn
-          // TODO: adjust this once swaps on the origin chain are supported
-          ticker.originToken.token.toLowerCase() === tokenIn.toLowerCase()
-      )
-      .map((ticker) => ({
-        // TODO: Create Query struct for tokenIn -> ticker.originToken swap
-        originQuery: createNoSwapQuery(tokenIn, BigNumber.from(amountIn)),
-        ticker,
+    // Get queries for swaps on the origin chain into the "RFQ-supported token"
+    const originQueries = await this.getOriginQueries(
+      originChainId,
+      tokenIn,
+      amountIn,
+      allQuotes.map((quote) => quote.ticker.originToken.token)
+    )
+    return allQuotes
+      .map((quote, index) => ({
+        quote,
+        originQuery: originQueries[index],
+        // Apply quote to the proceeds of the origin swap
+        destAmountOut: applyQuote(quote, originQueries[index].minAmountOut),
       }))
-      .filter(({ originQuery }) => !originQuery.minAmountOut.isZero())
-    // Figure out RFQ quotes for each origin route
-    const destAmountOuts = await Promise.all(
-      originRoutes.map(({ originQuery, ticker }) =>
-        this.getQuote(ticker, originQuery.minAmountOut)
-      )
-    )
-    // Zip originRoutes with destAmountOuts
-    return originRoutes.map((originRoute, index) => {
-      // Swaps on destination chain are not supported in RFQ
-      const destQuery: Query = createNoSwapQuery(
-        tokenOut,
-        destAmountOuts[index]
-      )
-      // Use the ticker as "bridge symbol"
-      const bridgeToken: BridgeToken = {
-        symbol: marshallTicker(originRoute.ticker),
-        token: originRoute.ticker.destToken.token,
-      }
-      return {
+      .filter(({ destAmountOut }) => destAmountOut.gt(0))
+      .map(({ quote, originQuery, destAmountOut }) => ({
         originChainId,
         destChainId,
-        bridgeToken,
-        originQuery: originRoute.originQuery,
-        destQuery,
+        bridgeToken: {
+          symbol: marshallTicker(quote.ticker),
+          token: quote.ticker.destToken.token,
+        },
+        originQuery,
+        // On-chain swaps are not supported for RFQ tokens
+        destQuery: createNoSwapQuery(tokenOut, destAmountOut),
         bridgeModuleName: this.bridgeModuleName,
-      }
-    })
+      }))
   }
 
   /**
@@ -147,34 +134,42 @@ export class FastBridgeSet extends SynapseModuleSet {
   }
 
   /**
-   * Get the list of tickers supported by the RFQ between the two chains.
-   *
-   * @param originChainId - The ID of the origin chain.
-   * @param destChainId - The ID of the destination chain.
-   * @returns A promise that resolves to the list of supported tickers.
+   * Returns the origin query for every "RFQ token" on the origin chain,
+   * that contains the information for tokenIn -> RFQ token swaps.
    */
-  private async getSupportedTickers(
+  private async getOriginQueries(
     originChainId: number,
-    destChainId: number
-  ): Promise<Ticker[]> {
-    // TODO: hit Quoter API to get supported tickers, then unmarshall them
-    console.log(originChainId, destChainId)
-    return []
+    tokenIn: string,
+    amountIn: BigintIsh,
+    rfqTokens: string[]
+  ): Promise<Query[]> {
+    // Check if the RFQ token matches tokenIn
+    // TODO: change this to "find best path" once swaps on the origin chain are supported
+    invariant(originChainId, 'Origin chain ID is required')
+    return rfqTokens
+      .filter((rfqToken) => rfqToken.toLowerCase() === tokenIn.toLowerCase())
+      .map((rfqToken) => createNoSwapQuery(rfqToken, BigNumber.from(amountIn)))
   }
 
   /**
-   * Get the quote for a given ticker and amount.
+   * Get the list of quotes between two chains for a given final token.
    *
-   * @param ticker - The ticker to get the quote for.
-   * @param amount - The amount to get the quote for.
-   * @returns A promise that resolves to the quote.
+   * @param originChainId - The ID of the origin chain.
+   * @param destChainId - The ID of the destination chain.
+   * @param tokenOut - The final token of the cross-chain swap.
+   * @returns A promise that resolves to the list of supported tickers.
    */
-  private async getQuote(
-    ticker: Ticker,
-    amount: BigNumber
-  ): Promise<BigNumber> {
-    // TODO: hit Quoter API to get the quote for ticker.originToken -> ticker.destToken with the given amount
-    console.log(ticker, amount)
-    return BigNumber.from(0)
+  private async getQuotes(
+    originChainId: number,
+    destChainId: number,
+    tokenOut: string
+  ): Promise<FastBridgeQuote[]> {
+    const allQuotes = await getAllQuotes()
+    return allQuotes.filter(
+      (quote) =>
+        quote.ticker.originToken.chainId === originChainId &&
+        quote.ticker.destToken.chainId === destChainId &&
+        quote.ticker.destToken.token.toLowerCase() === tokenOut.toLowerCase()
+    )
   }
 }
