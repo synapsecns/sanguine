1419de640c5c6e29e733b4bb4cd03423e07d1632
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-23 13:18:13 +0000

    Implement `getAllQuotes`

--- packages/sdk-router/src/rfq/api.ts
+++ packages/sdk-router/src/rfq/api.ts
@@ -1,4 +1,11 @@
-import { FastBridgeQuote } from './quote'
+import {
+  FastBridgeQuote,
+  FastBridgeQuoteAPI,
+  unmarshallFastBridgeQuote,
+} from './quote'
+
+// TODO: fill
+const API_URL = 'API_URL'
 
 /**
  * Hits Quoter API /quotes endpoint to get all quotes.
@@ -6,6 +13,12 @@ import { FastBridgeQuote } from './quote'
  * @returns A promise that resolves to the list of quotes.
  */
 export const getAllQuotes = async (): Promise<FastBridgeQuote[]> => {
-  // TODO: Implement
-  return []
+  const response = await fetch(API_URL + '/quotes')
+  // Return empty list if response is not ok
+  if (!response.ok) {
+    return []
+  }
+  // The response is a list of quotes in the FastBridgeQuoteAPI format
+  const quotes: FastBridgeQuoteAPI[] = await response.json()
+  return quotes.map(unmarshallFastBridgeQuote)
 }
