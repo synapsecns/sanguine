e2e89b25b58e6a913d70ec2d185c2584106a189d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-27 18:27:33 +0000

    Add unit tests for quote pricing

--- packages/sdk-router/src/rfq/quote.test.ts
+++ packages/sdk-router/src/rfq/quote.test.ts
@@ -1,4 +1,4 @@
-import { BigNumber } from '@ethersproject/bignumber'
+import { BigNumber, parseFixed } from '@ethersproject/bignumber'
 
 import {
   FastBridgeQuote,
@@ -7,6 +7,128 @@ import {
   unmarshallFastBridgeQuote,
   applyQuote,
 } from './quote'
+
+const createZeroAmountTests = (quote: FastBridgeQuote) => {
+  describe('Returns zero', () => {
+    it('If origin amount is zero', () => {
+      expect(applyQuote(quote, BigNumber.from(0))).toEqual(BigNumber.from(0))
+    })
+
+    it('If origin amount is lower than fixed fee', () => {
+      expect(applyQuote(quote, quote.fixedFee.sub(1))).toEqual(
+        BigNumber.from(0)
+      )
+    })
+
+    it('If origin amount is equal to fixed fee', () => {
+      expect(applyQuote(quote, quote.fixedFee)).toEqual(BigNumber.from(0))
+    })
+
+    it('If origin amount is greater than max origin amount + fixed fee', () => {
+      const amount = quote.maxOriginAmount.add(quote.fixedFee).add(1)
+      expect(applyQuote(quote, amount)).toEqual(BigNumber.from(0))
+    })
+  })
+
+  describe('Returns non-zero', () => {
+    it('If origin amount is equal to max origin amount', () => {
+      expect(applyQuote(quote, quote.maxOriginAmount)).not.toEqual(
+        BigNumber.from(0)
+      )
+    })
+
+    it('If origin amount is 1 wei greater than max origin amount', () => {
+      const amount = quote.maxOriginAmount.add(1)
+      expect(applyQuote(quote, amount)).not.toEqual(BigNumber.from(0))
+    })
+
+    it('If origin amount is max origin amount + fixed fee', () => {
+      const amount = quote.maxOriginAmount.add(quote.fixedFee)
+      expect(applyQuote(quote, amount)).not.toEqual(BigNumber.from(0))
+    })
+  })
+}
+
+const createCorrectAmountTest = (
+  quote: FastBridgeQuote,
+  amount: BigNumber,
+  expected: BigNumber
+) => {
+  it(`${amount.toString()} -> ${expected.toString()}`, () => {
+    expect(applyQuote(quote, amount)).toEqual(expected)
+  })
+}
+
+const createQuoteTests = (
+  quoteTemplate: FastBridgeQuote,
+  originDecimals: number,
+  destDecimals: number
+) => {
+  describe(`Origin decimals: ${originDecimals}, dest decimals: ${destDecimals}`, () => {
+    describe(`origin:destination price 1:1`, () => {
+      const quote: FastBridgeQuote = {
+        ...quoteTemplate,
+        maxOriginAmount: parseFixed('100000', originDecimals),
+        destAmount: parseFixed('100000', destDecimals),
+        fixedFee: parseFixed('1', originDecimals),
+      }
+
+      // 10 origin -> 9 dest
+      createCorrectAmountTest(
+        quote,
+        parseFixed('10', originDecimals),
+        parseFixed('9', destDecimals)
+      )
+      createZeroAmountTests(quote)
+    })
+
+    describe(`origin:destination price 1:1.0001`, () => {
+      const quote: FastBridgeQuote = {
+        ...quoteTemplate,
+        maxOriginAmount: parseFixed('100000', originDecimals),
+        destAmount: parseFixed('100010', destDecimals),
+        fixedFee: parseFixed('1', originDecimals),
+      }
+
+      // 10 origin -> 9.0009 dest
+      createCorrectAmountTest(
+        quote,
+        parseFixed('10', originDecimals),
+        parseFixed('9.0009', destDecimals)
+      )
+      // Should round down 9999 * 1.0001 to 9999
+      createCorrectAmountTest(
+        quote,
+        parseFixed('11', originDecimals).add(9999),
+        parseFixed('10.001', destDecimals).add(9999)
+      )
+      createZeroAmountTests(quote)
+    })
+
+    describe(`origin:destination price 1:0.9999`, () => {
+      const quote: FastBridgeQuote = {
+        ...quoteTemplate,
+        maxOriginAmount: parseFixed('100000', originDecimals),
+        destAmount: parseFixed('99990', destDecimals),
+        fixedFee: parseFixed('1', originDecimals),
+      }
+
+      // 10 origin -> 8.9991 dest
+      createCorrectAmountTest(
+        quote,
+        parseFixed('10', originDecimals),
+        parseFixed('8.9991', destDecimals)
+      )
+      // Should round down 1 * 0.9999 to 0
+      createCorrectAmountTest(
+        quote,
+        parseFixed('11', originDecimals).add(1),
+        parseFixed('9.999', destDecimals)
+      )
+      createZeroAmountTests(quote)
+    })
+  })
+}
 
 describe('quote', () => {
   const quoteAPI: FastBridgeQuoteAPI = {
@@ -48,34 +170,13 @@ describe('quote', () => {
   })
 
   describe('applyQuote', () => {
-    it('Returns zero if origin amount is lower than fixed fee', () => {
-      expect(applyQuote(quote, quote.fixedFee.sub(1))).toEqual(
-        BigNumber.from(0)
-      )
-    })
+    // Equal decimals
+    createQuoteTests(quote, 18, 18)
 
-    it('Returns zero if origin amount is equal to fixed fee', () => {
-      expect(applyQuote(quote, quote.fixedFee)).toEqual(BigNumber.from(0))
-    })
+    // Bigger decimals
+    createQuoteTests(quote, 6, 18)
 
-    it('Returns zero if origin amount is greater than max origin amount', () => {
-      expect(applyQuote(quote, quote.maxOriginAmount.add(1))).toEqual(
-        BigNumber.from(0)
-      )
-    })
-
-    it('Returns zero if resulted quote is higher than dest amount', () => {
-      // Modify dest amount to be lower than the max origin amount
-      quote.destAmount = BigNumber.from(10).pow(18).mul(100)
-      expect(
-        applyQuote(quote, quote.destAmount.add(quote.fixedFee).add(1))
-      ).toEqual(BigNumber.from(0))
-    })
-
-    it('Returns a correct quote', () => {
-      expect(applyQuote(quote, BigNumber.from(10).pow(18).mul(100))).toEqual(
-        BigNumber.from(10).pow(18).mul(99)
-      )
-    })
+    // Smaller decimals
+    createQuoteTests(quote, 18, 6)
   })
 })
