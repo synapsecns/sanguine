01e23e823ff232bec9b4074126dd03d528e87cbb
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2023-12-28 22:36:02 +0000

    Temp FE fix: don't apply slippage to RFQ

--- packages/synapse-interface/pages/state-managed-bridge/index.tsx
+++ packages/synapse-interface/pages/state-managed-bridge/index.tsx
@@ -239,12 +239,13 @@ const StateManagedBridge = () => {
         )
       }
 
-      const originMinWithSlippage = subtractSlippage(
+      // TODO: do this properly (RFQ needs no slippage, others do)
+      const originMinWithSlippage = bridgeModuleName === "SynapseRFQ" ? (originQuery?.minAmountOut ?? 0n) : subtractSlippage(
         originQuery?.minAmountOut ?? 0n,
         'ONE_TENTH',
         null
       )
-      const destMinWithSlippage = subtractSlippage(
+      const destMinWithSlippage = bridgeModuleName === "SynapseRFQ" ? (destQuery?.minAmountOut ?? 0n) : subtractSlippage(
         destQuery?.minAmountOut ?? 0n,
         'ONE_TENTH',
         null
