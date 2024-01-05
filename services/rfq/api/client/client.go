ca9c253f89b5f3f5476a58ebb2d3772273ee16b3
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-01-05 10:09:57 -0600

    Feat: include both origin and dest addresses in resp

--- services/rfq/api/client/client.go
+++ services/rfq/api/client/client.go
@@ -175,14 +175,15 @@ func (c *unauthenticatedClient) GetQuoteByRelayerAddress(relayerAddr string) ([]
 
 // APIQuotePutRequest is the struct for the quote API.
 type APIQuotePutRequest struct {
-	OriginChainID     string `json:"origin_chain_id"`
-	OriginTokenAddr   string `json:"origin_token_addr"`
-	DestChainID       string `json:"dest_chain_id"`
-	DestTokenAddr     string `json:"dest_token_addr"`
-	DestAmount        string `json:"dest_amount"`
-	MaxOriginAmount   string `json:"max_origin_amount"`
-	FixedFee          string `json:"fixed_fee"`
-	FastBridgeAddress string `json:"fast_bridge_address"`
+	OriginChainID           string `json:"origin_chain_id"`
+	OriginTokenAddr         string `json:"origin_token_addr"`
+	DestChainID             string `json:"dest_chain_id"`
+	DestTokenAddr           string `json:"dest_token_addr"`
+	DestAmount              string `json:"dest_amount"`
+	MaxOriginAmount         string `json:"max_origin_amount"`
+	FixedFee                string `json:"fixed_fee"`
+	OriginFastBridgeAddress string `json:"origin_fast_bridge_address"`
+	DestFastBridgeAddress   string `json:"dest_fast_bridge_address"`
 }
 
 // APIQuoteSpecificGetRequest is the struct for the quote API.
