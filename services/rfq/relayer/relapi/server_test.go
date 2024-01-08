8645575dc30adef0b1b7b130099a5feb698b1aec
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-01-08 16:57:12 -0600

    Feat: add TestGetQuoteRequestByTxHash

--- services/rfq/relayer/relapi/server_test.go
+++ services/rfq/relayer/relapi/server_test.go
@@ -1,14 +1,22 @@
 package relapi_test
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"math/big"
 	"net/http"
 	"strconv"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/synapsecns/sanguine/core/retry"
 	"github.com/synapsecns/sanguine/ethergo/signer/wallet"
+	"github.com/synapsecns/sanguine/services/rfq/contracts/fastbridge"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/relapi"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/reldb"
 )
 
 func (c *RelayerServerSuite) TestNewAPIServer() {
@@ -22,8 +30,58 @@ func (c *RelayerServerSuite) TestNewAPIServer() {
 	c.GetTestContext().Done()
 }
 
-func (c *RelayerServerSuite) TestPutAndGetQuote() {
+func getTestQuoteRequest(status reldb.QuoteRequestStatus) reldb.QuoteRequest {
+	txIDRaw := hexutil.Encode(crypto.Keccak256([]byte("test")))
+	var txID [32]byte
+	copy(txID[:], txIDRaw)
+	return reldb.QuoteRequest{
+		OriginTokenDecimals: 6,
+		DestTokenDecimals:   6,
+		TransactionID:       txID,
+		Status:              status,
+		Transaction: fastbridge.IFastBridgeBridgeTransaction{
+			OriginAmount: big.NewInt(100),
+			DestAmount:   big.NewInt(100),
+			Deadline:     big.NewInt(time.Now().Unix()),
+			Nonce:        big.NewInt(1),
+		},
+		OriginTxHash: common.HexToHash("0x0000000"),
+		DestTxHash:   common.HexToHash("0x0000001"),
+	}
+}
+
+func (c *RelayerServerSuite) TestGetQuoteRequestByTxHash() {
 	c.startAPIServer()
+
+	// Insert quote request to db
+	quoteRequest := getTestQuoteRequest(reldb.Seen)
+	err := c.database.StoreQuoteRequest(c.GetTestContext(), quoteRequest)
+	c.Require().NoError(err)
+
+	// Fetch the quote request by tx hash
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(c.GetTestContext(), http.MethodGet, fmt.Sprintf("http://localhost:%d/status?hash=%s", c.port, quoteRequest.OriginTxHash), nil)
+	c.Require().NoError(err)
+	resp, err := client.Do(req)
+	c.Require().NoError(err)
+	defer func() {
+		err = resp.Body.Close()
+		c.Require().NoError(err)
+	}()
+	c.Equal(http.StatusOK, resp.StatusCode)
+
+	// Compare to expected result
+	var result relapi.GetQuoteRequestStatusResponse
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	c.Require().NoError(err)
+	expectedResult := relapi.GetQuoteRequestStatusResponse{
+		Status:       quoteRequest.Status.String(),
+		TxID:         hexutil.Encode(quoteRequest.TransactionID[:]),
+		OriginTxHash: quoteRequest.OriginTxHash.String(),
+		DestTxHash:   quoteRequest.DestTxHash.String(),
+	}
+	c.Equal(expectedResult, result)
+	c.GetTestContext().Done()
 }
 
 // startAPIServer starts the API server and waits for it to initialize.
@@ -32,7 +90,15 @@ func (c *RelayerServerSuite) startAPIServer() {
 		err := c.RelayerAPIServer.Run(c.GetTestContext())
 		c.Require().NoError(err)
 	}()
-	time.Sleep(2 * time.Second) // Wait for the server to start.
+
+	// Wait for the server to start
+	retry.WithBackoff(c.GetTestContext(), func(ctx context.Context) error {
+		client := &http.Client{}
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("http://localhost:%d/health", c.port), nil)
+		c.Require().NoError(err)
+		_, err = client.Do(req)
+		return err
+	}, retry.WithMaxTotalTime(10*time.Second))
 }
 
 // prepareAuthHeader generates an authorization header using EIP191 signature with the given private key.
