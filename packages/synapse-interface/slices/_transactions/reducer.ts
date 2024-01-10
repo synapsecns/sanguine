35c8007c1a7ebb8be33612e74055b061097b8e57
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2024-01-10 01:15:33 +0000

    check if state.transactions exists

--- packages/synapse-interface/slices/_transactions/reducer.ts
+++ packages/synapse-interface/slices/_transactions/reducer.ts
@@ -30,7 +30,10 @@ export const transactionsSlice = createSlice({
   initialState,
   reducers: {
     addTransaction: (state, action: PayloadAction<any>) => {
-      state.transactions.push(action.payload)
+      if (!Array.isArray(state.transactions)) {
+        state.transactions = []; // Initialize to an empty array if not already an array
+      }
+      state.transactions.push(action.payload);
     },
     removeTransaction: (
       state,
