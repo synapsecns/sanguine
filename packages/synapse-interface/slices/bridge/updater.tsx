d011e05a544ab41b5a2fa3585c356e12f886bc9b
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-01-11 18:02:06 -0800

    rm

--- packages/synapse-interface/slices/bridge/updater.tsx
+++ packages/synapse-interface/slices/bridge/updater.tsx
@@ -18,18 +18,7 @@ import { hasOnlyZeroes } from '@/utils/hasOnlyZeroes'
 
 export default function Updater(): null {
   const dispatch = useAppDispatch()
-  const { synapseSDK } = useSynapseContext()
-  const {
-    fromChainId,
-    toChainId,
-    fromToken,
-    toToken,
-    toTokens,
-    fromValue,
-    bridgeQuote,
-    debouncedFromValue,
-    debouncedToTokensFromValue,
-  }: BridgeState = useBridgeState()
+  const { fromValue, debouncedFromValue }: BridgeState = useBridgeState()
 
   /**
    * Debounce user input to fetch primary bridge quote (in ms)
@@ -69,80 +58,5 @@ export default function Updater(): null {
     }
   }, [debouncedFromValue])
 
-  // Conditions for fetching alternative bridge quotes
-  // useEffect(() => {
-  //   const userInputExists: boolean =
-  //     debouncedToTokensFromValue !== initialState.debouncedToTokensFromValue
-  //   const userInputIsZero: boolean = hasOnlyZeroes(debouncedFromValue)
-
-  //   if (!userInputIsZero) {
-  //     if (fromChainId && toChainId && fromToken && toToken && synapseSDK) {
-  //       const bridgeQuoteRequests: BridgeQuoteRequest[] = toTokens.map(
-  //         (token: Token) => {
-  //           return {
-  //             originChainId: fromChainId,
-  //             originToken: fromToken as Token,
-  //             destinationChainId: toChainId,
-  //             destinationTokenAddress: token?.addresses[toChainId] as Address,
-  //             destinationToken: token as Token,
-  //             amount: stringToBigInt(
-  //               userInputExists
-  //                 ? debouncedToTokensFromValue
-  //                 : getDefaultBridgeAmount(fromToken),
-  //               fromToken?.decimals[fromChainId]
-  //             ),
-  //           }
-  //         }
-  //       )
-  //       dispatch(
-  //         fetchAndStoreBridgeQuotes({
-  //           requests: bridgeQuoteRequests,
-  //           synapseSDK,
-  //         })
-  //       )
-  //     }
-  //   }
-
-  //   if (!fromToken || !userInputExists) {
-  //     dispatch(resetFetchedBridgeQuotes())
-  //   }
-  // }, [debouncedToTokensFromValue, toTokens])
-
-  // Clear bridge quote if input is empty
-  // useEffect(() => {
-  //   if (debouncedFromValue === initialState.debouncedFromValue) {
-  //     dispatch(resetBridgeQuote())
-  //   }
-  // }, [debouncedFromValue, bridgeQuote])
-
   return null
-}
-
-enum SwappableTypes {
-  STABLE = 'USD',
-  ETH = 'ETH',
-  BTC = 'WBTC',
-}
-
-enum DefaultBridgeAmount {
-  STABLE = '50',
-  ETH = '0.01',
-  BTC = '0.001',
-}
-
-export const getDefaultBridgeAmount = (
-  originToken: Token
-): DefaultBridgeAmount => {
-  const swappableType: string = originToken.swapableType
-
-  switch (swappableType) {
-    case SwappableTypes.STABLE:
-      return DefaultBridgeAmount.STABLE
-    case SwappableTypes.ETH:
-      return DefaultBridgeAmount.ETH
-    case SwappableTypes.BTC:
-      return DefaultBridgeAmount.BTC
-    default:
-      return DefaultBridgeAmount.STABLE
-  }
 }
