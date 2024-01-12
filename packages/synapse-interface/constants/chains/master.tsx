e79cca3540bebce30dd2dfcaefff60ec7ae5fab5
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-01-11 17:08:36 -0800

    Refetch balances after resolved tx

--- packages/synapse-interface/constants/chains/master.tsx
+++ packages/synapse-interface/constants/chains/master.tsx
@@ -155,7 +155,7 @@ export const OPTIMISM: Chain = {
   codeName: 'optimism',
   blockTime: 2000,
   rpcUrls: {
-    primary: 'https://optimism.llamarpc.com',
+    primary: 'https://optimism.publicnode.com',
     fallback: 'https://1rpc.io/op',
   },
   nativeCurrency: { name: 'Ethereum', symbol: 'ETH', decimals: 18 },
