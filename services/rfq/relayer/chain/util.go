ee72578a2da8bc3049510a965950d001149075e9
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-01-17 11:53:09 -0600

    Cleanup: lint

--- services/rfq/relayer/chain/util.go
+++ services/rfq/relayer/chain/util.go
@@ -2,6 +2,7 @@ package chain
 
 import "github.com/ethereum/go-ethereum/common"
 
+// EthAddress is the address of a chain's native gas token.
 var EthAddress = common.HexToAddress("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
 
 // IsGasToken returns true if the given token is the gas token.
