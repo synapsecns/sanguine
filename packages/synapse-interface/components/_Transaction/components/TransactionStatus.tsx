c4b6cd790965b3aaef7697b572e6ce94badfb2bf
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-01-19 15:36:33 -0800

    Update TransactionStatus

--- packages/synapse-interface/components/_Transaction/components/TransactionStatus.tsx
+++ packages/synapse-interface/components/_Transaction/components/TransactionStatus.tsx
@@ -1,3 +1,13 @@
-export const TransactionStatus = ({ string }) => {
-  return <div id="transaction-status">{string}</div>
+const TransactionStatus = ({
+  string,
+  className,
+}: {
+  string: string
+  className?: string
+}) => {
+  return (
+    <div id="transaction-status" className={className}>
+      {string}
+    </div>
+  )
 }
