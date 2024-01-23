beecf1b0f582feb04c3cf0638ee5b40a2d027566
abtestingalpha <abtestingalpha@gmail.com>
2024-01-22 19:48:54 -0500

    new webpack config and updated next config

--- packages/synapse-constants/webpack.config.js
+++ packages/synapse-constants/webpack.config.js
@@ -1,83 +1,39 @@
 const path = require('path')
 
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
-
-const config = {
-  stats: {
-    errorDetails: true,
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx'],
-  },
+module.exports = {
   mode: 'production',
-  entry: './dist/index.js',
+
+  entry: './dist/',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  target: 'web',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    modules: [path.resolve(__dirname, '../../node_modules')],
+  },
+
   module: {
     rules: [
       {
-        test: /\.(js|ts)$/,
+        test: /\.ts|tsx$/,
+        loader: 'ts-loader',
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-        type: 'asset',
       },
       {
         test: /\.svg$/,
-        use:{
-          loader: 'svg-inline-loader',
-        },
-        type: 'asset',
+        loader: 'svg-inline-loader',
       },
       {
-        test: /\.(png|jpe?g)$/i,
-        use: {
-          loader: 'url-loader',
-        },
+        test: /\.(png|jpg|jpeg|gif)$/,
         type: 'asset',
       },
     ],
   },
+
   optimization: {
-    minimizer: [
-      '...',
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            // Lossless optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ['optipng', { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-              [
-                'svgo',
-                {
-                  plugins: [
-                    {
-                      name: 'preset-default',
-                      params: {
-                        overrides: {
-                          removeViewBox: false,
-                          inlineStyles: {
-                            onlyMatchedOnce: false,
-                          },
-                        },
-                      },
-                    },
-                  ],
-                },
-              ],
-            ],
-          },
-        },
-      }),
-    ],
+    minimize: true,
   },
 }
-
-module.exports = config
