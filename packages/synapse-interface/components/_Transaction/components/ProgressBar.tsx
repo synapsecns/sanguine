27e5283266e48260416553a924d51766fb1d764c
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-01-24 14:05:41 -0800

    ProgressBar filled based on props

--- packages/synapse-interface/components/_Transaction/components/ProgressBar.tsx
+++ packages/synapse-interface/components/_Transaction/components/ProgressBar.tsx
@@ -1,7 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+type ProgressBarProps = {
+  elapsedTime: number
+  estimatedTotalTime: number
+}
+
 export const ProgressBar = ({
   elapsedTime,
-  currentTime,
   estimatedTotalTime,
-}) => {
-  return <div id="progress-bar"></div>
+}: ProgressBarProps) => {
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((elapsedTime / estimatedTotalTime) * 100)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [elapsedTime, estimatedTotalTime])
+
+  return (
+    <div id="progress-bar" className="w-full h-5 bg-gray-200">
+      <div
+        style={{ width: `${progress}%`, transition: 'width 1s ease-in-out' }}
+        className="h-full bg-green-300"
+      ></div>
+    </div>
+  )
 }
