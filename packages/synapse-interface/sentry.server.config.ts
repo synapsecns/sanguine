03b6be80559f337ebb66e2b5697e0fb1b2ecd60e
Trajan0x <trajan0x@users.noreply.github.com>
2024-01-25 13:26:07 -0500

    sentry

---
+++ packages/synapse-interface/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8925a8c7b5b5c4c39fa08134d184d722@o4506632893431808.ingest.sentry.io/4506632899067904",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
