a59806bf8745908cd0744b852863ebfb018edf90
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-01-26 14:45:18 -0600

    [goreleaser] Fix: handle ETH in getDecimals()

--- services/rfq/relayer/service/chainindexer.go
+++ services/rfq/relayer/service/chainindexer.go
@@ -9,6 +9,7 @@ import (
 	"github.com/synapsecns/sanguine/core/metrics"
 	"github.com/synapsecns/sanguine/services/rfq/contracts/fastbridge"
 	"github.com/synapsecns/sanguine/services/rfq/contracts/ierc20"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/chain"
 	"github.com/synapsecns/sanguine/services/rfq/relayer/reldb"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
@@ -100,6 +101,8 @@ func (r *Relayer) runChainIndexer(ctx context.Context, chainID int) (err error) 
 	return nil
 }
 
+const ethDecimals = 18
+
 // getDecimals gets the decimals for the origin and dest tokens.
 func (r *Relayer) getDecimals(parentCtx context.Context, bridgeTx fastbridge.IFastBridgeBridgeTransaction) (_ *decimalsRes, err error) {
 	ctx, span := r.metrics.Tracer().Start(parentCtx, "getDecimals", trace.WithAttributes(
@@ -111,46 +114,63 @@ func (r *Relayer) getDecimals(parentCtx context.Context, bridgeTx fastbridge.IFa
 	}()
 
 	// TODO: add a cache for decimals.
+	var originERC20, destERC20 *ierc20.IERC20
 	res := decimalsRes{}
 
-	// TODO: cleanup duplication, but keep paralellism.
-	// this is  a bit of a pain since it deals w/ 3 different fields, but shouldn't take too long
-	originClient, err := r.client.GetChainClient(ctx, int(bridgeTx.OriginChainId))
-	if err != nil {
-		return nil, fmt.Errorf("could not get origin client: %w", err)
+	if bridgeTx.OriginToken == chain.EthAddress {
+		res.originDecimals = ethDecimals
+	} else {
+		// TODO: cleanup duplication, but keep paralellism.
+		// this is  a bit of a pain since it deals w/ 3 different fields, but shouldn't take too long
+		originClient, err := r.client.GetChainClient(ctx, int(bridgeTx.OriginChainId))
+		if err != nil {
+			return nil, fmt.Errorf("could not get origin client: %w", err)
+		}
+		originERC20, err = ierc20.NewIERC20(bridgeTx.OriginToken, originClient)
+		if err != nil {
+			return nil, fmt.Errorf("could not get origin token")
+		}
 	}
 
-	destClient, err := r.client.GetChainClient(ctx, int(bridgeTx.DestChainId))
-	if err != nil {
-		return nil, fmt.Errorf("could not get dest client: %w", err)
+	if bridgeTx.DestToken == chain.EthAddress {
+		res.destDecimals = ethDecimals
+	} else {
+		destClient, err := r.client.GetChainClient(ctx, int(bridgeTx.DestChainId))
+		if err != nil {
+			return nil, fmt.Errorf("could not get dest client: %w", err)
+		}
+		destERC20, err = ierc20.NewIERC20(bridgeTx.DestToken, destClient)
+		if err != nil {
+			return nil, fmt.Errorf("could not get dest token")
+		}
 	}
 
-	originERC20, err := ierc20.NewIERC20(bridgeTx.OriginToken, originClient)
-	if err != nil {
-		return nil, fmt.Errorf("could not get origin token")
+	// return early if both dest and origin are ETH.
+	if originERC20 == nil && destERC20 == nil {
+		return &res, nil
 	}
 
-	destERC20, err := ierc20.NewIERC20(bridgeTx.DestToken, destClient)
-	if err != nil {
-		return nil, fmt.Errorf("could not get dest token")
-	}
-
+	// do rpc calls to fetch the erc20 decimals.
 	g, ctx := errgroup.WithContext(ctx)
-	g.Go(func() error {
-		res.originDecimals, err = originERC20.Decimals(&bind.CallOpts{Context: ctx})
-		if err != nil {
-			return fmt.Errorf("could not get dest decimals: %w", err)
-		}
-		return nil
-	})
+	if originERC20 != nil {
+		g.Go(func() error {
+			res.originDecimals, err = originERC20.Decimals(&bind.CallOpts{Context: ctx})
+			if err != nil {
+				return fmt.Errorf("could not get dest decimals: %w", err)
+			}
+			return nil
+		})
+	}
 
-	g.Go(func() error {
-		res.destDecimals, err = destERC20.Decimals(&bind.CallOpts{Context: ctx})
-		if err != nil {
-			return fmt.Errorf("could not get origin decimals: %w", err)
-		}
-		return nil
-	})
+	if destERC20 != nil {
+		g.Go(func() error {
+			res.destDecimals, err = destERC20.Decimals(&bind.CallOpts{Context: ctx})
+			if err != nil {
+				return fmt.Errorf("could not get origin decimals: %w", err)
+			}
+			return nil
+		})
+	}
 
 	err = g.Wait()
 	if err != nil {
