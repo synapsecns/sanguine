4b267ce2578089ea31192e3e5f78cb223e7722cf
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-01-30 09:58:06 -0800

    Add DialogWrapper for opaque bg covering page

--- packages/synapse-interface/components/Activity/AirdropRewards.tsx
+++ packages/synapse-interface/components/Activity/AirdropRewards.tsx
@@ -100,9 +100,6 @@ export const AirdropRewards = () => {
     }
   }, [connectedAddress])
 
-  // console.log('rewards:', rewards)
-  // console.log('transactions:', transactions)
-
   const [open, setOpen] = useState<boolean>(false)
 
   const handleToggle = () => {
@@ -145,6 +142,18 @@ export const AirdropRewards = () => {
   )
 }
 
+const DialogWrapper = ({ open, children }) => {
+  return (
+    <div
+      className={`${
+        open &&
+        'fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80'
+      }`}
+    >
+      {children}
+    </div>
+  )
+}
 const RewardsDialog = ({
   open,
   setOpen,
@@ -165,71 +174,73 @@ const RewardsDialog = ({
   useCloseOnOutsideClick(dialogRef, onClose)
 
   return (
-    <dialog
-      id="rewards-dialog"
-      ref={dialogRef}
-      open={open}
-      className="fixed top-[40%] z-50 p-4 text-white border rounded-lg bg-background w-96 border-separator cursor-default"
-    >
-      <div className="space-y-4">
-        <div className="flex justify-between mb-2">
-          <div className="text-2xl">ARB Rewards</div>
-          <CloseButton onClick={onClose} />
-        </div>
+    <DialogWrapper open={open}>
+      <dialog
+        id="rewards-dialog"
+        ref={dialogRef}
+        open={open}
+        className="absolute top-[40%] z-50 p-4 text-white border rounded-lg bg-background w-96 border-separator cursor-default"
+      >
+        <div className="space-y-4">
+          <div className="flex justify-between mb-2">
+            <div className="text-2xl">ARB Rewards</div>
+            <CloseButton onClick={onClose} />
+          </div>
 
-        <p>
-          Through Mar 31, ARB rewards are automatically applied to select routes
-          to and from Arbitrum.
-        </p>
+          <p>
+            Through Mar 31, ARB rewards are automatically applied to select
+            routes to and from Arbitrum.
+          </p>
 
-        <p>
-          Click{' '}
-          <Link
-            href="https://synapse.mirror.xyz/NpzSkXDUlistuxNQaMwP6HQ9k2gVJsI-G1Y7-gaLxfQ"
-            target="_blank"
-            className="underline text-blueText"
-          >
-            here
-          </Link>{' '}
-          for full route and rebate inforamtion.
-        </p>
+          <p>
+            Click{' '}
+            <Link
+              href="https://synapse.mirror.xyz/NpzSkXDUlistuxNQaMwP6HQ9k2gVJsI-G1Y7-gaLxfQ"
+              target="_blank"
+              className="underline text-blueText"
+            >
+              here
+            </Link>{' '}
+            for full route and rebate inforamtion.
+          </p>
 
-        <div className="flex text-left">
-          <div className="w-1/2">
-            <div className="text-lg text-greenText">Total Arb</div>
-            <div className="flex space-x-1">
-              <div className="text-2xl text-greenText">+{rewards}</div>
-              <div className="text-2xl text-secondary">
-                (${convertTokensToDollarValue(rewards, tokenPrice)})
+          <div className="flex text-left">
+            <div className="w-1/2">
+              <div className="text-lg text-greenText">Total Arb</div>
+              <div className="flex space-x-1">
+                <div className="text-2xl text-greenText">+{rewards}</div>
+                <div className="text-2xl text-secondary">
+                  (${convertTokensToDollarValue(rewards, tokenPrice)})
+                </div>
               </div>
             </div>
+            <div className="w-1/2">
+              <div className="text-lg">Days remaining</div>
+              <div className="text-2xl">{calculateDaysUntilStipEnds()}</div>
+            </div>
           </div>
-          <div className="w-1/2">
-            <div className="text-lg">Days remaining</div>
-            <div className="text-2xl">{calculateDaysUntilStipEnds()}</div>
-          </div>
-        </div>
 
-        <AirdropTxHeader />
-        {_.isEmpty(transactions) ? (
-          <div className="text-center text-secondary">
-            Individual rebates will appear here
-          </div>
-        ) : (
-          transactions.map((transaction) => (
-            <AirdropTransaction
-              transactionHash={transaction.transactionHash}
-              tokenValue={parseTokenValue(
-                transaction.transferValue,
-                ARB.decimals
-              )} // TODO: Make dynamic so we do not hardcode decimals
-              tokenPrice={tokenPrice}
-              explorerUrl={ARB.explorerUrl}
-            />
-          ))
-        )}
-      </div>
-    </dialog>
+          <AirdropTxHeader />
+          {_.isEmpty(transactions) ? (
+            <div className="text-center text-secondary">
+              Individual rebates will appear here
+            </div>
+          ) : (
+            transactions.map((transaction) => (
+              <AirdropTransaction
+                transactionHash={transaction.transactionHash}
+                tokenValue={parseTokenValue(
+                  transaction.transferValue,
+                  ARB.decimals
+                )} // TODO: Make dynamic so we do not hardcode decimals
+                tokenPrice={tokenPrice}
+                explorerUrl={ARB.explorerUrl}
+              />
+            ))
+          )}
+        </div>
+      </dialog>
+    </DialogWrapper>
   )
 }
 
