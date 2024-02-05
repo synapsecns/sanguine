83e88bf0913793cd9109dfc3000cbb41fac4b969
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-05 10:10:08 -0800

    better cursor styling logic

--- packages/widget/src/components/ui/ToggleTabs.tsx
+++ packages/widget/src/components/ui/ToggleTabs.tsx
@@ -15,27 +15,27 @@ export const ToggleTabs: React.FC<ToggleTabsProps> = ({
 }) => {
   const { protocolName } = useBridgeState()
   const baseTabClass = 'basis-full rounded p-1.5'
+  const activeTabClass = `${baseTabClass} cursor-pointer hover:opacity-70`
   const activeStyle = {
     background: 'var(--synapse-button-bg)',
     color: 'var(--synapse-button-text',
-    cursor: 'default',
   }
 
   return (
     <div
-      className="flex mx-1 my-2 text-sm text-center cursor-pointer"
+      className="flex mx-1 my-2 text-sm text-center cursor-default"
       style={{ background: 'var(--synapse-root'}}
       role="group"
     >
       <div
-        className={`${baseTabClass} ${selectedTab !== 'All' && 'hover:opacity-70'}`}
+        className={selectedTab === 'All' ? baseTabClass : activeTabClass}
         style={selectedTab === 'All' ? activeStyle : {}}
         onClick={() => onTabSelect('All')}
       >
         All
       </div>
       <div
-        className={`${baseTabClass} ${selectedTab !== 'Target' && 'hover:opacity-70'}`}
+        className={selectedTab === 'Target' ? baseTabClass : activeTabClass}
         style={selectedTab === 'Target' ? activeStyle : {}}
         onClick={() => onTabSelect('Target')}
       >
