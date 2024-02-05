c2d5d473dafc9a1f3583e589f499bbeb623ccf72
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-05 10:03:08 -0800

    tab appearance refinements

--- packages/widget/src/components/ui/ToggleTabs.tsx
+++ packages/widget/src/components/ui/ToggleTabs.tsx
@@ -14,54 +14,33 @@ export const ToggleTabs: React.FC<ToggleTabsProps> = ({
   isOrigin,
 }) => {
   const { protocolName } = useBridgeState()
-  const baseTabClass =
-    'flex-grow text-sm font-medium text-center text-[--synapse-primary] rounded-sm p-1 '
-
-  const activeTabClass = 'bg-[var(--synapse-surface)]'
-  const inactiveTabClass =
-    'bg-[var(--synapse-select-bg)] hover:bg-[var-(--synapse-surface)] hover:cursor-pointer'
+  const baseTabClass = 'basis-full rounded p-1.5'
+  const activeStyle = {
+    background: 'var(--synapse-button-bg)',
+    color: 'var(--synapse-button-text',
+    cursor: 'default',
+  }
 
   return (
-    <div className="flex mt-2 mb-2" role="group">
-      {isOrigin ? (
-        <>
-          <div
-            className={`${baseTabClass} ${
-              selectedTab === 'All' ? activeTabClass : inactiveTabClass
-            }`}
-            onClick={() => onTabSelect('All')}
-          >
-            All
-          </div>
-          <div
-            className={`${baseTabClass} ${
-              selectedTab === 'Target' ? activeTabClass : inactiveTabClass
-            }`}
-            onClick={() => onTabSelect('Target')}
-          >
-            {protocolName ?? 'Target'}
-          </div>
-        </>
-      ) : (
-        <>
-          <div
-            className={`${baseTabClass} ${
-              selectedTab === 'Target' ? activeTabClass : inactiveTabClass
-            }`}
-            onClick={() => onTabSelect('Target')}
-          >
-            {protocolName ?? 'Target'}
-          </div>
-          <div
-            className={`${baseTabClass} ${
-              selectedTab === 'All' ? activeTabClass : inactiveTabClass
-            }`}
-            onClick={() => onTabSelect('All')}
-          >
-            All
-          </div>
-        </>
-      )}
+    <div
+      className="flex mx-1 my-2 text-sm text-center cursor-pointer"
+      style={{ background: 'var(--synapse-root'}}
+      role="group"
+    >
+      <div
+        className={`${baseTabClass} ${selectedTab !== 'All' && 'hover:opacity-70'}`}
+        style={selectedTab === 'All' ? activeStyle : {}}
+        onClick={() => onTabSelect('All')}
+      >
+        All
+      </div>
+      <div
+        className={`${baseTabClass} ${selectedTab !== 'Target' && 'hover:opacity-70'}`}
+        style={selectedTab === 'Target' ? activeStyle : {}}
+        onClick={() => onTabSelect('Target')}
+      >
+        {protocolName ?? 'Target'}
+      </div>
     </div>
   )
 }
