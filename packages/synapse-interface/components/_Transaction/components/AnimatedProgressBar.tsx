0750efe20761bb66eb8ba58f1162f7504527c275
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-02-06 14:14:47 -0800

    Update ProgressBar animation

--- packages/synapse-interface/components/_Transaction/components/AnimatedProgressBar.tsx
+++ packages/synapse-interface/components/_Transaction/components/AnimatedProgressBar.tsx
@@ -33,28 +33,63 @@ export const AnimatedProgressBar = memo(
           height="3"
           xmlns="http://www.w3.org/2000/svg"
           className="rounded-sm"
+          style={{ background: '#444' }}
         >
-          <rect width="100%" height="100%" fill="#444" />
-          <rect height="100%">
-            <animate
-              attributeName="width"
-              values={`${percentElapsed}%; 100%`}
-              dur={duration}
-              fill="freeze"
-              calcMode={isComplete && 'spline'}
-              keySplines=".8 0 .2 1;"
-            />
-            <animate
-              attributeName="fill"
-              values={`${synapsePurple}; ${
-                isComplete
-                  ? `hsl(185deg 100% 40%); hsl(105deg 100% 60%)`
-                  : `${synapsePurple}; hsl(15deg 100% 65%)`
-              }`}
-              keyTimes={`0; ${isComplete ? 0.5 : 1}; 1`}
-              dur={duration}
-              fill="freeze"
-            />
+          <defs>
+            <linearGradient id="progress" spreadMethod="reflect" x1="0" x2="1">
+              <stop stop-color={synapsePurple} />
+              <stop stop-color={synapsePurple} offset=".25" />
+              <stop
+                stop-color={synapsePurple}
+                stop-opacity=".67"
+                offset=".75"
+              />
+              <stop stop-color={synapsePurple} stop-opacity=".67" offset="1" />
+              <animate
+                attributeName="x1"
+                values="0%; -6%"
+                dur=".67s"
+                repeatCount="indefinite"
+              />
+              <animate
+                attributeName="x2"
+                values="3%; -3%"
+                dur=".67s"
+                repeatCount="indefinite"
+              />
+            </linearGradient>
+            <clipPath id="mask">
+              <rect height="100%">
+                <animate
+                  attributeName="width"
+                  values={`${percentElapsed}%; 100%`}
+                  dur={duration}
+                  fill="freeze"
+                  calcMode={isComplete && 'spline'}
+                  keySplines=".8 0 .2 1;"
+                />
+              </rect>
+            </clipPath>
+          </defs>
+          <rect
+            width="100%"
+            height="2"
+            fill="url(#progress)"
+            clip-path="url(#mask)"
+          >
+            {isComplete && (
+              <animate
+                attributeName="fill"
+                values={`${synapsePurple}; ${
+                  isComplete
+                    ? `hsl(185deg 100% 40%); hsl(105deg 100% 60%)`
+                    : `${synapsePurple}; hsl(15deg 100% 65%)`
+                }`}
+                keyTimes={`0; .5; 1`}
+                dur={duration}
+                fill="freeze"
+              />
+            )}
           </rect>
         </svg>
       </div>
