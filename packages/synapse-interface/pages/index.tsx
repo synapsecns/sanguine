286077da89a375de2491f1c135b358eaf814c4e7
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-02-09 13:25:29 -0800

    Rm testing code

--- packages/synapse-interface/pages/index.tsx
+++ packages/synapse-interface/pages/index.tsx
@@ -5,8 +5,6 @@ import { LandingPageWrapper } from '@/components/layouts/LandingPageWrapper'
 import ReactGA from 'react-ga'
 import useSyncQueryParamsWithBridgeState from '@/utils/hooks/useSyncQueryParamsWithBridgeState'
 
-import { TestProgressBar } from '@/components/_Transaction/components/TestProgressBar'
-
 // TODO: someone should add this to the .env, disable if blank, etc.
 // this is being added as a hotfix to assess user load on the synapse explorer api
 // I'd recommend moving this to a sushi-style analytics provider wrapper.
@@ -22,7 +20,6 @@ const Home = () => {
         data-test-id="bridge-page"
         className="relative z-0 flex-1 h-full overflow-y-auto focus:outline-none"
       >
-        <TestProgressBar />
         <Banner />
         <div className="flex flex-col-reverse justify-center gap-16 px-4 py-20 mx-auto lg:flex-row 2xl:w-3/4 sm:mt-6 sm:px-8 md:px-12">
           <Portfolio />
