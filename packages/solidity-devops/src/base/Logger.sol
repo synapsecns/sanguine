bf822e0459a6f68800ff9227fd8023fed8ae8077
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-12 12:53:26 +0000

    Move base script templates

---
+++ packages/solidity-devops/src/base/Logger.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12;
+pragma experimental ABIEncoderV2;
+
+import {StringUtils} from "../libs/StringUtils.sol";
+
+import {console2} from "forge-std/Script.sol";
+
+abstract contract Logger {
+    using StringUtils for *;
+
+    string private constant TAB = "    ";
+
+    /// @notice Current indent level for all log messages
+    uint256 private currentIndentLevel;
+
+    modifier withIndent() {
+        increaseIndent();
+        _;
+        decreaseIndent();
+    }
+
+    /// @notice Returns the current indent string.
+    /// @dev Handy if the log message contains arguments other than strings and printLog() is not used.
+    function currentIndent() internal view returns (string memory) {
+        return TAB.duplicate(currentIndentLevel);
+    }
+
+    /// @notice Increases the indent level for all log messages.
+    function increaseIndent() internal {
+        ++currentIndentLevel;
+    }
+
+    /// @notice Decreases the indent level for all log messages.
+    function decreaseIndent() internal {
+        require(currentIndentLevel > 0, "Indent level cannot be negative");
+        --currentIndentLevel;
+    }
+
+    /// @notice Prints the log message with the current indent level.
+    function printLog(string memory logString) internal view {
+        console2.log("%s%s", currentIndent(), logString);
+    }
+
+    /// @notice Prints the informational log message with the current indent level.
+    function printInfo(string memory logString) internal view {
+        printLog(StringUtils.concat(unicode"💬 ", logString));
+    }
+
+    /// @notice Prints the log message with the current indent level plus one.
+    function printLogWithIndent(string memory logString) internal view {
+        printLog(TAB.concat(logString));
+    }
+
+    /// @notice Prints the "skipping" message with the current indent level plus one.
+    function printSkipWithIndent(string memory reason) internal view {
+        printLogWithIndent(StringUtils.concat(unicode"🟡 Skipping: ", reason));
+    }
+
+    /// @notice Prints the "fail" message with the current indent level plus one.
+    function printFailWithIndent(string memory logString) internal view {
+        printLogWithIndent(StringUtils.concat(unicode"❌ ", logString));
+    }
+
+    /// @notice Prints the "success" message with the current indent level plus one.
+    function printSuccessWithIndent(string memory logString) internal view {
+        printLogWithIndent(StringUtils.concat(unicode"✅ ", logString));
+    }
+}
