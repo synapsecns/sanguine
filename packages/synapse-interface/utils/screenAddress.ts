d9b47d75ae9d95935cfe045ecc46df5f9a20900b
abtestingalpha <abtestingalpha@gmail.com>
2024-02-12 11:41:57 -0500

    Risk event handling with message

--- packages/synapse-interface/utils/screenAddress.ts
+++ packages/synapse-interface/utils/screenAddress.ts
@@ -1,5 +1,8 @@
 import { Address } from 'viem'
 
+import { GlobalEventEmitter } from '@/utils/globalEventEmitter'
+import { DISCORD_URL } from '@/constants/urls'
+
 export const screenAddress = (address: Address | string) => {
   const url = `https://screener.omnirpc.io/fe/address/${address}`
 
@@ -9,7 +12,13 @@ export const screenAddress = (address: Address | string) => {
     .then((response) => response.json())
     .then(({ risk }) => {
       if (risk) {
-        document.body = document.createElement('body')
+        const event = new CustomEvent('riskDetected', {
+          detail: {
+            message: `Your address is considered at risk. Please contact <a style="text-decoration: underline; text-underline-offset: 0.2em;" href=${DISCORD_URL} target="_blank" rel="noopener noreferrer">support</a> if you feel this message is in error.`,
+          },
+        })
+
+        GlobalEventEmitter.dispatchEvent(event)
       }
     })
     .catch((error) => console.error('Error:', error))
