e7caa097b6218d68d0182eba7e4d0076d25d7b4c
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-02-12 16:22:59 -0600

    WIP: supply mock call data

--- services/rfq/relayer/pricer/calls.go
+++ services/rfq/relayer/pricer/calls.go
@@ -1,15 +1,38 @@
 package pricer
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/synapsecns/sanguine/core/retry"
 	"github.com/synapsecns/sanguine/ethergo/util"
 	"github.com/synapsecns/sanguine/services/rfq/contracts/fastbridge"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/chain"
 )
+
+const mockRequestData = "0x000000000000000000000000000000000000000000000000000000000000a4b1000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000b73acb429ba868984c0236bdf940d4fe1e643f27000000000000000000000000b73acb429ba868984c0236bdf940d4fe1e643f27000000000000000000000000af88d065e77c8cc2239327c5edb3a432268e58310000000000000000000000000b2c639c533813f4aa9d7837caf62653d097ff8500000000000000000000000000000000000000000000000000000000002dc6c00000000000000000000000000000000000000000000000000000000000104d35000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000065a17be3000000000000000000000000000000000000000000000000000000000000001c"
+
+func getCallRequestData() []byte {
+	data, err := hexutil.Decode(mockRequestData)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func getCallAddress() common.Address {
+	return chain.EthAddress
+}
+
+func getCallHash() [32]byte {
+	return [32]byte{}
+}
 
 type callType int
 
@@ -36,16 +59,38 @@ func (c callType) String() string {
 
 func getCall(transactor *bind.TransactOpts, bridge *fastbridge.FastBridgeRef, cType callType) (call *ethereum.CallMsg, err error) {
 	var tx *types.Transaction
+	var callFunc func(context.Context) error
 	switch cType {
 	case claimCallType:
-		tx, err = bridge.Claim(transactor, []byte{}, common.HexToAddress(""))
+		callFunc = func(context.Context) error {
+			tx, err = bridge.Claim(transactor, getCallRequestData(), getCallAddress())
+			if err != nil {
+				fmt.Printf("inner call err: %v\n", err)
+			}
+			return err
+		}
 	case proveCallType:
-		tx, err = bridge.Prove(transactor, []byte{}, [32]byte{})
+		callFunc = func(context.Context) error {
+			tx, err = bridge.Prove(transactor, getCallRequestData(), getCallHash())
+			if err != nil {
+				fmt.Printf("inner call err: %v\n", err)
+			}
+			return err
+		}
 	case relayCallType:
-		tx, err = bridge.Relay(transactor, []byte{})
+		callFunc = func(context.Context) error {
+			tx, err = bridge.Relay(transactor, getCallRequestData())
+			if err != nil {
+				fmt.Printf("inner call err: %v\n", err)
+			}
+			return err
+		}
 	default:
 		return nil, fmt.Errorf("unknown call type: %d", cType)
 	}
+	err = retry.WithBackoff(transactor.Context, func(ctx context.Context) error {
+		return callFunc(transactor.Context)
+	}, retry.WithMaxTotalTime(5*time.Second))
 	if err != nil {
 		fmt.Printf("call err: %v\n", err)
 		return nil, fmt.Errorf("could not get tx with type %s: %w", cType.String(), err)
