92230ce1e478e38d9597177bdded50d964870443
caesar <caesar0x@users.noreply.github.com>
2024-02-14 11:10:15 -0500

    rm more redundant

--- packages/synapse-interface/constants/bridge.ts
+++ packages/synapse-interface/constants/bridge.ts
@@ -20,45 +20,12 @@ export const EMPTY_BRIDGE_QUOTE = {
 }
 
 export const EMPTY_BRIDGE_QUOTE_ZERO = {
-  outputAmount: 0n,
+  ...EMPTY_BRIDGE_QUOTE,
   outputAmountString: '0',
-  routerAddress: '',
-  allowance: 0n,
-  exchangeRate: 0n,
-  feeAmount: 0n,
-  delta: 0n,
-  originQuery: null,
-  destQuery: null,
-  estimatedTime: null,
-  bridgeModuleName: null,
-  gasDropAmount: 0n,
 }
 
 
-/**
- * number of required confirmations from bridge
- */
-export const BRIDGE_REQUIRED_CONFIRMATIONS = {
-  [CHAINS.ETH.id]:       33,
-  [CHAINS.BNB.id]:       33,
-  [CHAINS.POLYGON.id]:   128,
-  [CHAINS.FANTOM.id]:    80,
-  [CHAINS.BOBA.id]:      33,
-  [CHAINS.OPTIMISM.id]:  750,
-  [CHAINS.MOONBEAM.id]:  33,
-  [CHAINS.MOONRIVER.id]: 33,
-  [CHAINS.ARBITRUM.id]:  200,
-  [CHAINS.AVALANCHE.id]: 80,
-  [CHAINS.DFK.id]:       33,
-  [CHAINS.HARMONY.id]:   33,
-  [CHAINS.AURORA.id]:    33,
-  [CHAINS.CRONOS.id]:    33,
-  [CHAINS.METIS.id]:     33,
-  [CHAINS.DOGE.id]:      33,
-  [CHAINS.CANTO.id]:     20,
-  [CHAINS.BASE.id]:      750,
-  [CHAINS.KLAYTN.id]:    20,
-}
+
 
 export const DEFAULT_FROM_TOKEN_SYMBOL = 'USDC'
 export const DEFAULT_TO_TOKEN_SYMBOL = 'USDC'
--- packages/synapse-interface/constants/swap.ts
+++ packages/synapse-interface/constants/swap.ts
@@ -12,36 +12,10 @@ export const EMPTY_SWAP_QUOTE: SwapQuote = {
   quote: null,
 }
 export const EMPTY_SWAP_QUOTE_ZERO: SwapQuote = {
-  outputAmount: 0n,
+  ...EMPTY_SWAP_QUOTE,
   outputAmountString: '0',
-  routerAddress: '',
-  allowance: 0n,
-  exchangeRate: 0n,
-  delta: 0n,
-  quote: null,
 }
-/**
- * number of required confirmations from bridge
- */
-export const BRIDGE_REQUIRED_CONFIRMATIONS = {
-  [CHAINS.ETH.id]: 33,
-  [CHAINS.BNB.id]: 14,
-  [CHAINS.POLYGON.id]: 128,
-  [CHAINS.FANTOM.id]: 5,
-  [CHAINS.BOBA.id]: 1, // rewrite
-  [CHAINS.OPTIMISM.id]: 1, // rewrite
-  [CHAINS.MOONBEAM.id]: 21,
-  [CHAINS.MOONRIVER.id]: 21, // 5,
-  [CHAINS.ARBITRUM.id]: 40,
-  [CHAINS.AVALANCHE.id]: 5,
-  [CHAINS.DFK.id]: 6,
-  [CHAINS.HARMONY.id]: 1, // rewrite
-  [CHAINS.AURORA.id]: 5,
-  [CHAINS.CRONOS.id]: 6,
-  [CHAINS.METIS.id]: 6,
-  [CHAINS.DOGE.id]: 20,
-  [CHAINS.CANTO.id]: 20,
-}
+
 
 export const DEFAULT_FROM_TOKEN_SYMBOL = 'USDC'
 export const DEFAULT_TO_TOKEN_SYMBOL = 'DAI'
