e039749c0c7d5b910f9aa713ca72993ef62a2dc9
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-14 12:57:45 -0800

    generate txs

--- packages/synapse-interface/pages/teaser/index.tsx
+++ packages/synapse-interface/pages/teaser/index.tsx
@@ -5,6 +5,7 @@ import { useAccount } from 'wagmi'
 import { segmentAnalyticsEvent } from '@/contexts/SegmentAnalyticsProvider'
 
 import { SynapseIcon } from './SynapseIcon'
+import { generateTx } from './strings'
 
 const LandingPage = () => {
   const { address: currentAddress } = useAccount()
@@ -19,6 +20,7 @@ const LandingPage = () => {
   }, [])
 
   // Detect and set 'prefers-color-scheme: dark'
+  // https://stackoverflow.com/questions/61117608/how-do-i-set-system-preference-dark-mode-in-a-react-app-but-also-allow-users-to
   const prefersColorScheme = localStorage.getItem('prefers-color-scheme')
 
   const windowPrefersDark = window.matchMedia('(prefers-color-scheme: dark)')
@@ -28,11 +30,6 @@ const LandingPage = () => {
       ? prefersColorScheme === 'dark'
       : windowPrefersDark.matches
   )
-
-  function togglePrefersDark() {
-    localStorage.setItem('prefers-color-scheme', prefersDark ? 'light' : 'dark')
-    setPrefersDark(!prefersDark)
-  }
 
   function selectPrefersDark(e) {
     switch(e.target.value) {
@@ -55,10 +52,35 @@ const LandingPage = () => {
   return (
     <div className={prefersDark && 'dark'}>
       <div className="w-screen h-screen bg-white dark:bg-black overflow-scroll text-black dark:text-white tracking-wide leading-normal">
-        <div>
-          Ticker
+        {/* Ticker */}
+        <div className="mb-2 bg-zinc-100 dark:bg-zinc-900 text-sm border-y border-zinc-200 dark:border-zinc-800">
+          <div className="absolute bg-inherit px-4 py-1 border-r border-zinc-200 dark:border-zinc-800 flex items-center gap-2 z-10">
+            <svg width="8" height="8" viewBox="-4 -4 8 8" overflow="visible" className="fill-green-500 stroke-green-500" xmlns="http://www.w3.org/2000/svg">
+              <circle r="4">
+                <animate
+                  attributeName="stroke-width"
+                  values="0; 16"
+                  dur="1.5s"
+                  repeatCount="indefinite"
+                />
+                <animate
+                  attributeName="stroke-opacity"
+                  values=".5; 0"
+                  dur="1.5s"
+                  repeatCount="indefinite"
+                />
+              </circle>
+            </svg>
+            Live
+          </div>
+          <ul className="flex whitespace-nowrap py-1 list-disc opacity-50">
+            {[...Array(20)].map((x, i) => {
+              return <li className="pl-2 ml-7"><a href="#" className="hover:underline">{generateTx()}</a></li>
+            })}
+          </ul>
         </div>
-        <header className="p-4 flex items-center">
+        {/* Hero */}
+        <header className="m-8 flex items-center max-w-7xl relative mx-auto">
           <div className="text-3xl font-medium flex gap-2 items-center absolute">
             <SynapseIcon width={40} height={40} /><span className="-mt-2">Synapse</span>
           </div>
@@ -70,15 +92,14 @@ const LandingPage = () => {
             <li><a className="px-3 py-2" href="#">Explorer</a></li>
           </ul>
           <form className="absolute right-0 flex gap-2 p-4 items-center">
-            <select className="bg-white dark:bg-black text-inherit" onChange={selectPrefersDark}>
+            <select className="bg-white dark:bg-black text-inherit cursor-pointer rounded" onChange={selectPrefersDark}>
               <option selected={prefersColorScheme === 'dark'}>Dark mode</option>
               <option selected={prefersColorScheme === 'light'}>Light mode</option>
               <option selected={!prefersColorScheme}>System {windowPrefersDark.matches ? 'dark' : 'light'}</option>
             </select>
-            {/* <label>Prefers Dark</label>
-            <input type="checkbox" checked={prefersDark} onClick={togglePrefersDark} /> */}
           </form>
         </header>
+        {/* Main */}
         <main className="mx-auto w-full max-w-7xl">
           <header className="p-8 text-center max-w-2xl mx-auto">
             <div className="text-6xl font-semibold leading-[1.1] m-4">
@@ -93,7 +114,12 @@ const LandingPage = () => {
               Say goodbye to centralized resource pools for cross-chain communication. Synapse lets you customize literally every aspect of your interchain communcations.
             </p>
           </header>
-
+          <ul className="w-fit flex text-lg justify-center bg-zinc-100 dark:bg-zinc-900 rounded-md px-6 py-2 gap-8 mx-auto">
+            <li>50 blockchains</li>
+            <li>50,000 validators</li>
+            <li>10.2B messages</li>
+            <li>1.2B transferred</li>
+          </ul>
         </main>
         <footer>
           Footer
---
+++ packages/synapse-interface/pages/teaser/strings.tsx
@@ -0,0 +1,26 @@
+const Chains = ['Ethereum', 'Arbitrum', 'Avalanche', 'Base', 'BNB Chain', 'Optimism', 'Polygon', 'DFK Chain', 'Canto', 'Fantom', 'Klaytn', 'Aurora', 'Boba Chain', 'Cronos', 'Metis', 'Dogechain', 'Moonbeam', 'Moonriver']
+
+const Tokens = ['USDC', 'USDT', 'DAI', 'nUSD', 'crvUSD', 'FRAX', 'LUSD', 'JEWEL', 'AVAX', 'BTC.b', 'AVAX', 'WJEWEL', 'synJEWEL', 'LINK', 'ETH', 'nETH', 'SYN', 'NOTE', 'sUSD', 'GMX', 'MATIC', 'MOVR', 'WMOVR', 'WKLAY', 'WMATIC', 'xJEWEL', 'synFRAX', 'DOG', 'gOHM', 'H2O', 'JUMP', 'L2DAI', 'NEWO', 'PLS', 'SDT', 'SFI', 'UNIDX', 'veSOLAR', 'VSTA']
+
+export const generateTx = () => {
+  let amount = Math.round(Math.random() * 10000)
+  let amountStr = amount.toString()
+
+  if (Math.random() < .5) {
+    amountStr = `${amountStr.slice(0, 1)}.${amountStr.slice(2)}`
+  } else for (let i = 3; i < amountStr.length; i += 4)
+    amountStr = `${amountStr.slice(0, amountStr.length - i)},${amountStr.slice(-i)}`
+
+  if (amountStr.match(/\./)) {
+    if (Math.random() < .5)
+      amountStr = amountStr.replace(/\d/, '0')
+
+    while (amountStr.length < (+amountStr < 1 ? 6 : 5))
+      amountStr += '0'
+  }
+
+  const token = Tokens[Math.round(Math.random() * (Tokens.length - 1))]
+  const chain = Chains[Math.round(Math.random() * (Chains.length - 1))]
+
+  return `${amountStr} ${token} to ${chain}`
+}
