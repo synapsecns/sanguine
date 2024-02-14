2ae33f78edcec3cf2298433e0e741013f04fc7fa
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-13 16:06:45 -0800

    hero placeholder

---
+++ packages/synapse-interface/pages/teaser/SynapseIcon.tsx
@@ -0,0 +1,3 @@
+export function SynapseIcon({ width, height }) {
+  return <svg width={width} height={height} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="a"><stop stop-color="#E54DE5"/><stop offset="1" stop-color="#B580FF"/></linearGradient><marker id="b" viewBox="-.5 -.5 1 1"> <circle r=".4" fill="url(#a)"/></marker></defs><path d="M24,42 42,24 6,24 24,6" stroke-width="5" stroke-linejoin="bevel" stroke="url(#a)" stroke-opacity=".5" marker-start="url(#b)" marker-mid="url(#b)" marker-end="url(#b)"/></svg>
+}
