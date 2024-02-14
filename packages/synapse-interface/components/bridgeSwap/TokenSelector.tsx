dde2fe49ed7dc3e3f413a34e2874329860bf9475
caesar <caesar0x@users.noreply.github.com>
2024-02-14 15:31:00 -0500

    cleaner arrow

--- packages/synapse-interface/components/bridgeSwap/TokenSelector.tsx
+++ packages/synapse-interface/components/bridgeSwap/TokenSelector.tsx
@@ -9,38 +9,13 @@ import {
 
 export const TokenSelector = ({ token, label, onClick, ...props }) => {
 
-  let buttonContent
 
-  if (token) {
-    const src = token?.icon?.src
-    const symbol = token?.symbol
-
-    buttonContent = (
-      <div className="flex items-center space-x-3">
-        <div className="flex-none hidden md:inline-block">
-          <img src={src} alt={symbol} className="w-6 h-6" />
-        </div>
-        <div className="text-left">
-          <div className="text-lg text-primaryTextColor">{symbol}</div>
-        </div>
-        <DropDownArrowSvg className="flex-none" />
-      </div>
-    )
-  } else {
-    buttonContent = (
-      <div className="flex items-center space-x-3">
-        <div className="text-left">
-          <div className="text-lg text-primaryTextColor">{label}</div>
-        </div>
-        <DropDownArrowSvg className="flex-none" />
-      </div>
-    )
-  }
 
   return (
     <button
       {...props}
       className={`
+        group
         p-md rounded-sm min-w-[80px]
         bg-slate-400/10 hover:bg-slate-400/20
         border border-white/10
@@ -49,7 +24,27 @@ export const TokenSelector = ({ token, label, onClick, ...props }) => {
       `}
       onClick={onClick}
     >
-      {buttonContent}
+      <div className="flex items-center space-x-3">
+        { token &&
+          <div className="flex-none hidden md:inline-block">
+            <img src={token?.icon?.src} alt={token?.symbol} className="w-6 h-6" />
+          </div>
+        }
+        <div className="text-left">
+          <div className="text-lg text-primaryTextColor">
+            {token?.symbol ?? label}
+          </div>
+        </div>
+        <ChevronDownIcon
+          className={`
+              flex-none
+              rotate-0
+              text-white/30 group-hover:text-white/80 w-4 h-4
+              active:text-white/80
+              group-active:rotate-180 transition-all
+          `}
+        />
+      </div>
     </button>
   )
 }
