e26b37779518d76d0208d32095c469940cd6e110
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-15 12:28:36 -0800

    footer component

--- packages/synapse-interface/pages/teaser/SynapseIcon.tsx
+++ packages/synapse-interface/pages/teaser/SynapseIcon.tsx
@@ -1,3 +1,24 @@
 export function SynapseIcon({ width, height }) {
-  return <svg width={width} height={height} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="a"><stop stop-color="#E54DE5"/><stop offset="1" stop-color="#B580FF"/></linearGradient><marker id="b" viewBox="-.5 -.5 1 1"> <circle r=".4" fill="url(#a)"/></marker></defs><path d="M24,42 42,24 6,24 24,6" stroke-width="5" stroke-linejoin="bevel" stroke="url(#a)" stroke-opacity=".5" marker-start="url(#b)" marker-mid="url(#b)" marker-end="url(#b)"/></svg>
+  return (
+    <svg width={width} height={height} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <linearGradient id="linear-gradient">
+          <stop stop-color="#e54de5"/>
+          <stop offset="1" stop-color="#b580ff"/>
+        </linearGradient>
+        <marker id="marker" viewBox="-1 -1 2 2">
+          <circle r=".8" fill="url(#linear-gradient)"/>
+        </marker>
+      </defs>
+      <path
+        d="M24,42 42,24 6,24 24,6"
+        stroke="url(#linear-gradient)"
+        stroke-width="5"
+        stroke-linejoin="bevel"
+        stroke-opacity=".5"
+        marker-start="url(#marker)"
+        marker-mid="url(#marker)"
+        marker-end="url(#marker)"
+      />
+    </svg>)
 }
