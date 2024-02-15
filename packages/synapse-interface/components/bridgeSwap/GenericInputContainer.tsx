9fee492b0be42067da0d712ee6c6b8fdd0d7405e
caesar <caesar0x@users.noreply.github.com>
2024-02-15 12:11:34 -0500

    rm unused import

--- packages/synapse-interface/components/bridgeSwap/GenericInputContainer.tsx
+++ packages/synapse-interface/components/bridgeSwap/GenericInputContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react'
-import { useDispatch } from 'react-redux'
+
 import { useAccount } from 'wagmi'
 
 import MiniMaxButton from '../buttons/MiniMaxButton'
