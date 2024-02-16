93606620d79e04993d17184f1a0726e0c17ed762
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-16 16:29:19 +0000

    Implement management actions

--- packages/contracts-communication/contracts/modules/ThresholdECDSAModule.sol
+++ packages/contracts-communication/contracts/modules/ThresholdECDSAModule.sol
@@ -4,31 +4,52 @@ pragma solidity 0.8.20;
 import {InterchainModule} from "./InterchainModule.sol";
 
 import {IThresholdECDSAModule} from "../interfaces/IThresholdECDSAModule.sol";
+import {IThresholdECDSAModuleEvents} from "../interfaces/IThresholdECDSAModuleEvents.sol";
 
 import {InterchainEntry} from "../libs/InterchainEntry.sol";
 import {ThresholdECDSA} from "../libs/ThresholdECDSA.sol";
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ThresholdECDSAModule is InterchainModule, Ownable, IThresholdECDSAModule {
+contract ThresholdECDSAModule is InterchainModule, Ownable, IThresholdECDSAModule, IThresholdECDSAModuleEvents {
+    /// @dev Struct to hold the verifiers and the threshold for the module.
+    ThresholdECDSA internal _verifiers;
+
     /// @inheritdoc IThresholdECDSAModule
     address public gasOracle;
 
-    constructor(address interchainDB, address initialOwner) InterchainModule(interchainDB) Ownable(initialOwner) {}
+    constructor(address interchainDB, address initialOwner) InterchainModule(interchainDB) Ownable(initialOwner) {
+        // Explicitly disable the module functionality until the threshold is set.
+        _setThreshold(type(uint256).max);
+    }
 
     // ═══════════════════════════════════════════════ PERMISSIONED ════════════════════════════════════════════════════
 
     /// @inheritdoc IThresholdECDSAModule
-    function addVerifier(address verifier) external onlyOwner {}
+    function addVerifier(address verifier) external onlyOwner {
+        _verifiers.addSigner(verifier);
+        emit VerifierAdded(verifier);
+    }
 
     /// @inheritdoc IThresholdECDSAModule
-    function removeVerifier(address verifier) external onlyOwner {}
+    function removeVerifier(address verifier) external onlyOwner {
+        _verifiers.removeSigner(verifier);
+        emit VerifierRemoved(verifier);
+    }
 
     /// @inheritdoc IThresholdECDSAModule
-    function setThreshold(uint256 threshold) external onlyOwner {}
+    function setThreshold(uint256 threshold) external onlyOwner {
+        _setThreshold(threshold);
+    }
 
     /// @inheritdoc IThresholdECDSAModule
-    function setGasOracle(address gasOracle_) external onlyOwner {}
+    function setGasOracle(address gasOracle_) external onlyOwner {
+        if (gasOracle_.code.length == 0) {
+            revert ThresholdECDSAModule__GasOracleNotContract(gasOracle_);
+        }
+        gasOracle = gasOracle_;
+        emit GasOracleChanged(gasOracle_);
+    }
 
     // ══════════════════════════════════════════════ PERMISSIONLESS ═══════════════════════════════════════════════════
 
@@ -38,15 +59,27 @@ contract ThresholdECDSAModule is InterchainModule, Ownable, IThresholdECDSAModul
     // ═══════════════════════════════════════════════════ VIEWS ═══════════════════════════════════════════════════════
 
     /// @inheritdoc IThresholdECDSAModule
-    function getThreshold() external view returns (uint256) {}
+    function getThreshold() external view returns (uint256) {
+        return _verifiers.getThreshold();
+    }
 
     /// @inheritdoc IThresholdECDSAModule
-    function getVerifiers() external view returns (address[] memory) {}
+    function getVerifiers() external view returns (address[] memory) {
+        return _verifiers.getSigners();
+    }
 
     /// @inheritdoc IThresholdECDSAModule
-    function isVerifier(address account) external view returns (bool) {}
+    function isVerifier(address account) external view returns (bool) {
+        return _verifiers.isSigner(account);
+    }
 
     // ══════════════════════════════════════════════ INTERNAL LOGIC ═══════════════════════════════════════════════════
+
+    /// @dev Sets the threshold for the module. Permissions should be checked in the calling function.
+    function _setThreshold(uint256 threshold) internal {
+        _verifiers.modifyThreshold(threshold);
+        emit ThresholdChanged(threshold);
+    }
 
     /// @dev Internal logic to request the verification of an entry on the destination chain.
     function _requestVerification(uint256 destChainId, bytes memory encodedEntry) internal override {}
