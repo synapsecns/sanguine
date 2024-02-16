da71e45630efba3c91ba717ec4c5ba39a9a8908f
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-02-16 11:39:17 -0800

    Refactor TopBarNavLink

--- packages/synapse-interface/components/layouts/LandingPageWrapper/TopBarNavLink.tsx
+++ packages/synapse-interface/components/layouts/LandingPageWrapper/TopBarNavLink.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import classNames from 'classnames'
 
 export function TopBarNavLink({
   labelText,
@@ -15,48 +16,54 @@ export function TopBarNavLink({
   const router = useRouter()
 
   const isInternal = to[0] === '/' || to[0] === '#'
-  const linkContent = (
+
+  const isRouteMatched =
+    match &&
+    (typeof match === 'string'
+      ? router.asPath === match ||
+        router.asPath === '/' ||
+        router.asPath.startsWith(match + '?')
+      : match instanceof RegExp
+      ? match.test(router.asPath)
+      : router.asPath.startsWith(match.startsWith))
+
+  const linkClassName = classNames(
+    'px-2 tracking-wide transform-gpu transition-all duration-75 text-white',
+    {
+      'text-opacity-100': isRouteMatched,
+      'text-opacity-30': !isRouteMatched,
+      'hover:text-opacity-100': true,
+    },
+    className
+  )
+
+  const LinkComponent = isInternal ? Link : 'a'
+
+  return (
+    <LinkComponent
+      href={to}
+      className={linkClassName}
+      target={!isInternal ? '_blank' : undefined}
+      rel={!isInternal ? 'noopener noreferrer' : undefined}
+      data-test-id="nav-link"
+    >
+      <LinkContent className={className} labelText={labelText} />
+    </LinkComponent>
+  )
+}
+
+const LinkContent = ({
+  className,
+  labelText,
+}: {
+  className: string
+  labelText: string
+}) => {
+  return (
     <div className={`py-2 px-2 ${className}`}>
       <span className="transition-all duration-75 transform-gpu whitespace-nowrap">
         {labelText}
       </span>
     </div>
   )
-
-  const linkClassName = `
-    px-2 tracking-wide
-    transform-gpu transition-all duration-75
-    text-white ${
-      match &&
-      (typeof match === 'string'
-        ? router.asPath === match ||
-          router.asPath === '/' ||
-          router.asPath.startsWith(match + '?')
-        : match instanceof RegExp
-        ? match.test(router.asPath)
-        : router.asPath.startsWith(match.startsWith))
-        ? 'text-opacity-100'
-        : 'text-opacity-30'
-    }
-    hover:text-opacity-100
-  `
-
-  if (isInternal) {
-    return (
-      <Link
-        href={to}
-        className={linkClassName}
-        // activeClassName="!text-opacity-100"
-        data-test-id="nav-link"
-      >
-        {linkContent}
-      </Link>
-    )
-  } else {
-    return (
-      <a href={to} target="_blank" className={linkClassName}>
-        {linkContent}
-      </a>
-    )
-  }
 }
