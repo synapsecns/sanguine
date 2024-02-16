7fa4cf596f82f7366c413e41a6c38a33bf947acd
caesar <caesar0x@users.noreply.github.com>
2024-02-16 13:30:31 -0500

    memoization in so many places literally adds MORE overhead

--- packages/synapse-interface/components/bridgeSwap/ConnectStatusIndicator.tsx
+++ packages/synapse-interface/components/bridgeSwap/ConnectStatusIndicator.tsx
@@ -1,33 +1,24 @@
-import { useMemo } from 'react'
 import { useAccount, useNetwork } from 'wagmi'
 import {
   ConnectToNetworkButton,
   ConnectWalletButton,
   ConnectedIndicator,
 } from '@/components/ConnectionIndicators'
-import { useHasMounted } from '@/utils/hooks/useHasMounted'
+
 
 
 
 export const ConnectStatusIndicator = ({ targetChainId }) => {
   const { isConnected } = useAccount()
   const { chain } = useNetwork()
-  const hasMounted = useHasMounted()
 
-
-  const connectedStatus = useMemo(() => {
-    if (hasMounted) {
-      if (isConnected) {
-        if (targetChainId === chain?.id) {
-          return <ConnectedIndicator />
-        } else {
-          return <ConnectToNetworkButton chainId={targetChainId} />
-        }
-      } else {
-        return <ConnectWalletButton />
-      }
+  if (isConnected) {
+    if (targetChainId === chain?.id) {
+      return <ConnectedIndicator />
+    } else {
+      return <ConnectToNetworkButton chainId={targetChainId} />
     }
-  }, [chain, targetChainId, isConnected, hasMounted])
-
-  return <>{connectedStatus}</>
+  } else {
+    return <ConnectWalletButton />
+  }
 }
