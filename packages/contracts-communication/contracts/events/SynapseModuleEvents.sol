d76deb095a9ecee322d7664f3a3765eade97db94
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-16 16:48:47 +0000

    More events to a separate dir

--- packages/contracts-communication/contracts/events/SynapseModuleEvents.sol
+++ packages/contracts-communication/contracts/events/SynapseModuleEvents.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import {InterchainEntry} from "../libs/InterchainEntry.sol";
 
-interface ISynapseModuleEvents {
+abstract contract SynapseModuleEvents {
     event VerificationRequested(uint256 indexed destChainId, InterchainEntry entry, bytes32 signableEntryHash);
     event EntryVerified(InterchainEntry entry, bytes32 signableEntryHash);
 }
