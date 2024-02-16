017b1dc0c57ac72d08fd965bd54dce387ea79a38
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-16 13:20:47 -0800

    better tx generation

--- packages/synapse-interface/pages/teaser/Ticker.tsx
+++ packages/synapse-interface/pages/teaser/Ticker.tsx
@@ -31,22 +31,18 @@ export default function Ticker() {
           return (
             <>
               <dt
-                className={`relative group px-4 row-start-1 bg-zinc-50 dark:bg-zinc-950 border-y border-zinc-200 dark:border-zinc-900 ${styles.tx}`}
+                className="relative group row-start-1 bg-zinc-50 dark:bg-zinc-950 border-y border-zinc-200 dark:border-zinc-900"
                 style={{ gridColumnStart: i + 1}}
               >
-                <a
-                  href="#"
-                  className="text-zinc-500 hover:text-inherit hover:underline py-1.5 inline-block"
-                >
+                <a href="#" className="text-zinc-500 px-4 hover:text-inherit hover:underline py-1.5 inline-block">
                   {`${tx.origin.formattedAmount} ${tx.origin.payload} to ${tx.destination.chain}`}
                 </a>
               </dt>
               <dd
-                className="row-start-2 animate-slide-down origin-top relative"
+                className="row-start-2 animate-slide-down origin-top relative p-2"
                 style={{ gridColumnStart: i + 1}}
               >
-                <div className="absolute top-2 left-2 px-3 py-2 bg-zinc-50 dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-900 rounded items-center grid gap-x-4 gap-y-1">
-
+                <a href="#" className="absolute px-3 py-2 bg-zinc-50 dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-900 rounded items-center grid gap-x-4 gap-y-1 shadow-sm">
                   <ul className="inline">
                     <li>{tx.origin.formattedAmount} {tx.origin.payload}</li>
                     <li>{tx.origin.chain}</li>
@@ -59,7 +55,7 @@ export default function Ticker() {
                     <li>{tx.destination.chain}</li>
                   </ul>
                   <header className="text-zinc-500 row-start-2 col-span-3">{formatTimestamp(tx)}</header>
-                </div>
+                </a>
               </dd>
             </>
           )
--- packages/synapse-interface/pages/teaser/strings.tsx
+++ packages/synapse-interface/pages/teaser/strings.tsx
@@ -1,42 +1,46 @@
-const Chains = ['Ethereum', 'Arbitrum', 'Avalanche', 'Base', 'BNB Chain', 'Optimism', 'Polygon', 'DFK Chain', 'Canto', 'Fantom', 'Klaytn', 'Aurora', 'Boba Chain', 'Cronos', 'Metis', 'Dogechain', 'Moonbeam', 'Moonriver']
+const Chains = ['Ethereum', 'Arbitrum', 'Avalanche', 'Base', 'BNB Chain', 'Optimism', 'Polygon', 'DFK Chain', 'Canto', 'Fantom', 'Klaytn', 'Aurora', 'Boba Chain', 'Cronos', 'Metis', 'Moonbeam', 'Moonriver']
 
-const Tokens = ['USDC', 'USDT', 'DAI', 'nUSD', 'crvUSD', 'FRAX', 'LUSD', 'JEWEL', 'AVAX', 'BTC.b', 'AVAX', 'WJEWEL', 'synJEWEL', 'LINK', 'ETH', 'nETH', 'SYN', 'NOTE', 'sUSD', 'GMX', 'MATIC', 'MOVR', 'WMOVR', 'WKLAY', 'WMATIC', 'xJEWEL', 'synFRAX', 'DOG', 'gOHM', 'H2O', 'JUMP', 'L2DAI', 'NEWO', 'PLS', 'SDT', 'SFI', 'UNIDX', 'veSOLAR', 'VSTA']
+const Tokens = ['USDC', 'USDT', 'DAI', 'crvUSD', 'FRAX', 'LUSD', 'JEWEL', 'AVAX', 'BTC.b', 'AVAX', 'LINK', 'ETH', 'SYN', 'NOTE', 'sUSD', 'GMX', 'MATIC', 'MOVR', 'gOHM', 'H2O', 'L2DAO', 'NEWO', 'PLS', 'SDT', 'SFI', 'UNIDX', 'veSOLAR', 'VSTA']
+
+const formatAmount = amount => {
+  let [, left, right] = amount.toString().match(/(\d+)\.?(\d*)/) ?? ['', '', '']
+
+
+  for (let i = 3; i < left.length; i += 4)
+    left = `${left.slice(0, left.length - i)},${left.slice(-i)}`
+
+  right += '0000'
+
+  return (left.length < 4)
+    ? left + '.' + right.slice(0, left === '0' ? 4 :  4 - left.length)
+    : left
+}
+
+const randHex = () => {
+  const x = () => Math.round(Math.random() * 16).toString(16)
+  return `#${x() + x()}…${x() + x() + x() + x()}`
+}
 
 export const generateTx = () => {
-  let amount = Math.round(Math.random() * 10000)
-  let amountStr = amount.toString()
-
-  if (Math.random() < .5) {
-    amountStr = `${amountStr.slice(0, 1)}.${amountStr.slice(2)}`
-  } else for (let i = 3; i < amountStr.length; i += 4)
-    amountStr = `${amountStr.slice(0, amountStr.length - i)},${amountStr.slice(-i)}`
-
-  if (amountStr.match(/\./)) {
-    if (Math.random() < .5)
-      amountStr = amountStr.replace(/\d/, '0')
-
-    while (amountStr.length < (+amountStr < 1 ? 6 : 5))
-      amountStr += '0'
-  }
-
-  const token = Tokens[Math.round(Math.random() * (Tokens.length - 1))]
-  const chain = Chains[Math.round(Math.random() * (Chains.length - 1))]
-
+  let originAmount = Math.random() < .5 ? Math.round(Math.random() * 10000) : Math.random()
+  let destinationAmount = originAmount * (100 - Math.random() * 5) / 100
 
   const origin = {
     payload: Tokens[Math.round(Math.random() * (Tokens.length - 1))],
     chain: Chains[Math.round(Math.random() * (Chains.length - 1))],
-    formattedAmount: amountStr,
-    timestamp: Date.now()
+    originAmount,
+    formattedAmount: formatAmount(originAmount),
+    timestamp: Date.now(),
+    hash: randHex(),
   }
   const destination = {
     payload: origin.payload,
     chain: Chains[Math.round(Math.random() * (Chains.length - 1))],
-    formattedAmount: amountStr,
-    timestamp: origin.timestamp + Math.round(Math.random() * 600000)
+    destinationAmount,
+    formattedAmount: formatAmount(destinationAmount),
+    timestamp: origin.timestamp + Math.round(Math.random() * 600000),
+    hash: randHex(),
   }
-
-  const description = `${origin.formattedAmount} ${origin.payload} to ${destination.chain}`
 
   return { origin, destination }
 }
