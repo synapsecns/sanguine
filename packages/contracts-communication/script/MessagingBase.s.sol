c836743b883c3044c773865bd24a6c9b0fdf42a8
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2024-02-19 12:27:15 -0500

    init basic script for testnet deploy

---
+++ packages/contracts-communication/script/MessagingBase.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+
+import { Script } from "forge-std/Script.sol";
+
+import { InterchainDB } from "../contracts/InterchainDB.sol";
+
+import { InterchainClientV1 } from "../contracts/InterchainClientV1.sol";
+
+import { SynapseModule } from "../contracts/modules/SynapseModule.sol";
+
+import { GasOracleMock } from "../test/mocks/GasOracleMock.sol";
+
+
+
+contract MessagingBase is Script {
+    InterchainDB public icDB;
+    InterchainClientV1 public icClient;
+    SynapseModule public synapseModule;
+    GasOracleMock public gasOracleMock;
+
+    function run() external {
+        vm.startBroadcast();
+        icDB = new InterchainDB();
+        // icClient deployment & config
+        icClient = new InterchainClientV1();
+        icClient.setInterchainDB(address(icDB));
+
+
+        synapseModule = new SynapseModule(address(icDB), msg.sender);
+        gasOracleMock = new GasOracleMock();
+        synapseModule.setGasOracle(address(gasOracleMock));
+
+        vm.stopBroadcast();
+    }
+}
