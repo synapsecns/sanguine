267a5345c36be06493629b3112c2f86f69cb9759
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-20 14:29:17 +0000

    Use dbNonce in `InterchainEntry` struct

--- packages/contracts-communication/contracts/libs/InterchainEntry.sol
+++ packages/contracts-communication/contracts/libs/InterchainEntry.sol
@@ -3,27 +3,30 @@ pragma solidity ^0.8.0;
 
 import {TypeCasts} from "./TypeCasts.sol";
 
-/// @notice Struct representing an entry in the Interchain DataBase
+/// @notice Struct representing an entry in the Interchain DataBase.
+/// Entry has a globally unique identifier (key) and a value.
+/// - key: srcChainId + dbNonce
+/// - value: srcWriter + dataHash
 /// @param srcChainId   The chain id of the source chain
+/// @param dbNonce      The database nonce of the entry on the source chain
 /// @param srcWriter    The address of the writer on the source chain
-/// @param writerNonce  The nonce of the writer on the source chain
 /// @param dataHash     The hash of the data written on the source chain
 struct InterchainEntry {
     uint256 srcChainId;
+    uint256 dbNonce;
     bytes32 srcWriter;
-    uint256 writerNonce;
     bytes32 dataHash;
 }
 
 library InterchainEntryLib {
     /// @notice Constructs an InterchainEntry struct to be written on the local chain
+    /// @param dbNonce      The database nonce of the entry on the source chain
     /// @param srcWriter    The address of the writer on the local chain
-    /// @param writerNonce  The nonce of the writer on the local chain
     /// @param dataHash     The hash of the data written on the local chain
     /// @return entry       The constructed InterchainEntry struct
     function constructLocalEntry(
+        uint256 dbNonce,
         address srcWriter,
-        uint256 writerNonce,
         bytes32 dataHash
     )
         internal
@@ -32,14 +35,19 @@ library InterchainEntryLib {
     {
         return InterchainEntry({
             srcChainId: block.chainid,
+            dbNonce: dbNonce,
             srcWriter: TypeCasts.addressToBytes32(srcWriter),
-            writerNonce: writerNonce,
             dataHash: dataHash
         });
     }
 
     /// @notice Returns the globally unique identifier of the entry
-    function entryId(InterchainEntry memory entry) internal pure returns (bytes32) {
-        return keccak256(abi.encode(entry.srcChainId, entry.srcWriter, entry.writerNonce));
+    function entryKey(InterchainEntry memory entry) internal pure returns (bytes32) {
+        return keccak256(abi.encode(entry.srcChainId, entry.dbNonce));
+    }
+
+    /// @notice Returns the value of the entry: writer + dataHash hashed together
+    function entryValue(InterchainEntry memory entry) internal pure returns (bytes32) {
+        return keccak256(abi.encode(entry.srcWriter, entry.dataHash));
     }
 }
