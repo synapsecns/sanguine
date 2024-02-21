6f057cbbea792baae6c336b388917514aa8ebfaf
Trajan0x <trajan0x@users.noreply.github.com>
2024-02-21 00:06:44 +0000

    [goreleaser] add sin executor

--- sin-executor/main.go
+++ sin-executor/main.go
@@ -1,1 +1,11 @@
-package sin_executor
+package main
+
+import (
+	"github.com/synapsecns/sanguine/sin-executor/cmd"
+	"github.com/synapsecns/sanguine/sin-executor/metadata"
+	"os"
+)
+
+func main() {
+	cmd.Start(os.Args, metadata.BuildInfo())
+}
