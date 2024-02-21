6b8d983fc6b63a52d494ac7ecbd291a0f03caeb8
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-21 15:29:22 -0800

    connect every blockchain svg

--- packages/synapse-interface/pages/teaser/ValueProps.tsx
+++ packages/synapse-interface/pages/teaser/ValueProps.tsx
@@ -2,48 +2,62 @@ import PulseDot from './PulseDot'
 
 export default function ValueProps() {
   return (
-    <article className="grid gap-8 px-4">
-      <section className="grid md:grid-cols-2 gap-x-12">
-        <div>
-          <h2 className="text-4xl font-medium my-4">
-            Securely connect every blockchain
-          </h2>
-          <p className="text-lg leading-relaxed">
-            Synapse is comprised of a cross-chain messaging framework and an
-            economically secure method to reach consensus on the validity of
-            cross-chain transactions, enabling developers to build truly native
-            cross-chain apps.
-          </p>
-        </div>
-        <div className="w-full h-full grid items-center justify-center">
+    <article className="grid gap-24 px-4">
+      <section className="flex even:flex-col odd:flex-col-reverse md:grid grid-cols-2 gap-12 items-center">
+        <div className="grid place-items-center">
           <svg
             width="100%"
-            height="50%"
-            viewBox="-4 -4 8 8"
-            stroke-width=".1"
-            vector-effect="non-scaling-size"
-            fill="none"
+            height="100%"
+            viewBox="-9 -9 170 118"
+            vector-effect="non-scaling-stroke"
+            stroke-width=".25"
             overflow="visible"
-            className="stroke-fuchsia-500"
+            // preserveAspectRatio="none"
+            className="stroke-fuchsia-800/75 dark:stroke-purple-200/75 fill-fuchsia-200/50 dark:fill-fuchsia-300/5 drop-shadow-xl"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <circle r="4" />
+            <rect
+              width="168"
+              height="116"
+              x="-8"
+              y="-8"
+              rx="4"
+              className="stroke-purple-400/80 fill-[hsl(275deg_100%_96%)] dark:fill-[hsl(300deg_40%_10%)]"
+            />
+            <path d="m48,11 4,0 m22,0 4,0 m50,11 0,4 m0,22 0,4 m-24,24 -4,0 m-48,0 -4,0 m0,-39 4,0 m11,11 0,4 m0,-30 0,4" />
+            <rect width="48" height="48" rx="2" />
+            <rect width="22" height="22" x="52" rx="2" />
+            <rect width="74" height="22" x="78" rx="2" />
+            <rect width="100" height="22" x="52" y="26" rx="2" />
+            <rect width="48" height="48" x="0" y="52" rx="2" />
+            <rect width="48" height="48" x="52" y="52" rx="2" />
+            <rect width="48" height="48" x="104" y="52" rx="2" />
           </svg>
         </div>
-      </section>
-      <section className="grid md:grid-cols-2 gap-x-12">
         <div>
-          <h2 className="text-4xl font-medium my-4">
+          <h2 className="text-4xl font-medium mb-4">
+            Securely connect every blockchain
+          </h2>
+          <p className="text-lg leading-relaxed mb-3">
+            Synapse enables developers to build truly native cross-chain
+            applications with an economically secure method to reach consensus
+            on interchain transactions,
+          </p>
+        </div>
+      </section>
+      <section className="flex even:flex-col odd:flex-col-reverse md:grid grid-cols-2 gap-12 items-center">
+        <div>
+          <h2 className="text-4xl font-medium mb-4">
             Powering the most popular bridge
           </h2>
-          <p className="text-lg leading-relaxed">
+          <p className="text-lg leading-relaxed mb-4">
             Synapse Bridge is built on top of the cross-chain infrastructure
             enabling users to seamlessly transfer assets across all blockchains.
             The Bridge has become the most widely-used method to move assets
             cross-chain, offering low cost, fast, and secure bridging.
           </p>
         </div>
-        <div className="w-full h-full grid items-center justify-center md:row-start-1 col-start-1 row-start-2 relative">
+        <div className="grid justify-center">
           <FauxBridge />
         </div>
       </section>
@@ -102,8 +116,8 @@ export default function ValueProps() {
 }
 
 const FauxBridge = () => {
-  const formStyle =
-    'text-black dark:text-white bg-zinc-100 dark:bg-zinc-900 p-3 rounded-md border border-zinc-200 dark:border-zinc-800 shadow-xl grid gap-4 w-96'
+  const cardStyle =
+    'text-black dark:text-white bg-zinc-100 dark:bg-zinc-900 p-3 rounded-md border border-zinc-200 dark:border-zinc-800 shadow-xl grid gap-4 max-w-sm'
   const sectionStyle =
     'bg-zinc-50 dark:bg-zinc-800 rounded-md px-2.5 py-3 grid gap-3 grid-cols-2 border border-zinc-300 dark:border-transparent'
   const selectStyle =
@@ -114,7 +128,7 @@ const FauxBridge = () => {
     'bg-inherit border-none w-full p-1.5 text-xxl font-normal dark:font-light tracking-wide rounded'
 
   return (
-    <form className={formStyle}>
+    <div className={cardStyle}>
       <section className={sectionStyle}>
         <select className={`ml-0.5 ${selectStyle}`}>
           <option>Chain</option>
@@ -158,7 +172,7 @@ const FauxBridge = () => {
       >
         &nbsp;Bridge {'->'}
       </a>
-    </form>
+    </div>
   )
 }
 
