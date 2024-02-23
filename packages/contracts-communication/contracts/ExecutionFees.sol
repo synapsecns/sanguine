ceebbd66a673690441c8f2a94ad0ba4fcd92a103
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-23 13:18:00 +0000

    Simplify implementation by making mappings public

--- packages/contracts-communication/contracts/ExecutionFees.sol
+++ packages/contracts-communication/contracts/ExecutionFees.sol
@@ -10,10 +10,14 @@ import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 contract ExecutionFees is AccessControl, ExecutionFeesEvents, IExecutionFees {
     bytes32 public constant RECORDER_ROLE = keccak256("RECORDER_ROLE");
 
-    mapping(uint256 chainId => mapping(bytes32 transactionId => uint256 fee)) internal _executionFees;
-    mapping(address executor => uint256 totalAccumulated) internal _accumulatedRewards;
-    mapping(address executor => uint256 totalClaimed) internal _unclaimedRewards;
-    mapping(uint256 chainId => mapping(bytes32 transactionId => address executor)) internal _recordedExecutor;
+    /// @inheritdoc IExecutionFees
+    mapping(uint256 chainId => mapping(bytes32 transactionId => uint256 fee)) public executionFee;
+    /// @inheritdoc IExecutionFees
+    mapping(address executor => uint256 totalAccumulated) public accumulatedRewards;
+    /// @inheritdoc IExecutionFees
+    mapping(address executor => uint256 totalClaimed) public unclaimedRewards;
+    /// @inheritdoc IExecutionFees
+    mapping(uint256 chainId => mapping(bytes32 transactionId => address executor)) public recordedExecutor;
 
     constructor(address initialAdmin) {
         _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
@@ -22,10 +26,10 @@ contract ExecutionFees is AccessControl, ExecutionFeesEvents, IExecutionFees {
     // @inheritdoc IExecutionFees
     function addExecutionFee(uint256 dstChainId, bytes32 transactionId) external payable {
         if (msg.value == 0) revert ExecutionFees__ZeroAmount();
-        _executionFees[dstChainId][transactionId] += msg.value;
+        executionFee[dstChainId][transactionId] += msg.value;
         // Use the new total fee as the event parameter.
-        emit ExecutionFeeAdded(dstChainId, transactionId, _executionFees[dstChainId][transactionId]);
-        address executor = _recordedExecutor[dstChainId][transactionId];
+        emit ExecutionFeeAdded(dstChainId, transactionId, executionFee[dstChainId][transactionId]);
+        address executor = recordedExecutor[dstChainId][transactionId];
         // If the executor is recorded, the previous fee has been awarded already. Award the new fee.
         if (executor != address(0)) {
             _awardFee(executor, msg.value);
@@ -42,46 +46,26 @@ contract ExecutionFees is AccessControl, ExecutionFeesEvents, IExecutionFees {
         onlyRole(RECORDER_ROLE)
     {
         if (executor == address(0)) revert ExecutionFees__ZeroAddress();
-        if (_recordedExecutor[dstChainId][transactionId] != address(0)) revert ExecutionFees__AlreadyRecorded();
-        uint256 fee = _executionFees[dstChainId][transactionId];
-        _recordedExecutor[dstChainId][transactionId] = executor;
+        if (recordedExecutor[dstChainId][transactionId] != address(0)) revert ExecutionFees__AlreadyRecorded();
+        uint256 fee = executionFee[dstChainId][transactionId];
+        recordedExecutor[dstChainId][transactionId] = executor;
         emit ExecutorRecorded(dstChainId, transactionId, executor);
         _awardFee(executor, fee);
     }
 
     // @inheritdoc IExecutionFees
     function claimExecutionFees(address executor) external {
-        uint256 amount = _unclaimedRewards[executor];
+        uint256 amount = unclaimedRewards[executor];
         if (amount == 0) revert ExecutionFees__ZeroAmount();
-        _unclaimedRewards[executor] = 0;
+        unclaimedRewards[executor] = 0;
         Address.sendValue(payable(executor), amount);
         emit ExecutionFeesClaimed(executor, amount);
     }
 
-    // @inheritdoc IExecutionFees
-    function getAccumulatedRewards(address executor) external view returns (uint256 accumulated) {
-        return _accumulatedRewards[executor];
-    }
-
-    // @inheritdoc IExecutionFees
-    function getUnclaimedRewards(address executor) external view returns (uint256 unclaimed) {
-        return _unclaimedRewards[executor];
-    }
-
-    // @inheritdoc IExecutionFees
-    function getExecutionFee(uint256 dstChainId, bytes32 transactionId) external view returns (uint256 fee) {
-        return _executionFees[dstChainId][transactionId];
-    }
-
-    // @inheritdoc IExecutionFees
-    function getRecordedExecutor(uint256 dstChainId, bytes32 transactionId) external view returns (address executor) {
-        return _recordedExecutor[dstChainId][transactionId];
-    }
-
     /// @dev Award the executor with the fee for completing the transaction.
     function _awardFee(address executor, uint256 fee) internal {
-        _accumulatedRewards[executor] += fee;
-        _unclaimedRewards[executor] += fee;
+        accumulatedRewards[executor] += fee;
+        unclaimedRewards[executor] += fee;
         emit ExecutionFeesAwarded(executor, fee);
     }
 }
