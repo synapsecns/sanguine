24e0ee1d67570b24a34966ff26017bcfa57cd0d0
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-02-26 11:15:27 -0800

    ticker loading state

--- packages/synapse-interface/pages/teaser/Ticker.tsx
+++ packages/synapse-interface/pages/teaser/Ticker.tsx
@@ -4,7 +4,7 @@ import {
   formatExplorerTxs,
 } from '../../utils/fakeDataGen/teaserMarquee'
 import PulseDot from './icons/PulseDot'
-import { useRef, useEffect, useState } from 'react'
+import { Fragment, useRef, useEffect, useState } from 'react'
 
 const testUrl =
   'https://explorer.omnirpc.io/graphql?query=%7B%0A%20%20bridgeTransactions(useMv%3Atrue%2C%20pending%3A%20false%2C%20startTime%3A1688815939%2C%20page%3A%201)%20%7B%0A%20%20%20%20fromInfo%20%7B%0A%20%20%20%20%20%20chainID%0A%20%20%20%20%20%20destinationChainID%0A%20%20%20%20%20%20address%0A%20%20%20%20%20%20txnHash%0A%20%20%20%20%20%20value%0A%20%20%20%20%20%20formattedValue%0A%20%20%20%20%20%20USDValue%0A%20%20%20%20%20%20tokenAddress%0A%20%20%20%20%20%20tokenSymbol%0A%20%20%20%20%20%20blockNumber%0A%20%20%20%20%20%20time%0A%20%20%20%20%20%20formattedTime%0A%20%20%20%20%20%20formattedEventType%0A%20%20%20%20%20%20eventType%0A%20%20%20%20%7D%0A%20%20%20%20toInfo%20%7B%0A%20%20%20%20%20%20chainID%0A%20%20%20%20%20%20destinationChainID%0A%20%20%20%20%20%20address%0A%20%20%20%20%20%20txnHash%0A%20%20%20%20%20%20value%0A%20%20%20%20%20%20formattedValue%0A%20%20%20%20%20%20USDValue%0A%20%20%20%20%20%20tokenAddress%0A%20%20%20%20%20%20tokenSymbol%0A%20%20%20%20%20%20blockNumber%0A%20%20%20%20%20%20time%0A%20%20%20%20%20%20formattedTime%0A%20%20%20%20%20%20formattedEventType%0A%20%20%20%20%20%20eventType%0A%20%20%20%20%7D%0A%20%20%20%20kappa%0A%20%20%7D%0A%7D%0A'
@@ -33,7 +33,7 @@ txs = []
 
 export default function Ticker() {
   const tickerRef = useRef(null)
-  const [txData, setTxData] = useState(txs)
+  const [txData, setTxData] = useState([])
 
   let start
   let requestId
@@ -53,15 +53,17 @@ export default function Ticker() {
     }
 
     const dl = tickerRef.current
-    const { left, width } = dl.firstElementChild.getBoundingClientRect()
+    if (dl.querySelector('dt')) {
+      const { left, width } = dl.firstElementChild.getBoundingClientRect()
 
-    if (left < -width) {
-      start -= width / PX_PER_SECOND
-      dl.appendChild(dl.firstElementChild) // <dt>
-      dl.appendChild(dl.firstElementChild) // <dd>
+      if (left < -width) {
+        start -= width / PX_PER_SECOND
+        dl.appendChild(dl.firstElementChild) // <dt>
+        dl.appendChild(dl.firstElementChild) // <dd>
+      }
+
+      dl.style.left = `${PX_PER_SECOND * (timestamp - start)}px`
     }
-
-    dl.style.left = `${PX_PER_SECOND * (timestamp - start)}px`
 
     requestId = window.requestAnimationFrame(step)
   }
@@ -123,67 +125,113 @@ export default function Ticker() {
     }
   }, [])
 
-  /* Ticker – Easter egg: define custom <marquee> element */
+  /* Easter egg: define custom <marquee> element */
 
   return (
-    <article className="flex w-full z-10 text-sm bg-zinc-50 dark:bg-zinc-950 absolute border-b border-zinc-300 dark:border-zinc-900 overflow-x-clip">
+    <article
+      className={`flex w-full z-10 text-sm bg-zinc-50 dark:bg-zinc-950 absolute border-b border-zinc-300 dark:border-zinc-900 overflow-x-clip justify-between ${
+        !txData.length ? 'opacity-70' : 'opacity-100'
+      }`}
+    >
       <button className="bg-zinc-50 dark:bg-zinc-950 px-4 py-1.5 border-r border-zinc-300 dark:border-zinc-800 flex items-center gap-2 z-10 bg-zinc-50">
-        <PulseDot />
-        <span className="md:after:content-['_–_All_transactions']">Live</span>
-        <span className="text-xxs">▼</span>
+        <svg
+          width="8"
+          height="8"
+          viewBox="-4 -4 8 8"
+          overflow="visible"
+          className={
+            txData.length
+              ? 'fill-green-500 stroke-green-500'
+              : 'fill-zinc-500 stroke-none'
+          }
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle r="4">
+            <animate
+              attributeName="stroke-width"
+              values="0; 16"
+              dur="1.5s"
+              repeatCount="indefinite"
+            />
+            <animate
+              attributeName="stroke-opacity"
+              values=".5; 0"
+              dur="1.5s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </svg>
+        {txData.length ? (
+          <>
+            {/* <span className="md:after:content-['_–_All_transactions']"> */}
+            Live
+            {/* </span> */}
+            {/* <span className="text-xxs">▼</span> */}
+          </>
+        ) : (
+          <>Loading…</>
+        )}
       </button>
       <dl
         ref={tickerRef}
-        className="relative grid grid-flow-col grid-rows-[1fr_0] w-0 grow cursor-pointer whitespace-nowrap"
+        className={`relative grid grid-flow-col grid-rows-[1fr_0] w-0 grow cursor-pointer whitespace-nowrap ${
+          !txData.length ? 'opacity-0' : 'opacity-100'
+        } transition-opacity`}
       >
-        {txData.length ? (
-          txData.map((tx, i) => (
-            <>
-              <dt className="row-start-1">
-                <a
-                  href="#"
-                  className="text-zinc-500 px-4 hover:text-inherit hover:underline py-1.5 block"
-                >
-                  {`${tx.origin.formattedAmount} ${tx.origin.payload} to ${tx.destination.chain}`}
-                </a>
-              </dt>
-              <dd className="row-start-2 animate-slide-down origin-top p-2 hidden [:hover_+_&]:block hover:block">
-                <a
-                  href="#"
-                  className="absolute px-3 py-2 bg-zinc-50 dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-900 rounded items-center grid gap-x-4 gap-y-1 shadow-sm"
-                >
-                  <ul className="inline">
-                    <li>
-                      {tx.origin.formattedAmount} {tx.origin.payload}
-                    </li>
-                    <li>{tx.origin.chain}</li>
-                  </ul>
-                  {arrowSvg()}
-                  <ul className="inline">
-                    <li>
-                      {tx.destination.formattedAmount} {tx.destination.payload}
-                    </li>
-                    <li>{tx.destination.chain}</li>
-                  </ul>
-                  <header className="text-zinc-500 row-start-2 col-span-3">
-                    {formatTimestamp(tx)}
-                  </header>
-                </a>
-              </dd>
-            </>
-          ))
-        ) : (
-          <>
-            <dt></dt>
-            <dd></dd>
-          </>
-        )}
+        {txData.map((tx) => (
+          <Fragment key={tx.origin.hash}>
+            <dt className="row-start-1">
+              <a
+                href="#"
+                className="text-zinc-500 px-4 hover:text-inherit hover:underline py-1.5 block"
+              >
+                {`${tx.origin.formattedAmount} ${tx.origin.payload} to ${tx.destination.chain}`}
+              </a>
+            </dt>
+            <dd className="row-start-2 animate-slide-down origin-top p-2 hidden [:hover_+_&]:block hover:block">
+              <a
+                href="#"
+                className="absolute px-3 py-2 bg-zinc-50 dark:bg-zinc-950 border border-zinc-200 dark:border-zinc-900 rounded items-center grid gap-x-4 gap-y-1 shadow-sm"
+              >
+                <ul className="inline">
+                  <li>
+                    {tx.origin.formattedAmount} {tx.origin.payload}
+                  </li>
+                  <li>{tx.origin.chain}</li>
+                </ul>
+                {arrowSvg()}
+                <ul className="inline">
+                  <li>
+                    {tx.destination.formattedAmount} {tx.destination.payload}
+                  </li>
+                  <li>{tx.destination.chain}</li>
+                </ul>
+                <header className="text-zinc-500 row-start-2 col-span-3">
+                  {formatTimestamp(tx)}
+                </header>
+              </a>
+            </dd>
+          </Fragment>
+        ))}
       </dl>
       <a
         href="#"
-        className="bg-inherit px-4 py-1.5 border-l border-zinc-300 dark:border-zinc-800 inline-block items-center gap-2 z-10 md:before:content-['Explorer_']"
+        className="bg-inherit px-4 py-1.5 border border-transparent border-l-zinc-300 dark:border-l-zinc-800 inline-block items-center z-10 md:before:content-['Explorer_'] md:before:mr-2.5 flex items-center hover:border-zinc-400 hover:dark:border-zinc-600 hover:rounded"
       >
-        {'->'}
+        <svg
+          width="5"
+          height="10"
+          viewBox="0 0 5 10"
+          fill="none"
+          stroke-width="2"
+          stroke-linejoin="round"
+          strokeLinecap="round"
+          overflow="visible"
+          className="inline stroke-zinc-600 dark:stroke-zinc-400"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M0,0 5,5 0,10" />
+        </svg>
       </a>
     </article>
   )
@@ -198,7 +246,7 @@ const arrowSvg = () => {
       fill="none"
       stroke-width="2"
       stroke-linejoin="round"
-      stroke-linecap="round"
+      strokeLinecap="round"
       overflow="visible"
       className="stroke-zinc-500"
       xmlns="http://www.w3.org/2000/svg"
