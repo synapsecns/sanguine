81d92b20c703bb08c8b4f135eaa37fa27c2e1dff
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-26 14:27:53 +0000

    Set app config

--- packages/contracts-communication/contracts/apps/InterchainAppBase.sol
+++ packages/contracts-communication/contracts/apps/InterchainAppBase.sol
@@ -4,9 +4,23 @@ pragma solidity ^0.8.0;
 import {InterchainAppBaseEvents} from "../events/InterchainAppBaseEvents.sol";
 import {IInterchainApp} from "../interfaces/IInterchainApp.sol";
 
+import {AppConfigV1} from "../libs/AppConfig.sol";
+
 abstract contract InterchainAppBase is InterchainAppBaseEvents, IInterchainApp {
-    // TODO: naming
+    // TODO: naming, visibility
     address public interchain;
+
+    /// @dev Required responses and optimistic period for the module responses.
+    AppConfigV1 private _appConfig;
+
+    /// @dev Sets the app config:
+    /// - requiredResponses: the number of module responses required for accepting the message
+    /// - optimisticPeriod: the minimum time after which the module responses are considered final
+    /// Note: Should be guarded with permissions check.
+    function _setAppConfigV1(AppConfigV1 memory appConfig) internal {
+        _appConfig = appConfig;
+        emit AppConfigV1Set(appConfig.requiredResponses, appConfig.optimisticPeriod);
+    }
 
     /// @dev Sets the interchain client address.
     /// Note: Should be guarded with permissions check.
--- packages/contracts-communication/contracts/events/InterchainAppBaseEvents.sol
+++ packages/contracts-communication/contracts/events/InterchainAppBaseEvents.sol
@@ -2,5 +2,6 @@
 pragma solidity ^0.8.0;
 
 abstract contract InterchainAppBaseEvents {
+    event AppConfigV1Set(uint256 requiredResponses, uint256 optimisticPeriod);
     event InterchainClientSet(address interchainClient);
 }
