4677035babb5936ab6b6f901e53eabe47da5c394
Trajan0x <trajan0x@users.noreply.github.com>
2024-02-28 04:02:38 +0000

    pass5

--- packages/contracts-communication/contracts/ExecutionService.sol
+++ packages/contracts-communication/contracts/ExecutionService.sol
@@ -70,7 +70,7 @@ contract ExecutionService is ExecutionServiceEvents, Ownable, IExecutionService 
     {
         (uint8 version, bytes memory data) = options.decodeVersionedOptions();
         if (version == OptionsLib.OPTIONS_V1) {
-            OptionsV1 memory optionsV1 = data.decodeOptionsV1();
+            OptionsV1 memory optionsV1 = options.decodeOptionsV1();
 //            uint256 baseCost = gasOracle.estimateTxCostInLocalUnits(dstChainId, optionsV1.gasLimit, txPayloadSize);
 //            if (optionsV1.gasAirdrop > 0) {
 //                baseCost += gasOracle.convertRemoteValueToLocalUnits(dstChainId, optionsV1.gasAirdrop);
