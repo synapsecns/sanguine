9236c5aa640b01e3786e4a5004941df23c25926c
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-29 20:40:03 +0000

    Reorder deployments

--- packages/contracts-communication/script/testnet-deploy.sh
+++ packages/contracts-communication/script/testnet-deploy.sh
@@ -12,13 +12,12 @@ fi
 shift 2
 options=$@
 
-# Deploy InterchainDB
 yarn sol-run-str script/DeployNoArgs.s.sol $chainName $walletName "InterchainDB" $options
-# Deploy Ownable contracts
-yarn sol-run-str script/DeployWithMsgSender.s.sol $chainName $walletName "InterchainAppExample" $options
-yarn sol-run-str script/DeployWithMsgSender.s.sol $chainName $walletName "ExecutionFees" $options
-yarn sol-run-str script/DeployWithMsgSender.s.sol $chainName $walletName "ExecutionService" $options
 
-# Deploy contracts that rely on other contracts
+yarn sol-run-str script/DeployWithMsgSender.s.sol $chainName $walletName "InterchainAppExample" $options
+
 yarn sol-run script/DeployInterchainClientV1.s.sol $chainName $walletName $options
 yarn sol-run script/DeploySynapseModule.s.sol $chainName $walletName $options
+
+yarn sol-run-str script/DeployWithMsgSender.s.sol $chainName $walletName "ExecutionFees" $options
+yarn sol-run-str script/DeployWithMsgSender.s.sol $chainName $walletName "ExecutionService" $options
