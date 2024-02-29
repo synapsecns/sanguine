fcf1ce8c7455800d3b649b45e9fde103e10fb642
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-02-28 18:51:55 -0800

    Basic Address Input

--- packages/synapse-interface/components/StateManagedBridge/_DestinationAddressInput.tsx
+++ packages/synapse-interface/components/StateManagedBridge/_DestinationAddressInput.tsx
@@ -1,7 +1,24 @@
-export const _DestinationAddressInput = () => {
+import { useState } from 'react'
+
+export const _DestinationAddressInput = ({
+  connectedAddress,
+}: {
+  connectedAddress: string
+}) => {
+  const [showWarning, setShowWarning] = useState<boolean>(true)
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const handleInput = (value) => {
+    setInputValue(value)
+  }
+
   return (
     <div id="destination-address-input">
-      <input />
+      <input
+        placeholder={connectedAddress ?? 'Connect Wallet'}
+        value={inputValue}
+        onChange={handleInput}
+      />
     </div>
   )
 }
