d32d59ee9ab79c892fa72a1acaa1fd19f8ada4a1
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-02-28 19:04:08 -0800

    Click on Input to trigger warning

--- packages/synapse-interface/components/StateManagedBridge/_DestinationAddressInput.tsx
+++ packages/synapse-interface/components/StateManagedBridge/_DestinationAddressInput.tsx
@@ -6,17 +6,27 @@ export const _DestinationAddressInput = ({
 }: {
   connectedAddress: string
 }) => {
-  const [showWarning, setShowWarning] = useState<boolean>(true)
+  // TODO: Lift state up to Redux slice after working example
+  const [enableInput, setEnableInput] = useState<boolean>(false)
+  const [showWarning, setShowWarning] = useState<boolean>(false)
   const [inputValue, setInputValue] = useState<string>('')
 
   const handleInput = (value) => {
     setInputValue(value)
   }
 
+  const handleActivateWarning = () => {
+    setShowWarning(true)
+  }
+
+  const handleEnableInput = () => {
+    setEnableInput(true)
+  }
+
   const isInputValidAddress = isValidAddress(inputValue)
 
   return (
-    <div id="destination-address-input">
+    <div id="destination-address-input" onClick={handleActivateWarning}>
       <input
         placeholder={connectedAddress ?? 'Connect Wallet'}
         value={inputValue}
