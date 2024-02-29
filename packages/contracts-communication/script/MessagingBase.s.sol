842c6998cc204fba370b5e8a0cd3b819ed78b75d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-02-29 11:49:09 +0000

    Chore: `forge fmt`

--- packages/contracts-communication/script/MessagingBase.s.sol
+++ packages/contracts-communication/script/MessagingBase.s.sol
@@ -65,7 +65,13 @@ contract MessagingBase is Script {
         uint256 _requiredResponses = 1;
         uint64 _optimisticTimePeriod = 1;
         icApp.setAppConfig(
-            chainIDs, linkedIApps, _sendingModules, _receivingModules, address(0), _requiredResponses, _optimisticTimePeriod
+            chainIDs,
+            linkedIApps,
+            _sendingModules,
+            _receivingModules,
+            address(0),
+            _requiredResponses,
+            _optimisticTimePeriod
         );
         vm.stopBroadcast();
     }
@@ -84,7 +90,13 @@ contract MessagingBase is Script {
         uint256 _requiredResponses = 1;
         uint64 _optimisticTimePeriod = 1;
         icApp.setAppConfig(
-            chainIDs, linkedIApps, _sendingModules, _receivingModules, address(0), _requiredResponses, _optimisticTimePeriod
+            chainIDs,
+            linkedIApps,
+            _sendingModules,
+            _receivingModules,
+            address(0),
+            _requiredResponses,
+            _optimisticTimePeriod
         );
         vm.stopBroadcast();
     }
