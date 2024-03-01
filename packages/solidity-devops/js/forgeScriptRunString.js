a9ab234772cb306883ae9cb5f38be1f2f9148fbd
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-01 19:22:00 +0300

    Fix: solidity-devops bin exports (#2186)
    * Try fixing exports
    
    * Another futile attempt at finding a good name
    
    * Chore: executable filemode
    
    * Fix: usage, script

--- packages/solidity-devops/js/forgeScriptRunString.js
+++ packages/solidity-devops/js/forgeScriptRunString.js
@@ -5,11 +5,11 @@ const { runCommand } = require('./utils/utils.js')
 const { positionalArgs, options } = parseCommandLineArgs({
   requiredArgsCount: 4,
   usage:
-    'Usage: "yarn sol-run-str <path-to-script> <chain-name> <wallet-name> <string-arg> [<options>]"',
+    'Usage: "yarn fsr-str <path-to-script> <chain-name> <wallet-name> <string-arg> [<options>]"',
 })
 
 const [scriptFN, chainName, walletName, stringArg] = positionalArgs
 
 const newOptions = addOptions(options, `--sig "run(string)" "${stringArg}"`)
-// launch run.js with the new options
-runCommand(`yarn sol-run ${scriptFN} ${chainName} ${walletName} ${newOptions}`)
+// launch forgeScriptRun.js with the new options
+runCommand(`yarn fsr ${scriptFN} ${chainName} ${walletName} ${newOptions}`)
