7c8ae0e02b5d391f25ee0662a9e8670abb491335
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-01 19:23:59 +0000

    Make gas limit configurable

--- packages/contracts-communication/contracts/apps/PingPongApp.sol
+++ packages/contracts-communication/contracts/apps/PingPongApp.sol
@@ -10,18 +10,27 @@ import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 /// @notice A simple app that sends a message to the remote PingPongApp, which will respond with a message back.
 /// This app can be loaded with a native asset, which will be used to pay for the messages sent.
 contract PingPongApp is OwnableApp {
-    uint256 public constant GAS_LIMIT = 500_000;
+    uint256 internal constant DEFAULT_GAS_LIMIT = 500_000;
+    uint256 public gasLimit;
 
+    event GasLimitSet(uint256 gasLimit);
     event PingDisrupted(uint256 counter);
     event PingReceived(uint256 counter);
     event PingSent(uint256 counter);
 
     error PingPongApp__LowBalance(uint256 required);
 
-    constructor(address owner_) OwnableApp(owner_) {}
+    constructor(address owner_) OwnableApp(owner_) {
+        _setGasLimit(DEFAULT_GAS_LIMIT);
+    }
 
     /// @notice Enables the contract to accept native asset.
     receive() external payable {}
+
+    /// @notice Allows the owner to set the gas limit for the interchain messages.
+    function setGasLimit(uint256 gasLimit_) external onlyOwner {
+        _setGasLimit(gasLimit_);
+    }
 
     /// @notice Allows the owner to withdraw the native asset from the contract.
     function withdraw() external onlyOwner {
@@ -36,7 +45,7 @@ contract PingPongApp is OwnableApp {
 
     /// @notice Returns the fee to send a single ping message to the remote PingPongApp.
     function getPingFee(uint256 dstChainId) external view returns (uint256) {
-        OptionsV1 memory options = OptionsV1({gasLimit: GAS_LIMIT, gasAirdrop: 0});
+        OptionsV1 memory options = OptionsV1({gasLimit: gasLimit, gasAirdrop: 0});
         bytes memory message = abi.encode(uint256(0));
         return _getInterchainFee(dstChainId, options, message);
     }
@@ -63,7 +72,7 @@ contract PingPongApp is OwnableApp {
     /// If `counter > 0`, the remote app will respond with a message to this app, decrementing the counter.
     /// Once the counter reaches 0, the remote app will not respond.
     function _sendPingPongMessage(uint256 dstChainId, uint256 counter, bool lowBalanceRevert) internal {
-        OptionsV1 memory options = OptionsV1({gasLimit: GAS_LIMIT, gasAirdrop: 0});
+        OptionsV1 memory options = OptionsV1({gasLimit: gasLimit, gasAirdrop: 0});
         bytes memory message = abi.encode(counter);
         uint256 messageFee = _getInterchainFee(dstChainId, options, message);
         if (address(this).balance < messageFee) {
@@ -76,5 +85,11 @@ contract PingPongApp is OwnableApp {
         }
         _sendInterchainMessage(dstChainId, messageFee, options, message);
         emit PingSent(counter);
+    }
+
+    /// @dev Sets the gas limit for the interchain messages.
+    function _setGasLimit(uint256 gasLimit_) internal {
+        gasLimit = gasLimit_;
+        emit GasLimitSet(gasLimit_);
     }
 }
