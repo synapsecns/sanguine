e3d7939131e318507e8e011aa99731b5ec413415
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-04 16:25:57 +0000

    InterchainModule: handle relayed entry+data

--- packages/contracts-communication/contracts/modules/InterchainModule.sol
+++ packages/contracts-communication/contracts/modules/InterchainModule.sol
@@ -5,7 +5,7 @@ import {InterchainModuleEvents} from "../events/InterchainModuleEvents.sol";
 import {IInterchainDB} from "../interfaces/IInterchainDB.sol";
 import {IInterchainModule} from "../interfaces/IInterchainModule.sol";
 
-import {InterchainEntry} from "../libs/InterchainEntry.sol";
+import {InterchainEntry, ModuleEntryLib} from "../libs/ModuleEntry.sol";
 
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
@@ -46,19 +46,23 @@ abstract contract InterchainModule is InterchainModuleEvents, IInterchainModule 
     /// @dev Should be called once the Module has verified the entry and needs to signal this
     /// to the InterchainDB.
     function _verifyEntry(bytes memory encodedEntry) internal {
-        InterchainEntry memory entry = abi.decode(encodedEntry, (InterchainEntry));
+        (InterchainEntry memory entry, bytes memory moduleData) = ModuleEntryLib.decodeModuleEntry(encodedEntry);
         if (entry.srcChainId == block.chainid) {
             revert InterchainModule__SameChainId();
         }
         IInterchainDB(INTERCHAIN_DB).verifyEntry(entry);
+        _receiveModuleData(entry.srcChainId, entry.dbNonce, moduleData);
         emit EntryVerified(
             entry.srcChainId, encodedEntry, MessageHashUtils.toEthSignedMessageHash(keccak256(encodedEntry))
         );
     }
 
+    // solhint-disable no-empty-blocks
     /// @dev Internal logic to request the verification of an entry on the destination chain.
-    // solhint-disable-next-line no-empty-blocks
     function _requestVerification(uint256 destChainId, bytes memory encodedEntry) internal virtual {}
+
+    /// @dev Internal logic to handle the auxiliary module data relayed from the remote chain.
+    function _receiveModuleData(uint256 srcChainId, uint256 dbNonce, bytes memory moduleData) internal virtual {}
 
     /// @dev Internal logic to get the module fee for verifying an entry on the specified destination chain.
     function _getModuleFee(uint256 destChainId) internal view virtual returns (uint256);
