90219ee902be3d5985d54a9ab2523db519473a72
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-03-05 16:59:33 -0600

    Feat: add circlecctp package for circle's MessageTransmitter go bindings

---
+++ services/cctp-relayer/contracts/circlecctp/doc.go
@@ -0,0 +1,2 @@
+// Package circlecctp contains a native interface for interacting with Circle's Cross-Chain transfer protocol.
+package circlecctp
---
+++ services/cctp-relayer/contracts/circlecctp/generate.go
@@ -0,0 +1,3 @@
+package circlecctp
+
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ../../external/evm-cctp-contracts/MessageTransmitter_flattened.sol --pkg circlecctp --sol-version 0.7.6 --filename circlecctp
