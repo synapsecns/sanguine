2acc1f7e848a2cf11dbcb09afb2fd6ded7e755ec
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-03-05 16:55:12 -0600

    Feat: add CircleCCTPAddress to chain config

--- services/cctp-relayer/config/chain.go
+++ services/cctp-relayer/config/chain.go
@@ -13,11 +13,18 @@ type ChainConfig struct { // ChainID is the ID of the chain.
 	ChainID uint32 `yaml:"chain_id"`
 	// SynapseCCTPAddress is the address of the SynapseCCTP contract.
 	SynapseCCTPAddress string `yaml:"synapse_cctp_address"`
+	// CircleCCTPAddress is the address of the CircleCCTP contract.
+	CircleCCTPAddress string `yaml:"circle_cctp_address"`
 }
 
 // GetSynapseCCTPAddress returns the SynapseCCTP address.
 func (c ChainConfig) GetSynapseCCTPAddress() common.Address {
 	return common.HexToAddress(c.SynapseCCTPAddress)
+}
+
+// GetCircleCCTPAddress returns the CircleCCTP address.
+func (c ChainConfig) GetCircleCCTPAddress() common.Address {
+	return common.HexToAddress(c.CircleCCTPAddress)
 }
 
 // ChainConfigs contains an array of ChainConfigs.
@@ -33,8 +40,12 @@ func (c ChainConfigs) IsValid(_ context.Context) (ok bool, err error) {
 		}
 		intSet.Add(cfg.ChainID)
 
-		if !common.IsHexAddress(cfg.SynapseCCTPAddress) {
+		if len(cfg.SynapseCCTPAddress) > 0 && !common.IsHexAddress(cfg.SynapseCCTPAddress) {
 			return false, fmt.Errorf("invalid address %s: %s", cfg.SynapseCCTPAddress, "invalid address")
+		}
+
+		if len(cfg.CircleCCTPAddress) > 0 && !common.IsHexAddress(cfg.CircleCCTPAddress) {
+			return false, fmt.Errorf("invalid address %s: %s", cfg.CircleCCTPAddress, "invalid address")
 		}
 	}
 
@@ -47,8 +58,8 @@ func (c ChainConfig) IsValid(ctx context.Context) (ok bool, err error) {
 		return false, fmt.Errorf("%s: chain ID cannot be 0", "invalid chain id")
 	}
 
-	if c.SynapseCCTPAddress == "" {
-		return false, fmt.Errorf("field SynapseCCTPAddress is required")
+	if c.SynapseCCTPAddress == "" && c.CircleCCTPAddress == "" {
+		return false, fmt.Errorf("at least one of SynapseCCTPAddress and CircleCCTPAddress is required")
 	}
 
 	return true, nil
