07f68fe47058f729efc6693ecfd0cf25fdbf6583
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-03-06 12:14:45 -0800

    Countdown title

--- packages/synapse-interface/pages/countdown/index.tsx
+++ packages/synapse-interface/pages/countdown/index.tsx
@@ -5,7 +5,13 @@ const Countdown = () => {
   const router = useRouter()
   const { query, pathname } = router
 
-  return <LandingPageWrapper>Countdown to Dencun Upgrade</LandingPageWrapper>
+  return (
+    <LandingPageWrapper>
+      <section className="flex justify-center py-24">
+        <div className="text-3xl text-white">Countdown to Dencun Upgrade</div>
+      </section>
+    </LandingPageWrapper>
+  )
 }
 
 export default Countdown
