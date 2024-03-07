da7b2a90f9067052e5f1a16d8ddf90c32c17692d
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-03-07 17:08:55 -0600

    Fix: cctp build

--- services/cctp-relayer/cmd/cmd.go
+++ services/cctp-relayer/cmd/cmd.go
@@ -23,7 +23,7 @@ func Start(args []string, buildInfo config.BuildInfo) {
 	}
 
 	// commands
-	app.Commands = cli.Commands{RunCommand}
+	app.Commands = cli.Commands{runCommand}
 	shellCommand := commandline.GenerateShellCommand(app.Commands)
 	app.Commands = append(app.Commands, shellCommand)
 	app.Action = shellCommand.Action
