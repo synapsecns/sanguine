c601dff0373559382ea45c3a9d7668e43219a5ae
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-03-08 11:24:23 -0800

    Improve isEmptyString

--- packages/synapse-interface/components/StateManagedBridge/_DestinationAddressInput.tsx
+++ packages/synapse-interface/components/StateManagedBridge/_DestinationAddressInput.tsx
@@ -28,7 +28,9 @@ export const _DestinationAddressInput = ({
 
   const isInputValidAddress = isValidAddress(destinationAddress)
   const isInputInvalid =
-    !isEmptyString(destinationAddress) && !isInputValidAddress
+    destinationAddress &&
+    !isEmptyString(destinationAddress) &&
+    !isInputValidAddress
 
   let placeholder
 
