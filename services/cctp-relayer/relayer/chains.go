e4d8dada4bc2cde3183a4d3214075ad14ce3ea2f
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-03-07 21:30:15 -0600

    Feat: export IsTestnetChainID, add reverse maps

--- services/cctp-relayer/relayer/chains.go
+++ services/cctp-relayer/relayer/chains.go
@@ -14,10 +14,6 @@ const sepoliaChainID = 11155111
 const opSepoliaChainID = 11155420
 const arbitrumSepoliaChainID = 421614
 
-func isTestnetChainID(chainID uint32) bool {
-	return chainID == sepoliaChainID || chainID == opSepoliaChainID || chainID == arbitrumSepoliaChainID
-}
-
 // see https://developers.circle.com/stablecoins/docs/evm-smart-contracts#mainnet-contract-addresses
 var mainnetDomainMap = map[uint32]uint32{
 	0: ethereumChainID,
@@ -30,6 +26,14 @@ var testnetDomainMap = map[uint32]uint32{
 	0: sepoliaChainID,
 	2: opSepoliaChainID,
 	3: arbitrumSepoliaChainID,
+}
+
+var testnetChainIDMap map[uint32]uint32 // Maps chainID to domain for testnet
+var mainnetChainIDMap map[uint32]uint32 // Maps chainID to domain for mainnet
+
+// IsTestnetChainID returns true if the chain ID is a testnet chain ID.
+func IsTestnetChainID(chainID uint32) bool {
+	return chainID == sepoliaChainID || chainID == opSepoliaChainID || chainID == arbitrumSepoliaChainID
 }
 
 // CircleDomainToChainID converts a Circle domain to a chain ID.
@@ -47,4 +51,38 @@ func CircleDomainToChainID(domain uint32, isTestnet bool) (chainID uint32, err e
 		return 0, fmt.Errorf("domain %d not found in mainnet domain map", domain)
 	}
 	return chainID, nil
+}
+
+// ChainIDToCircleDomain converts a chain ID to a Circle domain.
+func ChainIDToCircleDomain(chainID uint32, isTestnet bool) (domain uint32, err error) {
+	if testnetChainIDMap == nil || mainnetChainIDMap == nil {
+		initReverseMaps()
+	}
+
+	if isTestnet {
+		domain, ok := testnetChainIDMap[chainID]
+		if !ok {
+			return 0, fmt.Errorf("chain ID %d not found in testnet chain ID map", chainID)
+		}
+		return domain, nil
+	}
+
+	domain, ok := mainnetChainIDMap[chainID]
+	if !ok {
+		return 0, fmt.Errorf("chain ID %d not found in mainnet chain ID map", chainID)
+	}
+	return domain, nil
+}
+
+func initReverseMaps() {
+	testnetChainIDMap = make(map[uint32]uint32)
+	mainnetChainIDMap = make(map[uint32]uint32)
+
+	for domain, chainID := range testnetDomainMap {
+		testnetChainIDMap[chainID] = domain
+	}
+
+	for domain, chainID := range mainnetDomainMap {
+		mainnetChainIDMap[chainID] = domain
+	}
 }
