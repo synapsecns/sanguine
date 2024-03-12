b71f16d77d9fbbc09cf261e3500e2d1ee42501e3
abtestingalpha <abtestingalpha@gmail.com>
2024-03-12 17:00:28 -0500

    More robust ethers v5 provider support

--- packages/widget/src/state/slices/bridgeTransaction/hooks.ts
+++ packages/widget/src/state/slices/bridgeTransaction/hooks.ts
@@ -42,46 +42,50 @@ export const executeBridgeTxn = createAsyncThunk(
     signer: any
     synapseSDK: any
   }) => {
-    const data = await synapseSDK.bridge(
-      destinationAddress,
-      originRouterAddress,
-      originChainId,
-      destinationChainId,
-      tokenAddress,
-      amount,
-      originQuery,
-      destQuery
-    )
+    try {
+      const data = await synapseSDK.bridge(
+        destinationAddress,
+        originRouterAddress,
+        originChainId,
+        destinationChainId,
+        tokenAddress,
+        amount,
+        originQuery,
+        destQuery
+      )
 
-    const payload =
-      tokenAddress === ZeroAddress
-        ? {
-            data: data.data,
-            to: data.to,
-            value: amount,
-          }
-        : {
-            data: data.data,
-            to: data.to,
-          }
+      const payload =
+        tokenAddress === ZeroAddress
+          ? {
+              data: data.data,
+              to: data.to,
+              value: amount,
+            }
+          : {
+              data: data.data,
+              to: data.to,
+            }
 
-    const tx = await signer.sendTransaction(payload)
+      const tx = await signer.sendTransaction(payload)
 
-    const receipt = await tx.wait()
+      const receipt = await tx.wait()
 
-    const txHash = receipt?.hash
+      const txHash = receipt?.hash ?? receipt?.transactionHash
 
-    const timestamp = getTimeMinutesFromNow(0)
+      const timestamp = getTimeMinutesFromNow(0)
 
-    return {
-      txHash,
-      bridgeModuleName,
-      parsedOriginAmount,
-      originTokenSymbol,
-      originChainId,
-      destinationChainId,
-      estimatedTime,
-      timestamp,
+      return {
+        txHash,
+        bridgeModuleName,
+        parsedOriginAmount,
+        originTokenSymbol,
+        originChainId,
+        destinationChainId,
+        estimatedTime,
+        timestamp,
+      }
+    } catch (error) {
+      console.error('Error executing bridge: ', error)
     }
   }
 )
