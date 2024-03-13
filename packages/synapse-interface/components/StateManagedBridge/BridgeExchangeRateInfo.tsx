3d0ab007711033b3646308074b0aa11df2ca181c
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-03-12 23:36:50 -0700

    FE Release 2024-03-12 (#2271)

--- packages/synapse-interface/components/StateManagedBridge/BridgeExchangeRateInfo.tsx
+++ packages/synapse-interface/components/StateManagedBridge/BridgeExchangeRateInfo.tsx
@@ -24,7 +24,7 @@ const BridgeExchangeRateInfo = () => {
       <section className="p-2 space-y-1 text-sm border rounded-sm border-[#504952] text-secondary font-light">
         <GasDropLabel />
         <Router />
-        {/* <Rebate /> */}
+        <Rebate />
         <Slippage />
       </section>
     </div>
