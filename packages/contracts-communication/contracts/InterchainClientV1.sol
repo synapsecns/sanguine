432aeeeeaea432c57aad8ab88f4cf68f883a6d9e
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-13 13:20:31 +0000

    Fix: check for empty receiver field

--- packages/contracts-communication/contracts/InterchainClientV1.sol
+++ packages/contracts-communication/contracts/InterchainClientV1.sol
@@ -225,6 +225,7 @@ contract InterchainClientV1 is Ownable, InterchainClientV1Events, IInterchainCli
         returns (InterchainTxDescriptor memory desc)
     {
         _assertLinkedClient(dstChainId);
+        if (receiver == 0) revert InterchainClientV1__ZeroReceiver();
         // Check that options could be decoded on destination chain
         options.decodeOptionsV1();
         uint256 verificationFee = IInterchainDB(INTERCHAIN_DB).getInterchainFee(dstChainId, srcModules);
