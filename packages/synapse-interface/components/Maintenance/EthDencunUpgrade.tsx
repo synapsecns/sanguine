3bfa59f0e4fc6b0939ebf3aee1f8e56cb61aa4d3
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2024-03-13 05:55:29 +0000

    change copy

--- packages/synapse-interface/components/Maintenance/EthDencunUpgrade.tsx
+++ packages/synapse-interface/components/Maintenance/EthDencunUpgrade.tsx
@@ -4,7 +4,9 @@ import { AnnouncementBanner } from './AnnouncementBanner'
  * Start: 15 min prior to Eth Dencun Upgrade Time @ 3/13/24 13:55 UTC
  * End: 30 min after start of Eth Decun Upgrade Time
  */
-export const ETH_DENCUN_BANNER_START = new Date(Date.UTC(2024, 2, 12, 0, 0, 0))
+export const ETH_DENCUN_BANNER_START = new Date(
+  Date.UTC(2024, 2, 13, 13, 20, 0)
+)
 export const ETH_DENCUN_START_DATE = new Date(Date.UTC(2024, 2, 13, 13, 40, 0))
 export const ETH_DENCUN_END_DATE = new Date(Date.UTC(2024, 2, 13, 14, 25, 0))
 
@@ -15,10 +17,10 @@ export const EthDencunUpgradeBanner = () => {
       bannerContents={
         <div className="flex flex-col justify-center space-y-1 text-center">
           <div>
-            The Bridge + RFQ will be globally offline 15mins ahead of the Dencun
-            upgrade (March 13, 13:55 UTC, 9:55 EST).
+            Synapse Bridge is upgrading ahead of the Ethereum Dencun upgrade
+            (March 13, 13:55 UTC, 9:55 EST).
           </div>
-          <div>Will be back online about 15 - 30 mins after Dencun.</div>
+          <div>Will be back online shortly ahead of the network upgrade.</div>
         </div>
       }
       startDate={ETH_DENCUN_BANNER_START}
