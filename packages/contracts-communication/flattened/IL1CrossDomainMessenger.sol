4221b511b016566c142e5cb01f606cc9d1c12f65
Trajan0x <trajan0x@users.noreply.github.com>
2024-03-13 03:41:25 +0000

    do not flatten [goreleaser]

--- packages/contracts-communication/flattened/IL1CrossDomainMessenger.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.8.20;
-
-// contracts/modules/IL1CrossDomainMessenger.sol
-
-interface IL1CrossDomainMessenger {
-    function sendMessage(address target, bytes calldata message, uint32 gasLimit) external payable;
-
-    function xDomainMessageSender() external returns (address);
-}
