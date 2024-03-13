8cff1f6e82cd6e8ab0d77e10d043c604bdd51aed
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-03-13 07:12:29 -0700

    Add Ecotone Fork upgrade banner

---
+++ packages/synapse-interface/components/Maintenance/EcotoneForkUpgrade.tsx
@@ -0,0 +1,32 @@
+import { AnnouncementBanner } from './AnnouncementBanner'
+
+/**
+ * Start: 10 min prior to Ecotone Fork Upgrade Time @ (March 14, 00:00 UTC)
+ * End: 10 min after start of Ecotone Fork Upgrade Time
+ */
+export const ECOTONE_FORK_BANNERS_START = new Date(
+  Date.UTC(2024, 2, 13, 23, 20, 0)
+)
+export const ECOTONE_FORK_START_DATE = new Date(
+  Date.UTC(2024, 2, 13, 23, 50, 0)
+)
+export const ECOTONE_FORK_END_DATE = new Date(Date.UTC(2024, 2, 13, 24, 10, 0))
+
+export const EcotoneForkUpgradeBanner = () => {
+  return (
+    <AnnouncementBanner
+      bannerId="03142024-ecotone-fork"
+      bannerContents={
+        <div className="flex flex-col justify-center space-y-1 text-center">
+          <div>
+            Optimism + Base Bridging and RFQ will be paused 10 minutes ahead of
+            Ecotone (March 14, 00:00 UTC).
+          </div>
+          <div>Will be back online shortly following the network upgrade.</div>
+        </div>
+      }
+      startDate={ECOTONE_FORK_START_DATE}
+      endDate={ECOTONE_FORK_END_DATE}
+    />
+  )
+}
