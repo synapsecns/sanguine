19c25ee3fc854cb3628a8da2905f44a420c5977a
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-20 21:57:27 +0000

    Update ExampleApp

--- packages/contracts-communication/contracts/apps/ExampleAppV1.sol
+++ packages/contracts-communication/contracts/apps/ExampleAppV1.sol
@@ -1,22 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {OwnableApp} from "./OwnableApp.sol";
+import {ICAppV1} from "./ICAppV1.sol";
+
 import {InterchainTxDescriptor} from "../libs/InterchainTransaction.sol";
 import {OptionsV1} from "../libs/Options.sol";
 
-contract InterchainAppExample is OwnableApp {
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+contract ExampleAppV1 is ICAppV1 {
     event MessageReceived(uint256 srcChainId, bytes32 sender, uint256 dbNonce, uint64 entryIndex, bytes message);
     event MessageSent(uint256 dstChainId, uint256 dbNonce, uint64 entryIndex, bytes32 transactionId);
 
-    constructor(address owner_) OwnableApp(owner_) {}
+    constructor(address admin) ICAppV1(admin) {
+        _grantRole(IC_GOVERNOR_ROLE, admin);
+    }
+
+    /// @notice Allows the Admin to withdraw the native asset from the contract.
+    function withdraw() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        Address.sendValue(payable(msg.sender), address(this).balance);
+    }
 
     /// @notice Sends a basic message to the destination chain.
-    function sendMessage(uint256 dstChainId, uint256 gasLimit, bytes calldata message) external payable {
-        InterchainTxDescriptor memory desc = _sendInterchainMessage({
+    function sendMessage(
+        uint256 dstChainId,
+        uint256 gasLimit,
+        uint256 gasAirdrop,
+        bytes calldata message
+    )
+        external
+        payable
+    {
+        InterchainTxDescriptor memory desc = _sendToLinkedApp({
             dstChainId: dstChainId,
             messageFee: msg.value,
-            options: OptionsV1({gasLimit: gasLimit, gasAirdrop: 0}),
+            options: OptionsV1({gasLimit: gasLimit, gasAirdrop: gasAirdrop}),
             message: message
         });
         emit MessageSent(dstChainId, desc.dbNonce, desc.entryIndex, desc.transactionId);
