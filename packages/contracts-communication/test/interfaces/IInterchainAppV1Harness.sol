d5733ceffe1b5afedfe8e070b013434c49d14fd0
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-20 18:56:16 +0000

    Add App harness and client mock

---
+++ packages/contracts-communication/test/interfaces/IInterchainAppV1Harness.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IInterchainAppV1} from "../../contracts/interfaces/IInterchainAppV1.sol";
+import {InterchainTxDescriptor} from "../../contracts/libs/InterchainTransaction.sol";
+import {OptionsV1} from "../../contracts/libs/Options.sol";
+
+interface IInterchainAppV1Harness is IInterchainAppV1 {
+    event MessageReceived(uint256 srcChainId, bytes32 sender, uint256 dbNonce, uint64 entryIndex, bytes message);
+
+    function exposed__sendInterchainMessageEVM(
+        uint256 dstChainId,
+        address receiver,
+        uint256 messageFee,
+        bytes memory options,
+        bytes memory message
+    )
+        external
+        returns (InterchainTxDescriptor memory desc);
+
+    function exposed__sendInterchainMessage(
+        uint256 dstChainId,
+        bytes32 receiver,
+        uint256 messageFee,
+        bytes memory options,
+        bytes memory message
+    )
+        external
+        returns (InterchainTxDescriptor memory desc);
+
+    function exposed__sendToLinkedApp(
+        uint256 dstChainId,
+        uint256 messageFee,
+        OptionsV1 memory options,
+        bytes memory message
+    )
+        external
+        returns (InterchainTxDescriptor memory desc);
+
+    function exposed__getInterchainFee(
+        uint256 dstChainId,
+        bytes memory options,
+        bytes memory message
+    )
+        external
+        view
+        returns (uint256);
+}
