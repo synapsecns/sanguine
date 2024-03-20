18bf863fe6467c49a07f83f321a6f92c54fa3cab
abtestingalpha <abtestingalpha@gmail.com>
2024-03-20 12:59:22 -0400

    Pause bridging to metis

--- packages/synapse-interface/components/Maintenance/Events/example/EcotoneForkUpgrade.tsx
+++ packages/synapse-interface/components/Maintenance/Events/example/EcotoneForkUpgrade.tsx
@@ -1,6 +1,6 @@
 import { useBridgeState } from '@/slices/bridge/hooks'
 import { useIntervalTimer } from '@/utils/hooks/useIntervalTimer'
-import { OPTIMISM, BASE, METIS } from '@/constants/chains/master'
+import { METIS } from '@/constants/chains/master'
 import {
   useEventCountdownProgressBar,
   getCountdownTimeStatus,
@@ -21,8 +21,8 @@ import { WarningMessage } from '../../../Warning'
 export const ECOTONE_FORK_BANNERS_START = new Date(
   Date.UTC(2024, 2, 20, 16, 0, 0)
 )
-export const ECOTONE_FORK_START_DATE = new Date(Date.UTC(2024, 2, 20, 18, 0, 0))
-export const ECOTONE_FORK_END_DATE = new Date(Date.UTC(2024, 2, 14, 0, 25, 0))
+export const ECOTONE_FORK_START_DATE = new Date(Date.UTC(2024, 2, 20, 16, 0, 0))
+export const ECOTONE_FORK_END_DATE = new Date(Date.UTC(2024, 2, 20, 18, 0, 0))
 
 /** Previous implementation can be seen here: https://github.com/synapsecns/sanguine/pull/2294/files#diff-bbe6298d3dfbc80e46e2ff8b399a3e1822cede80f392b1af91875145ad4eeb19R19 */
 export const EcotoneForkUpgradeBanner = () => {
@@ -35,9 +35,9 @@ export const EcotoneForkUpgradeBanner = () => {
 
   return (
     <AnnouncementBanner
-      bannerId="03142024-ecotone-fork"
+      bannerId="03202024-metis-pause"
       bannerContents={
-        <>Base + Metis Bridging is paused. Will be back online shortly.</>
+        <>Metis Bridging is paused. Will be back online shortly.</>
       }
       startDate={ECOTONE_FORK_BANNERS_START}
       endDate={ECOTONE_FORK_END_DATE}
@@ -54,15 +54,14 @@ export const EcotoneForkUpgradeBanner = () => {
 export const EcotoneForkWarningMessage = () => {
   const { fromChainId, toChainId } = useBridgeState()
 
-  const isChainMetis = [fromChainId, toChainId].includes(METIS.id)
-  const isChainBase = [fromChainId, toChainId].includes(BASE.id)
+  const isToChainMetis = toChainId === METIS.id
 
-  if (isChainMetis || isChainBase) {
+  if (isToChainMetis) {
     return (
       <WarningMessage
         message={
           <>
-            <p>Metis Chain and Base Chain bridging are paused.</p>
+            <p>Metis Chain bridging are paused.</p>
           </>
         }
       />
@@ -96,23 +95,22 @@ export const EcotoneForkWarningMessage = () => {
 export const useEcotoneForkCountdownProgress = () => {
   const { fromChainId, toChainId } = useBridgeState()
 
-  const isChainOptimism = [fromChainId, toChainId].includes(OPTIMISM.id)
-  const isChainBase = [fromChainId, toChainId].includes(BASE.id)
+  const isToChainMetis = toChainId === METIS.id
 
   const {
     isPending: isEcotoneForkUpgradePending,
     EventCountdownProgressBar: EcotoneForkCountdownProgressBar,
   } = useEventCountdownProgressBar(
-    'Ecotone Fork upgrade in progress',
+    'Metis Bridging is paused',
     ECOTONE_FORK_START_DATE, // Countdown Bar will automatically appear after start time
     ECOTONE_FORK_END_DATE // Countdown Bar will automatically disappear when end time is reached
   )
 
   return {
     isEcotoneForkUpgradePending,
-    isCurrentChainDisabled:
-      (isChainOptimism || isChainBase) && isEcotoneForkUpgradePending, // Used to pause Bridge
-    EcotoneForkCountdownProgressBar:
-      isChainOptimism || isChainBase ? EcotoneForkCountdownProgressBar : null,
+    isCurrentChainDisabled: isToChainMetis, // Used to pause Bridge
+    EcotoneForkCountdownProgressBar: isToChainMetis
+      ? EcotoneForkCountdownProgressBar
+      : null,
   }
 }
