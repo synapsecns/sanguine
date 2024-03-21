84f238cd6999d0473a9a8a982d943abd8ff7cd21
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-21 16:00:04 +0000

    Implement fees/execution request

--- packages/contracts-communication/contracts/execution/SynapseExecutionServiceV1.sol
+++ packages/contracts-communication/contracts/execution/SynapseExecutionServiceV1.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.20;
 
 import {IExecutionService, ISynapseExecutionServiceV1} from "../interfaces/ISynapseExecutionServiceV1.sol";
 import {SynapseExecutionServiceEvents} from "../events/SynapseExecutionServiceEvents.sol";
+import {IGasOracle} from "../interfaces/IGasOracle.sol";
+import {OptionsLib, OptionsV1} from "../libs/Options.sol";
 
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
@@ -56,8 +58,13 @@ contract SynapseExecutionServiceV1 is
         bytes memory options
     )
         external
+        onlyRole(IC_CLIENT_ROLE)
     {
-        // TODO: implement
+        uint256 requiredFee = getExecutionFee(dstChainId, txPayloadSize, options);
+        if (executionFee < requiredFee) {
+            revert SynapseExecutionService__FeeAmountTooLow({actual: executionFee, required: requiredFee});
+        }
+        emit ExecutionRequested({transactionId: transactionId, client: msg.sender});
     }
 
     /// @inheritdoc IExecutionService
@@ -66,10 +73,31 @@ contract SynapseExecutionServiceV1 is
         uint256 txPayloadSize,
         bytes memory options
     )
-        external
+        public
         view
-        returns (uint256)
+        returns (uint256 executionFee)
     {
-        // TODO: implement
+        address cachedGasOracle = gasOracle;
+        if (cachedGasOracle == address(0)) {
+            revert SynapseExecutionService__GasOracleNotSet();
+        }
+        // TODO: the "exact version" check should be generalized
+        (uint8 version,) = OptionsLib.decodeVersionedOptions(options);
+        if (version > OptionsLib.OPTIONS_V1) {
+            revert SynapseExecutionService__OptionsVersionNotSupported(version);
+        }
+        OptionsV1 memory optionsV1 = OptionsLib.decodeOptionsV1(options);
+        // TODO: there should be a markup applied to the execution fee
+        executionFee = IGasOracle(cachedGasOracle).estimateTxCostInLocalUnits({
+            remoteChainId: dstChainId,
+            gasLimit: optionsV1.gasLimit,
+            calldataSize: txPayloadSize
+        });
+        if (optionsV1.gasAirdrop > 0) {
+            executionFee += IGasOracle(cachedGasOracle).convertRemoteValueToLocalUnits({
+                remoteChainId: dstChainId,
+                value: optionsV1.gasAirdrop
+            });
+        }
     }
 }
