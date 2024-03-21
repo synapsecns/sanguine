33b9333da46312fc5d1f37518222db983c83144d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-21 12:51:02 +0000

    Update `generate.go`

--- sin-executor/contracts/mocks/interchainapp/generate.go
+++ sin-executor/contracts/mocks/interchainapp/generate.go
@@ -1,3 +1,3 @@
 package interchainapp
 
-//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ../../../../packages/contracts-communication/flattened/InterchainAppExample.sol --pkg interchainapp --sol-version 0.8.20 --filename interchainapp --evm-version istanbul
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ../../../../packages/contracts-communication/flattened/ExampleAppV1.sol --pkg interchainapp --sol-version 0.8.20 --filename interchainapp --evm-version istanbul
