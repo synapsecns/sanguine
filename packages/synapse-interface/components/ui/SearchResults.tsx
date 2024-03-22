729feaf1d8144cdf07219b50f52a9dbcc5f446de
abtestingalpha <abtestingalpha@gmail.com>
2024-03-22 17:56:05 -0400

    SearchResults

--- packages/synapse-interface/components/ui/SearchResults.tsx
+++ packages/synapse-interface/components/ui/SearchResults.tsx
@@ -1,17 +1,11 @@
-export const SearchResults = ({
-  searchStr,
-  type,
-}: {
-  searchStr: string
-  type: string
-}) => {
+export const SearchResults = ({ searchStr }: { searchStr: string }) => {
   return (
     <div>
       {searchStr && (
         <div className="p-2 text-sm">
           No other results found for <q>{searchStr}</q>.
           <div className="pt-2 align-bottom text-primaryTextColor text-md">
-            Want to see a {type} supported on Synapse? Let us know!
+            Want to see it supported on Synapse? Let us know!
           </div>
         </div>
       )}
