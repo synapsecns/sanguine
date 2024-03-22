a6b1b8ac73e53b43552ddf830f4158fca17989b0
abtestingalpha <abtestingalpha@gmail.com>
2024-03-22 15:15:17 -0400

    Fixes decimal input with no leading zeros, wallet cancel on send

--- packages/widget/src/utils/isOnlyZeroes.ts
+++ packages/widget/src/utils/isOnlyZeroes.ts
@@ -1,3 +1,11 @@
+/**
+  This regex matches a string that either:
+  - consists only of one or more '0' characters
+  - consists of a single '0' followed by a '.' and zero or more '0' characters
+  - consists solely of a '.' followed by one or more '0' characters
+**/
 export const isOnlyZeroes = (input: string): boolean => {
-  return /^(0+(\.0*)?|\.\d+)$/.test(input.trim())
+  const regex = /^(0+|0?\.0+)$/
+
+  return regex.test(input.trim())
 }
