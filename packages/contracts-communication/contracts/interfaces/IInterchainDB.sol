e12f82ff021cd417597cf0cc0bc39f3e70463421
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-22 17:12:17 +0000

    Remove deprecated entries array

--- packages/contracts-communication/contracts/interfaces/IInterchainDB.sol
+++ packages/contracts-communication/contracts/interfaces/IInterchainDB.sol
@@ -5,14 +5,6 @@ import {InterchainEntry} from "../libs/InterchainEntry.sol";
 import {InterchainBatch} from "../libs/InterchainBatch.sol";
 
 interface IInterchainDB {
-    /// @notice Struct representing an entry from the local Interchain DataBase.
-    /// @param writer       The address of the writer on the local chain
-    /// @param dataHash     The hash of the data written on the local chain
-    struct LocalEntry {
-        address writer;
-        bytes32 dataHash;
-    }
-
     /// @notice Struct representing a batch of entries from the remote Interchain DataBase,
     /// verified by the Interchain Module.
     /// @param verifiedAt   The block timestamp at which the entry was verified by the module
@@ -122,14 +114,6 @@ interface IInterchainDB {
     /// @dev Will revert if the batch with the given nonce does not exist, or is not finalized.
     /// @param dbNonce      The database nonce of the finalized batch
     function getBatch(uint256 dbNonce) external view returns (InterchainBatch memory);
-
-    /// @notice Get the Interchain Entry written on the local chain with the given batch nonce and entry index.
-    /// Note: the batch does not have to be finalized to fetch the local entry.
-    /// @dev Will revert if the batch with the given nonce does not exist,
-    /// or the entry with the given index does not exist within the batch.
-    /// @param dbNonce      The database nonce of the existing batch
-    /// @param entryIndex   The index of the written entry within the batch
-    function getEntry(uint256 dbNonce, uint64 entryIndex) external view returns (InterchainEntry memory);
 
     /// @notice Get the Interchain Entry's value written on the local chain with the given batch nonce and entry index.
     /// Entry value is calculated as the hash of the writer address and the written data hash.
