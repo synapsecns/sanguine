2f21dcb458c2f0a07dacae98a7b4e3fbcfb94a15
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-22 15:02:01 +0300

    App abstraction (go changes) (#2349)
    * Update `generate.go`
    
    * Regenerate `sin-executor`
    
    * Update Example App references
    
    * Use non-zero period in tests
    
    * Use `GetMessageFee` for setting the message value
    
    * Use non-zero gas airdrop amount in tests

--- sin-executor/contracts/mocks/interchainapp/helpers.go
+++ sin-executor/contracts/mocks/interchainapp/helpers.go
@@ -10,7 +10,7 @@ import (
 
 // InterchainAppMockRef is a reference to an interchain db.
 type InterchainAppMockRef struct {
-	*InterchainAppExample
+	*ExampleAppV1
 	// address of the interchain client
 	address common.Address
 }
@@ -22,13 +22,13 @@ func (i *InterchainAppMockRef) Address() common.Address {
 
 // NewInterchainAppRef creates a new interchain client with a contract ref.
 func NewInterchainAppRef(address common.Address, backend bind.ContractBackend) (*InterchainAppMockRef, error) {
-	instance, err := NewInterchainAppExample(address, backend)
+	instance, err := NewExampleAppV1(address, backend)
 	if err != nil {
 		return nil, fmt.Errorf("could not create instance of InterchainClient: %w", err)
 	}
 	return &InterchainAppMockRef{
-		InterchainAppExample: instance,
-		address:              address,
+		ExampleAppV1: instance,
+		address:      address,
 	}, nil
 }
 
