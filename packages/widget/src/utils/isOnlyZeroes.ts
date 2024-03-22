7379024f284748cb707a5ebcd5b8f2798b09f3fd
abtestingalpha <abtestingalpha@gmail.com>
2024-03-22 15:23:51 -0400

    Fixes lint

--- packages/widget/src/utils/isOnlyZeroes.ts
+++ packages/widget/src/utils/isOnlyZeroes.ts
@@ -1,9 +1,10 @@
 /**
-  This regex matches a string that either:
-  - consists only of one or more '0' characters
-  - consists of a single '0' followed by a '.' and zero or more '0' characters
-  - consists solely of a '.' followed by one or more '0' characters
-**/
+ * This regex matches a string that either:
+ * 1. consists only of one or more '0' characters
+ * 2. consists of a single '0' followed by a '.' and zero or more '0' characters
+ * 3. consists solely of a '.' followed by one or more '0' characters
+ */
+
 export const isOnlyZeroes = (input: string): boolean => {
   const regex = /^(0+|0?\.0+)$/
 
