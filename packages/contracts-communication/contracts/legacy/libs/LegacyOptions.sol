fc7fd0565cb21cd8963e25f258f72021c1befa86
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-26 20:06:14 +0000

    Implement `LegacyOptionsLib`

--- packages/contracts-communication/contracts/legacy/libs/LegacyOptions.sol
+++ packages/contracts-communication/contracts/legacy/libs/LegacyOptions.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.0;
 
 library LegacyOptionsLib {
+    /// @notice Supported version of the legacy options format.
     uint16 internal constant LEGACY_OPTIONS_VERSION = 1;
+    /// @dev The length of the legacy options format: 2 bytes for the version and 32 bytes for the gas limit.
+    uint256 private constant LEGACY_OPTIONS_LENGTH = 34;
+    /// @dev The offset of the gas limit in the legacy options format.
+    uint256 private constant GAS_LIMIT_OFFSET = 2;
 
     error LegacyOptionsLib__InvalidOptions(bytes legacyOpts);
 
@@ -10,13 +15,20 @@ library LegacyOptionsLib {
     /// @param gasLimit     The gas limit to encode.
     /// @return legacyOpts  The encoded legacy options
     function encodeLegacyOptions(uint256 gasLimit) internal pure returns (bytes memory legacyOpts) {
-        // TODO: Implement
+        return abi.encodePacked(LEGACY_OPTIONS_VERSION, gasLimit);
     }
 
     /// @notice Decodes the gas limit from a legacy options format.
     /// @param legacyOpts   The encoded legacy options
     /// @return gasLimit    The gas limit
     function decodeLegacyOptions(bytes calldata legacyOpts) internal pure returns (uint256 gasLimit) {
-        // TODO: Implement
+        if (legacyOpts.length != LEGACY_OPTIONS_LENGTH) {
+            revert LegacyOptionsLib__InvalidOptions(legacyOpts);
+        }
+        uint16 version = uint16(bytes2(legacyOpts[:GAS_LIMIT_OFFSET]));
+        if (version != LEGACY_OPTIONS_VERSION) {
+            revert LegacyOptionsLib__InvalidOptions(legacyOpts);
+        }
+        gasLimit = uint256(bytes32(legacyOpts[GAS_LIMIT_OFFSET:]));
     }
 }
