eead1eea5db210b6cf343fba41d531501809cb60
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-03-27 14:43:28 -0700

    Update comment for isChainIncluded

--- packages/synapse-interface/utils/isChainIncluded.tsx
+++ packages/synapse-interface/utils/isChainIncluded.tsx
@@ -2,8 +2,8 @@
  * Checks if any of the chain IDs in `hasChains` are found within the `chainList` array.
  *
  * @param {number[]} chainList - The array of chain IDs to check against.
- * @param {number[]} hasChains - The array of chain IDs to find within `checkChains`.
- * @returns {boolean} - True if any chain ID from `hasChains` is found in `checkChains`, otherwise false.
+ * @param {number[]} hasChains - The array of chain IDs to find within `chainList`.
+ * @returns {boolean} - True if any chain ID from `hasChains` is found in `chainList`, otherwise false.
  */
 
 export const isChainIncluded = (chainList: number[], hasChains: number[]) => {
