727e9e2ff99c6068579c4ae44b51d4adda2ac03e
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-27 23:42:25 +0000

    Implement basic encode/decode

--- packages/contracts-communication/contracts/libs/VersionedPayload.sol
+++ packages/contracts-communication/contracts/libs/VersionedPayload.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// solhint-disable no-inline-assembly
 library VersionedPayloadLib {
     /// @notice Amount of bytes reserved for the version (uint16) in the versioned payload
     uint256 internal constant VERSION_LENGTH = 2;
@@ -11,21 +12,29 @@ library VersionedPayloadLib {
     /// @param version  The payload's version.
     /// @param payload  The payload to encode.
     function encodeVersionedPayload(uint16 version, bytes memory payload) internal pure returns (bytes memory) {
-        // TODO: implement
+        return abi.encodePacked(version, payload);
     }
 
     /// @notice Extracts the version from the versioned payload (calldata reference).
     /// @param versionedPayload     The versioned payload (calldata reference).
-    function getVersion(bytes calldata versionedPayload) internal pure returns (uint16) {
-        // TODO: implement
+    function getVersion(bytes calldata versionedPayload) internal pure returns (uint16 version) {
+        if (versionedPayload.length < VERSION_LENGTH) {
+            revert VersionedPayload__TooShort(versionedPayload);
+        }
+        assembly {
+            // We are only interested in the highest 16 bits of the loaded full 32 bytes word.
+            version := shr(240, calldataload(versionedPayload.offset))
+        }
     }
 
     /// @notice Extracts the payload from the versioned payload (calldata reference).
     /// @dev The extracted payload is also returned as a calldata reference.
     /// @param versionedPayload     The versioned payload.
     function getPayload(bytes calldata versionedPayload) internal pure returns (bytes calldata) {
-        // TODO: implement
-        return versionedPayload;
+        if (versionedPayload.length < VERSION_LENGTH) {
+            revert VersionedPayload__TooShort(versionedPayload);
+        }
+        return versionedPayload[VERSION_LENGTH:];
     }
 
     /// @notice Extracts the version from the versioned payload (memory reference).
