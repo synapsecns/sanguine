eac41413f059f09a119f0a36805ee4421992c924
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-28 19:46:31 +0000

    Update tests with expected behavior for versioned batches

--- packages/contracts-communication/test/mocks/InterchainModuleMock.sol
+++ packages/contracts-communication/test/mocks/InterchainModuleMock.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {InterchainBatch, IInterchainDB, IInterchainModule} from "../../contracts/interfaces/IInterchainModule.sol";
+import {IInterchainModule} from "../../contracts/interfaces/IInterchainModule.sol";
+import {IInterchainDB} from "../../contracts/interfaces/IInterchainDB.sol";
 
 // solhint-disable ordering
 // solhint-disable no-empty-blocks
 contract InterchainModuleMock is IInterchainModule {
-    function requestBatchVerification(uint256 dstChainId, InterchainBatch memory batch) external payable {}
+    function requestBatchVerification(uint256 dstChainId, bytes calldata versionedBatch) external payable {}
 
     function getModuleFee(uint256 dstChainId, uint256 dbNonce) external view returns (uint256) {}
 
-    function mockVerifyRemoteBatch(address interchainDB, InterchainBatch memory batch) external {
-        IInterchainDB(interchainDB).verifyRemoteBatch(batch);
+    function mockVerifyRemoteBatch(address interchainDB, bytes calldata versionedBatch) external {
+        IInterchainDB(interchainDB).verifyRemoteBatch(versionedBatch);
     }
 }
