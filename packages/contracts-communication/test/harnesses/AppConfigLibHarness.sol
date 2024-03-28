1d726af87472d96adac4c6d35dd26bc7842f5ca3
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-28 15:06:57 +0000

    Update AppConfig versioning

--- packages/contracts-communication/test/harnesses/AppConfigLibHarness.sol
+++ packages/contracts-communication/test/harnesses/AppConfigLibHarness.sol
@@ -4,20 +4,11 @@ pragma solidity 0.8.20;
 import {AppConfigV1, AppConfigLib} from "../../contracts/libs/AppConfig.sol";
 
 contract AppConfigLibHarness {
-    function encodeVersionedAppConfig(uint8 version, bytes calldata appConfig) external pure returns (bytes memory) {
-        return AppConfigLib.encodeVersionedAppConfig(version, appConfig);
-    }
-
-    function decodeVersionedAppConfig(bytes calldata data) external pure returns (uint8, bytes memory) {
-        (uint8 version, bytes memory appConfig) = AppConfigLib.decodeVersionedAppConfig(data);
-        return (version, appConfig);
+    function decodeAppConfigV1(bytes calldata data) external view returns (AppConfigV1 memory) {
+        return AppConfigLib.decodeAppConfigV1(data);
     }
 
     function encodeAppConfigV1(AppConfigV1 memory appConfig) external pure returns (bytes memory) {
         return AppConfigLib.encodeAppConfigV1(appConfig);
-    }
-
-    function decodeAppConfigV1(bytes calldata data) external pure returns (AppConfigV1 memory) {
-        return AppConfigLib.decodeAppConfigV1(data);
     }
 }
