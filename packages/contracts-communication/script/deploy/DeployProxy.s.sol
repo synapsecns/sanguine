d88d8ff0fefa9835766cad05da78694f68b087a4
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-03-28 10:32:51 +0000

    Chore: implicit TransparentUpgradeableProxy deployment, docs

--- packages/contracts-communication/script/deploy/DeployProxy.s.sol
+++ packages/contracts-communication/script/deploy/DeployProxy.s.sol
@@ -2,12 +2,18 @@
 pragma solidity 0.8.20;
 
 import {SynapseScript, StringUtils} from "@synapsecns/solidity-devops/src/SynapseScript.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
+// solhint-disable custom-errors
 abstract contract DeployProxy is SynapseScript {
     using StringUtils for *;
 
     bytes32 public constant ADMIN_SLOT = bytes32(uint256(keccak256("eip1967.proxy.admin")) - 1);
 
+    /// @notice Deploys a TransparentUpgradeableProxy contract, then saves the fresh deployment artifacts for:
+    /// - The proxy as ContractName.Proxy (with Proxy ABI)
+    /// - The proxy as ContractName (with implementation ABI)
+    /// - The ProxyAdmin as ContractName.ProxyAdmin
     function deployAndSaveProxy(
         string memory contractName,
         address implementation,
@@ -17,12 +23,14 @@ abstract contract DeployProxy is SynapseScript {
         internal
         returns (address deployedAt)
     {
+        require(implementation != address(0), "No implementation provided");
+        require(proxyAdminOwner != address(0), "No proxy admin owner provided");
         // Deploy the proxy and save the artifact with Proxy ABI as ContractName.Proxy
         deployedAt = deployAndSaveAs({
             contractName: "TransparentUpgradeableProxy",
             contractAlias: contractName.concat(".Proxy"),
             constructorArgs: abi.encode(implementation, proxyAdminOwner, initData),
-            deployCodeFunc: cbDeploy
+            deployCodeFunc: cdDeployTransparentUpgradeableProxy
         });
         // Save the proxy artifact with implementation ABI as ContractName
         if (!isDeployed(contractName)) {
@@ -45,6 +53,26 @@ abstract contract DeployProxy is SynapseScript {
         }
     }
 
+    /// @notice Callback to deploy a TransparentUpgradeableProxy contract.
+    /// Note: this version of TransparentUpgradeableProxy spawns its own ProxyAdmin contract,
+    /// and therefore the ProxyAdmin contract doesn't need to be deployed separately.
+    function cdDeployTransparentUpgradeableProxy(
+        string memory contractName,
+        bytes memory constructorArgs
+    )
+        internal
+        returns (address deployedAt)
+    {
+        require(
+            contractName.equals("TransparentUpgradeableProxy"), string.concat("Invalid contract name: ", contractName)
+        );
+        (address implementation, address proxyAdminOwner, bytes memory initData) =
+            abi.decode(constructorArgs, (address, address, bytes));
+        deployedAt = address(new TransparentUpgradeableProxy(implementation, proxyAdminOwner, initData));
+    }
+
+    /// @notice Retrieves the address from the ADMIN_SLOT of a TransparentUpgradeableProxy contract.
+    /// This is the address that is able to upgrade the proxy.
     function getProxyAdmin(address target) internal view returns (address admin) {
         bytes32 adminBytes32 = vm.load(target, ADMIN_SLOT);
         admin = address(uint160(uint256(adminBytes32)));
