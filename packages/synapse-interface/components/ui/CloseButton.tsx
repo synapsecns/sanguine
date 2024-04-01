980a6cd8b6d0693db071d3e159263c846599495f
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-04-01 11:02:48 -0700

    Fix destination input styling after bridge card updates

--- packages/synapse-interface/components/ui/CloseButton.tsx
+++ packages/synapse-interface/components/ui/CloseButton.tsx
@@ -9,7 +9,7 @@ export const CloseButton = ({
 }) => {
   return (
     <button
-      className="absolute flex items-center justify-center w-8 h-8 hover:cursor-pointer right-1"
+      className={`absolute flex items-center justify-center w-8 h-8 hover:cursor-pointer right-1 ${className}`}
       onClick={onClick}
     >
       <XIcon className="inline w-5 text-white" />
