73edf1574c0372cb6e2191ff7949cdf23f9deec1
dwasse <wassermandaniel8@gmail.com>
2024-04-01 19:17:55 -0500

    Submitter: bump fee cap with tip cap (#2412)
    * Feat: set DefaultGasBumpPercentage to 10
    
    * Feat: bump fee cap in addition to tip cap
    
    * Feat: traces
    
    * Fix: tests
    
    * Fix: test

--- ethergo/submitter/submitter.go
+++ ethergo/submitter/submitter.go
@@ -364,12 +364,27 @@ func (t *txSubmitterImpl) setGasPrice(ctx context.Context, client client.EVM,
 			transactor.GasFeeCap = maxPrice
 		}
 
+		span.SetAttributes(
+			attribute.String("gas_price", bigPtrToString(transactor.GasPrice)),
+			attribute.String("gas_fee_cap", bigPtrToString(transactor.GasFeeCap)),
+			attribute.String("gas_tip_cap", bigPtrToString(transactor.GasTipCap)),
+		)
 		metrics.EndSpanWithErr(span, err)
 	}()
 
 	// TODO: cache both of these values
+	shouldBump := true
 	if t.config.SupportsEIP1559(int(bigChainID.Uint64())) {
-		transactor.GasFeeCap = t.config.GetMaxGasPrice(chainID)
+		transactor.GasFeeCap, err = client.SuggestGasPrice(ctx)
+		if err != nil {
+			return fmt.Errorf("could not get gas price: %w", err)
+		}
+		// don't bump fee cap if we hit the max configured gas price
+		if transactor.GasFeeCap.Cmp(maxPrice) > 0 {
+			transactor.GasFeeCap = maxPrice
+			shouldBump = false
+			span.AddEvent("not bumping fee cap since max price is reached")
+		}
 
 		transactor.GasTipCap, err = client.SuggestGasTipCap(ctx)
 		if err != nil {
@@ -384,7 +399,7 @@ func (t *txSubmitterImpl) setGasPrice(ctx context.Context, client client.EVM,
 	t.applyBaseGasPrice(transactor, chainID)
 
 	//nolint: nestif
-	if prevTx != nil {
+	if prevTx != nil && shouldBump {
 		gasBlock, err := t.getGasBlock(ctx, client, chainID)
 		if err != nil {
 			span.AddEvent("could not get gas block", trace.WithAttributes(attribute.String("error", err.Error())))
