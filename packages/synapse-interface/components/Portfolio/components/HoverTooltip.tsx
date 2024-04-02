093aaba93a9f93828f6e824d9540b0c3a96625dc
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-04-02 09:35:18 -0700

    Center justify tooltip

--- packages/synapse-interface/components/Portfolio/components/HoverTooltip.tsx
+++ packages/synapse-interface/components/Portfolio/components/HoverTooltip.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { HoverContent } from './PortfolioTokenVisualizer'
 
 export const HoverTooltip = ({ children, hoverContent }) => {
   const [showTooltip, setShowTooltip] = useState(false)
@@ -17,4 +16,27 @@ export const HoverTooltip = ({ children, hoverContent }) => {
       <HoverContent isHovered={showTooltip}>{hoverContent}</HoverContent>
     </div>
   )
+}
+
+const HoverContent = ({
+  isHovered,
+  children,
+}: {
+  isHovered: boolean
+  children: React.ReactNode
+}) => {
+  if (isHovered) {
+    return (
+      <div
+        className={`
+          absolute left-1/2 bottom-full translate-x-[-50%]
+          z-50 hover-content px-2 py-1 text-white mb-1
+          border border-solid border-[#252537]
+          bg-[#101018] rounded-md text-left text-sm
+        `}
+      >
+        {children}
+      </div>
+    )
+  }
 }
