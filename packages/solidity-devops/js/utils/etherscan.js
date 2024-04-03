a9f4db1093b2365bbad7dd2a01e42a210b3e5608
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-04-03 20:11:45 +0100

    Fix: lint

--- packages/solidity-devops/js/utils/etherscan.js
+++ packages/solidity-devops/js/utils/etherscan.js
@@ -64,7 +64,9 @@ const getRequestStatus = (chainName, actionName, guid) => {
     }
     return response
   }
-  logError(`Request with GUID ${guid} not completed after ${MAX_ATTEMPTS} attempts. Consider checking the network status or increasing the retry interval.`)
+  logError(
+    `Request with GUID ${guid} not completed after ${MAX_ATTEMPTS} attempts. Consider checking the network status or increasing the retry interval.`
+  )
   return null
 }
 
