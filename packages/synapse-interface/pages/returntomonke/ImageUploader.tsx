c7196cdf69ca2d4023e6b98b869975c27a2680d2
defi-moses <jakedinero@protonmail.com>
2024-04-05 11:41:22 +0100

    adding return to monke

---
+++ packages/synapse-interface/pages/returntomonke/ImageUploader.tsx
@@ -0,0 +1,134 @@
+import _ from 'lodash'
+import { useState, useRef, useEffect } from 'react'
+import Button from '@tw/Button'
+
+export default function ImageUploader() {
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [processedImage, setProcessedImage] = useState(null);
+
+  const fileInput = useRef(null);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    if (uploadedImage) {
+      applyImageOverlay();
+    }
+  }, [uploadedImage]);
+
+  function handleFile(file) {
+    setUploadedImage(file);
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  function handleOnDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    handleFile(event.dataTransfer.files[0]);
+    console.log('File drop was a success');
+  }
+
+  const applyImageOverlay = () => {
+    const canvas = imgRef.current;
+    const ctx = canvas.getContext('2d');
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Load the uploaded image
+    const img = new Image();
+    img.src = URL.createObjectURL(uploadedImage);
+    img.onload = () => {
+      // Set the canvas size to match the uploaded image
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+
+      // Load the overlay image
+      const overlay = new Image();
+      overlay.src = './synpfpborder.png';
+      overlay.onload = () => {
+        // Draw the overlay image on top of the uploaded image
+        ctx.drawImage(overlay, 0, 0, canvas.width, canvas.height);
+
+        // Convert the canvas to a Blob and update the state
+        canvas.toBlob((blob) => {
+          setProcessedImage(blob);
+        }, 'image/jpeg');
+      };
+    };
+  };
+
+  return (
+    <div>
+      <div
+        className={`
+          flex ${processedImage ? '' : 'h-[200px]'}  w-full  rounded-xl cursor-pointer
+          border-dashed border-4 border-white border-opacity-50 mb-2
+          text-white text-opacity-50 hover:text-opacity-100
+          hover:border-white
+          transform transition-all duration-200
+        `}
+        style={{
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 100,
+          backgroundPosition: 'center',
+        }}
+        onDragOver={handleDragOver}
+        onDrop={handleOnDrop}
+        onClick={() => fileInput.current.click()}
+      >
+        <p className="p-3 m-auto text-lg text-center center">
+          {!processedImage && (
+            <>
+              Click to Upload or
+              <br />
+              Drag and drop image here...
+            </>
+          )}
+          {processedImage && <>Select different image?</>}
+        </p>
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInput}
+          hidden
+          onChange={(e) => handleFile(e.target.files[0])}
+        />
+      </div>
+      {processedImage && (
+        <img src={URL.createObjectURL(processedImage)} alt="preview" />
+      )}
+      <canvas ref={imgRef} style={{ display: 'none' }} />
+      {uploadedImage && (
+        <Button
+          className={`
+            w-full rounded-xl my-2 px-4 py-3 tracking-wide
+            text-white text-opacity-100
+            hover:opacity-80 disabled:opacity-100 disabled:text-[#88818C]
+            disabled:from-bgLight disabled:to-bgLight
+            bg-gradient-to-r from-[#CF52FE] to-[#AC8FFF]
+          `}
+          onClick={() => {
+            downloadBase64File(
+              URL.createObjectURL(processedImage),
+              `synape_${uploadedImage.name?.split('.')[0] ?? 'random'}.jpg`
+            );
+          }}
+        >
+          Download
+        </Button>
+      )}
+    </div>
+  );
+}
+
+function downloadBase64File(dataStr, fileName) {
+  const downloadLink = document.createElement('a');
+  downloadLink.href = dataStr;
+  downloadLink.download = fileName;
+  downloadLink.click();
+  console.log('evolving into a synape');
+}
