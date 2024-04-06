0b0d3614fc8204a500b3ce7bd9df5a33035dcaf5
aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>
2024-04-05 22:27:02 -0400

    Submitter/dynamic tx fees [goreleaser] (#2432)
    * attempt to fix dynamic gas fees bumping
    
    * [goreleaser]
    
    * metrics sync
    
    * [goreleaser]
    
    * allow zero gas tip cap [goreleaser]
    
    * fix errors [goreleaser]
    
    * [goreleaser] fix interface
    
    * fix [goreleaser]
    
    * [goreleaser] fix tx
    
    * 1 wei->10 wei [goreleaser]
    
    * Revert "1 wei->10 wei [goreleaser]"
    
    This reverts commit 21abb186a69a5575732ad2962e5da42f1215c1eb.
    
    * Revert "[goreleaser] fix tx"
    
    This reverts commit 94d3d6d6c7a82b490effae3b50db3228706260a8.
    
    * revert bumpDynamicTxFees to master
    
    * newFeeCap [goreleaser]
    
    * f
    
    * [goreleaser] revert me extremely, extremely aggressive printing
    
    * [goreleaser]
    
    * [goreleaser]
    
    * Revert "[goreleaser] revert me extremely, extremely aggressive printing"
    
    This reverts commit e961d54c6c92e793d320137de3cb41d01ebfa28c.
    
    * return nil a
    
    * [goreleaser]
    
    * Fix: check for eip1559 before flooring GasTipCap
    
    * [goreleaser]
    
    * Submitter: setGasPrice() refactor (#2462)
    
    * Feat: initial refactor of setGasPrice()
    
    * Feat: add more tracing
    
    * Cleanup: comments
    
    * Feat: BaseGasPrice -> MinGasPrice
    
    * Cleanup: remove applyMinGasPrice()
    
    * Feat: initial test cases
    
    * Feat: add WithOracleOverride cases
    
    * Feat: add PrevTx cases
    
    * Feat: add assertGasValues() helper
    
    * Cleanup: pass useDynamic to applyGasFloor()
    
    * Feat: remove old submitter test
    
    * Cleanup: lint
    
    * [goreleaser]
    
    * Cleanup: populateFromPrevTx() -> bumpFromPrevTx()
    
    * Feat: cap GasTipCap by GasFeeCap
    
    * [goreleaser]
    
    * Feat: default min gas price to 1 gwei
    
    * [goreleaser]
    
    * Feat: add minTipCap var
    
    * Fix: minTipCap = 10 wei
    
    * Cleanup: comment
    
    * [goreleaser]
    
    * Fix: test
    
    ---------
    
    Co-authored-by: Trajan0x <trajan0x@users.noreply.github.com>
    Co-authored-by: Daniel Wasserman <wassermandaniel8@gmail.com>

--- ethergo/submitter/submitter.go
+++ ethergo/submitter/submitter.go
@@ -17,6 +17,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/ipfs/go-log"
 	"github.com/synapsecns/sanguine/core"
 	"github.com/synapsecns/sanguine/core/mapmutex"
@@ -346,25 +347,20 @@ func (t *txSubmitterImpl) txTypeForChain(chainID *big.Int) (txType uint8) {
 }
 
 // setGasPrice sets the gas price for the transaction.
-// it bumps if prevtx is set
-// nolint: cyclop
-// TODO: use options.
+// If a prevTx is specified, a bump will be attempted; otherwise values will be
+// set from the gas oracle.
+// If gas values exceed the configured max, an error will be returned.
 func (t *txSubmitterImpl) setGasPrice(ctx context.Context, client client.EVM,
 	transactor *bind.TransactOpts, bigChainID *big.Int, prevTx *types.Transaction) (err error) {
 	ctx, span := t.metrics.Tracer().Start(ctx, "submitter.setGasPrice")
 
 	chainID := int(bigChainID.Uint64())
-	maxPrice := t.config.GetMaxGasPrice(chainID)
+	useDynamic := t.config.SupportsEIP1559(chainID)
 
 	defer func() {
-		if transactor.GasPrice != nil && maxPrice.Cmp(transactor.GasPrice) < 0 {
-			transactor.GasPrice = maxPrice
-		}
-		if transactor.GasFeeCap != nil && maxPrice.Cmp(transactor.GasFeeCap) < 0 {
-			transactor.GasFeeCap = maxPrice
-		}
-
 		span.SetAttributes(
+			attribute.Int(metrics.ChainID, chainID),
+			attribute.Bool("use_dynamic", useDynamic),
 			attribute.String("gas_price", bigPtrToString(transactor.GasPrice)),
 			attribute.String("gas_fee_cap", bigPtrToString(transactor.GasFeeCap)),
 			attribute.String("gas_tip_cap", bigPtrToString(transactor.GasTipCap)),
@@ -372,69 +368,171 @@ func (t *txSubmitterImpl) setGasPrice(ctx context.Context, client client.EVM,
 		metrics.EndSpanWithErr(span, err)
 	}()
 
-	// TODO: cache both of these values
-	shouldBump := true
-	if t.config.SupportsEIP1559(int(bigChainID.Uint64())) {
-		transactor.GasFeeCap, err = client.SuggestGasPrice(ctx)
-		if err != nil {
-			return fmt.Errorf("could not get gas price: %w", err)
-		}
-		// don't bump fee cap if we hit the max configured gas price
-		if transactor.GasFeeCap.Cmp(maxPrice) > 0 {
-			transactor.GasFeeCap = maxPrice
-			shouldBump = false
-			span.AddEvent("not bumping fee cap since max price is reached")
-		}
+	t.bumpGasFromPrevTx(ctx, transactor, prevTx, chainID, useDynamic)
+	t.applyGasFloor(ctx, transactor, chainID, useDynamic)
 
-		transactor.GasTipCap, err = client.SuggestGasTipCap(ctx)
-		if err != nil {
-			return fmt.Errorf("could not get gas tip cap: %w", err)
-		}
-	} else {
-		transactor.GasPrice, err = client.SuggestGasPrice(ctx)
-		if err != nil {
-			return fmt.Errorf("could not get gas price: %w", err)
-		}
+	err = t.applyGasFromOracle(ctx, transactor, client, useDynamic)
+	if err != nil {
+		return fmt.Errorf("could not populate gas from oracle: %w", err)
 	}
-	t.applyBaseGasPrice(transactor, chainID)
 
-	//nolint: nestif
-	if prevTx != nil && shouldBump {
-		gasBlock, err := t.getGasBlock(ctx, client, chainID)
-		if err != nil {
-			span.AddEvent("could not get gas block", trace.WithAttributes(attribute.String("error", err.Error())))
-			return err
-		}
-
-		// if the prev tx was greater than this one, we should bump the gas price from that point
-		comparison := gas.CompareGas(prevTx, gas.OptsToComparableTx(transactor), gasBlock.BaseFee)
-		if comparison > 0 {
-			if prevTx.Type() == types.LegacyTxType {
-				transactor.GasPrice = core.CopyBigInt(prevTx.GasPrice())
-			} else {
-				transactor.GasTipCap = core.CopyBigInt(prevTx.GasTipCap())
-				transactor.GasFeeCap = core.CopyBigInt(prevTx.GasFeeCap())
-			}
-		}
-		gas.BumpGasFees(transactor, t.config.GetGasBumpPercentage(chainID), gasBlock.BaseFee, maxPrice)
+	err = t.applyGasCeil(ctx, transactor, chainID, useDynamic)
+	if err != nil {
+		return fmt.Errorf("could not apply gas ceil: %w", err)
 	}
 	return nil
 }
 
-// applyBaseGasPrice applies the base gas price to the transactor if a gas price value is zero.
-func (t *txSubmitterImpl) applyBaseGasPrice(transactor *bind.TransactOpts, chainID int) {
-	if t.config.SupportsEIP1559(chainID) {
-		if transactor.GasFeeCap == nil || transactor.GasFeeCap.Cmp(big.NewInt(0)) == 0 {
-			transactor.GasFeeCap = t.config.GetBaseGasPrice(chainID)
-		}
-		if transactor.GasTipCap == nil || transactor.GasTipCap.Cmp(big.NewInt(0)) == 0 {
-			transactor.GasTipCap = t.config.GetBaseGasPrice(chainID)
+// bumpGasFromPrevTx populates the gas fields from the previous transaction and bumps
+// the appropriate values corresponding to the configured GasBumpPercentage.
+// Note that in the event of a tx type mismatch, gasFeeCap is copied to gasPrice,
+// and gasPrice is copied to both gasFeeCap and gasTipCap in the opposite scenario.
+//
+//nolint:nestif
+func (t *txSubmitterImpl) bumpGasFromPrevTx(ctx context.Context, transactor *bind.TransactOpts, prevTx *types.Transaction, chainID int, currentDynamic bool) {
+	if prevTx == nil {
+		return
+	}
+
+	_, span := t.metrics.Tracer().Start(ctx, "submitter.bumpGasFromPrevTx")
+
+	defer func() {
+		span.SetAttributes(
+			attribute.String("gas_price", bigPtrToString(transactor.GasPrice)),
+			attribute.String("gas_fee_cap", bigPtrToString(transactor.GasFeeCap)),
+			attribute.String("gas_tip_cap", bigPtrToString(transactor.GasTipCap)),
+		)
+		metrics.EndSpan(span)
+	}()
+
+	prevDynamic := prevTx.Type() == types.DynamicFeeTxType
+	bumpPct := t.config.GetGasBumpPercentage(chainID)
+	if currentDynamic {
+		if prevDynamic {
+			transactor.GasFeeCap = gas.BumpByPercent(core.CopyBigInt(prevTx.GasFeeCap()), bumpPct)
+			transactor.GasTipCap = gas.BumpByPercent(core.CopyBigInt(prevTx.GasTipCap()), bumpPct)
+		} else {
+			transactor.GasFeeCap = gas.BumpByPercent(core.CopyBigInt(prevTx.GasPrice()), bumpPct)
+			transactor.GasTipCap = gas.BumpByPercent(core.CopyBigInt(prevTx.GasPrice()), bumpPct)
 		}
 	} else {
-		if transactor.GasPrice == nil || transactor.GasPrice.Cmp(big.NewInt(0)) == 0 {
-			transactor.GasPrice = t.config.GetBaseGasPrice(chainID)
+		if prevDynamic {
+			transactor.GasPrice = gas.BumpByPercent(core.CopyBigInt(prevTx.GasFeeCap()), bumpPct)
+		} else {
+			transactor.GasPrice = gas.BumpByPercent(core.CopyBigInt(prevTx.GasPrice()), bumpPct)
 		}
 	}
+}
+
+var minTipCap = big.NewInt(10 * params.Wei)
+
+// applyGasFloor applies the min gas price from the config if values are unset.
+//
+//nolint:cyclop,nestif
+func (t *txSubmitterImpl) applyGasFloor(ctx context.Context, transactor *bind.TransactOpts, chainID int, useDynamic bool) {
+	_, span := t.metrics.Tracer().Start(ctx, "submitter.applyGasFloor")
+
+	defer func() {
+		span.SetAttributes(
+			attribute.String("gas_price", bigPtrToString(transactor.GasPrice)),
+			attribute.String("gas_fee_cap", bigPtrToString(transactor.GasFeeCap)),
+			attribute.String("gas_tip_cap", bigPtrToString(transactor.GasTipCap)),
+		)
+		metrics.EndSpan(span)
+	}()
+
+	gasFloor := t.config.GetMinGasPrice(chainID)
+	if useDynamic {
+		if transactor.GasFeeCap == nil || transactor.GasFeeCap.Cmp(gasFloor) < 0 {
+			transactor.GasFeeCap = gasFloor
+		}
+		if transactor.GasTipCap == nil || transactor.GasTipCap.Cmp(gasFloor) < 0 {
+			transactor.GasTipCap = minTipCap
+		}
+	} else if transactor.GasPrice == nil || transactor.GasPrice.Cmp(gasFloor) < 0 {
+		transactor.GasPrice = gasFloor
+	}
+}
+
+// applyGasFromOracle fetches gas values from a RPC endpoint and attempts to set them.
+// If values are already specified, they will be overridden if the oracle values are higher.
+func (t *txSubmitterImpl) applyGasFromOracle(ctx context.Context, transactor *bind.TransactOpts, client client.EVM, useDynamic bool) (err error) {
+	ctx, span := t.metrics.Tracer().Start(ctx, "submitter.applyGasFromOracle")
+
+	defer func() {
+		metrics.EndSpanWithErr(span, err)
+	}()
+
+	if useDynamic {
+		suggestedGasFeeCap, err := client.SuggestGasPrice(ctx)
+		if err != nil {
+			return fmt.Errorf("could not get gas fee cap: %w", err)
+		}
+		transactor.GasFeeCap = maxOfBig(transactor.GasFeeCap, suggestedGasFeeCap)
+		suggestedGasTipCap, err := client.SuggestGasTipCap(ctx)
+		if err != nil {
+			return fmt.Errorf("could not get gas tip cap: %w", err)
+		}
+		transactor.GasTipCap = maxOfBig(transactor.GasTipCap, suggestedGasTipCap)
+		span.SetAttributes(
+			attribute.String("suggested_gas_fee_cap", bigPtrToString(suggestedGasFeeCap)),
+			attribute.String("suggested_gas_tip_cap", bigPtrToString(suggestedGasTipCap)),
+			attribute.String("gas_fee_cap", bigPtrToString(transactor.GasFeeCap)),
+			attribute.String("gas_tip_cap", bigPtrToString(transactor.GasTipCap)),
+		)
+	} else {
+		suggestedGasPrice, err := client.SuggestGasPrice(ctx)
+		if err != nil {
+			return fmt.Errorf("could not get gas price: %w", err)
+		}
+		transactor.GasPrice = maxOfBig(transactor.GasPrice, suggestedGasPrice)
+		span.SetAttributes(
+			attribute.String("suggested_gas_price", bigPtrToString(suggestedGasPrice)),
+			attribute.String("gas_price", bigPtrToString(transactor.GasPrice)),
+		)
+	}
+	return nil
+}
+
+// applyGasCeil evaluates current gas values versus the configured maximum, and
+// returns an error if they exceed the maximum.
+func (t *txSubmitterImpl) applyGasCeil(ctx context.Context, transactor *bind.TransactOpts, chainID int, useDynamic bool) (err error) {
+	_, span := t.metrics.Tracer().Start(ctx, "submitter.applyGasCeil")
+
+	maxPrice := t.config.GetMaxGasPrice(chainID)
+
+	defer func() {
+		span.SetAttributes(attribute.String("max_price", bigPtrToString(maxPrice)))
+		metrics.EndSpanWithErr(span, err)
+	}()
+
+	if useDynamic {
+		if transactor.GasFeeCap.Cmp(maxPrice) > 0 {
+			return fmt.Errorf("gas fee cap %s exceeds max price %s", transactor.GasFeeCap, maxPrice)
+		}
+		if transactor.GasTipCap.Cmp(transactor.GasFeeCap) > 0 {
+			transactor.GasTipCap = core.CopyBigInt(transactor.GasFeeCap)
+			span.AddEvent("tip cap exceeds fee cap; setting tip cap to fee cap")
+		}
+	} else {
+		if transactor.GasPrice.Cmp(maxPrice) > 0 {
+			return fmt.Errorf("gas price %s exceeds max price %s", transactor.GasPrice, maxPrice)
+		}
+	}
+	return nil
+}
+
+func maxOfBig(a, b *big.Int) *big.Int {
+	if a == nil {
+		return b
+	}
+	if b == nil {
+		return a
+	}
+	if a.Cmp(b) > 0 {
+		return a
+	}
+	return b
 }
 
 // getGasBlock gets the gas block for the given chain.
