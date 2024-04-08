83c6ddb022e191c2d43931dc9d54b9f2888c579a
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2024-04-08 21:07:47 +0300

    fix(contracts-communication): Integration tests for message bus, testnet deployment (#2463)
    * Port `SynMessagingReceiver`
    
    * Port `PingPongApp`
    
    * Make the integration test template less opinionated
    
    * Add MessageBus harness, correct LegacyPP name
    
    * Add integration tests for Legacy PingPong
    
    * Add `.payloadSize()` predictor in Legacy Message library
    
    * Fix test: expect call with encoded legacy msg length
    
    * Fix: MessageBus fees estimation
    
    * Chore: remove unnecessary quotes in bash scripts
    
    * Add configuration scripts
    
    * Update bash scripts
    
    * Deploy Legacy contracts

--- packages/contracts-communication/script/testnet-config.sh
+++ packages/contracts-communication/script/testnet-config.sh
@@ -13,11 +13,14 @@ fi
 # Get the rest of the options
 shift 2
 # Synapse contracts
-yarn fsr-str script/config/ConfigureSynapseModule.s.sol "$chainName" "$walletName" "testnet" "$@"
-yarn fsr-str script/config/ConfigureSynapseExecutionServiceV1.s.sol "$chainName" "$walletName" "testnet" "$@"
-yarn fsr-str script/config/ConfigureSynapseGasOracleV1.s.sol "$chainName" "$walletName" "testnet" "$@"
+yarn fsr-str script/config/ConfigureSynapseModule.s.sol "$chainName" "$walletName" testnet "$@"
+yarn fsr-str script/config/ConfigureSynapseExecutionServiceV1.s.sol "$chainName" "$walletName" testnet "$@"
+yarn fsr-str script/config/ConfigureSynapseGasOracleV1.s.sol "$chainName" "$walletName" testnet "$@"
 # Client contracts
-yarn fsr-str script/config/ConfigureClientV1.s.sol "$chainName" "$walletName" "testnet" "$@"
+yarn fsr-str script/config/ConfigureClientV1.s.sol "$chainName" "$walletName" testnet "$@"
 yarn fsr script/config/ConfigureExecutionFees.s.sol "$chainName" "$walletName" "$@"
 # Ping-Pong App
-yarn fsr-str script/config/ConfigurePingPongApp.s.sol "$chainName" "$walletName" "testnet" "$@"
+yarn fsr-str script/config/ConfigurePingPongApp.s.sol "$chainName" "$walletName" testnet "$@"
+# Legacy contracts
+yarn fsr-str script/config/legacy/ConfigureMessageBus.s.sol "$chainName" "$walletName" testnet "$@"
+yarn fsr-str script/config/legacy/ConfigureLegacyPingPong.s.sol "$chainName" "$walletName" testnet "$@"
