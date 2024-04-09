3f786d246fd9cc845be8bd6ae9d08225036bfa36
abtestingalpha <abtestingalpha@gmail.com>
2024-04-09 18:49:35 -0400

    Uses supportedChains

--- packages/synapse-interface/pages/_app.tsx
+++ packages/synapse-interface/pages/_app.tsx
@@ -1,56 +1,11 @@
 import '@/styles/global.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { PersistGate } from 'redux-persist/integration/react'
-import {
-  ARBITRUM,
-  AURORA,
-  AVALANCHE,
-  BASE,
-  BLAST,
-  BNB,
-  BOBA,
-  CANTO,
-  CRONOS,
-  DFK,
-  DOGE,
-  ETH,
-  FANTOM,
-  HARMONY,
-  KLAYTN,
-  METIS,
-  MOONBEAM,
-  MOONRIVER,
-  OPTIMISM,
-  POLYGON,
-} from '@/constants/chains/master'
 
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import {
-  mainnet,
-  arbitrum,
-  aurora,
-  avalanche,
-  base,
-  blast,
-  bsc,
-  canto,
-  fantom,
-  harmonyOne,
-  metis,
-  moonbeam,
-  moonriver,
-  optimism,
-  polygon,
-  klaytn,
-  cronos,
-  dfk,
-  dogechain,
-  boba,
-} from '@wagmi/core/chains'
 import { Provider } from 'react-redux'
 import { store, persistor } from '@/store/store'
 import { SegmentAnalyticsProvider } from '@/contexts/SegmentAnalyticsProvider'
@@ -63,7 +18,7 @@ import { SynapseProvider } from '@/utils/providers/SynapseProvider'
 
 import LogRocket from 'logrocket'
 import setupLogRocketReact from 'logrocket-react'
-import { wagmiConfig } from '@/wagmiConfig'
+import { supportedChains, wagmiConfig } from '@/wagmiConfig'
 
 // only initialize when in the browser
 if (
@@ -81,109 +36,6 @@ if (
   })
 }
 
-const chains = [
-  {
-    ...mainnet,
-    configRpc: ETH.rpcUrls.primary,
-    fallbackRpc: ETH.rpcUrls.fallback,
-  },
-  {
-    ...arbitrum,
-    configRpc: ARBITRUM.rpcUrls.primary,
-    fallbackRpc: ARBITRUM.rpcUrls.fallback,
-  },
-  {
-    ...aurora,
-    configRpc: AURORA.rpcUrls.primary,
-    fallbackRpc: AURORA.rpcUrls.fallback,
-  },
-  {
-    ...avalanche,
-    configRpc: AVALANCHE.rpcUrls.primary,
-    fallbackRpc: AVALANCHE.rpcUrls.fallback,
-  },
-  {
-    ...base,
-    configRpc: BASE.rpcUrls.primary,
-    fallbackRpc: BASE.rpcUrls.fallback,
-  },
-  {
-    ...blast,
-    configRpc: BLAST.rpcUrls.primary,
-    fallbackRpc: BLAST.rpcUrls.fallback,
-  },
-  {
-    ...bsc,
-    configRpc: BNB.rpcUrls.primary,
-    fallbackRpc: BNB.rpcUrls.fallback,
-  },
-  {
-    ...canto,
-    configRpc: CANTO.rpcUrls.primary,
-    fallbackRpc: CANTO.rpcUrls.fallback,
-  },
-  {
-    ...fantom,
-    configRpc: FANTOM.rpcUrls.primary,
-    fallbackRpc: FANTOM.rpcUrls.fallback,
-  },
-  {
-    ...harmonyOne,
-    configRpc: HARMONY.rpcUrls.primary,
-    fallbackRpc: HARMONY.rpcUrls.fallback,
-  },
-  {
-    ...metis,
-    configRpc: METIS.rpcUrls.primary,
-    fallbackRpc: METIS.rpcUrls.fallback,
-  },
-  {
-    ...moonbeam,
-    configRpc: MOONBEAM.rpcUrls.primary,
-    fallbackRpc: MOONBEAM.rpcUrls.fallback,
-  },
-  {
-    ...moonriver,
-    configRpc: MOONRIVER.rpcUrls.primary,
-    fallbackRpc: MOONRIVER.rpcUrls.fallback,
-  },
-  {
-    ...optimism,
-    configRpc: OPTIMISM.rpcUrls.primary,
-    fallbackRpc: OPTIMISM.rpcUrls.fallback,
-  },
-  {
-    ...polygon,
-    configRpc: POLYGON.rpcUrls.primary,
-    fallbackRpc: POLYGON.rpcUrls.fallback,
-  },
-  {
-    ...klaytn,
-    configRpc: KLAYTN.rpcUrls.primary,
-    fallbackRpc: KLAYTN.rpcUrls.fallback,
-  },
-  {
-    ...cronos,
-    configRpc: CRONOS.rpcUrls.primary,
-    fallbackRpc: CRONOS.rpcUrls.fallback,
-  },
-  {
-    ...dfk,
-    configRpc: DFK.rpcUrls.primary,
-    fallbackRpc: DFK.rpcUrls.fallback,
-  },
-  {
-    ...dogechain,
-    configRpc: DOGE.rpcUrls.primary,
-    fallbackRpc: DOGE.rpcUrls.fallback,
-  },
-  {
-    ...boba,
-    configRpc: BOBA.rpcUrls.primary,
-    fallbackRpc: BOBA.rpcUrls.fallback,
-  },
-]
-
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -195,7 +47,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
           <RainbowKitProvider>
-            <SynapseProvider chains={chains}>
+            <SynapseProvider chains={supportedChains}>
               <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                   <SegmentAnalyticsProvider>
