e5e840b6a5c78b7fa826bba08ae5199cdf3196c6
abtestingalpha <abtestingalpha@gmail.com>
2024-04-10 10:43:05 -0400

    Refactors for wagmi config organization

--- packages/synapse-interface/wagmiConfig.ts
+++ packages/synapse-interface/wagmiConfig.ts
@@ -1,26 +1,6 @@
 import '@/patch'
-import { Transport, type Chain } from 'viem'
-import {
-  arbitrum,
-  aurora,
-  avalanche,
-  base,
-  bsc,
-  blast,
-  boba,
-  canto,
-  cronos,
-  fantom,
-  klaytn,
-  harmonyOne,
-  mainnet,
-  metis,
-  moonbeam,
-  moonriver,
-  optimism,
-  polygon,
-} from '@wagmi/core/chains'
-import { createConfig, fallback, http } from '@wagmi/core'
+import { type Chain } from 'viem'
+import { createConfig } from '@wagmi/core'
 import {
   metaMaskWallet,
   rabbyWallet,
@@ -34,38 +14,9 @@ import {
 } from '@rainbow-me/rainbowkit/wallets'
 import { connectorsForWallets } from '@rainbow-me/rainbowkit'
 
-import { CHAINS_BY_ID } from '@/constants/chains'
-import { dfk, dogechain } from '@/constants/extraWagmiChains'
+import { createTransports } from '@/utils/createTransports'
+import { supportedChains } from '@/constants/chains/supportedChains'
 
-export const supportedChains = [
-  mainnet,
-  arbitrum,
-  aurora,
-  avalanche,
-  base,
-  blast,
-  bsc,
-  canto,
-  fantom,
-  harmonyOne,
-  metis,
-  moonbeam,
-  moonriver,
-  optimism,
-  polygon,
-  klaytn,
-  cronos,
-  dfk,
-  dogechain,
-  boba,
-].map((chain) => {
-  return {
-    ...chain,
-    configRpc: CHAINS_BY_ID[chain.id]?.rpcUrls.primary,
-    fallbackRpc: CHAINS_BY_ID[chain.id]?.rpcUrls.fallback,
-    iconUrl: CHAINS_BY_ID[chain.id]?.chainImg.src,
-  }
-})
 const appName = 'Synapse'
 const projectId = 'ab0a846bc693996606734d788cb6561d'
 
@@ -91,20 +42,6 @@ const connectors = connectorsForWallets(
     appName,
   }
 )
-
-type Transports = Record<Chain['id'], Transport>
-
-const createTransports = (chains: Chain[]): Transports => {
-  return chains.reduce<Transports>((acc, chain) => {
-    const synapseChain = CHAINS_BY_ID[chain.id]
-
-    acc[chain.id] = fallback([
-      http(synapseChain.rpcUrls.primary),
-      http(synapseChain.rpcUrls.fallback),
-    ])
-    return acc
-  }, {})
-}
 
 const transports = createTransports(supportedChains as Chain[])
 
