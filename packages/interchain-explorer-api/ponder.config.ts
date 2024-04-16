c73bb71cdd9527c24be2af5321fa25bbaeedcbb8
abtestingalpha <abtestingalpha@gmail.com>
2024-04-16 19:12:24 -0400

    Adds InterchainDB, SynapseModule, updates contract addresses

--- packages/interchain-explorer-api/ponder.config.ts
+++ packages/interchain-explorer-api/ponder.config.ts
@@ -1,7 +1,9 @@
 import { createConfig } from '@ponder/core'
 import { http } from 'viem'
 
-import { InterchainClientV1Abi } from './abis/InterchainClientV1Abi'
+import { InterchainClientV1Abi } from '@/abis/InterchainClientV1Abi'
+import { InterchainDBAbi } from './abis/InterchainDBAbi'
+import { SynapseModuleAbi } from './abis/SynapseModuleAbi'
 
 const arbSepoliaTransport = http(process.env.ARB_SEPOLIA_RPC)
 const ethSepoliaTransport = http(process.env.ETH_SEPOLIA_RPC)
@@ -21,15 +23,41 @@ export default createConfig({
     InterchainClientV1: {
       network: {
         ethSepolia: {
-          address: '0x5a81Dfa5885058ED838fa750060C804B49F69991',
-          startBlock: 5628137,
+          address: '0xfcb988E117dbAa8c8b48047afd7c22d4a8321bCA',
+          startBlock: 5712788,
         },
         arbSepolia: {
-          address: '0xBdC170214726a994D9A837b15C0039bdeE090878',
-          startBlock: 30278491,
+          address: '0x4AdfEb01C090e14BFA84411b74D0d03dDE12e39b',
+          startBlock: 34358231,
         },
       },
       abi: InterchainClientV1Abi,
+    },
+    InterchainDB: {
+      network: {
+        ethSepolia: {
+          address: '0x4361F461c5Df0DCf109BA2CF0E46dfA26e73f54f',
+          startBlock: 5628137,
+        },
+        arbSepolia: {
+          address: '0x4361F461c5Df0DCf109BA2CF0E46dfA26e73f54f',
+          startBlock: 30278491,
+        },
+      },
+      abi: InterchainDBAbi,
+    },
+    SynapseModule: {
+      network: {
+        ethSepolia: {
+          address: '0x95f2e2fAFE38f2aAdC9F9cBef98785809cc4bb6B',
+          startBlock: 5628137,
+        },
+        arbSepolia: {
+          address: '0xC13e2b478f6531Ef096FF05733Ed65E3bc7fC5AF',
+          startBlock: 30278491,
+        },
+      },
+      abi: SynapseModuleAbi,
     },
   },
 })
