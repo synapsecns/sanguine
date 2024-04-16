ed4a9688ddc9798f57d1279dc669f232b8d9ac07
abtestingalpha <abtestingalpha@gmail.com>
2024-04-16 15:01:44 -0400

    Moves queries to queries file

--- packages/interchain-explorer-ui/src/graphql/queries.ts
+++ packages/interchain-explorer-ui/src/graphql/queries.ts
@@ -48,3 +48,53 @@ export const GET_INTERCHAIN_TRANSACTIONS = gql`
     }
   }
 `
+
+export const GET_STATS = gql`
+  query GetStats($limit: Int) {
+    interchainTransactions(
+      orderBy: "sentAt"
+      orderDirection: "desc"
+      limit: $limit
+    ) {
+      items {
+        interchainTransactionSent {
+          id
+          count
+        }
+        interchainTransactionReceived {
+          id
+          count
+        }
+      }
+    }
+  }
+`
+
+export const GET_INTERCHAIN_TRANSACTION = gql`
+  query GetInterchainTransaction($id: String!) {
+    interchainTransaction(id: $id) {
+      id
+      interchainTransactionSent {
+        id
+        chainId
+        address
+        srcSender
+        dstChainId
+        dstReceiver
+        transactionHash
+        options
+        timestamp
+      }
+      interchainTransactionReceived {
+        id
+        chainId
+        address
+        srcSender
+        srcChainId
+        dstReceiver
+        transactionHash
+        timestamp
+      }
+    }
+  }
+`
