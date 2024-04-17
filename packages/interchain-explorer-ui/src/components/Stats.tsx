12e433c83719d51c874a1d8477f6c5201298ebd6
abtestingalpha <abtestingalpha@gmail.com>
2024-04-17 10:54:58 -0400

    Handles in-flight count

--- packages/interchain-explorer-ui/src/components/Stats.tsx
+++ packages/interchain-explorer-ui/src/components/Stats.tsx
@@ -7,10 +7,13 @@ export const Stats = () => {
     return null
   }
 
-  const {
-    interchainTransactionSent: { count: numSent },
-    interchainTransactionReceived: { count: numRecived },
-  } = response.data
+  const numSent = response.data.find(
+    (t) => t.interchainTransactionSent !== null
+  )?.interchainTransactionSent?.count
+
+  const numReceived = response.data.find(
+    (t) => t.interchainTransactionReceived !== null
+  )?.interchainTransactionReceived?.count
 
   return (
     <div className="grid grid-cols-4 gap-4 mb-4">
@@ -22,7 +25,7 @@ export const Stats = () => {
         <p className="text-xs uppercase text-gray-400">
           Total Messages Received
         </p>
-        <p className="text-xl">{numRecived}</p>
+        <p className="text-xl">{numReceived}</p>
       </div>
       <div className="bg-gray-800 p-4 rounded">
         <p className="text-xs uppercase text-gray-400">Networks</p>
