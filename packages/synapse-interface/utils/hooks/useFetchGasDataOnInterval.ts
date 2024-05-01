8fe478b9fc9e5b060ece930ac679e4a044aa4016
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-05-01 13:56:42 -0700

    Simplify useFetchGasDataOnInterval

--- packages/synapse-interface/utils/hooks/useFetchGasDataOnInterval.ts
+++ packages/synapse-interface/utils/hooks/useFetchGasDataOnInterval.ts
@@ -1,28 +1,26 @@
 import { useEffect } from 'react'
 
 import { useAppDispatch } from '@/store/hooks'
-import { fetchGasData, resetGasData } from '@/slices/gasDataSlice'
 import { useBridgeState } from '@/slices/bridge/hooks'
+import { fetchGasData, resetGasData } from '@/slices/gasDataSlice'
 
 export const useFetchGasDataOnInterval = () => {
   const dispatch = useAppDispatch()
   const { fromChainId } = useBridgeState()
 
-  const fetchData = (chainId: number) => {
-    dispatch(fetchGasData(chainId))
+  const fetchGas = () => {
+    if (fromChainId) {
+      dispatch(fetchGasData(fromChainId))
+    }
   }
 
   useEffect(() => {
     // Reset gas data when selecting new chain
     dispatch(resetGasData())
 
-    // Fetch when chainId available
-    if (fromChainId) {
-      fetchData(fromChainId)
-    }
+    fetchGas()
 
-    // Fetch every 60 seconds
-    const interval = setInterval(fetchGasData, 60000)
+    const interval = setInterval(fetchGas, 60000)
 
     return () => clearInterval(interval)
   }, [dispatch, fromChainId])
