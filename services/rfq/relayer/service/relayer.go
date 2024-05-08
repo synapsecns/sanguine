9367858bd6ad732443f9d19dd9515fb04509cf9e
dwasse <wassermandaniel8@gmail.com>
2024-05-08 11:52:29 -0500

    CCTP: use chain listener instead of scribe (#2592)
    * WIP: replace scribe with chain listener
    
    * Fix: build
    
    * Fix: tests
    
    * Fix: rfq build
    
    * Cleanup: remove unused enum
    
    * Cleanup: revert test change
    
    * Cleanup: comments
    
    * Cleanup: lint
    
    * Fix: build
    
    * Fix: test

--- services/rfq/relayer/service/relayer.go
+++ services/rfq/relayer/service/relayer.go
@@ -29,7 +29,6 @@ import (
 	"github.com/synapsecns/sanguine/services/rfq/relayer/relconfig"
 	"github.com/synapsecns/sanguine/services/rfq/relayer/reldb"
 	"github.com/synapsecns/sanguine/services/rfq/relayer/reldb/connect"
-	"github.com/synapsecns/sanguine/services/scribe/client"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -274,10 +273,9 @@ func (r *Relayer) startCCTPRelayer(ctx context.Context) (err error) {
 	if err != nil {
 		return fmt.Errorf("could not connect to database: %w", err)
 	}
-	scribeClient := client.NewRemoteScribe(uint16(cctpCfg.ScribePort), cctpCfg.ScribeURL, r.metrics).ScribeClient
 	omnirpcClient := omniClient.NewOmnirpcClient(cctpCfg.BaseOmnirpcURL, r.metrics, omniClient.WithCaptureReqRes())
 	attAPI := attestation.NewCircleAPI(cctpCfg.CircleAPIURl)
-	cctpRelayer, err := relayer.NewCCTPRelayer(ctx, *cctpCfg, store, scribeClient, omnirpcClient, r.metrics, attAPI, relayer.WithSubmitter(r.submitter))
+	cctpRelayer, err := relayer.NewCCTPRelayer(ctx, *cctpCfg, store, omnirpcClient, r.metrics, attAPI, relayer.WithSubmitter(r.submitter))
 	if err != nil {
 		return fmt.Errorf("could not create cctp relayer: %w", err)
 	}
