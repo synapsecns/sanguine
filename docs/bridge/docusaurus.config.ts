b6a29a336459e216607b305c49cac460ad0bbc09
Trajan0x <trajan0x@users.noreply.github.com>
2024-05-10 10:33:48 +0100

    fix synapse-interface breaking

--- docs/bridge/docusaurus.config.ts
+++ docs/bridge/docusaurus.config.ts
@@ -1,6 +1,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import * as path from "path";
 
 const config: Config = {
   title: 'Synapse Bridge Docs',
@@ -142,7 +143,21 @@ const config: Config = {
           },
         }
       },
-    ]
+    ],
+    // please see: https://github.com/facebook/docusaurus/issues/8091#issuecomment-1269112001 for an explanation.
+    () => ({
+      name: 'resolve-react',
+      configureWebpack() {
+        return {
+          resolve: {
+            alias: {
+              // assuming root node_modules is up from "./packages/<your-docusaurus>
+              react: path.resolve('../../node_modules/react'),
+            },
+          },
+        };
+      },
+    }),
   ],
 };
 
