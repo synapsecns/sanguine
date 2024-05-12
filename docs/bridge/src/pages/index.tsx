bdad9894a124c3ac215bc810be4cbe4375929d7a
Trajan0x <trajan0x@users.noreply.github.com>
2024-05-12 21:27:43 +0100

    merge

--- docs/bridge/src/pages/index.tsx
+++ docs/bridge/src/pages/index.tsx
@@ -4,6 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import  { Redirect } from 'react-router-dom';
 
 import styles from './index.module.css';
 
@@ -36,6 +37,7 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
+      <Redirect to={"/docs/CCTP/Overview"} />
       {/*<HomepageHeader />*/}
       <main>
         {/*<HomepageFeatures />*/}
