f3f4dd69ef2d5a23b035f03a1bb86a8659e55e50
Trajan0x <trajan0x@users.noreply.github.com>
2024-06-09 11:47:53 -0400

    cleanup

--- ethergo/util/converter.go
+++ ethergo/util/converter.go
@@ -14,14 +14,24 @@ func TxToCall(transaction *types.Transaction) (*ethereum.CallMsg, error) {
 		return nil, errors.Wrap(err, "could not convert to call")
 	}
 
-	return &ethereum.CallMsg{
+	callMessage := ethereum.CallMsg{
 		From:      rawMsg.From,
 		To:        transaction.To(),
 		Gas:       transaction.Gas(),
 		GasPrice:  transaction.GasPrice(),
-		GasFeeCap: transaction.GasTipCap(),
-		GasTipCap: transaction.GasFeeCap(),
+		GasFeeCap: transaction.GasFeeCap(),
+		GasTipCap: transaction.GasTipCap(),
 		Value:     transaction.Value(),
 		Data:      transaction.Data(),
-	}, nil
+	}
+
+	// gas price will getset to gastipcap + gas fee cap to account for legacy behavior so if
+	// tip/fee cap are set we need to make sure we nil gas price
+	if transaction.Type() == types.LegacyTxType {
+		callMessage.GasTipCap = nil
+		callMessage.GasFeeCap = nil
+	} else {
+		callMessage.GasPrice = nil
+	}
+	return &callMessage, nil
 }
