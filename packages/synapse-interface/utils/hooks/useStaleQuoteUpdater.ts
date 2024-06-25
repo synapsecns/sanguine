0ee5b6966c36bbd347067ae8f271606adba7949b
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-06-25 14:38:50 -0700

    Fire single refresh quote callback regardless of time passed (#2764)

--- packages/synapse-interface/utils/hooks/useStaleQuoteUpdater.ts
+++ packages/synapse-interface/utils/hooks/useStaleQuoteUpdater.ts
@@ -1,5 +1,5 @@
 import { isNull, isNumber } from 'lodash'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 import { BridgeQuote } from '@/utils/types'
 import { calculateTimeBetween } from '@/utils/time'
@@ -9,7 +9,7 @@ import { useIntervalTimer } from '@/utils/hooks/useIntervalTimer'
  * Refreshes quotes based on selected stale timeout duration.
  * Will refresh quote when browser is active and wallet prompt is not pending.
  */
-export const useStaleQuoteRefresher = (
+export const useStaleQuoteUpdater = (
   quote: BridgeQuote,
   refreshQuoteCallback: () => Promise<void>,
   isQuoteLoading: boolean,
@@ -19,15 +19,28 @@ export const useStaleQuoteRefresher = (
   const quoteTime = quote?.timestamp
   const isValidQuote = isNumber(quoteTime) && !isNull(quoteTime)
   const currentTime = useIntervalTimer(staleTimeout, !isValidQuote)
+  const eventListenerRef = useRef<null | (() => void)>(null)
 
   useEffect(() => {
     if (isValidQuote && !isQuoteLoading && !isWalletPending) {
       const timeDifference = calculateTimeBetween(currentTime, quoteTime)
       const isStaleQuote = timeDifference >= staleTimeout
+
       if (isStaleQuote) {
-        document.addEventListener('mousemove', refreshQuoteCallback, {
+        if (eventListenerRef.current) {
+          document.removeEventListener('mousemove', eventListenerRef.current)
+        }
+
+        const newEventListener = () => {
+          refreshQuoteCallback()
+          eventListenerRef.current = null
+        }
+
+        document.addEventListener('mousemove', newEventListener, {
           once: true,
         })
+
+        eventListenerRef.current = newEventListener
       }
     }
   }, [currentTime, staleTimeout])
