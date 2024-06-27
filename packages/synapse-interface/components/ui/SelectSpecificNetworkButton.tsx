406b7dbcf06b23ca140bc284cf96595585819b63
abtestingalpha <abtestingalpha@gmail.com>
2024-06-27 12:26:33 -0400

    removes old

--- packages/synapse-interface/components/ui/SelectSpecificNetworkButton.tsx
+++ packages/synapse-interface/components/ui/SelectSpecificNetworkButton.tsx
@@ -151,13 +151,3 @@ function HoverIcon({ token }) {
     </span>
   )
 }
-
-const NewTag = () => {
-  const className = joinClassNames({
-    space: 'px-2 py-[2px] rounded-md',
-    border: 'border border-fuchsia-500',
-    background: 'bg-gradient-to-r from-fuchsia-950 to-purple-900',
-    font: 'text-sm ',
-  })
-  return <div className={className}>New!</div>
-}
