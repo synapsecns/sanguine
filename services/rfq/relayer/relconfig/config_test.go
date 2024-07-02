dce9713849eecd950cd365c4bdf9684a4b594c21
Trajan0x <trajan0x@users.noreply.github.com>
2024-07-02 10:57:19 -0400

    address lint issue from 5ed6d9a306afa89a55e9768a3a1054e4b6147227

--- services/rfq/relayer/relconfig/config_test.go
+++ services/rfq/relayer/relconfig/config_test.go
@@ -489,7 +489,8 @@ func TestDecodeTokenID(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
+	for i := range tests {
+		tt := tests[i]
 		t.Run(tt.name, func(t *testing.T) {
 			gotChain, gotAddr, err := relconfig.DecodeTokenID(tt.id)
 			if tt.wantErr {
