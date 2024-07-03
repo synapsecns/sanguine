bdbaa7ebe112c3a1ef9d90685dc8fe29b5bdab39
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-07-03 11:40:52 -0500

    Fix: guarddb models

--- services/rfq/guard/guarddb/base/store.go
+++ services/rfq/guard/guarddb/base/store.go
@@ -36,7 +36,7 @@ func (s Store) SubmitterDB() submitterDB.Service {
 // GetAllModels gets all models to migrate
 // see: https://medium.com/@SaifAbid/slice-interfaces-8c78f8b6345d for an explanation of why we can't do this at initialization time
 func GetAllModels() (allModels []interface{}) {
-	allModels = append(txdb.GetAllModels(), &PendingProvenModel{})
+	allModels = append(txdb.GetAllModels(), &PendingProvenModel{}, &BridgeRequestModel{})
 	allModels = append(allModels, listenerDB.GetAllModels()...)
 	return allModels
 }
