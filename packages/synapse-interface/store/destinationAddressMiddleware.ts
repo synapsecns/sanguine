14a827288f5ee67788959e73b0163c46983769e6
abtestingalpha <abtestingalpha@gmail.com>
2024-07-12 13:33:50 -0400

    Fixes linting error

--- packages/synapse-interface/store/destinationAddressMiddleware.ts
+++ packages/synapse-interface/store/destinationAddressMiddleware.ts
@@ -4,7 +4,7 @@ import { screenAddress } from '@/utils/screenAddress'
 import { setDestinationAddress } from '@/slices/bridge/reducer'
 
 export const destinationAddressMiddleware: Middleware =
-  (store) => (next) => async (action) => {
+  (_store) => (next) => async (action) => {
     if (setDestinationAddress.match(action) && action.payload !== null) {
       const isRisky = await screenAddress(action.payload)
       if (!isRisky) {
