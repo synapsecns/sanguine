a04b6bfcdadd0e102c4118c64e4070084ee44e23
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-07-17 18:09:33 +0100

    feat: make scripts environment-aware, testnet helpers

---
+++ packages/contracts-rfq/script/testnet-fb-config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Usage: ./script/testnet-fb-deploy.sh [...options]
+
+trap "echo 'Deployment script terminated by user'; exit" INT
+
+walletName="testnet_deployer"
+
+npx fsr script/ConfigureFastBridge.s.sol arb_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/ConfigureFastBridge.s.sol base_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/ConfigureFastBridge.s.sol eth_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/ConfigureFastBridge.s.sol op_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/ConfigureFastBridge.s.sol scroll_sepolia "$walletName" --sig runTestnet "$@"
---
+++ packages/contracts-rfq/script/testnet-fb-deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Usage: ./script/testnet-fb-deploy.sh [...options]
+
+trap "echo 'Deployment script terminated by user'; exit" INT
+
+walletName="testnet_deployer"
+
+npx fsr script/DeployFastBridge.CREATE2.s.sol arb_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/DeployFastBridge.CREATE2.s.sol base_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/DeployFastBridge.CREATE2.s.sol eth_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/DeployFastBridge.CREATE2.s.sol op_sepolia "$walletName" --sig runTestnet "$@"
+npx fsr script/DeployFastBridge.CREATE2.s.sol scroll_sepolia "$walletName" --sig runTestnet "$@"
