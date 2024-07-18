c72b63ba053de3d33344c082597fd110e805b29b
Trajan0x <trajan0x@users.noreply.github.com>
2024-07-18 13:37:40 -0400

    clean

--- core/processlog/options.go
+++ core/processlog/options.go
@@ -181,7 +181,7 @@ func makeRawArgs(opts []StdStreamLogArgsOption) *stdStreamLogArgs {
 	args.LogFrequency = time.Second
 	// not a problem unless it's missing from user too, we'll validate at the end
 	args.LogDir, _ = os.MkdirTemp("/tmp/logs", "")
-	_ = os.MkdirAll(args.LogDir, os.ModePerm)
+	_ = os.MkdirAll("/tmp/logs", os.ModePerm)
 
 	args.LogFileName = "log"
 	args.PrintFunc = func(s []byte) {
