87e369dddb64ac3868b8e973e7237039dffe6455
golangisfun123 <simon.shamoon@protochainresearch.com>
2024-07-22 14:57:39 -0500

    fix txage for l2s

--- contrib/opbot/botmd/commands_test.go
+++ contrib/opbot/botmd/commands_test.go
@@ -1,8 +1,13 @@
 package botmd_test
 
 import (
-	"github.com/synapsecns/sanguine/contrib/opbot/botmd"
+	"context"
 	"testing"
+
+	"github.com/synapsecns/sanguine/contrib/opbot/botmd"
+	"github.com/synapsecns/sanguine/core/metrics"
+	omnirpcClient "github.com/synapsecns/sanguine/services/omnirpc/client"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/relapi"
 )
 
 func TestStripLinks(t *testing.T) {
@@ -11,5 +16,27 @@ func TestStripLinks(t *testing.T) {
 
 	if got := botmd.StripLinks(testLink); got != expected {
 		t.Errorf("StripLinks(%s) = %s; want %s", testLink, got, expected)
+	}
+}
+
+func TestTxAge(t *testing.T) {
+	notExpected := "unknown time ago" // should be a definite time
+
+	status := botmd.Status{
+		GetQuoteRequestStatusResponse: &relapi.GetQuoteRequestStatusResponse{
+			OriginTxHash:  "0x954264d120f5f3cf50edc39ebaf88ea9dc647d9d6843b7a120ed3677e23d7890",
+			OriginChainID: 421611,
+		},
+	}
+	ctx := context.Background()
+
+	client := omnirpcClient.NewOmnirpcClient("https://arb1.arbitrum.io/rpc", metrics.Get())
+	cc, err := client.GetChainClient(ctx, int(status.OriginChainID))
+	if err != nil {
+		t.Fatalf("GetChainClient() failed: %v", err)
+	}
+
+	if got := botmd.GetTxAge(context.Background(), cc, status); got == notExpected {
+		t.Errorf("TxAge(%s) = %s; want not %s", status.OriginTxHash, got, notExpected)
 	}
 }
--- contrib/opbot/botmd/export_test.go
+++ contrib/opbot/botmd/export_test.go
@@ -1,5 +1,15 @@
 package botmd
 
+import (
+	"context"
+
+	"github.com/synapsecns/sanguine/ethergo/client"
+)
+
 func StripLinks(input string) string {
 	return stripLinks(input)
+}
+
+func GetTxAge(ctx context.Context, client client.EVM, res Status) string {
+	return getTxAge(ctx, client, res)
 }
