d7616741cf7a11745e447e06d096026b446a6eba
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-07-22 17:09:05 -0700

    refactor: clean bridge settings ui logic

--- packages/synapse-interface/pages/state-managed-bridge/index.tsx
+++ packages/synapse-interface/pages/state-managed-bridge/index.tsx
@@ -21,7 +21,7 @@ import { SwitchButton } from '@/components/buttons/SwitchButton'
 import { PageHeader } from '@/components/PageHeader'
 import SettingsSlideOver from '@/components/StateManagedBridge/SettingsSlideOver'
 import Button from '@/components/ui/tailwind/Button'
-import { SettingsIcon } from '@/components/icons/SettingsIcon'
+import { SettingsToggle } from '@/components/StateManagedBridge/SettingsToggle'
 import { BridgeCard } from '@/components/ui/BridgeCard'
 import { ConfirmDestinationAddressWarning } from '@/components/StateManagedBridge/BridgeWarnings'
 import { EMPTY_BRIDGE_QUOTE_ZERO } from '@/constants/bridge'
@@ -563,22 +563,11 @@ const StateManagedBridge = () => {
           <div>
             <Button
               className="flex items-center p-3 text-opacity-75 bg-bgLight hover:bg-bgLighter text-secondaryTextColor hover:text-white"
-              onClick={() => {
-                if (showSettingsSlideOver === true) {
-                  dispatch(setShowSettingsSlideOver(false))
-                } else {
-                  dispatch(setShowSettingsSlideOver(true))
-                }
-              }}
+              onClick={() =>
+                dispatch(setShowSettingsSlideOver(!showSettingsSlideOver))
+              }
             >
-              {!showSettingsSlideOver ? (
-                <>
-                  <SettingsIcon className="w-5 h-5 mr-2" />
-                  <span>Settings</span>
-                </>
-              ) : (
-                <span>Close</span>
-              )}
+              <SettingsToggle showSettingsToggle={!showSettingsSlideOver} />
             </Button>
           </div>
         </div>
