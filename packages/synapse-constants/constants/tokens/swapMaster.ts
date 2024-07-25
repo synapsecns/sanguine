8e30911660b8284b34554122c573766c6a802a67
Moses <103143573+Defi-Moses@users.noreply.github.com>
2024-07-25 12:26:08 +0100

    [goreleaser] Adding new rfq deployments to explorer (#2909)
    * adding new chains and new contracts
    
    * adding additional token addresses
    
    * adding USDT to scroll
    
    * removing weth from linea
    
    * fixing bridgeMap

--- packages/synapse-constants/constants/tokens/swapMaster.ts
+++ packages/synapse-constants/constants/tokens/swapMaster.ts
@@ -1,7 +1,6 @@
 import { Token } from '../types'
 import usdtLogo from '../assets/icons/usdt.svg'
 import usdcLogo from '../assets/icons/usdc.svg'
-
 import * as CHAINS from '../chains/master'
 
 export const SwapUSDC = new Token({
