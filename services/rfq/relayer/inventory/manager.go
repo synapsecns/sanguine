4f9c224d685f53a90c99e67424c2f69385b81557
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-07-29 12:29:22 +0200

    WIP: dont' set chain name

--- services/rfq/relayer/inventory/manager.go
+++ services/rfq/relayer/inventory/manager.go
@@ -604,7 +604,7 @@ func (i *inventoryManagerImpl) initializeTokens(parentCtx context.Context, cfg r
 				deferredCalls[chainID] = append(deferredCalls[chainID],
 					eth.CallFunc(funcBalanceOf, token, i.relayerAddress).Returns(rtoken.Balance),
 					eth.CallFunc(funcDecimals, token).Returns(&rtoken.Decimals),
-					eth.CallFunc(funcName, token).Returns(&rtoken.ChainName),
+					// eth.CallFunc(funcName, token).Returns(&rtoken.ChainName),
 					eth.CallFunc(funcAllowance, token, i.relayerAddress, rfqAddr).Returns(rtoken.Allowances[contractRFQ]),
 				)
 				cctpAddr, addrErr := cfg.GetSynapseCCTPAddress(chainID)
