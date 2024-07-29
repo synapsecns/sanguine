8fde07aaf790a33c114b246169355e8a6d467ae0
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-07-29 02:40:06 -0700

    feat(synapse-interface): adds BSC, Blast, Linea to RFQ (#2905)
    * Adds BSC to RFQ
    
    * Update banner dates
    
    * Add Blast and Linea to RFQ
    
    * adds linea and blast rfq routes to fe
    
    * changing linea logo
    
    * Update Linea svg
    
    * update copy
    
    ---------
    
    Co-authored-by: abtestingalpha <abtestingalpha@gmail.com>
    Co-authored-by: defi-moses <jakedinero@protonmail.com>
    Co-authored-by: aureliusbtc <82057759+aureliusbtc@users.noreply.github.com>

--- packages/synapse-interface/components/layouts/LandingPageWrapper/index.tsx
+++ packages/synapse-interface/components/layouts/LandingPageWrapper/index.tsx
@@ -51,10 +51,10 @@ export function LandingPageWrapper({ children }: { children: any }) {
     <div className="dark">
       <div className={wrapperClassName} style={TODO_REMOVE_wrapperStyle}>
         <AnnouncementBanner
-          bannerId="2024-05-02-scroll-launch"
-          bannerContent="Synapse Protocol now available on Scroll"
-          startDate={new Date('2024-05-08T18:45:09+00:00')}
-          endDate={new Date('2024-06-15T18:45:09+00:00')}
+          bannerId="2024-07-22-rfq"
+          bannerContent="Synapse now supports Linea - bridge to & from in 10 seconds"
+          startDate={new Date('2024-07-10T18:45:09+00:00')}
+          endDate={new Date('2024-08-15T18:45:09+00:00')}
         />
         <MaintenanceBanners />
         <LandingNav />
