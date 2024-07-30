cf2bb630e2d4eb22b5a0c2d6177ef9498b3b41ad
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-07-30 13:27:27 +0200

    Display full amount on hover

--- packages/explorer-ui/components/misc/IconAndAmount.tsx
+++ packages/explorer-ui/components/misc/IconAndAmount.tsx
@@ -12,39 +12,23 @@ export const IconAndAmount = ({
   iconSize = 'w-4 h-4 rounded-full',
   className = '',
 }) => {
-  let amount
   const decimals = addressToDecimals({ tokenAddress, chainId })
   const formattedValue = formatBigIntToString(value, decimals)
-  // const displayValue = amount ? formatAmount(amount) : '< 0.001'
-  const displayAmount = formatAmount(formattedValue)
-
-  if (tokenSymbol) {
-    const dec = 10 ** addressToDecimals({ tokenAddress, chainId })
-    if (value > 10000000) {
-      amount = value / (dec / 10 ** 6)
-    } else {
-      amount = value
-    }
-  } else {
-    const dec = 10 ** addressToDecimals({ tokenAddress, chainId })
-    amount = value / (dec / 10 ** 6)
-  }
 
   return (
     <div className={`flex items-center ${className}`}>
       <div className="flex flex-row items-center text-white">
         <AssetImage
           tokenAddress={tokenAddress}
-          // tokenSymbol={tokenSymbol}
           chainId={chainId}
           className={`${iconSize} min-w-[1rem] min-h-[1rem] inline rounded-full`}
         />
         <div
-          data-tooltip-content={displayAmount}
+          data-tooltip-content={formattedValue}
           data-tooltip-id="amount"
           className="flex-1 pl-1 mr-1 text-white"
         >
-          {displayAmount}
+          {formatAmount(formattedValue)}
         </div>
       </div>
       <span className="text-white">{tokenSymbol}</span>
