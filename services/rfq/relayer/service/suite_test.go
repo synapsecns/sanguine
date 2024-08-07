e4d2b27b2a2d2ff7fbab7ac256656d6f6761993f
golangisfun123 <simon.shamoon@protochainresearch.com>
2024-08-07 11:24:10 -0500

    remove remnants from the  past

--- services/rfq/relayer/service/suite_test.go
+++ services/rfq/relayer/service/suite_test.go
@@ -4,16 +4,21 @@ import (
 	"math/big"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/Flaque/filet"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/suite"
 	"github.com/synapsecns/sanguine/core/dbcommon"
 	"github.com/synapsecns/sanguine/core/metrics"
 	"github.com/synapsecns/sanguine/core/testsuite"
 	"github.com/synapsecns/sanguine/ethergo/backends"
 	"github.com/synapsecns/sanguine/ethergo/backends/geth"
+	"github.com/synapsecns/sanguine/ethergo/mocks"
 	omnirpcHelper "github.com/synapsecns/sanguine/services/omnirpc/testhelper"
+	"github.com/synapsecns/sanguine/services/rfq/contracts/testcontracts/fastbridgemock"
 	"github.com/synapsecns/sanguine/services/rfq/relayer/relconfig"
+	"github.com/synapsecns/sanguine/services/rfq/relayer/service"
 	"github.com/synapsecns/sanguine/services/rfq/testutil"
 )
 
@@ -73,4 +78,78 @@ func (r *RelayerTestSuite) SetupTest() {
 		},
 		OmniRPCURL: serverURL,
 	}
+}
+func (r *RelayerTestSuite) TestStore() {
+	r.T().Skip("TODO, test storage")
+
+	rel, err := service.NewRelayer(r.GetTestContext(), r.metrics, r.cfg)
+	r.NoError(err)
+
+	go func() {
+		r.NoError(rel.StartChainParser(r.GetTestContext()))
+	}()
+
+	_, oc := r.manager.GetMockFastBridge(r.GetTestContext(), r.originBackend)
+
+	auth := r.originBackend.GetTxContext(r.GetTestContext(), nil)
+
+	_, originToken := r.manager.GetMockERC20(r.GetTestContext(), r.originBackend)
+	r.NoError(err)
+
+	_, destToken := r.manager.GetMockERC20(r.GetTestContext(), r.destBackend)
+	r.NoError(err)
+
+	//nolint: typecheck
+	tx, err := oc.MockBridgeRequest(auth.TransactOpts, [32]byte(crypto.Keccak256([]byte("3"))), mocks.MockAddress(), fastbridgemock.IFastBridgeBridgeParams{
+		DstChainId:   uint32(r.destBackend.GetChainID()),
+		To:           mocks.MockAddress(),
+		OriginToken:  originToken.Address(),
+		DestToken:    destToken.Address(),
+		OriginAmount: big.NewInt(1),
+		DestAmount:   big.NewInt(2),
+		Deadline:     big.NewInt(3),
+	})
+	r.originBackend.WaitForConfirmation(r.GetTestContext(), tx)
+
+	r.T().Skip("TODO, test storage")
+	// TODO: check db
+	time.Sleep(time.Second * 1000)
+}
+
+func (r *RelayerTestSuite) TestCommit() {
+	r.T().Skip("TODO, test storage")
+
+	rel, err := service.NewRelayer(r.GetTestContext(), r.metrics, r.cfg)
+	r.NoError(err)
+
+	go func() {
+		r.NoError(rel.StartChainParser(r.GetTestContext()))
+	}()
+
+	_, oc := r.manager.GetMockFastBridge(r.GetTestContext(), r.originBackend)
+
+	auth := r.originBackend.GetTxContext(r.GetTestContext(), nil)
+
+	_, originToken := r.manager.GetMockERC20(r.GetTestContext(), r.originBackend)
+	r.NoError(err)
+
+	_, destToken := r.manager.GetMockERC20(r.GetTestContext(), r.destBackend)
+	r.NoError(err)
+
+	//nolint: typecheck
+	tx, err := oc.MockBridgeRequest(auth.TransactOpts, [32]byte(crypto.Keccak256([]byte("3"))), mocks.MockAddress(), fastbridgemock.IFastBridgeBridgeParams{
+		DstChainId:   uint32(r.destBackend.GetChainID()),
+		To:           mocks.MockAddress(),
+		OriginToken:  originToken.Address(),
+		DestToken:    destToken.Address(),
+		OriginAmount: big.NewInt(1),
+		DestAmount:   big.NewInt(2),
+		Deadline:     big.NewInt(3),
+	})
+	r.originBackend.WaitForConfirmation(r.GetTestContext(), tx)
+
+	r.T().Skip("TODO, test storage")
+	// TODO: check db
+	time.Sleep(time.Second * 100000)
+
 }
