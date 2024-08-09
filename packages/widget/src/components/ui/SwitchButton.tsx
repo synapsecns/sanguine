1825d2a23bb253810fc8dbf95e9a3fe2b071e903
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-08-08 17:12:51 -0700

    style: adjust placement of switch button

--- packages/widget/src/components/ui/SwitchButton.tsx
+++ packages/widget/src/components/ui/SwitchButton.tsx
@@ -15,22 +15,24 @@ export const SwitchButton = ({ onClick }: { onClick: () => void }) => {
     <svg
       onClick={handleClick}
       className={`
-        -mt-4 -mb-4 rounded z-10 justify-self-center
-        bg-[--synapse-button-bg]
-        border border-solid border-[--synapse-button-border]
-        stroke-2 stroke-[--synapse-button-text]
-        hover:opacity-80 cursor-pointer transition-transform
+        -mt-3 -mb-3 rounded z-10 justify-self-center
+        bg-[--synapse-surface]
+        border-2 border-solid border-[--synapse-root]
+        stroke-2 stroke-[--synapse-secondary]
+        hover:stroke-[--synapse-text] cursor-pointer transition-transform
         ${isActive ? `duration-${ms} rotate-180 ease-in-out` : 'ease-out'}
       `}
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      strokeLinecap="round"
-      strokeLinejoin="round"
     >
-      <path d="M11,22V8M11,8L7,12M11,8L15,12" />
-      <path d="M21,9V23M21,23L25,19M21,23L17,19" />
+      <path
+        d="M12 6V18M12 18L16 14M12 18L8 14"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
     </svg>
   )
 }
