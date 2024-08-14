037b4c1de1369e04d40cfad943a4e1760ef16a45
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-08-13 17:21:13 -0700

    Update button size

--- packages/widget/src/components/ui/SwitchButton.tsx
+++ packages/widget/src/components/ui/SwitchButton.tsx
@@ -22,14 +22,15 @@ export const SwitchButton = ({ onClick }: { onClick: () => void }) => {
         hover:stroke-[--synapse-text] cursor-pointer transition-transform
         ${isActive ? `duration-${ms} rotate-180 ease-in-out` : 'ease-out'}
       `}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        d="M12 6V18M12 18L16 14M12 18L8 14"
+        d="M14 8V20M14 20L18 16M14 20L10 16"
+        stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
