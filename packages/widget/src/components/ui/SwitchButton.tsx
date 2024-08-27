8226c60a5b30b0f977ffef0cdb30c2d01f6ef3b5
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-08-27 11:46:30 -0700

    feat(widget): visual update (#2992)
    * feat: prevent details close on quote refresh, apply default / fetching values
    
    * feat: display chain dropdown label
    
    * feat: hide switch text no connected address
    
    * feat(widget): switch button (#2993)
    
    * feat: switch button
    
    * style: adjust placement of switch button
    
    * fix: dropdown height shift on switch
    
    * fix: switch chain prior balances showing, show receive text on destination
    
    * available balance sync with originChainId
    
    * feat: notify user to select source chain if not selected after switch
    
    * Update button size
    
    * feat: separate balances and allowances fetch state to display loading
    
    * fix: flip origin/dest switch setting
    
    * fix attribute names

---
+++ packages/widget/src/components/ui/SwitchButton.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const ms = 300
+
+export const SwitchButton = ({ onClick }: { onClick: () => void }) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const handleClick = () => {
+    onClick()
+    setIsActive(true)
+    setTimeout(() => setIsActive(false), ms)
+  }
+
+  return (
+    <svg
+      onClick={handleClick}
+      className={`
+        -mt-3 -mb-3 rounded z-10 justify-self-center
+        bg-[--synapse-surface]
+        border-2 border-solid border-[--synapse-root]
+        stroke-2 stroke-[--synapse-secondary]
+        hover:stroke-[--synapse-text] cursor-pointer transition-transform
+        ${isActive ? `duration-${ms} rotate-180 ease-in-out` : 'ease-out'}
+      `}
+      width="28"
+      height="28"
+      viewBox="0 0 28 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 8V20M14 20L18 16M14 20L10 16"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
