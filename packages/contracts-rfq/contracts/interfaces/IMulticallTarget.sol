cf3d1a5fba1f9ef9c7d160f16f430ddf8c6d2d42
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-08-28 17:01:17 +0100

    feat: scaffold `MulticallTarget`

---
+++ packages/contracts-rfq/contracts/interfaces/IMulticallTarget.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @notice Interface for a contract that can be called multiple times by the same caller. Inspired by MulticallV3:
+/// https://github.com/mds1/multicall/blob/master/src/Multicall3.sol
+interface IMulticallTarget {
+    struct Result {
+        bool success;
+        bytes returnData;
+    }
+
+    function multicallNoResults(bytes[] calldata data, bool ignoreReverts) external;
+    function multicallWithResults(
+        bytes[] calldata data,
+        bool ignoreReverts
+    )
+        external
+        returns (Result[] memory results);
+}
---
+++ packages/contracts-rfq/contracts/utils/MulticallTarget.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IMulticallTarget} from "../interfaces/IMulticallTarget.sol";
+
+abstract contract MulticallTarget is IMulticallTarget {
+    function multicallNoResults(bytes[] calldata data, bool ignoreReverts) external {
+        // TODO: Implement
+    }
+
+    function multicallWithResults(
+        bytes[] calldata data,
+        bool ignoreReverts
+    )
+        external
+        returns (Result[] memory results)
+    {
+        // TODO: Implement
+    }
+}
