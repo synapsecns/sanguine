8df64be61cff2b0825117c6be071a1b887fd382e
golangisfun123 <simon.shamoon@protochainresearch.com>
2024-08-30 11:04:20 -0500

    fix refund logic

--- contrib/opbot/botmd/commands.go
+++ contrib/opbot/botmd/commands.go
@@ -278,63 +278,67 @@ func (b *Bot) rfqRefund() *slacker.CommandDefinition {
 				return
 			}
 
+			var rawRequest *relapi.GetQuoteRequestResponse
+			var err error
 			for _, relayer := range b.cfg.RelayerURLS {
 				relClient := relapi.NewRelayerClient(b.handler, relayer)
-
-				rawRequest, err := getQuoteRequest(ctx.Context(), relClient, tx)
-				if err != nil {
-					_, err := ctx.Response().Reply("error fetching quote request")
-					if err != nil {
-						log.Println(err)
-					}
-					return
+				rawRequest, err = getQuoteRequest(ctx.Context(), relClient, tx)
+				if err == nil {
+					break
 				}
-
-				fastBridgeContract, err := b.makeFastBridge(ctx.Context(), rawRequest)
-				if err != nil {
-					_, err := ctx.Response().Reply(err.Error())
-					if err != nil {
-						log.Println(err)
-					}
-					return
-				}
-
-				canRefund, err := b.screener.ScreenAddress(ctx.Context(), rawRequest.Sender)
-				if err != nil {
-					_, err := ctx.Response().Reply("error screening address")
-					if err != nil {
-						log.Println(err)
-					}
-					return
-				}
-
-				if !canRefund {
-					_, err := ctx.Response().Reply("address cannot be refunded")
-					if err != nil {
-						log.Println(err)
-					}
-					return
-				}
-
-				nonce, err := b.submitter.SubmitTransaction(ctx.Context(), big.NewInt(int64(rawRequest.OriginChainID)), func(transactor *bind.TransactOpts) (tx *types.Transaction, err error) {
-					tx, err = fastBridgeContract.Refund(transactor, common.Hex2Bytes(rawRequest.QuoteRequestRaw))
-					if err != nil {
-						return nil, fmt.Errorf("error submitting refund: %w", err)
-					}
-					return tx, nil
-				})
-				if err != nil {
-					log.Printf("error submitting refund: %v\n", err)
-					continue
-				}
-
-				// TODO: follow the lead of https://github.com/synapsecns/sanguine/pull/2845
-				_, err = ctx.Response().Reply(fmt.Sprintf("refund submitted with nonce %d", nonce))
+			}
+			if err != nil {
+				_, err := ctx.Response().Reply("error fetching quote request")
 				if err != nil {
 					log.Println(err)
 				}
 				return
 			}
+
+			fastBridgeContract, err := b.makeFastBridge(ctx.Context(), rawRequest)
+			if err != nil {
+				_, err := ctx.Response().Reply(err.Error())
+				if err != nil {
+					log.Println(err)
+				}
+				return
+			}
+
+			canRefund, err := b.screener.ScreenAddress(ctx.Context(), rawRequest.Sender)
+			if err != nil {
+				_, err := ctx.Response().Reply("error screening address")
+				if err != nil {
+					log.Println(err)
+				}
+				return
+			}
+
+			if !canRefund {
+				_, err := ctx.Response().Reply("address cannot be refunded")
+				if err != nil {
+					log.Println(err)
+				}
+				return
+			}
+
+			nonce, err := b.submitter.SubmitTransaction(ctx.Context(), big.NewInt(int64(rawRequest.OriginChainID)), func(transactor *bind.TransactOpts) (tx *types.Transaction, err error) {
+				tx, err = fastBridgeContract.Refund(transactor, common.Hex2Bytes(rawRequest.QuoteRequestRaw))
+				if err != nil {
+					return nil, fmt.Errorf("error submitting refund: %w", err)
+				}
+				return tx, nil
+			})
+			if err != nil {
+				log.Printf("error submitting refund: %v\n", err)
+				return
+			}
+
+			// TODO: follow the lead of https://github.com/synapsecns/sanguine/pull/2845
+			_, err = ctx.Response().Reply(fmt.Sprintf("refund submitted with nonce %d", nonce))
+			if err != nil {
+				log.Println(err)
+			}
+			return
 		},
 	}
 }
