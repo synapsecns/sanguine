48d8082adbb2192ac9c0c17cb6b47c522c6db06d
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-09-03 13:11:15 -0700

    chore(synapse-interface): tailwind intellisense setting (#3085)
    * Tailwind intellisense sensitivity
    * Apply className changes to objects for legibility

--- packages/synapse-interface/components/ui/NewTag.tsx
+++ packages/synapse-interface/components/ui/NewTag.tsx
@@ -1,11 +1,11 @@
 import { joinClassNames } from '@/utils/joinClassNames'
 
 export const NewTag = () => {
-  const className = joinClassNames({
+  const classNames = {
     space: 'px-2 py-[2px] rounded-md',
     border: 'border border-fuchsia-500',
     background: 'bg-gradient-to-r from-fuchsia-950 to-purple-900',
     font: 'text-sm ',
-  })
-  return <div className={className}>New!</div>
+  }
+  return <div className={joinClassNames(classNames)}>New!</div>
 }
