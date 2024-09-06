f1c948af7d48b415cea98b7ece482732a77dbe8e
parodime <jordan@protochainresearch.com>
2024-09-06 15:01:14 -0400

    lint

--- services/rfq/api/rest/versions.go
+++ services/rfq/api/rest/versions.go
@@ -1,14 +1,16 @@
 package rest
 
+// ApiVersionHistroy lists historical ApiVersion structs in descending chronological order (current version index 0), each containing further detail about the version.
 type ApiVersionHistory struct {
 	Versions []ApiVersion `json:"versions"`
 }
 
+// ApiVersion specifies the date of a particular API version along with any comments & alerts for integrators to review.
 type ApiVersion struct {
-	Version string `json:"version"`
-	Date    string `json:"date"`
-	Comment string `json:"_comment"`
-	Alerts  string `json:"_alerts"`
+	Version  string `json:"version"`
+	Date     string `json:"date"`
+	Comments string `json:"comments"`
+	Alerts   string `json:"alerts"`
 }
 
 // ApiVersionHistory contains version information for the Synapse RFQ API. Deprecation notices & other alerts, if any, will be listed here.
@@ -16,10 +18,10 @@ type ApiVersion struct {
 var apiVersions = ApiVersionHistory{
 	Versions: []ApiVersion{
 		{
-			Version: "1.0",
-			Date:    "2024-01-01",
-			Comment: "",
-			Alerts:  "",
+			Version:  "1.0",
+			Date:     "2024-01-01",
+			Comments: "",
+			Alerts:   "",
 		},
 	},
 }
