080c5ffa27cf32b5065b3db9c6e6fdd341a78f41
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-09-12 11:59:35 -0700

    update spinner animation

--- packages/synapse-interface/components/StateManagedBridge/BridgeQuoteResetTimer.tsx
+++ packages/synapse-interface/components/StateManagedBridge/BridgeQuoteResetTimer.tsx
@@ -39,23 +39,64 @@ const AnimatedProgressCircle = ({
   }, [animateKey])
 
   return (
+    // <svg
+    //   key={animationKey}
+    //   width="24"
+    //   height="24"
+    //   viewBox="-12 -12 24 24"
+    //   stroke="currentcolor"
+    //   stroke-opacity=".33"
+    //   fill="none"
+    //   className="absolute -rotate-90"
+    // >
+    //   <circle r="8" />
+    //   <circle r="8" stroke-dasharray="1" pathLength="1">
+    //     <animate
+    //       attributeName="stroke-dashoffset"
+    //       values="2; 1"
+    //       dur={`${convertMsToSeconds(duration)}s`}
+    //       fill="freeze"
+    //     />
+    //   </circle>
+    // </svg>
     <svg
       key={animationKey}
       width="24"
       height="24"
       viewBox="-12 -12 24 24"
       stroke="currentcolor"
-      stroke-opacity=".33"
       fill="none"
-      className="absolute -rotate-90"
+      className="absolute block -rotate-90"
     >
-      <circle r="8" />
+      <circle r="8" pathLength="1" stroke-opacity=".25">
+        <animate
+          attributeName="stroke-dashoffset"
+          to="-1"
+          dur="2.5s"
+          repeatCount="indefinite"
+        />
+        <set
+          attributeName="stroke-dasharray"
+          to="0.05"
+          begin={`${convertMsToSeconds(duration)}s`}
+        />
+        <set
+          attributeName="stroke-opacity"
+          to="0.5"
+          begin={`${convertMsToSeconds(duration)}s`}
+        />
+      </circle>
       <circle r="8" stroke-dasharray="1" pathLength="1">
         <animate
           attributeName="stroke-dashoffset"
           values="2; 1"
           dur={`${convertMsToSeconds(duration)}s`}
           fill="freeze"
+        />
+        <animate
+          attributeName="stroke-opacity"
+          values="0; 1"
+          dur={`${convertMsToSeconds(duration)}s`}
         />
       </circle>
     </svg>
