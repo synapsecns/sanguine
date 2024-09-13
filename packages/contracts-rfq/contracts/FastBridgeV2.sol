1a521d671e1d93890a2ce9ea5029b91a5c565917
parodime <jordan@protochainresearch.com>
2024-09-13 11:39:07 -0400

    pragma lock, contract relabel

--- packages/contracts-rfq/contracts/FastBridgeV2.sol
+++ packages/contracts-rfq/contracts/FastBridgeV2.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.24;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -10,7 +10,7 @@ import {Admin} from "./Admin.sol";
 import {IFastBridge} from "./interfaces/IFastBridge.sol";
 import {IFastBridgeV2} from "./interfaces/IFastBridgeV2.sol";
 
-contract FastBridge is IFastBridge, IFastBridgeV2, Admin {
+contract FastBridgeV2 is IFastBridge, IFastBridgeV2, Admin {
     using SafeERC20 for IERC20;
     using UniversalTokenLib for address;
 
