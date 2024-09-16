e1d45dce8543769316ec387b394cdaaf90ee4a03
abtestingalpha <104046418+abtestingalpha@users.noreply.github.com>
2024-09-16 13:30:19 -0400

    [SLT-157] feat(rest-api): Refactor for RESTful architectural standardization (#3126)
    * RESTful refactor
    
    * Adds controllers and routes
    * Validations
    * Middleware for error handling
    * Adds tests
    
    ---------
    
    Co-authored-by: defi-moses <jakedinero@protonmail.com>

---
+++ packages/rest-api/src/controllers/bridgeTxInfoController.ts
@@ -0,0 +1,50 @@
+import { validationResult } from 'express-validator'
+import { parseUnits } from '@ethersproject/units'
+
+import { Synapse } from '../services/synapseService'
+
+export const bridgeTxInfoController = async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    const { fromChain, toChain, amount, destAddress } = req.query
+    const fromTokenInfo = res.locals.tokenInfo.fromToken
+    const toTokenInfo = res.locals.tokenInfo.toToken
+
+    const amountInWei = parseUnits(amount.toString(), fromTokenInfo.decimals)
+
+    const quotes = await Synapse.allBridgeQuotes(
+      Number(fromChain),
+      Number(toChain),
+      fromTokenInfo.address,
+      toTokenInfo.address,
+      amountInWei
+    )
+
+    const txInfoArray = await Promise.all(
+      quotes.map(async (quote) => {
+        const txInfo = await Synapse.bridge(
+          destAddress,
+          quote.routerAddress,
+          Number(fromChain),
+          Number(toChain),
+          fromTokenInfo.address,
+          amountInWei,
+          quote.originQuery,
+          quote.destQuery
+        )
+        return txInfo
+      })
+    )
+    res.json(txInfoArray)
+  } catch (err) {
+    res.status(500).json({
+      error:
+        'An unexpected error occurred in /bridgeTxInfo. Please try again later.',
+      details: err.message,
+    })
+  }
+}
