eebb57c10386235ab8c82a78b34cda7196be23e9
Lawson Kight <lawsonkight@users.noreply.github.com>
2024-09-17 16:07:23 -0700

    eslint fix

--- docs/bridge/docs/rfq/API/sidebar.ts
+++ docs/bridge/docs/rfq/API/sidebar.ts
@@ -1,26 +1,26 @@
-import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs'
 
 const sidebar: SidebarsConfig = {
   apisidebar: [
     {
-      type: "category",
-      label: "quotes",
+      type: 'category',
+      label: 'quotes',
       items: [
         {
-          type: "doc",
-          id: "rfq/API/get-quotes",
-          label: "Get quotes",
-          className: "api-method get",
+          type: 'doc',
+          id: 'rfq/API/get-quotes',
+          label: 'Get quotes',
+          className: 'api-method get',
         },
         {
-          type: "doc",
-          id: "rfq/API/upsert-quote",
-          label: "Upsert quote",
-          className: "api-method put",
+          type: 'doc',
+          id: 'rfq/API/upsert-quote',
+          label: 'Upsert quote',
+          className: 'api-method put',
         },
       ],
     },
   ],
-};
+}
 
-export default sidebar.apisidebar;
+export default sidebar.apisidebar
