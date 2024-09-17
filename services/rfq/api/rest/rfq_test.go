ee408a961031980727a25a489894065838dd8127
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-09-17 13:39:06 -0500

    Feat: add MultipleRelayers case

--- services/rfq/api/rest/rfq_test.go
+++ services/rfq/api/rest/rfq_test.go
@@ -153,58 +153,58 @@ func (c *ServerSuite) TestHandleActiveRFQ() {
 		c.Assert().Equal("no quotes found", userQuoteResp.Reason)
 	})
 
-	// c.Run("MultipleRelayers", func() {
-	// 	// Prepare a user quote request
-	// 	userRequestAmount := big.NewInt(1_000_000)
-	// 	userQuoteReq := &model.PutUserQuoteRequest{
-	// 		Data: model.QuoteData{
-	// 			OriginChainID:    1,
-	// 			OriginTokenAddr:  "0x1111111111111111111111111111111111111111",
-	// 			DestChainID:      2,
-	// 			DestTokenAddr:    "0x2222222222222222222222222222222222222222",
-	// 			OriginAmount:     userRequestAmount.String(),
-	// 			ExpirationWindow: 5000,
-	// 		},
-	// 		QuoteTypes: []string{"active"},
-	// 	}
+	c.Run("MultipleRelayers", func() {
+		// Prepare a user quote request
+		userRequestAmount := big.NewInt(1_000_000)
+		userQuoteReq := &model.PutUserQuoteRequest{
+			Data: model.QuoteData{
+				OriginChainID:    1,
+				OriginTokenAddr:  "0x1111111111111111111111111111111111111111",
+				DestChainID:      2,
+				DestTokenAddr:    "0x2222222222222222222222222222222222222222",
+				OriginAmount:     userRequestAmount.String(),
+				ExpirationWindow: 5000,
+			},
+			QuoteTypes: []string{"active"},
+		}
 
-	// 	// Prepare the relayer quote responses
-	// 	originAmount := userRequestAmount.String()
-	// 	destAmount := new(big.Int).Sub(userRequestAmount, big.NewInt(1000))
-	// 	destAmountStr := destAmount.String()
-	// 	quoteResp := model.RelayerWsQuoteResponse{
-	// 		Data: model.QuoteData{
-	// 			OriginChainID:   userQuoteReq.Data.OriginChainID,
-	// 			OriginTokenAddr: userQuoteReq.Data.OriginTokenAddr,
-	// 			DestChainID:     userQuoteReq.Data.DestChainID,
-	// 			DestTokenAddr:   userQuoteReq.Data.DestTokenAddr,
-	// 			DestAmount:      &destAmountStr,
-	// 			OriginAmount:    originAmount,
-	// 		},
-	// 	}
-	// 	respCtx, cancel := context.WithCancel(c.GetTestContext())
-	// 	defer cancel()
-	// 	runMockRelayer(respCtx, c.relayerWallets[0], &quoteResp)
+		// Prepare the relayer quote responses
+		originAmount := userRequestAmount.String()
+		destAmount := new(big.Int).Sub(userRequestAmount, big.NewInt(1000))
+		destAmountStr := destAmount.String()
+		quoteResp := model.RelayerWsQuoteResponse{
+			Data: model.QuoteData{
+				OriginChainID:   userQuoteReq.Data.OriginChainID,
+				OriginTokenAddr: userQuoteReq.Data.OriginTokenAddr,
+				DestChainID:     userQuoteReq.Data.DestChainID,
+				DestTokenAddr:   userQuoteReq.Data.DestTokenAddr,
+				DestAmount:      &destAmountStr,
+				OriginAmount:    originAmount,
+			},
+		}
+		respCtx, cancel := context.WithCancel(c.GetTestContext())
+		defer cancel()
 
-	// 	// Send additional responses with worse prices
-	// 	quoteResp2 := quoteResp
-	// 	destAmount2 := new(big.Int).Sub(destAmount, big.NewInt(1000)).String()
-	// 	quoteResp2.Data.DestAmount = &destAmount2
-	// 	runMockRelayer(respCtx, c.relayerWallets[0], &quoteResp2)
+		// Create additional responses with worse prices
+		quoteResp2 := quoteResp
+		destAmount2 := new(big.Int).Sub(destAmount, big.NewInt(1000)).String()
+		quoteResp2.Data.DestAmount = &destAmount2
+		quoteResp3 := quoteResp
+		destAmount3 := new(big.Int).Sub(destAmount, big.NewInt(2000)).String()
+		quoteResp3.Data.DestAmount = &destAmount3
 
-	// 	quoteResp3 := quoteResp
-	// 	destAmount3 := new(big.Int).Sub(destAmount, big.NewInt(2000)).String()
-	// 	quoteResp3.Data.DestAmount = &destAmount3
-	// 	runMockRelayer(respCtx, c.relayerWallets[0], &quoteResp3)
+		runMockRelayer(respCtx, c.relayerWallets[0], &quoteResp)
+		runMockRelayer(respCtx, c.relayerWallets[1], &quoteResp2)
+		runMockRelayer(respCtx, c.relayerWallets[2], &quoteResp3)
 
-	// 	// Submit the user quote request
-	// 	userQuoteResp, err := userClient.PutUserQuoteRequest(c.GetTestContext(), userQuoteReq)
-	// 	c.Require().NoError(err)
+		// Submit the user quote request
+		userQuoteResp, err := userClient.PutUserQuoteRequest(c.GetTestContext(), userQuoteReq)
+		c.Require().NoError(err)
 
-	// 	// Assert the response
-	// 	c.Assert().True(userQuoteResp.Success)
-	// 	c.Assert().Equal("active", userQuoteResp.QuoteType)
-	// 	c.Assert().Equal(destAmountStr, *userQuoteResp.Data.DestAmount)
-	// 	c.Assert().Equal(originAmount, userQuoteResp.Data.OriginAmount)
-	// })
+		// Assert the response
+		c.Assert().True(userQuoteResp.Success)
+		c.Assert().Equal("active", userQuoteResp.QuoteType)
+		c.Assert().Equal(destAmountStr, *userQuoteResp.Data.DestAmount)
+		c.Assert().Equal(originAmount, userQuoteResp.Data.OriginAmount)
+	})
 }
