6845d9ea8bb7c5c16439d6ece0bb4c67f8c08340
defi-moses <jakedinero@protonmail.com>
2024-09-17 14:41:30 +0100

    rough pass at docs and some linting and fixes yarn

--- packages/rfq-indexer/api/src/index.ts
+++ packages/rfq-indexer/api/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { createYoga } from 'graphql-yoga'
+
 import { schema } from './graphql/schema'
 import { overrideJsonBigIntSerialization } from './utils/overrideJsonBigIntSerialization'
 import { resolvers } from './graphql/resolvers'
@@ -13,12 +14,13 @@ const yoga = createYoga({ schema })
 app.use(yoga.graphqlEndpoint, yoga)
 
 app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello World!' })
-  })  
+  res.json({ message: 'Hello World!' })
+})
 
 app.get('/api/pending-transactions-missing-relay', async (req, res) => {
   try {
-    const pendingTransactions = await resolvers.Query.pendingTransactionsMissingRelay();
+    const pendingTransactions =
+      await resolvers.Query.pendingTransactionsMissingRelay()
     res.json(pendingTransactions);
   } catch (error) {
     console.error('Error fetching pending transactions:', error);
@@ -28,7 +30,8 @@ app.get('/api/pending-transactions-missing-relay', async (req, res) => {
 
 app.get('/api/pending-transactions-missing-proof', async (req, res) => {
   try {
-    const pendingTransactionsMissingProof = await resolvers.Query.pendingTransactionsMissingProof();
+    const pendingTransactionsMissingProof =
+      await resolvers.Query.pendingTransactionsMissingProof()
     res.json(pendingTransactionsMissingProof);
   } catch (error) {
     console.error('Error fetching pending transactions missing proof:', error);
@@ -38,7 +41,8 @@ app.get('/api/pending-transactions-missing-proof', async (req, res) => {
 
 app.get('/api/pending-transactions-missing-claim', async (req, res) => {
   try {
-    const pendingTransactionsMissingClaim = await resolvers.Query.pendingTransactionsMissingClaim();
+    const pendingTransactionsMissingClaim =
+      await resolvers.Query.pendingTransactionsMissingClaim()
     res.json(pendingTransactionsMissingClaim);
   } catch (error) {
     console.error('Error fetching pending transactions missing claim:', error);
