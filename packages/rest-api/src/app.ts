191f7f32c49f664167b3b682308f8917481482c5
abtestingalpha <abtestingalpha@gmail.com>
2024-09-20 14:41:57 -0400

    Prevent caching of api docs

--- packages/rest-api/src/app.ts
+++ packages/rest-api/src/app.ts
@@ -9,7 +9,21 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+app.use(
+  '/api-docs',
+  (_req, res, next) => {
+    res.set(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, proxy-revalidate'
+    )
+    res.set('Pragma', 'no-cache')
+    res.set('Expires', '0')
+    res.set('Surrogate-Control', 'no-store')
+    next()
+  },
+  swaggerUi.serve,
+  swaggerUi.setup(specs)
+)
 
 app.use('/', routes)
 
