53e3c87ab343cf571b82985d3d3aa0be00307e29
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-09-24 11:25:35 -0700

    adds swagger doc

--- packages/rest-api/src/routes/bridgeLimitsRoute.ts
+++ packages/rest-api/src/routes/bridgeLimitsRoute.ts
@@ -9,6 +9,90 @@ import { isTokenAddress } from '../utils/isTokenAddress'
 
 const router = express.Router()
 
+/**
+ * @openapi
+ * /bridgeLimits:
+ *   get:
+ *     summary: Get min/max origin values for bridge quote
+ *     description: Retrieve minimum and maximum bridgeable amounts to bridge from source chain to destination chain.
+ *     parameters:
+ *       - in: query
+ *         name: fromChain
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The source chain ID.
+ *       - in: query
+ *         name: toChain
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The destination chain ID.
+ *       - in: query
+ *         name: fromToken
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The address of the token on the source chain.
+ *       - in: query
+ *         name: toToken
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The address of the token on the destination chain.
+ *     responses:
+ *       200:
+ *         description: Successful response containing min and max origin amounts.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 maxOriginAmount:
+ *                   type: string
+ *                   description: Maximum amount of tokens that can be bridged from the origin chain.
+ *                 minOriginAmount:
+ *                   type: string
+ *                   description: Minimum amount of tokens that can be bridged from the origin chain.
+ *             example:
+ *               maxOriginAmount: "999600"
+ *               minOriginAmount: "4"
+ *       400:
+ *         description: Invalid input
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     value:
+ *                       type: string
+ *                     message:
+ *                       type: string
+ *                     field:
+ *                       type: string
+ *                     location:
+ *                       type: string
+ *             example:
+ *               error:
+ *                 value: "999"
+ *                 message: "Unsupported fromChain"
+ *                 field: "fromChain"
+ *                 location: "query"
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 details:
+ *                   type: string
+ */
 router.get(
   '/',
   [
