60138d40ffc3b02a805a4d3f80cf751207d6a926
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2024-09-23 19:06:17 -0700

    middleware check order

--- packages/rest-api/src/routes/bridgeLimitsRoute.ts
+++ packages/rest-api/src/routes/bridgeLimitsRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { check } from 'express-validator'
-import { isAddress } from '@ethersproject/address'
 
 import { CHAINS_ARRAY } from '../constants/chains'
 import { showFirstValidationError } from '../middleware/showFirstValidationError'
 import { bridgeLimitsController } from '../controllers/bridgeLimitsController'
+import { isTokenSupportedOnChain } from './../utils/isTokenSupportedOnChain'
+import { isTokenAddress } from '../utils/isTokenAddress'
 
 const router = express.Router()
 
@@ -12,27 +13,35 @@ router.get(
   '/',
   [
     check('fromChain')
+      .exists()
+      .withMessage('fromChain is required')
       .isNumeric()
       .custom((value) => CHAINS_ARRAY.some((c) => c.id === Number(value)))
-      .withMessage('Unsupported fromChain')
-      .exists()
-      .withMessage('originChainId is required'),
+      .withMessage('Unsupported fromChain'),
     check('toChain')
+      .exists()
+      .withMessage('Unsupported toChain')
       .isNumeric()
       .custom((value) => CHAINS_ARRAY.some((c) => c.id === Number(value)))
-      .withMessage('Unsupported toChain')
-      .exists()
       .withMessage('toChain is required'),
     check('fromToken')
       .exists()
       .withMessage('fromToken is required')
-      .custom((value) => isAddress(value))
-      .withMessage('Invalid fromToken address'),
+      .custom((value) => isTokenAddress(value))
+      .withMessage('Invalid fromToken address')
+      .custom((value, { req }) =>
+        isTokenSupportedOnChain(value, req.query.fromChain as string)
+      )
+      .withMessage('Token not supported on specified chain'),
     check('toToken')
       .exists()
       .withMessage('toToken is required')
-      .custom((value) => isAddress(value))
-      .withMessage('Invalid toToken address'),
+      .custom((value) => isTokenAddress(value))
+      .withMessage('Invalid toToken address')
+      .custom((value, { req }) =>
+        isTokenSupportedOnChain(value, req.query.toChain as string)
+      )
+      .withMessage('Token not supported on specified chain'),
   ],
   showFirstValidationError,
   bridgeLimitsController
