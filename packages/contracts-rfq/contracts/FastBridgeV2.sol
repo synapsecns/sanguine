c15ec8691902817f096589553bac360b53ba40cf
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2024-09-25 13:29:04 +0200

    fix(contracts-rfq): limit the amount of solhint warnings [SLT-245] (#3182)
    * ci: limit the amount of solhint warnings
    
    * refactor: move the errors into the separate interface
    
    * refactor: errors imports in tests

--- packages/contracts-rfq/contracts/FastBridgeV2.sol
+++ packages/contracts-rfq/contracts/FastBridgeV2.sol
@@ -3,14 +3,14 @@ pragma solidity 0.8.24;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "./libs/Errors.sol";
 import {UniversalTokenLib} from "./libs/UniversalToken.sol";
 
 import {Admin} from "./Admin.sol";
 import {IFastBridge} from "./interfaces/IFastBridge.sol";
 import {IFastBridgeV2} from "./interfaces/IFastBridgeV2.sol";
+import {IFastBridgeV2Errors} from "./interfaces/IFastBridgeV2Errors.sol";
 
-contract FastBridgeV2 is Admin, IFastBridgeV2 {
+contract FastBridgeV2 is Admin, IFastBridgeV2, IFastBridgeV2Errors {
     using SafeERC20 for IERC20;
     using UniversalTokenLib for address;
 
