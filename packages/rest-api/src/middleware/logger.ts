2538a4bf692d21572ed10b89cd7db177e632c089
abtestingalpha <abtestingalpha@gmail.com>
2024-10-04 12:42:59 -0400

    Denest keys

--- packages/rest-api/src/middleware/logger.ts
+++ packages/rest-api/src/middleware/logger.ts
@@ -1,5 +1,4 @@
 import { Writable } from 'stream'
-
 import winston from 'winston'
 
 const transports = []
@@ -20,6 +19,14 @@ export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
+    winston.format((info) => {
+      if (typeof info.message === 'object') {
+        Object.assign(info, info.message)
+      }
+      info.msg = info.msg || info.message
+      delete info.message
+      return info
+    })(),
     winston.format.json()
   ),
   transports,
