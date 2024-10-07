11824e814c9e4038bdca137452123e9e60dc58bf
abtestingalpha <abtestingalpha@gmail.com>
2024-10-07 17:31:48 -0400

    Adds types

--- packages/rest-api/src/validations/validSwap.ts
+++ packages/rest-api/src/validations/validSwap.ts
@@ -1,6 +1,10 @@
 import { tokenAddressToToken } from '../utils/tokenAddressToToken'
 
-export const validSwap = (chain, fromToken, toToken) => {
+export const validSwap = (
+  chain: number | string,
+  fromToken: string,
+  toToken: string
+) => {
   const fromTokenInfo = tokenAddressToToken(chain.toString(), fromToken)
   const toTokenInfo = tokenAddressToToken(chain.toString(), toToken)
 
