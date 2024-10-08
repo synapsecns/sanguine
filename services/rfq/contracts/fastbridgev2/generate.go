c6a1fdc4b21b68fb914de994d5a7b48f76bac5b0
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-10-08 14:45:02 -0500

    Feat: initial fastbridgev2 bindings

---
+++ services/rfq/contracts/fastbridgev2/generate.go
@@ -0,0 +1,3 @@
+package fastbridgev2
+
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ../../../../packages/contracts-rfq/flattened/FastBridgeV2.sol --pkg fastbridgev2 --sol-version 0.8.20 --filename fastbridgev2 --evm-version istanbul
