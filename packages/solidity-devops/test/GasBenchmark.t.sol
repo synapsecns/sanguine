bd792eac6d70b2d6a09a2631c008a15af4b302ec
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-10-09 14:30:52 +0100

    ci: no-op GasBenchmark test

---
+++ packages/solidity-devops/test/GasBenchmark.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Test} from "forge-std/Test.sol";
+
+/// @notice No gas benchmark is required for solidity-devops
+contract GasBenchmarkTest is Test {
+    // solhint-disable-next-line no-empty-blocks
+    function testGasBenchmark() public {}
+}
