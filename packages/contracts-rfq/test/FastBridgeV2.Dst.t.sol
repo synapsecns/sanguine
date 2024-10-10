d575cbec1e0853d82d1501947a595e4ab69bbe25
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-10-10 10:33:52 +0100

    test: more incorrect relay scenarios

--- packages/contracts-rfq/test/FastBridgeV2.Dst.t.sol
+++ packages/contracts-rfq/test/FastBridgeV2.Dst.t.sol
@@ -555,18 +555,52 @@ contract FastBridgeV2DstTest is FastBridgeV2DstBaseTest {
         relay({caller: relayerA, msgValue: tokenParams.destAmount, bridgeTx: tokenTx});
     }
 
+    function test_relay_token_withRelayerAddress_revert_approvedZero() public {
+        vm.prank(relayerB);
+        dstToken.approve(address(fastBridge), 0);
+        vm.expectRevert();
+        relayWithAddress({caller: relayerB, relayer: relayerA, msgValue: 0, bridgeTx: tokenTx});
+    }
+
+    function test_relay_token_withRelayerAddress_revert_approvedNotEnough() public {
+        vm.prank(relayerB);
+        dstToken.approve(address(fastBridge), tokenParams.destAmount - 1);
+        vm.expectRevert();
+        relayWithAddress({caller: relayerB, relayer: relayerA, msgValue: 0, bridgeTx: tokenTx});
+    }
+
+    function test_relay_token_withRelayerAddress_revert_nonZeroMsgValue() public {
+        vm.expectRevert();
+        relayWithAddress({caller: relayerB, relayer: relayerA, msgValue: tokenParams.destAmount, bridgeTx: tokenTx});
+    }
+
     function test_relay_eth_revert_lowerMsgValue() public {
         vm.expectRevert();
-        relay({caller: relayerA, msgValue: ethParams.destAmount - 1, bridgeTx: ethTx});
+        relay({caller: relayerB, msgValue: ethParams.destAmount - 1, bridgeTx: ethTx});
     }
 
     function test_relay_eth_revert_higherMsgValue() public {
         vm.expectRevert();
-        relay({caller: relayerA, msgValue: ethParams.destAmount + 1, bridgeTx: ethTx});
+        relay({caller: relayerB, msgValue: ethParams.destAmount + 1, bridgeTx: ethTx});
     }
 
     function test_relay_eth_revert_zeroMsgValue() public {
         vm.expectRevert();
-        relay({caller: relayerA, msgValue: 0, bridgeTx: ethTx});
+        relay({caller: relayerB, msgValue: 0, bridgeTx: ethTx});
+    }
+
+    function test_relay_eth_withRelayerAddress_revert_lowerMsgValue() public {
+        vm.expectRevert();
+        relayWithAddress({caller: relayerA, relayer: relayerB, msgValue: ethParams.destAmount - 1, bridgeTx: ethTx});
+    }
+
+    function test_relay_eth_withRelayerAddress_revert_higherMsgValue() public {
+        vm.expectRevert();
+        relayWithAddress({caller: relayerA, relayer: relayerB, msgValue: ethParams.destAmount + 1, bridgeTx: ethTx});
+    }
+
+    function test_relay_eth_withRelayerAddress_revert_zeroMsgValue() public {
+        vm.expectRevert();
+        relayWithAddress({caller: relayerA, relayer: relayerB, msgValue: 0, bridgeTx: ethTx});
     }
 }
