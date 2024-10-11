5b6ec120272e6a14f0e239925b7a36786e458c29
Daniel Wasserman <wassermandaniel8@gmail.com>
2024-10-11 13:58:00 -0500

    Feat: add v2 of fastbridgemock

---
+++ services/rfq/contracts/testcontracts/fastbridgemockv2/generate.go
@@ -0,0 +1,3 @@
+package fastbridgemockv2
+
+//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ../../../../../packages/contracts-rfq/flattened/FastBridgeMock.sol --pkg fastbridgemockv2 --sol-version 0.8.20 --filename fastbridgemockv2 --evm-version istanbul
