275dfaa7fe60b6ad35a7432186f4928d1c6b58f0
golangisfun123 <simon.shamoon@protochainresearch.com>
2024-10-14 15:18:10 -0500

    add tracing support

--- services/omnirpc/http/resty.go
+++ services/omnirpc/http/resty.go
@@ -3,8 +3,11 @@ package http
 import (
 	"context"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/go-resty/resty/v2"
 	"github.com/synapsecns/sanguine/core/metrics"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 // RestyClient is a resty client for making requests to the http client.
@@ -33,33 +36,87 @@ func (r RestyClient) NewRequest() Request {
 
 // SetHeaderBytes is a wrapper around SetHeadre for bytes.
 func (r *restyRequest) SetHeaderBytes(key, value []byte) Request {
+	_, span := r.handler.Tracer().Start(
+		r.Request.Context(),
+		"SetHeaderBytes",
+		trace.WithAttributes(
+			attribute.String("key", common.Bytes2Hex(key)),
+			attribute.String("value", common.Bytes2Hex(value)),
+		))
+	defer func() {
+		metrics.EndSpan(span)
+	}()
 	r.Request.SetHeader(string(key), string(value))
 	return r
 }
 
 func (r *restyRequest) SetBody(body []byte) Request {
+	_, span := r.handler.Tracer().Start(
+		r.Request.Context(),
+		"SetBody",
+		trace.WithAttributes(attribute.String("body", common.Bytes2Hex(body))),
+	)
+	defer func() {
+		metrics.EndSpan(span)
+	}()
 	r.Request.SetBody(body)
 	return r
 }
 
 func (r *restyRequest) SetContext(ctx context.Context) Request {
+	_, span := r.handler.Tracer().Start(
+		r.Request.Context(),
+		"SetContext",
+	)
+	span.AddEvent("SetContext")
+	defer func() {
+		metrics.EndSpan(span)
+	}()
 	r.Request.SetContext(ctx)
 	return r
 }
 
 func (r *restyRequest) SetHeader(key, value string) Request {
+	_, span := r.handler.Tracer().Start(
+		r.Request.Context(),
+		"SetHeader",
+		trace.WithAttributes(
+			attribute.String("SetHeader", key),
+			attribute.String("value", value),
+		))
+	defer func() {
+		metrics.EndSpan(span)
+	}()
 	r.Request.SetHeader(key, value)
 	return r
 }
 
 func (r *restyRequest) SetRequestURI(uri string) Request {
+	_, span := r.handler.Tracer().Start(
+		r.Request.Context(),
+		"SetRequestURI",
+		trace.WithAttributes(attribute.String("uri", uri)),
+	)
+	defer func() {
+		metrics.EndSpan(span)
+	}()
 	r.endpoint = uri
 	return r
 }
 
-func (r *restyRequest) Do() (Response, error) {
-	//nolint: wrapcheck
-	return r.Request.Post(r.endpoint)
+func (r *restyRequest) Do() (_ Response, err error) {
+	_, span := r.handler.Tracer().Start(
+		r.Request.Context(),
+		"Do",
+	)
+	defer func() {
+		metrics.EndSpanWithErr(span, err)
+	}()
+	resp, err := r.Request.Post(r.endpoint)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (r *restyRequest) WithMetrics(metrics metrics.Handler) Request {
