411555ec26cdf527f1541699ce7167c93868a4dc
golangisfun123 <simon.shamoon@protochainresearch.com>
2024-10-22 11:37:18 -0500

    remove unneeded fiels

--- packages/rest-api/src/swagger.ts
+++ packages/rest-api/src/swagger.ts
@@ -1,32 +1,3 @@
-// import swaggerJsdoc from 'swagger-jsdoc'
-//
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const packageJson = require('../package.json')
-//
-// const isDevelopment = process.env.NODE_ENV === 'development'
-// const serverUrl = isDevelopment
-//   ? 'http://localhost:3000'
-//   : 'https://api.synapseprotocol.com'
-//
-// const options: swaggerJsdoc.Options = {
-//   definition: {
-//     openapi: '3.0.0',
-//     info: {
-//       title: 'Synapse Protocol REST API',
-//       version: packageJson.version,
-//       description: 'API documentation for the Synapse Protocol REST API',
-//     },
-//     servers: [
-//       {
-//         url: serverUrl,
-//       },
-//     ],
-//   },
-//   apis: ['./src/routes/*.ts', './src/*.ts'],
-// }
-//
-// export const specs = swaggerJsdoc(options)
-
 import fs from 'fs'
 import path from 'path'
 
