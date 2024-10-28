5e51cfe34a18c160bb80af322c3f006504597357
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-10-28 09:10:01 +0000

    fix: pragma

--- packages/contracts-rfq/contracts/interfaces/IZapRecipient.sol
+++ packages/contracts-rfq/contracts/interfaces/IZapRecipient.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 interface IZapRecipient {
     function zap(address token, uint256 amount, bytes memory zapData) external payable returns (bytes4);
