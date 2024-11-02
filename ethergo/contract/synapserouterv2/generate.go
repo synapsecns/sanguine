2575cd600b42081c135df9930bfc138902fec8f5
Trajan0x <trajan0x@users.noreply.github.com>
2024-11-02 12:51:08 -0400

    Revert "add contract"
    This reverts commit 91f8158c32ecd2ce81582f2538a370436a0839cd.

--- ethergo/contract/synapserouterv2/generate.go
+++
@@ -1,3 +0,0 @@
-package synapserouterv2
-
-//go:generate go run github.com/synapsecns/sanguine/tools/abigen generate --sol ./SynapseRouterV2.sol --pkg synapserouterv2 --sol-version 0.8.17 --filename synapserouterv2 --evm-version istanbul
