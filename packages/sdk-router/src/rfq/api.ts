6c66758cc0f2d2021936e712d4f750c3675c3384
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-11-06 20:43:46 +0000

    feat: initial impl for `getBestRFQQuote`

--- packages/sdk-router/src/rfq/api.ts
+++ packages/sdk-router/src/rfq/api.ts
@@ -9,6 +9,7 @@ import {
 
 const API_URL = 'https://rfq-api.omnirpc.io'
 const API_TIMEOUT = 2000
+const EXPIRATION_WINDOW = 1000
 
 export type PutRFQRequestAPI = {
   user_address: string
@@ -42,11 +43,12 @@ export type Quote = {
 
 const fetchWithTimeout = async (
   url: string,
-  timeout: number
+  timeout: number,
+  init?: RequestInit
 ): Promise<Response> => {
   const controller = new AbortController()
   const timeoutId = setTimeout(() => controller.abort(), timeout)
-  return fetch(url, { signal: controller.signal }).finally(() =>
+  return fetch(url, { signal: controller.signal, ...init }).finally(() =>
     clearTimeout(timeoutId)
   )
 }
@@ -83,7 +85,7 @@ export const getAllQuotes = async (): Promise<FastBridgeQuote[]> => {
 }
 
 /**
- * Hits Quoter API /rfq endpoint to get the best quote for a given ticker and origin amount.
+ * Hits Quoter API /rfq PUT endpoint to get the best quote for a given ticker and origin amount.
  *
  * @returns A promise that resolves to the best quote.
  * Will return null if the request fails or times out.
@@ -92,4 +94,59 @@ export const getBestRFQQuote = async (
   ticker: Ticker,
   originAmount: BigNumber,
   originUserAddress?: string
-): Promise<Quote | null> => {}
+): Promise<Quote | null> => {
+  if (!originUserAddress) {
+    console.error('No origin user address provided')
+    return null
+  }
+  try {
+    const rfqRequest: PutRFQRequestAPI = {
+      user_address: originUserAddress,
+      // TODO: add active quotes once they are fixed
+      quote_types: ['passive'],
+      data: {
+        origin_chain_id: ticker.originToken.chainId,
+        dest_chain_id: ticker.destToken.chainId,
+        origin_token_addr: ticker.originToken.token,
+        dest_token_addr: ticker.destToken.token,
+        origin_amount: originAmount.toString(),
+        // TODO: should this be configurable?
+        expiration_window: EXPIRATION_WINDOW,
+      },
+    }
+    const response = await fetchWithTimeout(`${API_URL}/rfq`, API_TIMEOUT, {
+      method: 'PUT',
+      body: JSON.stringify(rfqRequest),
+    })
+    if (!response.ok) {
+      console.error('Error fetching quote:', response.statusText)
+      return null
+    }
+    // Check that response is successful, contains non-zero dest amount, and has a relayer address
+    const rfqResponse: PutRFQResponseAPI = await response.json()
+    if (!rfqResponse.success) {
+      console.log(rfqResponse.reason ?? 'No RFQ quote returned')
+      return null
+    }
+    if (!rfqResponse.dest_amount || !rfqResponse.relayer_address) {
+      console.error(
+        'Error fetching quote: missing dest_amount or relayer_address in response:',
+        rfqResponse
+      )
+      return null
+    }
+    const destAmount = BigNumber.from(rfqResponse.dest_amount)
+    if (destAmount.lte(0)) {
+      console.log('No RFQ quote returned')
+      return null
+    }
+    return {
+      destAmount,
+      relayerAddress: rfqResponse.relayer_address,
+      quoteID: rfqResponse.quote_id,
+    }
+  } catch (error) {
+    console.error('Error fetching quote:', error)
+    return null
+  }
+}
