6572f96a2fc630182c2caeb54a6a88748433b0bf
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-11-06 15:39:28 +0000

    test: add coverage for getBestRFQQuote

--- packages/sdk-router/src/rfq/api.integration.test.ts
+++ packages/sdk-router/src/rfq/api.integration.test.ts
@@ -1,11 +1,58 @@
-import { getAllQuotes } from './api'
+import { parseFixed } from '@ethersproject/bignumber'
+
+import { getAllQuotes, getBestRFQQuote } from './api'
+import { Ticker } from './ticker'
+import { ETH_NATIVE_TOKEN_ADDRESS } from '../utils/handleNativeToken'
 
 global.fetch = require('node-fetch')
 
-describe('getAllQuotes', () => {
-  it('Integration test', async () => {
+describe('Integration test: getAllQuotes', () => {
+  it('returns a non-empty array', async () => {
     const result = await getAllQuotes()
     // console.log('Current quotes: ' + JSON.stringify(result, null, 2))
     expect(result.length).toBeGreaterThan(0)
+  })
+})
+
+describe('Integration test: getBestRFQQuote', () => {
+  const ticker: Ticker = {
+    originToken: {
+      chainId: 42161,
+      token: ETH_NATIVE_TOKEN_ADDRESS,
+    },
+    destToken: {
+      chainId: 10,
+      token: ETH_NATIVE_TOKEN_ADDRESS,
+    },
+  }
+  const userAddress = '0x0000000000000000000000000000000000007331'
+
+  it('ARB ETH -> OP ETH; 1337 wei => no quote returned', async () => {
+    const result = await getBestRFQQuote(
+      ticker,
+      parseFixed('1337'),
+      userAddress
+    )
+    expect(result).toBeNull()
+  })
+
+  it('ARB ETH -> OP ETH; 0.01 ETH => quote returned', async () => {
+    const result = await getBestRFQQuote(
+      ticker,
+      parseFixed('0.01', 18),
+      userAddress
+    )
+    expect(result).not.toBeNull()
+    expect(result?.destAmount.gt(0)).toBe(true)
+    expect(result?.relayerAddress).toBeDefined()
+  })
+
+  it('ARB ETH -> OP ETH; 10**36 wei => no quote returned', async () => {
+    const result = await getBestRFQQuote(
+      ticker,
+      parseFixed('1', 36),
+      userAddress
+    )
+    expect(result).toBeNull()
   })
 })
