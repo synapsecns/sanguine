31997a82672f41e339abe8da76617f9ffdae7852
χ² <88190723+ChiTimesChi@users.noreply.github.com>
2024-11-22 19:16:32 +0000

    test(contracts-rfq): gas bench for `TokenZapV1` (#3406)
    * test: add minimal mock for a vault contract
    
    * test: gas bench for TokenZap

--- packages/contracts-rfq/test/mocks/VaultManyArguments.sol
+++ packages/contracts-rfq/test/mocks/VaultManyArguments.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.20;
 
 import {VaultMock} from "./VaultMock.sol";
 
+// solhint-disable no-empty-blocks
 /// @notice Vault mock for testing purposes. DO NOT USE IN PRODUCTION.
 contract VaultManyArguments is VaultMock {
     error VaultManyArguments__SomeError();
+
+    /// @notice We include an empty "test" function so that this contract does not appear in the coverage report.
+    function testSimpleVaultMock() external {}
 
     function deposit(
         address token,
