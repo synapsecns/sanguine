2305d939b12c383a52d61976d44e88a7e1d73021
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-11-28 14:03:23 +0000

    chore: silence solhint warning in the test mock

--- packages/contracts-rfq/test/mocks/WETHMock.sol
+++ packages/contracts-rfq/test/mocks/WETHMock.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
+// solhint-disable no-empty-blocks
 /// @notice WETH mock for testing purposes. DO NOT USE IN PRODUCTION.
 contract WETHMock is ERC20 {
     constructor() ERC20("Mock Wrapped Ether", "Mock WETH") {}
