eb33a1ec0c811d5a70a52026746ab98cd6dcd691
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2024-12-30 11:31:57 +0000

    Revert "fix(synapse-interface): non-RFQ quotes"
    This reverts commit d13041c5d93e6b6fbe98b1238da2764059764482.

--- packages/synapse-interface/slices/bridgeQuote/thunks.ts
+++ packages/synapse-interface/slices/bridgeQuote/thunks.ts
@@ -72,7 +72,7 @@ export const fetchBridgeQuote = createAsyncThunk(
     )
 
     const nonRfqQuote = activeQuotes.find(
-      (quote) => quote.bridgeModuleName !== 'SynapseIntents'
+      (quote) => quote.bridgeModuleName !== 'SynapseRFQ'
     )
 
     let quote
