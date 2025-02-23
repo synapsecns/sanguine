229f41f1158229387973f37419863ee64610346d
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-02-23 17:52:36 -0600

    feat: add HyperEVM to the list of chains

--- packages/synapse-interface/constants/chains/extraWagmiChains.ts
+++ packages/synapse-interface/constants/chains/extraWagmiChains.ts
@@ -144,3 +144,30 @@ export const berachain = {
     },
   },
 }
+
+export const hyperEVM = {
+  id: 999,
+  name: 'HyperEVM',
+  network: 'hyperevm',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'HYPE',
+    symbol: 'HYPE',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.hyperliquid.xyz/evm'] },
+    public: { http: ['https://rpc.hyperliquid.xyz/evm'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Purrsec',
+      url: 'https://purrsec.com/',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 13051,
+    },
+  },
+}
