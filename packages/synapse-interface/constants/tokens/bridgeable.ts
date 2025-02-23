24ff10d239c012b7e008b5c55081d9bbf8454bee
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-02-23 10:38:51 -0600

    feat: add BNB, BERA and other native tokens

--- packages/synapse-interface/constants/tokens/bridgeable.ts
+++ packages/synapse-interface/constants/tokens/bridgeable.ts
@@ -2,6 +2,7 @@ import { zeroAddress } from 'viem'
 import ageurLogo from '@assets/icons/ageur.svg'
 import avaxLogo from '@assets/icons/avax.svg'
 import btcLogo from '@assets/icons/btc.svg'
+import bnbLogo from '@assets/icons/bnb.svg'
 import busdLogo from '@assets/icons/busd.svg'
 import crvusdLogo from '@assets/icons/crvusd.svg'
 import linkLogo from '@assets/icons/link.svg'
@@ -42,6 +43,11 @@ import usdtLogo from '@assets/icons/usdt.svg'
 import vstaLogo from '@assets/icons/vsta.svg'
 import wbtcLogo from '@assets/icons/wbtc.svg'
 import wethLogo from '@assets/icons/weth.svg'
+// TODO: correct logo for these native tokens
+import beraLogo from '@assets/chains/berachain.svg'
+import cantoLogo from '@assets/chains/canto.svg'
+import croLogo from '@assets/chains/cronos.svg'
+import glmrLogo from '@assets/chains/moonbeam.svg'
 
 import { Token } from '@/utils/types'
 import * as CHAINS from '@/constants/chains/master'
@@ -1253,4 +1259,85 @@ export const METIS = new Token({
   logo: metisLogo,
   color: 'blue',
   routeSymbol: 'Metis',
+})
+
+export const BERA = new Token({
+  addresses: {
+    [CHAINS.BERACHAIN.id]: zeroAddress,
+  },
+  decimals: 18,
+  symbol: 'BERA',
+  name: 'Bera',
+  logo: beraLogo,
+  isNative: true,
+  swapableType: 'BERA',
+  // TODO: correct color for BERA token
+  color: 'gray',
+  visibilityRank: 90,
+  priorityRank: 300,
+  routeSymbol: 'BERA',
+})
+
+export const BNB = new Token({
+  addresses: {
+    [CHAINS.BNB.id]: zeroAddress,
+  },
+  decimals: 18,
+  symbol: 'BNB',
+  name: 'BNB',
+  logo: bnbLogo,
+  isNative: true,
+  swapableType: 'BNB',
+  color: 'yellow',
+  visibilityRank: 90,
+  priorityRank: 300,
+  routeSymbol: 'BNB',
+})
+
+export const CANTO = new Token({
+  addresses: {
+    [CHAINS.CANTO.id]: zeroAddress,
+  },
+  decimals: 18,
+  symbol: 'CANTO',
+  name: 'Canto',
+  logo: cantoLogo,
+  isNative: true,
+  swapableType: 'CANTO',
+  color: 'green',
+  visibilityRank: 90,
+  priorityRank: 300,
+  routeSymbol: 'CANTO',
+})
+
+export const CRO = new Token({
+  addresses: {
+    [CHAINS.CRONOS.id]: zeroAddress,
+  },
+  decimals: 18,
+  symbol: 'CRO',
+  name: 'Cronos',
+  logo: croLogo,
+  isNative: true,
+  swapableType: 'CRO',
+  color: 'blue',
+  visibilityRank: 90,
+  priorityRank: 300,
+  routeSymbol: 'CRO',
+})
+
+export const GLMR = new Token({
+  addresses: {
+    [CHAINS.MOONBEAM.id]: zeroAddress,
+  },
+  decimals: 18,
+  symbol: 'GLMR',
+  name: 'Glimmer',
+  logo: glmrLogo,
+  isNative: true,
+  swapableType: 'GLMR',
+  color: 'purple',
+  visibilityRank: 90,
+  priorityRank: 300,
+  routeSymbol: 'GLMR',
 })
