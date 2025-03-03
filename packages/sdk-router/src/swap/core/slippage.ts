55be8ab3d26dc30340f650212fb6900be8c247c3
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-03-03 19:54:40 +0000

    feat: slippage utils

---
+++ packages/sdk-router/src/swap/core/slippage.ts
@@ -0,0 +1,39 @@
+import { BigNumber } from 'ethers'
+import { WeiPerEther } from '@ethersproject/constants'
+
+export type Slippage = {
+  numerator: number
+  denominator: number
+}
+
+// Max slippage that can be used by the swap engines, 100 bips (1%)
+// TODO: consider lowering this value
+export const SlippageMax: Slippage = {
+  numerator: 100,
+  denominator: 10000,
+}
+
+export const toBasisPoints = (slippage: Slippage): number => {
+  return Math.round((slippage.numerator * 10000) / slippage.denominator)
+}
+
+export const toPercentFloat = (slippage: Slippage): number => {
+  return (slippage.numerator * 100) / slippage.denominator
+}
+
+export const toFloat = (slippage: Slippage): number => {
+  return slippage.numerator / slippage.denominator
+}
+
+export const toWei = (slippage: Slippage): BigNumber => {
+  return BigNumber.from(slippage.numerator)
+    .mul(WeiPerEther)
+    .div(slippage.denominator)
+}
+
+export const applySlippage = (
+  amount: BigNumber,
+  slippage: Slippage
+): BigNumber => {
+  return amount.sub(amount.mul(slippage.numerator).div(slippage.denominator))
+}
