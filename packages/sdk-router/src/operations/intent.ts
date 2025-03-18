bf7d92a2cd2a8aca83322b1b21f6b09673c0677a
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-03-18 17:55:15 +0000

    feat: single-chain swap intents

--- packages/sdk-router/src/operations/intent.ts
+++ packages/sdk-router/src/operations/intent.ts
@@ -1,8 +1,11 @@
+import { Zero } from '@ethersproject/constants'
 import { BigNumber, BigNumberish } from 'ethers'
+import { uuidv7 } from 'uuidv7'
 
 import { IntentStep } from '../module'
 import { SynapseSDK } from '../sdk'
 import { Slippage } from '../swap'
+import { swapV2 } from './swap'
 
 export type IntentParameters = {
   fromChainId: number
@@ -31,6 +34,58 @@ export type IntentQuote = {
 }
 
 export async function intent(
+  this: SynapseSDK,
+  params: IntentParameters
+): Promise<IntentQuote[]> {
+  return params.fromChainId === params.toChainId
+    ? _getSameChainIntentQuotes.call(this, params)
+    : _getCrossChainIntentQuotes.call(this, params)
+}
+
+async function _getSameChainIntentQuotes(
+  this: SynapseSDK,
+  params: IntentParameters
+): Promise<IntentQuote[]> {
+  const swapQuote = await swapV2.call(this, {
+    chainId: params.fromChainId,
+    fromToken: params.fromToken,
+    toToken: params.toToken,
+    fromAmount: params.fromAmount,
+    toRecipient: params.toRecipient || params.fromSender,
+    slippage: params.slippage,
+    deadline: params.deadline,
+  })
+  if (!swapQuote || swapQuote.expectedToAmount.isZero()) {
+    return []
+  }
+  const intentCommon = {
+    fromChainId: params.fromChainId,
+    fromToken: params.fromToken,
+    fromAmount: BigNumber.from(params.fromAmount),
+    toChainId: params.toChainId,
+    toToken: params.toToken,
+    expectedToAmount: swapQuote.expectedToAmount,
+    minToAmount: swapQuote.minToAmount,
+    // TODO: chain block time
+    estimatedTime: 0,
+  }
+  const swapStep: IntentStep = {
+    ...intentCommon,
+    routerAddress: swapQuote.routerAddress,
+    moduleName: swapQuote.moduleName,
+    gasDropAmount: Zero,
+    tx: swapQuote.tx,
+  }
+  const intentQuote: IntentQuote = {
+    id: uuidv7(),
+    ...intentCommon,
+    steps: [swapStep],
+  }
+  // TODO: do we need to return multiple quotes?
+  return [intentQuote]
+}
+
+async function _getCrossChainIntentQuotes(
   this: SynapseSDK,
   params: IntentParameters
 ): Promise<IntentQuote[]> {
