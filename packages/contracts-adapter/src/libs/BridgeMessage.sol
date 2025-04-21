dc4c01ab5ce9b444e12bf2955307bce99e30a7a0
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-04-21 17:35:21 +0100

    feat: bridge message lib

---
+++ packages/contracts-adapter/src/libs/BridgeMessage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+library BridgeMessage {
+    uint256 private constant BRIDGE_MESSAGE_LENGTH = 96;
+
+    error BridgeMessage__InvalidPayload();
+
+    function encodeBridgeMessage(
+        address recipient,
+        bytes31 symbol,
+        uint256 amount
+    )
+        public
+        pure
+        returns (bytes memory)
+    {
+        return abi.encode(recipient, symbol, amount);
+    }
+
+    function decodeBridgeMessage(bytes calldata payload)
+        public
+        pure
+        returns (address recipient, bytes31 symbol, uint256 amount)
+    {
+        if (payload.length != BRIDGE_MESSAGE_LENGTH) revert BridgeMessage__InvalidPayload();
+        return abi.decode(payload, (address, bytes31, uint256));
+    }
+}
