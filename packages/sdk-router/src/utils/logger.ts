331f094966c6656e867b07e0479b0cd5e58a40f1
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-04-23 19:04:14 +0100

    fix: make logExecutionTime more generic

--- packages/sdk-router/src/utils/logger.ts
+++ packages/sdk-router/src/utils/logger.ts
@@ -2,15 +2,18 @@ export const logger = console
 
 // Decorator to log the execution time of a function
 export const logExecutionTime =
-  (functionName: string) =>
-  (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => {
+  () => (_target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
     const originalMethod = descriptor.value
 
     descriptor.value = async function (...args: any[]) {
       const startTime = Date.now()
       const result = await originalMethod.apply(this, args)
       const elapsedTime = Date.now() - startTime
-      logger.info({ args }, `${functionName} execution time: ${elapsedTime}ms`)
+
+      const className = this.constructor.name
+      const functionName = `${className}.${propertyKey}`
+
+      logger.info(`⌛ ${functionName}: ${elapsedTime}ms`)
       return result
     }
 
