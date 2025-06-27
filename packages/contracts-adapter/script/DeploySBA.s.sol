1e93c149a0e50464f173b41c53007d7b406ecebf
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2025-06-27 19:04:03 +0100

    feat: deployment script

--- packages/contracts-adapter/script/DeploySBA.s.sol
+++ packages/contracts-adapter/script/DeploySBA.s.sol
@@ -1,16 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {SynapseBridgeAdapter} from "../src/SynapseBridgeAdapter.sol";
-
-import {SynapseScript} from "@synapsecns/solidity-devops/src/SynapseScript.sol";
+import {SynapseScript, stdJson} from "@synapsecns/solidity-devops/src/SynapseScript.sol";
 
 // solhint-disable no-empty-blocks
 contract DeploySBA is SynapseScript {
+    using stdJson for string;
+
     /// @notice We include an empty "test" function so that this contract does not appear in the coverage report.
     function testDeploySBA() external {}
 
     function run() external broadcastWithHooks {
-        // TODO: get endpoint address for chain
+        string memory chainsConfig = readGlobalDeployProdConfig({contractName: "chains", revertIfNotFound: true});
+        string memory chainKey = string.concat(".", activeChain);
+        if (!chainsConfig.keyExists(chainKey)) {
+            printFailWithIndent(string.concat("Chain ", activeChain, " not found in chains config"));
+            assert(false);
+        }
+        address endpointV2 = chainsConfig.readAddress(string.concat(chainKey, ".endpointV2"));
+        // constructor(address endpoint_, address owner_)
+        bytes memory constructorArgs = abi.encode(endpointV2, msg.sender);
+        printInfo(string.concat("EndpointV2: ", vm.toString(endpointV2)));
+        printInfo(string.concat("Initial Owner: ", vm.toString(msg.sender)));
+        deployAndSave({
+            contractName: "SynapseBridgeAdapter",
+            constructorArgs: constructorArgs,
+            deployCodeFunc: cbDeployCreate2
+        });
     }
 }
