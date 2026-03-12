5dd5bef3fd4e2c5103944accdd8a845b9f35d9c0
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2026-03-12 14:11:11 +0000

    feat(contracts-adapter): add SBA ownership transfer script

---
+++ packages/contracts-adapter/script/transfer-ownership.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Usage: ./script/transfer-ownership.sh <walletName> [...options]
+
+trap "echo 'Transfer ownership script terminated by user'; exit" INT
+
+walletName=$1
+if [ -z "$walletName" ]; then
+  echo "Usage: ./script/transfer-ownership.sh <walletName> [...options]"
+  exit 1
+fi
+shift 1
+
+chains=$(jq -r '.chains | keys[]' devops.json)
+echo Will transfer ownership for chains: "$(echo "$chains" | tr ' ' ',')"
+
+for chain in $chains; do
+  echo Transferring ownership on chain: "$chain"
+  npx fsr ./script/TransferOwnershipSBA.s.sol "$chain" "$walletName" "$@"
+done
