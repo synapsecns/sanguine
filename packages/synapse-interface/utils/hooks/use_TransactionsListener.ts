3ad0fb3f20ecee9320f7370ea632be6ff602fdd0
ChiTimesChi <88190723+ChiTimesChi@users.noreply.github.com>
2026-03-18 11:05:11 +0000

    fix(synapse-interface): rerun transactions listener on account resolve

--- packages/synapse-interface/utils/hooks/use_TransactionsListener.ts
+++ packages/synapse-interface/utils/hooks/use_TransactionsListener.ts
@@ -44,7 +44,7 @@ export const use_TransactionsListener = () => {
         }
       })
     }
-  }, [pendingBridgeTransactions, transactions])
+  }, [address, pendingBridgeTransactions, transactions])
 
   return null
 }
